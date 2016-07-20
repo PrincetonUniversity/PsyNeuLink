f27de00af285c9590f62f5ff7d7138be822cfd3d
jdc@princeton.edu <jdc@princeton.edu>
2016-07-20 18:43:25 -0400

    WORKING
    Simple DDM Script
    
    [FROM Minstrel]

--- Scripts/• DDM Test Script (simple).py
+++ Scripts/• DDM Test Script (simple).py
@@ -2,5 +2,5 @@ from Functions.Process import Process_Base
 from Functions.Mechanisms.DDM import *
 from Globals.Keywords import *
 
-simple_ddm = Process_Base(params={kwConfiguration:[(DDM,None,0)]})
+simple_ddm = Process_Base(params={kwConfiguration:[DDM]})
 simple_ddm.execute()
