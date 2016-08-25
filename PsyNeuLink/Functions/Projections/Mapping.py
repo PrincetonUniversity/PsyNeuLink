9a9a1dcb9a5b22f14367b9dd35aeda3ada3d80f1
jdc@princeton.edu <jdc@princeton.edu>
2016-08-24 23:38:13 -0400

    INTERIM
    • Function:
       - working on assignnent of ControlSignal() as arg
    
    [FROM Minstrel]

--- PsyNeuLink/Functions/Projections/Mapping.py
+++ PsyNeuLink/Functions/Projections/Mapping.py
@@ -147,7 +147,7 @@ IMPLEMENTATION NOTE:  *** DOCUMENTATION NEEDED (SEE CONTROL SIGNAL)
 
         # Assign args to params and functionParams dicts (kwConstants must == arg names)
         params = self.assign_args_to_param_dicts(function=function,
-                                                 kwFunctionParams={kwMatrix: matrix},
+                                                 function_params={kwMatrix: matrix},
                                                  param_modulation_operation=param_modulation_operation,
                                                  params=params)
 
