39acef0e63ccb21dc3412855dff00344dcd5b350
jdc@princeton.edu <jdc@princeton.edu>
2016-08-27 06:16:55 -0400

    RUNNING
    [FROM Minstrel]

--- PsyNeuLink/Globals/Defaults.py
+++ PsyNeuLink/Globals/Defaults.py
@@ -33,4 +33,4 @@ class DefaultControlAllocationMode(Enum):
 defaultControlAllocation = DefaultControlAllocationMode.BADGER_MODE.value
 
 # IMPLEMENTATION NOTE:  WOULD REQUIRE A DEFAULT MECHANISM AS WELL
-DEFAULT_ALLOCATION_SAMPLES = [0.0, 1.0, 0.5] # min, max, step size
+DEFAULT_ALLOCATION_SAMPLES = [0.0, 1.0, 0.1] # min, max, step size
