8c60c4fd88aac593687cf877af6a7ee937a9c073
jdc@princeton.edu <jdc@princeton.edu>
2016-08-30 10:12:13 -0400

    INTERIM
    • Project:
       - *execute_method* -> *function*
    
    • ShellClass:
      - removed def execute()
    
    • Function:
      - added def execute()
      - refactored validate_function() and instantiate_function()
    
    • System, Process:
       - self.function = self.execute
    
    • Utility:
      - def execute() -> def function()
    
    • ControlSignal, LearningSignal, State, InputState, ParameterState:
      - self.execute -> self.function
    
    • State:
       - def update() -> def execute()
    
    • Transfer, Comparator
        - <specialty_function> -> function
    
    • WeightedError
       - self.function = self.execute
    
    • ControlMechanism, DefaultControlMechanism
        - update() -> execute()
    
    • DDM:
      - refactoring partial / commented out
    
    [FROM Minstrel]

--- TODO List.py
+++ TODO List.py
@@ -142,6 +142,23 @@
 #         + kwTransfer_NUnits (float): (default: Transfer_DEFAULT_NUNITS
 #             specifies number of units (length of input array)
 #
+# NEED FOR EVC MODEL:
+# - Sequential adjust effects:
+#   "Reactive":  simple controlMechanism that maps input values into ControlSignal intensities
+#   "Simple Exhaustive Search": find optimal policy for stimulus/reward values
+#   "Feature-based model learning" (Falk & Tom)
+#   "Exhaustive Search + learning":
+#       searches through all ControlSignals to find the optimal one
+#       stimulus prediction
+#       reward prediction
+#       automatic component of the drift for each stimulus (== weight matrix)
+#    *  d(parameter_value)/d(control signal intensity) for each control signal == differential of the parameterModulationFunction
+#       NOTE:  THIS IS DISTINCT FROM THE ControlSignal.function (== intensity_function) WHICH MAPS ALLCATION -> ControlSignal Intensity
+#              BUT IS ISOMORPHIC IF ControlSignal.function IS Linear with slope = 1 and offsent 0 (i.e,. its default)
+#       QUESTION:  DO WE CARE ABOUT THE DIFFERENTIAL ON ALLOCATION -> parameter_value (.e., ControlSiganl.function)
+#                       OR ControlSignal Intensity -> parameter_value (i.e., parameterModulation function)??
+#        SEBASTRIAN FAVORS LEAVING IT AS DIFFERENTIAL ON parameterModulation function
+#    *  Parameters of parameterModulation function should be accessible
 
 #endregion
 
