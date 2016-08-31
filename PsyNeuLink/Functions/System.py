008bdaa67274e6c016ec8d2eadd69983e8a4cb65
jdc@princeton.edu <jdc@princeton.edu>
2016-08-31 19:18:05 -0400

    RUNNING
    • Function:
       - added create_attributes_for_user_params (from EXECUTE_+_PARAMS_ATTRIBUTES)
    
    • Utility:
       - implement_matrix modified to work (though still needs attention)
    
    [FROM Minstrel]

--- PsyNeuLink/Functions/System.py
+++ PsyNeuLink/Functions/System.py
@@ -363,8 +363,8 @@ class System_Base(System):
         register_category(self, System_Base, SystemRegistry, context=context)
 
         if context is NotImplemented:
-            # context = self.__class__.__name__
-            context = kwInit + self.name
+            # context = kwInit + self.name
+            context = kwInit + self.name + kwSeparator + kwSystemInit
 
         super(System_Base, self).__init__(variable_default=default_input_value,
                                            param_defaults=params,
@@ -776,7 +776,7 @@ class System_Base(System):
             try:
                 if self.controller.phaseSpec == (CentralClock.time_step % (self.phaseSpecMax +1)):
                     self.controller.execute(time_scale=TimeScale.TRIAL,
-                                            runtime_params=NotImplemented,
+                                            runtime_params=None,
                                             context=context)
                     if report_output:
                         print("{0}: {1} executed".format(self.name, self.controller.name))
