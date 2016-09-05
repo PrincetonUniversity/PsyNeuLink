4e4c743e862d71769db1b0b75fd08613239bfaa4
jdc@princeton.edu <jdc@princeton.edu>
2016-09-05 17:50:58 -0400

    RUNNING
    • LearningSignal:
      - fixed bug in automatic assignment of Mapping projection from errorSource to MonitoringMechanism (Comparator):
         previously assigned default (random) matrix;  now, for BP assigns kwIdentityMatrix, for RL assigns kwFullConnectivityMatrix
    
    • Reinforcement Learning Test Script:
       - implemented correct assignment of reward value (using lambda function to derive value from action selected)
       - cleaned up and commented
    
    [FROM Minstrel]

--- Scripts/Reinforcement Learning Test Script.py
+++ Scripts/Reinforcement Learning Test Script.py
@@ -10,21 +10,19 @@ action_selection = Transfer(default_input_value=[0,0,0],
                                              gain=1.0),
                             name='Action Selection')
 
-# # Explicit delcaration of Mapping projection to be learned:
-# reward_prediction = Mapping(sender=input_layer,
-#                             receiver=action_selection,
-#                             matrix=(kwIdentityMatrix, LearningSignal(function=Reinforcement(learning_rate=.05))))
-#
-# p = process(default_input_value=[0, 0, 0],
-#             configuration=[input_layer, reward_prediction, action_selection])
+# Explicit delcaration of Mapping projection to be learned:
+reward_prediction = Mapping(sender=input_layer,
+                            receiver=action_selection,
+                            matrix=(kwIdentityMatrix, LearningSignal(function=Reinforcement(learning_rate=.05))))
 
-# Learning specified for process (rather than explicitly declared projection)
 p = process(default_input_value=[0, 0, 0],
-            configuration=[input_layer, kwIdentityMatrix, action_selection],
-            learning=LearningSignal(function=Reinforcement(learning_rate=.05)))
+            configuration=[input_layer, reward_prediction, action_selection])
 
-
-
+# # Learning specified for process (rather than explicitly declared projection)
+# NOTE: Not current working - need to implement ability to assign matrix by keyword in Process configuration
+# p = process(default_input_value=[0, 0, 0],
+#             configuration=[input_layer, kwIdentityMatrix, action_selection],
+#             learning=LearningSignal(function=Reinforcement(learning_rate=.05)))
 
 print ('reward prediction weights: \n', action_selection.inputState.receivesFromProjections[0].matrix)
 print ('comparator weights: \n', action_selection.outputState.sendsToProjections[0].matrix)
@@ -34,32 +32,20 @@ actions = ['left', 'middle', 'right']
 reward_values = [15, 7, 13]
 first_reward = 0
 
-# Must initialize for reward lambda function to work (won't be used)
+# Must initialize reward (won't be used, but needed for declaration of lambda function)
 action_selection.outputState.value = [0, 0, 1]
+# Get reward value for selected action)
 reward = lambda : [reward_values[int(np.nonzero(action_selection.outputState.value)[0])]]
 
 # Run process with RL
 for i in range(100):
 
     # # Execute process, including weight adjustment based on last reward
-    # result = p.execute([[1, 1, 1], [reward]])
-    # result = p.execute([[1, 1, 1], reward(result)])
     result = p.execute([[1, 1, 1], reward])
 
     print ('result: ', result)
     # print ('reward: ', reward())
 
-    # DOESN"T SHOW NEW WEIGHTS, SINCE UPDATING OF PARAMETER STATES IS "LAZY" (NOT DONE UNTIL CALLED)
+    # Note: this shows weights updated on prior trial, not current one
+    #       (this is a result of parameterState "lazy updating" -- only updated when called)
     print ('\nreward prediction weights: \n', action_selection.inputState.receivesFromProjections[0].matrix)
-
-    # # Get index for action taken
-    # action_taken = int(np.nonzero(result)[0])
-    #
-    # # Set select reward for action taken
-    # reward = reward_values[action_taken]
-
-    # print ('result: ', result)
-    # print ('action taken: ', actions[action_taken])
-    # print ('reward: ', reward)
-    # print ('weights: \n', reward_prediction.matrix)
-
