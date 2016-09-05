ddb59f602f95d5847f748ccb3222b95f2e08f941
jdc@princeton.edu <jdc@princeton.edu>
2016-09-05 15:48:40 -0400

    RUNNING
    • LearningSignal:
      - fixed bug in automatic assignment of Mapping projection from errorSource to MonitoringMechanism (Comparator):
         previously assigned default (random) matrix;  now, for BP assigns kwIdentityMatrix, for RL assigns kwFullConnectivityMatrix
    
    [FROM Minstrel]

--- Scripts/Reinforcement Learning Test Script.py
+++ Scripts/Reinforcement Learning Test Script.py
@@ -7,37 +7,55 @@ input_layer = Transfer(default_input_value=[0,0,0],
 
 action_selection = Transfer(default_input_value=[0,0,0],
                             function=SoftMax(output=PROB,
-                                             gain=.5),
+                                             gain=1.0),
                             name='Action Selection')
 
-reward_prediction = Mapping(sender=input_layer,
-                            receiver=action_selection,
-                            matrix=(kwIdentityMatrix, LearningSignal(function=Reinforcement(learning_rate=.1))))
+# # Explicit delcaration of Mapping projection to be learned:
+# reward_prediction = Mapping(sender=input_layer,
+#                             receiver=action_selection,
+#                             matrix=(kwIdentityMatrix, LearningSignal(function=Reinforcement(learning_rate=.1))))
+#
+# p = process(default_input_value=[0, 0, 0],
+#             configuration=[input_layer, reward_prediction, action_selection])
+#
+# print ('weights: \n', reward_prediction.matrix)
 
+# Learning specified for process (rather than explicitly declared projection)
 p = process(default_input_value=[0, 0, 0],
-            configuration=[input_layer, reward_prediction, action_selection])
+            configuration=[input_layer, action_selection],
+            learning=LearningSignal(function=Reinforcement(learning_rate=.05)))
 
-
-print ('weights: \n', reward_prediction.matrix)
+print ('input weights: \n', action_selection.inputState.receivesFromProjections[0].matrix)
+print ('comparator weights: \n', action_selection.outputState.sendsToProjections[0].matrix)
 
 
 actions = ['left', 'middle', 'right']
 reward_values = [15, 7, 13]
 first_reward = 0
-reward = first_reward
+# reward = first_reward
+result = np.array([0, 1, 0])
+reward = lambda action: [reward_values[int(np.nonzero(action)[0])]]
 
-for i in range(100):
 
-    # Execute process, including weight adjustment based on last reward
-    result = p.execute([[1, 1, 1], [reward]])
+# Run process with RL
+for i in range(2):
 
-    # Get index for action taken
-    action_taken = int(np.nonzero(result)[0])
-
-    # Set select reward for action taken
-    reward = reward_values[action_taken]
+    # # Execute process, including weight adjustment based on last reward
+    # result = p.execute([[1, 1, 1], [reward]])
+    result = p.execute([[1, 1, 1], reward(result)])
 
     print ('result: ', result)
-    print ('action taken: ', actions[action_taken])
-    print ('reward: ', reward)
-    print ('weights: \n', reward_prediction.matrix)
+    print ('reward: ', reward(result))
+
+    # # Get index for action taken
+    # action_taken = int(np.nonzero(result)[0])
+    #
+    # # Set select reward for action taken
+    # reward = reward_values[action_taken]
+
+    # print ('result: ', result)
+    # print ('action taken: ', actions[action_taken])
+    # print ('reward: ', reward)
+    # print ('weights: \n', reward_prediction.matrix)
+    print ('weights: \n', action_selection.inputState.receivesFromProjections[0].matrix)
+
--- TODO List.py
+++ TODO List.py
@@ -123,7 +123,15 @@
 #                     fixed process factory method (can now call process instead of Process_Base)
 #                     flattened DDM arg structure (see DDM Test Script)
 #                         QUESTION: should defaults be numbers or values??
-# IMPLEMENT **RL (Based on BP)
+# QUESTION: RL:
+#           Option 1 - Provide Process with reward for option selected: more natural, but introduces timing problems:
+#               - how to provide reward for outcome of first trial, if it is selected probablisitically
+#               - must process trial, get reward from environment, then execute learning
+#           Option 2 - Provide Process with reward vector, and let comparator choose reward based on action vector
+#               - softamx should pass vector with one non-zero element, that is the one rewarded by comoparator
+#           SOLUTION:  use Option 2 for Process, and implement Option 1 at System level (which can control timing):
+#               - system should be take functions that specify values to use as inputs based on outputs
+#               - same for process??
 
 # QUESTION: ??OPTION (reshapeWeightMatrixOption for Mapping) TO SUPPRESS RESHAPING (FOR FULL CONNECTIVITY)
 #
