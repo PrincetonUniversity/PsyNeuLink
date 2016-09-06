c772f68421b9d97777620defd2ea80065fadbebe
jdc <jdc@princeton.edu>
2016-09-06 14:06:45 -0400

    RUNNING
    • PsyNeuLink.__init__.py:
       - added imports
       - add __all__
    
    • Mapping:
       - fixed bug with using class name (ControlSignal or LearningSignal) specification of Mapping matrix param
    
    • Utility:
      - modified Softmax params, added return of probabilistically chosen item
    
    [FROM Quill]

--- TODO List.py
+++ TODO List.py
@@ -112,7 +112,7 @@
 
 #endregion
 
-#region EVC MEETING: -------------------------------------------------------------------------------------------------------
+#region EVC MEETING: ---------------------------------------------------------------------------------------------------
 #
 # IMPLEMENT: Rename Function -> Block (or Component or Module or Structure)
 # IMPLEMENT: Refactoring of DDM (solutions are now functions.. see DDM Test Script for example)
@@ -164,8 +164,10 @@
 #       stimulus prediction
 #       reward prediction
 #       automatic component of the drift for each stimulus (== weight matrix)
-#    *  d(parameter_value)/d(control signal intensity) for each control signal == differential of the parameterModulationFunction
-#       NOTE:  THIS IS DISTINCT FROM THE ControlSignal.function (== intensity_function) WHICH MAPS ALLCATION -> ControlSignal Intensity
+#    *  d(parameter_value)/d(control signal intensity) for each control signal ==
+#                                                          differential of the parameterModulationFunction
+#       NOTE:  THIS IS DISTINCT FROM THE ControlSignal.function
+#                                               (== intensity_function) WHICH MAPS ALLCATION -> ControlSignal Intensity
 #              BUT IS ISOMORPHIC IF ControlSignal.function IS Linear with slope = 1 and offsent 0 (i.e,. its default)
 #       QUESTION:  DO WE CARE ABOUT THE DIFFERENTIAL ON ALLOCATION -> parameter_value (.e., ControlSiganl.function)
 #                       OR ControlSignal Intensity -> parameter_value (i.e., parameterModulation function)??
@@ -179,7 +181,6 @@
 # 8/25/16:
 
 # PROJECTION is_projection_spec:
-    # FIX: INCLUDE CHECK FOR TUPLE WITH FIRST ITEM = PROJECTION (RECURSIVE CALL) AND SECOND ITEM IS CONTROL OR LEARING SIGNAL
 # IMPLEMENT: Register allowable specs for a class, that is used by generic is_component for validation
 #            (would replace Projection is_projection, others?)
 
@@ -439,7 +440,7 @@
 #      ALLOW IT TO BE:  MonitoredOutputStatesOption, Mechanism, OutputState or list containing any of those
 # FIX: NEED TO SOMEHOW CALL validate_monitored_state FOR kwMonitoredOutputStates IN SYSTEM.params[]
 # FIX: CALL instantiate_monitored_output_states AFTER instantiate_prediction_mechanism (SO LATTER CAN BE MONITORED)
-# FIX: QUESTION:  WHICH SHOULD HAVE PRECEDENCE FOR kwMonitoredOutputStates default:  System, Mechanism or ConrolMechanism?
+# FIX: QUESTION:  WHICH SHOULD HAVE PRECEDENCE FOR kwMonitoredOutputStates default: System,Mechanism or ConrolMechanism?
 #
 # 7/13/16:
 #
@@ -451,7 +452,8 @@
 # 7/9/16
 #
 # FIX: ERROR in "Sigmoid" script:
-# Functions.Projections.Projection.ProjectionError: 'Length (1) of outputState for Process-1_ProcessInputState must equal length (2) of variable for Mapping projection'
+# Functions.Projections.Projection.ProjectionError:
+#  'Length (1) of outputState for Process-1_ProcessInputState must equal length (2) of variable for Mapping projection'
 #       PROBLEM: Mapping.instantiate_function() compares length of sender.value, which for DDM is 3 outputStates
 #                                                     with length of receiver, which for DDM is just a single inputState
 #
@@ -485,7 +487,7 @@
 # FIX (PROJECTION): FIX MESS CONCERNING VARIABLE AND SENDER ASSIGNMENTS:
 #         SHOULDN'T variable_default HERE BE sender.value ??  AT LEAST FOR Mapping?, WHAT ABOUT ControlSignal??
 #                     MODIFIED 6/12/16:  ADDED ASSIGNMENT ABOVE
-#                                       (TO HANDLE INSTANTIATION OF DEFAULT ControlSignal SENDER -- BUT WHY ISN'T VALUE ESTABLISHED YET?
+#                      (TO HANDLE INSTANTIATION OF DEFAULT ControlSignal SENDER -- BUT WHY ISN'T VALUE ESTABLISHED YET?
 # --------------
 # FIX: (OutputState 5/26/16
         # IMPLEMENTATION NOTE:
@@ -544,10 +546,11 @@
 # FIX: ConrolSignal.set_intensity SHOULD CHANGE paramInstanceDefaults
 # CONFIRM:  ControlSignal.intensity GETS COMBINED WITH allocadtion_source USING ModulationOperation
 # QUESTION: WHAT DOES SETTING ControlSignal.set_intensity() DO NOW??
-# IMPLEMENT: ADD PARAM TO DDM (AKIN TO kwDDM_AnayticSolution) THAT SPECIFIES PRIMARY INPUTSTATE (i.e., DRIFT_RATE, BIAS, THRSHOLD)
+# IMPLEMENT: ADD PARAM TO DDM (AKIN TO kwDDM_AnayticSolution) THAT SPECIFIES PRIMARY INPUTSTATE
+#                              (i.e., DRIFT_RATE, BIAS, THRSHOLD)
 #endregion
 
-#region GENERAL: -------------------------------------------------------------------------------------------------------------
+#region GENERAL: -------------------------------------------------------------------------------------------------------
 #
 # - Register name:
 #    PsyNeuLink
@@ -655,7 +658,7 @@
 #   - get rid of tests for PROGRAM ERROR
 # endregion
 
-#region DOCUMENT: ------------------------------------------------------------------------------------------------------------
+#region DOCUMENT: ------------------------------------------------------------------------------------------------------
 #
 #  CLEAN UP THE FOLLOWING
 # - Combine "Parameters" section with "Initialization arguments" section in:
