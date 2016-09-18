ccb399a9809eca64e78b751b400fc61045618275
jdc@princeton.edu <jdc@princeton.edu>
2016-09-18 08:07:31 -0400

    RUNNING
    • __init__.py
       - Modified default preferenceSets for System and Process
    
    • FunctionPreferenceSet:
       - kpPreferenceKeywords -> PREFERENCE_KEYWORDS
    
    • System:
       - Implemented report_system_initiation() and report_system_completion()
    
    • Process:
       - Implemented report_process_initiation() and report_process_completion()
       - Deprecated reporting of mechanism execution (relegated to Mechanism)
    
    • Mechanism:
       - implemented report_mechanism_execution() which now handles all reporting
          (can be overridden by subclass to customize what is reported -- see Transfer for example)
    
    [FROM Minstrel]

--- Scripts/Stroop Model Test Script.py
+++ Scripts/Stroop Model Test Script.py
@@ -1,22 +1,13 @@
-import random as rnd
-
-from PsyNeuLink.Functions.Process import process
 from PsyNeuLink.Functions.Utilities.Utility import Linear, Logistic
 from PsyNeuLink.Functions.Mechanisms.ProcessingMechanisms.Transfer import Transfer
-from PsyNeuLink.Functions.Projections.Mapping import Mapping
 from PsyNeuLink.Functions.System import *
 from PsyNeuLink.Globals.Keywords import *
 
-process_prefs = {
-    REPORT_OPUTPUT_PREF: True,
-    VERBOSE_PREF: False
-}
+process_prefs = {REPORT_OPUTPUT_PREF: True,
+                 VERBOSE_PREF: False}
 
-system_prefs = {
-    REPORT_OPUTPUT_PREF: True,
-    VERBOSE_PREF: True
-}
-
+system_prefs = {REPORT_OPUTPUT_PREF: True,
+                VERBOSE_PREF: True}
 
 colors = Transfer(default_input_value=[0,0],
                         function=Linear,
@@ -32,14 +23,12 @@ response = Transfer(default_input_value=[0,0],
 
 color_naming_process = process(default_input_value=[1, 2.5],
                                configuration=[(colors, 0), FULL_CONNECTIVITY_MATRIX, (response,0)],
-                               # configuration=[(colors), FULL_CONNECTIVITY_MATRIX, (response)],
                                name='Color Naming',
                                prefs=process_prefs
                                )
 
 word_reading_process = process(default_input_value=[.5, 3],
                                configuration=[(words, 0), FULL_CONNECTIVITY_MATRIX, (response,0)],
-                               # configuration=[(words), FULL_CONNECTIVITY_MATRIX, (response)],
                                name='Word Reading',
                                prefs=process_prefs
                                )
@@ -49,7 +38,7 @@ mySystem = system(processes=[color_naming_process, word_reading_process],
                   prefs=system_prefs
                   )
 
-
+# TEST REPORT_OUTPUT_PREFs:
 # colors.reportOutputPref = True
 # words.reportOutputPref = True
 # response.reportOutputPref = True
@@ -57,42 +46,22 @@ mySystem = system(processes=[color_naming_process, word_reading_process],
 # word_reading_process.reportOutputPref =  False
 # process_prefs.reportOutputPref = PreferenceEntry(True, PreferenceLevel.CATEGORY)
 
-
 # mySystem.reportOutputPref = True
 
+# TEST VERBOSE_PREFs:
 # colors.verbosePref = True
 # words.verbosePref = True
 color_naming_process.verbosePref = True
 word_reading_process.verbosePref = True
 mySystem.verbosePref = True
 
+# Execute processes:
 # color_naming_process.execute([2, 2])
 # word_reading_process.execute([3, 3])
 
+# Execute system:
 mySystem.execute(inputs=[[1,1],[1,1]])
 
-
-# #region Inspect
+# INSPECTIONS:
 # mySystem.inspect()
 # mySystem.controller.inspect()
-# #endregion
-
-# #region Run
-# numTrials = 10
-# for i in range(0, numTrials):
-#     stimulus = rnd.random()*3 - 2
-#
-#     # Present stimulus:
-#     CentralClock.time_step = 0
-#     mySystem.execute([[stimulus, stimulus],[0, 0]])
-#     print ('\n{0}\n{1}'.format(mySystem.terminalMechanisms.outputStateLabels,
-#                                mySystem.terminalMechanisms.outputStateValues))
-#
-#     # Present feedback:
-#     CentralClock.time_step = 1
-#     mySystem.execute([[0,0],[1,1]])
-#     print ('\n{0}\n{1}'.format(mySystem.terminalMechanisms.outputStateLabels,
-#                                mySystem.terminalMechanisms.outputStateValues))
-#
-
-#endregion
