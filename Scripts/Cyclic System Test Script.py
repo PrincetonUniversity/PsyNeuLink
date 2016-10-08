16116befb7f7ccd763b8ecd617b25f367c234d2d
jdc@princeton.edu <jdc@princeton.edu>
2016-10-08 09:23:29 -0400

    RUNNING
    • Project:
       - replaced phaseSpecMax with numPhases wherever appropriate
    
    • Function:
       - implemented initialize()
    
    • System:
       - added SINGLETON to tests for ORIGIN
       - revised assignment of ORIGIN:
              only requirement is that it not receive projections from another process
              *can* receive feedback projection from within its own process (e.g., [a, b, a])
              or another for which it is an origin (e.g., [a, b, a], [a, c, a])
       - implemented initial_values arg and initialize() method
       - changed INITIALIZE to INITIALIZE_CYCLE designation for senders of a feedback connection
       - added CYCLE designation for receivers of a feedback connection
       - self.processes: now points to self.processList.processes (after constructing process_tuples and instantiating processList)
       - implemented self.mechainsms (points to self.allMechanisms.mechanisms
    
    • Process:
       - implemented initial_values arg and initialize() method
       - added SINGLETON to mechanism process status (if it is both the first and last mechanism in the process)
       - refactored self.processes to be a list of processes (vs. tuples);  tuples assigned directly to self.process_tuples in instantiate_processes
       - refactored assignment of self.origin_mech_tuples and self.terminal_mech_tuples:
            only assign to lists if not from a controllerProcess
            avoids having to get indices and sort (since self.graph is now an OrderedDict that is constructed in order of self.processes)
    
    • Mechanism:
       - implemented initial() method
    
     • Graph Test Script
    
    [FROM Minstrel]

--- Scripts/Cyclic System Test Script.py
+++ Scripts/Cyclic System Test Script.py
@@ -3,12 +3,6 @@ from PsyNeuLink.Functions.Process import process
 from PsyNeuLink.Functions.Mechanisms.ProcessingMechanisms.Transfer import Transfer
 from PsyNeuLink.Functions.Process import Mapping
 
-a = Transfer(name='a',default_input_value=[0,0])
-b = Transfer(name='b',default_input_value=[0,0])
-c = Transfer(name='c')
-d = Transfer(name='d')
-e = Transfer(name='e')
-f = Transfer(name='f')
 
 # fb1 = Mapping(sender=c, receiver=b, name='fb1')
 # fb2 = Mapping(sender=d, receiver=e, name = 'fb2')
@@ -17,112 +11,169 @@ f = Transfer(name='f')
 
 # p1 = process(configuration=[a, b, c, d], name='p1')
 
-# @@@@@@@@@@@@@@@@@@@@@@@@@@
-# a -> origin
-# b -> internal
-# c -> internal
-# d -> initialize
-# e -> terminal
 
-# # CYCLIC:
-# p2 = process(configuration=[e, c, b, d], name='p2')
-# p1e = process(configuration=[a, b, c, d], name='p1e')
-# p2 = process(configuration=[e, c, b, d], name='p2')
-# p2 = process(configuration=[e, c, f, b, d], name='p2')
+print ('*****************************************************************************')
 
-# CYCLIC INCLUDING ORIGIN IN CYCLE:
-# p1e = process(default_input_value=[[0,0]], configuration=[a, b], name='p1e')
-# p1e = process(configuration=[a, b], name='p1e')
-p1e = process(configuration=[a, b, a], name='p1e')
-p2 = process(configuration=[a, c, a], name='p2')
-# p2 = process(configuration=[e, f], name='p2')
+# BRANCH -----------------------------------------------------------------------------
 
+a = Transfer(name='a',default_input_value=[0,0])
+b = Transfer(name='b',default_input_value=[0,0])
+c = Transfer(name='c')
+d = Transfer(name='d')
 
-# # BRANCH:
-# p1e = process(configuration=[a, b, c], name='p1e')
-# p2 = process(configuration=[a, b, d], name='p2')
+p1 = process(configuration=[a, b, c], name='p1')
+p2 = process(configuration=[a, b, d], name='p2')
 
-# # # BYPASS (OUT OF PROCESS BRANCH):
-# p1e = process(configuration=[a, b, c, d], name='p1e')
-# p2 = process(configuration=[a, b, d], name='p2')
-
-
-# # CHAIN:
-# p1e = process(configuration=[a, b, c], name='p1e')
-# p2 = process(configuration=[c, d, e], name='p2')
-
-# CONVERGENT:
-# p1e = process(configuration=[a, b, e], name='p1e')
-# p2 = process(configuration=[c, d, e], name='p2')
-
-
-# WORKS (treats e as an origin):
-# s = system(default_input_value=[[0,0]], processes=[p1e], name='system')
-# s = system(processes=[p1e], name='system')
-
-s = system(processes=[p1e, p2],
-           name='system',
+s = system(processes=[p1, p2],
+           name='Branch System',
            initial_values={a:[1,1]})
-# s = system(processes=[p2, p1e],
-#            name='system',
-#            initial_values={a:[1,1]})
 
-# s = system(processes=[p2 ,p1e], name='system')
-        # Senders to b:
-        # 	a
-        # 	e
-        # Senders to c:
-        # 	b
-        # a is origin
-        # e is origin
-        # Senders to d:
-        # 	c
-        #
-
-# HAS CYCLE (does NOT treat e as an origin):
-# a = system(processes=[p2, p1e], name='system')
-        # Senders to e:
-        # 	c
-        # a is origin
-        # Senders to d:
-        # 	c
-        # Senders to b:
-        # 	e
-        # 	a
-        # Senders to c:
-        # 	b
-
-# SEQUENTIAL PROCESSES:
-# p_1 = process(configuration=[e, a, b, c, b], name='p_1')
-# p_2 = process(configuration=[b, d, e], name='p_2')
-# p_1 = process(configuration=[a, b], name='p_1')
-# p_2 = process(configuration=[b, a], name='p_2')
-# a = system(processes=[p_1, p_2], name='seq_system')
-
-
-
-
-# p4 = process(configuration=[a, b, c], name='p4')
-# p5= process(configuration=[c, d, e], name='p5')
-# a = system(processes=[p1, p2, p3], name='system')
-
-
-# a = system(processes=[p4, p5], name='system')
-
-# s.inspect()
 s.inspect()
 
 print ('A: ',a.systems[s])
-# print ('A: ',a.processes)
 print ('B: ',b.systems[s])
 print ('C: ',c.systems[s])
-# print ('D: ',d.systems[s])
-# print ('E: ',e.systems[s])
+print ('D: ',d.systems[s])
 
-for projection in e.inputState.receivesFromProjections:
-    print("Projection name: {}; sender: {};  receiver: {}".
-          format(projection.name, projection.sender.owner.name, projection.receiver.owner.name))
 
-# s.execute(inputs=[[0,0]])
-# s.execute(inputs=[[0,0],[0,0]])
-# s.execute()
+print ('*****************************************************************************')
+
+# BYPASS -----------------------------------------------------------------------------
+
+a = Transfer(name='a',default_input_value=[0,0])
+b = Transfer(name='b',default_input_value=[0,0])
+c = Transfer(name='c')
+d = Transfer(name='d')
+
+p1 = process(configuration=[a, b, c, d], name='p1')
+p2 = process(configuration=[a, b, d], name='p2')
+
+s = system(processes=[p1, p2],
+           name='Bypass System',
+           initial_values={a:[1,1]})
+
+s.inspect()
+
+print ('A: ',a.systems[s])
+print ('B: ',b.systems[s])
+print ('C: ',c.systems[s])
+print ('D: ',d.systems[s])
+
+
+print ('*****************************************************************************')
+
+# CHAIN -----------------------------------------------------------------------------
+
+a = Transfer(name='a',default_input_value=[0,0])
+b = Transfer(name='b',default_input_value=[0,0])
+c = Transfer(name='c')
+d = Transfer(name='d')
+e = Transfer(name='e')
+
+p1 = process(configuration=[a, b, c], name='p1')
+p2 = process(configuration=[c, d, e], name='p2')
+
+s = system(processes=[p1, p2],
+           name='Chain System',
+           initial_values={a:[1,1]})
+
+s.inspect()
+
+print ('A: ',a.systems[s])
+print ('B: ',b.systems[s])
+print ('C: ',c.systems[s])
+print ('D: ',d.systems[s])
+print ('E: ',e.systems[s])
+
+
+print ('*****************************************************************************')
+
+# CONVERGENT -----------------------------------------------------------------------------
+
+a = Transfer(name='a',default_input_value=[0,0])
+b = Transfer(name='b',default_input_value=[0,0])
+c = Transfer(name='c')
+d = Transfer(name='d')
+e = Transfer(name='e')
+
+p1 = process(configuration=[a, b, e], name='p1')
+p2 = process(configuration=[c, d, e], name='p2')
+
+s = system(processes=[p1, p2],
+           name='Chain System',
+           initial_values={a:[1,1]})
+
+s.inspect()
+
+print ('A: ',a.systems[s])
+print ('B: ',b.systems[s])
+print ('C: ',c.systems[s])
+print ('D: ',d.systems[s])
+print ('E: ',e.systems[s])
+
+
+print ('*****************************************************************************')
+
+# CYCLIC INCLUDING ORIGIN IN CYCLE (ONE PROCESS) ------------------------------------
+
+a = Transfer(name='a',default_input_value=[0,0])
+b = Transfer(name='b',default_input_value=[0,0])
+
+p1 = process(configuration=[a, b, a], name='p1')
+
+s = system(processes=[p1],
+           name='Cyclic System with one Process',
+           initial_values={a:[1,1]})
+
+s.inspect()
+
+print ('A: ',a.systems[s])
+print ('B: ',b.systems[s])
+
+
+print ('*****************************************************************************')
+
+# CYCLIC INCLUDING ORIGIN IN CYCLE (TWO PROCESSES) -----------------------------------
+
+a = Transfer(name='a',default_input_value=[0,0])
+b = Transfer(name='b',default_input_value=[0,0])
+c = Transfer(name='c',default_input_value=[0,0])
+
+p1 = process(configuration=[a, b, a], name='p1')
+p2 = process(configuration=[a, c, a], name='p2')
+
+s = system(processes=[p1, p2],
+           name='Cyclic System with one Process',
+           initial_values={a:[1,1]})
+
+s.inspect()
+
+print ('A: ',a.systems[s])
+print ('B: ',b.systems[s])
+print ('C: ',c.systems[s])
+
+
+# CYCLIC WITH TWO PROCESSES AND AN EXTENDED LOOP ------------------------------------
+
+a = Transfer(name='a',default_input_value=[0,0])
+b = Transfer(name='b',default_input_value=[0,0])
+c = Transfer(name='c')
+d = Transfer(name='d')
+e = Transfer(name='e')
+f = Transfer(name='f')
+
+p1 = process(configuration=[a, b, c, d], name='p1')
+p2 = process(configuration=[e, c, f, b, d], name='p2')
+
+s = system(processes=[p1, p2],
+           name='Cyclic System with Extended Loop',
+           initial_values={a:[1,1]})
+
+s.inspect()
+
+print ('A: ',a.systems[s])
+print ('B: ',b.systems[s])
+print ('C: ',c.systems[s])
+print ('D: ',d.systems[s])
+print ('E: ',e.systems[s])
+print ('F: ',f.systems[s])
