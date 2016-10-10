2db75d70f49b9f5a5d343096202543d2855d510b
jdc@princeton.edu <jdc@princeton.edu>
2016-10-09 22:21:25 -0400

    RUNNING
    • Project:
       - replaced phaseSpecMax with numPhases wherever appropriate
    
    • Function:
       - implemented initialize()
    
    • System:
       - fixed bugs and substantially enhanced flexiblity and robustness of construct_inputs():
              can now handle stimuli of different lengths for different origin mechanisms, and various levels of array embeddings
       - implemented initial_values arg and initialize() method
       - revised assignment of ORIGIN:
              only requirement is that it not receive projections from another process
              *can* receive feedback projection from within its own process (e.g., [a, b, a])
              or another for which it is an origin (e.g., [a, b, a], [a, c, a])
       - changed INITIALIZE to INITIALIZE_CYCLE designation for senders of a feedback connection
       - added SINGLETON to tests for ORIGIN
       - added CYCLE designation for receivers of a feedback connection
       - self.processes: now points to self.processList.processes (after constructing process_tuples and instantiating processList)
       - implemented self.mechanisms (points to self.allMechanisms.mechanisms
    
    • Process:
       - implemented initial_values arg and initialize() method
       - added SINGLETON to mechanism process status (if it is both the first and last mechanism in the process)
       - refactored self.processes to be a list of processes (vs. tuples);  tuples assigned directly to self.process_tuples in instantiate_processes
       - refactored assignment of self.origin_mech_tuples and self.terminal_mech_tuples:
            only assign to lists if not from a controllerProcess
            avoids having to get indices and sort (since self.graph is now an OrderedDict that is constructed in order of self.processes)
    
    • Mechanism:
       - implemented initial() method
    
     • System Graph and Input Test Script:
       - tests various combinations of inputs for run()
    
    [FROM Minstrel]

--- TODO List.py
+++ TODO List.py
@@ -216,7 +216,6 @@
 #region CURRENT: -------------------------------------------------------------------------------------------------------
 
 # 10/9/16:
-# FIX: Finish cleaning up and testing System Graph and Input Test
 # FIX: Fix headers in construct_input()
 # FIX: PROCESS.RUN() NEEDS TO BE ABLE TO TTAKE TARGET
 
