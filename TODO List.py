eaaebbd95c441908064ca27de68c9dba5161f8a7
jdc@princeton.edu <jdc@princeton.edu>
2016-10-12 22:24:04 -0400

    RUNNING
    • Project:
       - replaced phaseSpecMax with numPhases wherever appropriate
       - changed inspect() to show() throughout
    
    • Run Module:
      - run():  runs system or process
      - construct_inputs():  constructs input suitable for use by run()
      - validate_inputs()
    
    • Function:
       - implemented initialize()
    
    • System:
       - fixed bugs and substantially enhanced flexiblity and robustness of construct_inputs():
              can now handle stimuli of different lengths for different origin mechanisms, and various levels of array embeddings
       - implemented initial_values arg and initialize() method
       - revised assignment of ORIGIN:
              only requirement is that it not receive projections from another process
              *can* receive feedback projection from within its own process (e.g., [a, b, a])
              or another for which it is an origin (e.g., [a, b, a], [a, c, a])
       - changed INITIALIZE to INITIALIZE_CYCLE designation for senders of a feedback connection
       - added SINGLETON to tests for ORIGIN
       - added CYCLE designation for receivers of a feedback connection
       - self.processes: now points to self.processList.processes (after constructing process_tuples and instantiating processList)
       - implemented self.mechanisms (points to self.allMechanisms.mechanisms
       - implemented monitoringMechanisms, learningMechanisms, and controlMechanisms attributes
       - implemented new version of inspect() that returns dict with values of various attributes and data structures
    
    • Process:
       - implemented initial_values arg and initialize() method
       - added SINGLETON to mechanism process status (if it is both the first and last mechanism in the process)
       - refactored self.processes to be a list of processes (vs. tuples);  tuples assigned directly to self.process_tuples in instantiate_processes
       - refactored assignment of self.origin_mech_tuples and self.terminal_mech_tuples:
            only assign to lists if not from a controllerProcess
            avoids having to get indices and sort (since self.graph is now an OrderedDict that is constructed in order of self.processes)
    
    • Mechanism:
       - implemented initial() method
    
     • System Graph and Input Test Script:
       - tests various combinations of inputs for run()
    
    [FROM Minstrel]

--- TODO List.py
+++ TODO List.py
@@ -242,7 +242,7 @@
 #    result = Result(5, 6)
 
 # 10/6/16:
-# TEST (in Cycle System Test Script): inputs=s.construct_input(inputs=[    [   [  [2,2],  [0]     ]   ]  ])
+# TEST (in Cycle System Test Script): inputs=s.construct_inputs(inputs=[    [   [  [2,2],  [0]     ]   ]  ])
 # FIX: Add apostrophes to append_type_name(); and also allow object to be passed, in which case type can be inferred
 #        or just add method to Function that returns formatted name!
 # FIX: System.construct_inputs:  add HEADER keyword for use of header in trial list (so stimuli can contain non-numerics)
