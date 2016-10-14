3edfcd183e1a7e8c9e2b1ff24d83e11469ab4539
jdc@princeton.edu <jdc@princeton.edu>
2016-10-13 21:37:44 -0400

    RUNNING
    • Project:
       - prependend internal methods with _:
           Function:
           _validate_variable
           _validate_params
           _validate_function
           _deferred_init
           _assign_args_to_param_dicts
           _create_attributes_for_user_params
           _check_args
           _check_kwFunction
           _instantiate_attributes_before_function
           _instantiate_function
           _instantiate_attributes_after_function
           _update_value
          Process:
           _instantiate_processes
           _instantiate_graph
           _assign_output_states
           _report_system_initiation
           _report_system_completion
    
    • System:
      - fixed bug in MechanismList.mechanismNames
      - deprecated ProcessMechanismList and SystemMechanismList (consolidated under MechanismList)
      - changes
    
    [IN PROCESS:
    • Process:
       - changed process.mechanismList to process.mech_tuples
    
    • META Test Script:
       - changed f to file (to avoid homonymy with other objects called f)
    
    [FROM Minstrel]

--- PsyNeuLink/Functions/Process.py
+++ PsyNeuLink/Functions/Process.py
@@ -123,7 +123,7 @@ class Process_Base(Process):
 #               but not necessarily one without outgoing projections or the last one to be executed:
 #               if learning is enabled, then a comparator mechanism is assigned to which a terminal mechanism projects;
 #               this is relevant in constructing system graphs
-#               (see System instantiate_graph() and spanning_multi_origin_partial_order()
+#               (see System _instantiate_graph() and spanning_multi_origin_partial_order()
 # DOCUMENT: One and only one comparator mechanism must be assigned to a process if learning is specified and enabled
 #           That comparator mechanism will have the process listed in its processes attribute
 # DOCUMENT: DURING EXECUTE, FOR EACH PROJECTION TO INPUT_STATE OF EACH MECHANISMS,
