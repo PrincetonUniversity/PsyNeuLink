63c3529614305f5187ec4ad429fc9ca109c942d2
Nate Wilson <natemwilson@gmail.com>
2016-11-01 13:06:45 -0400

    adding random seeds to Jenkins META test script
    makes random calls deterministic

--- Scripts/Jenkins META Test Script.py
+++ Scripts/Jenkins META Test Script.py
@@ -1,3 +1,10 @@
+import numpy as np
+import random
+
+# Insures that runs are deterministic for use in Jenkins testing
+random.seed(0)
+np.random.seed(0)
+
 scripts =  ['Scripts/EVC System Laming Validation Test Script.py',
             'Scripts/Multilayer Learning Test Script.py',
             'Scripts/Reinforcement Learning Test Script.py',
