17863eff20b707ec644890ce506727d0d461967d
Nate Wilson <natemwilson@gmail.com>
2016-11-03 16:04:36 -0400

    updating jenkins meta test bash script, removing Jenkins META Test Script python script, in favor of simply using META Test Script

--- jenkins/jenkins_meta_test.sh
+++ jenkins/jenkins_meta_test.sh
@@ -1,13 +1,32 @@
 #! /bin/bash
+# you must run this script in the top level dir
 
-# replace this echo statement with a python test script
-python Scripts/Jenkins\ META\ Test\ Script.py > jenkins/test_output.txt
+# create the environment
+conda create -n psyneulink_environment python=3.5
 
-# compare reference output to test output
+# activate the environment
+source activate psyneulink_environment
+
+# install all necessary dependencies
+pip install .
+
+# change dirs into scripts dir
+cd Scripts
+
+# run meta test script and save output in jenkins/testoutput
+python META\ Test\ Script.py > ../jenkins/test_output.txt
+
+# compare reference output to test output and save the exit code for later
 diff jenkins/test_output.txt jenkins/reference_output.txt
+DIFF_EXIT_CODE=$?
+
+# deactivate the environment
+source deactivate
+
+conda remove --name psyneulink_environment --all
 
 # return exit code of diff
-if [ $? -eq 0 ]
+if [ $DIFF_EXIT_CODE -eq 0 ]
 	then
 		exit 0
 	else
