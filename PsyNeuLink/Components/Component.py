ebbb1694502f598fa9138dffa25cf17dd47199ef
jdc <jdc@princeton.edu>
2016-12-02 10:17:12 -0500

    RUNNING
    • Project:
      - kwStateParams -> STATE_PARAMS
    
    • Components:
       - assign_defaults -> _assign_defaults
       - added assign_params
       - kwFunctionInit -> COMPONENT_INIT
       - fix various bugs in specification of projections and function class as params
    
    • ParameterStates:
       - docstring -> rST
    
    • Utilities:
       - is_numerical -> is_numeric; is_numerical_or_none -> is_numeric_or_none
    
    [FROM Quill]

--- PsyNeuLink/Components/Component.py
+++ PsyNeuLink/Components/Component.py
@@ -1271,13 +1271,15 @@ class Component(object):
 
             # If self is a Function:
             #    if param is a tuple, get its value (since Functions can't take projection specifications)
-            #    if param is a class ref for function, instantiate it as a function
+            #    if param is a class ref for function, instantiate it as the function
             from PsyNeuLink.Components.Functions.Function import Function_Base
             if isinstance(self, Function_Base):
                 if isinstance(param_value, tuple):
+                    # Get value and assign to param_value for compatibility check below
                     param_value = self._get_param_value_from_tuple(param_value)
                 elif (inspect.isclass(param_value) and
                           issubclass(param_value, self.paramClassDefaults[param_name])):
+                    # Assign to target and move on (compatiblity check no longer needed and can't handle function)
                     target_set[param_name] = param_value()
                     continue
 
