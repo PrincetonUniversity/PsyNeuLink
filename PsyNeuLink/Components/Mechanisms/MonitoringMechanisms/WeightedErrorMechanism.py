e4a6cf1789aceb60cbad661565d33a24c31375ad
jdc@princeton.edu <jdc@princeton.edu>
2016-12-09 10:44:45 -0500

    RUNNING
    • Project:
      kwNameArg -> NAME_ARG
      kwParamsArg -> PARAMS_ARG
      kwPrefsArg -> PREFS_ARG
      ANALYZE -> CALCULATE
    
    • Process:
       - added LEARNING for specification of ``learning`` argument/attribute
       - no longer require target if TERMINAL_MECHANISM belongs to another process that has a comparator mechanism
       - removed type-checking for target attribute (was interfereing with assignment of np.array)
    
    • Mechanism
        - converted outputState assignments to new format for all but ControlMechanisms
        - implemented call to outputState.update in _update_output_states
    
    • OutputState:
        - added index arg
        - impelmented update()
    
    • EVC System Laming Validation Test Script
      - modified output format (get rid of array notation, convert to floats)
    
    [FROM Minstrel]

--- PsyNeuLink/Components/Mechanisms/MonitoringMechanisms/WeightedErrorMechanism.py
+++ PsyNeuLink/Components/Mechanisms/MonitoringMechanisms/WeightedErrorMechanism.py
@@ -250,15 +250,6 @@ class WeightedErrorMechanism(MonitoringMechanism_Base):
                                      " must equal length of error_signal ({})".
                                      format(cols,self.name,error_signal_len))
 
-    def _instantiate_attributes_before_function(self, context=None):
-
-        # Map indices of output to outputState(s)
-        self._outputStateValueMapping = {}
-        self._outputStateValueMapping[WEIGHTED_ERRORS] = WeightedErrorOutput.ERROR_SIGNAL.value
-
-        super()._instantiate_attributes_before_function(context=context)
-
-
     def __execute__(self,
                 variable=None,
                 params=None,
@@ -297,7 +288,11 @@ class WeightedErrorMechanism(MonitoringMechanism_Base):
         # Compute summed error for use by callers to decide whether to update
         self.summedErrorSignal = np.sum(self.weightedErrorSignal)
 
-        # Assign output values
-        self.outputValue[WeightedErrorOutput.ERROR_SIGNAL.value] = self.weightedErrorSignal
+        # # MODIFIED 12/9/16 OLD:
+        # # Assign output values
+        # self.outputValue[WeightedErrorOutput.ERROR_SIGNAL.value] = self.weightedErrorSignal
+        # return self.outputValue
+        # MODIFIED 12/9/16 NEW:
+        return self.weightedErrorSignal
+        # MODIFIED 12/9/16 END
 
-        return self.outputValue
