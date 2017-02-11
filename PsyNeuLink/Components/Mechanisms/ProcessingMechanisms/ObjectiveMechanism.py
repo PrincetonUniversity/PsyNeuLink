ab73dca018f37006a01242d71a596d0e515f1777
jdc@princeton.edu <jdc@princeton.edu>
2017-02-11 00:49:30 -0500

    RUNNING
    • ObjectiveMechanism re-subclassed to ProcessMechanism
    
    • WeightedErrorMechanism -> ObjectiveMechanism using WeightedError Function
    
    • WeightedError:
       - implemented
    
    [FROM Minstrel]

--- PsyNeuLink/Components/Mechanisms/ProcessingMechanisms/ObjectiveMechanism.py
+++ PsyNeuLink/Components/Mechanisms/ProcessingMechanisms/ObjectiveMechanism.py
@@ -215,7 +215,8 @@ class ObjectiveMechanism(ProcessingMechanism_Base):
         # # MODIFIED 2/10/17 OLD:
         # variable_item_index = self.variable.size-1
         # MODIFIED 2/10/17 NEW:
-        variable_item_index = len(self.variable[0])-1
+        # variable_item_index = len(self.variable[0])-1
+        variable_item_index = self.variable.shape[0]-1
         # MODIFIED 2/10/17 END
 
         # Instantiate inputState
