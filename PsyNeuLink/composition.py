2f87b81a23affcdf00e0e4a9bee0416d2f70594e
Peter Johnson <petersjohnson16@gmail.com>
2017-02-15 12:00:05 -0500

    working on composition

--- PsyNeuLink/composition.py
+++ PsyNeuLink/composition.py
@@ -2,16 +2,70 @@ from collections import OrderedDict
 import itertools
 # Needs to be created still| from PsyNeuLink.scheduling import Scheduler
 
-class Binding(object):
+class Edge(object):
     ########
     # Helper class for Compositions.
-    # Contains a sender, projection, and receiver
-    # Performs no operations outside getting and setting these attributes
+    # Contains a sender, projection, and receiver.
+    # Performs no operations outside getting and setting these attributes.
     ########
     def __init__(self, sender, projection, receiver):
-        self.sender = sender
-        self.projection = projection
-        self.receiver = receiver
+        self.sender = sender # Mechanism that feeds into edge
+        self.projection = projection # Projection that constitutes edge
+        self.receiver = receiver # Mechanism that projection ends at
+
+class Vertex(object):
+    ########
+    # Helper class for Compositions.
+    # Serves as vertex for composition graph.
+    # Contains lists of incoming edges and outgoing edges.
+    ########
+    def __init__(self, mechanism, incoming = None, outgoing = None):
+        self.mechanism = mechanism
+        if incoming is not None:
+            self.incoming = incoming
+        else: self.incoming = []
+        if outgoing is not None:
+            self.outgoing = outgoing
+        else: self.outgoing = []
+
+class Graph(object):
+    ########
+    # Helper class for Compositions.
+    # Serves to organize mechanisms.
+    # Contains a list of vertices.
+    ########
+    def __init__(self):
+        self.mech_to_vertex = OrderedDict() # Translate from mechanisms to related vertex
+        self.vertices = [] # List of vertices within graph
+        self.mechanisms = [] # List of mechanisms associated with graph vertices
+
+    def add_vertex(self, mechanism):
+        if mechanism not in self.mechanisms:
+            vertex = Vertex(mechanism)
+            self.mech_to_vertex[mechanism] = vertex
+            self.vertices.append(vertex)
+            self.mechanisms.append(mechanism)
+
+    def add_edge(self, sender, projection, receiver):
+        sender_vertex = self.mech_to_vertex[sender]
+        if projection not in sender_vertex.outgoing:
+            sender_vertex.outgoing.append(Edge(sender, projection, receiver))
+        receiver_vertex = self.mech_to_vertex[receiver]
+        if projection not in receiver_vertex.incoming:
+            receiver_vertex.incoming.append(Edge(sender, projection, receiver))
+
+    def get_incoming(self, mechanism):
+        return self.mech_to_vertex[mechanism].incoming
+
+    def get_outgoing(self, mechanism):
+        return self.mech_to_vertex[mechanism].outgoing
+
+    def get_children(self, mechanism):
+        return [edge.receiver for edge in self.mech_to_vertex[mechanism].outgoing]
+
+    def get_parents(self, mechanism):
+        return [edge.sender for edge in self.mech_to_vertex[mechanism].incoming]
+
 
 
 class Composition(object):
@@ -25,12 +79,12 @@ class Composition(object):
         # - self.scheduler is a Scheduler object (see PsyNeuLink.scheduler)
         #   that manages the order of mechanisms that fire on a given trial.
         # - self.graph_analyzed is a Boolean that keeps track of whether
-        #   self.???? is up to date. This allows for analysis of the graph
+        #   self.graph is up to date. This allows for analysis of the graph
         #   only when needed for running the Composition for efficiency.
         # - self.*_mechanisms is a list of Mechanisms that are of a certain
         #   class within the composition graph.
         ########
-        self.graph = OrderedDict() # Graph of the Composition
+        self.graph = Graph() # Graph of the Composition
         # Needs to be created still| self.scheduler = Scheduler()
         self.graph_analyzed = False # Tracks if the Composition is ready to run
 
@@ -46,6 +100,7 @@ class Composition(object):
         self.explicit_input_mechanisms = [] # Need to track to know which to leave untouched
         self.all_input_mechanisms = []
         self.explicit_output_mechanisms = [] # Need to track to know which to leave untouched
+        self.all_output_mechanisms = []
         self.target_mechanisms = [] # Do not need to track explicit as they mush be explicit
 
     def add_mechanism(self, mech):
@@ -53,8 +108,8 @@ class Composition(object):
         # Adds a new Mechanism to the Composition.
         # If the mechanism has already been added, passes.
         ########
-        if mech not in self.graph.keys(): # Only add if it doesn't already exist in graph
-            self.graph[mech] = [] # Set Connection list of mech to empty
+        if mech not in self.graph.mechanisms: # Only add if it doesn't already exist in graph
+            self.graph.add_vertex(mech) # Set incoming edge list of mech to empty
             self.graph_analyzed = False # Added mech so must re-analyze graph
 
     def add_projection(self, sender, projection, receiver):
@@ -62,21 +117,12 @@ class Composition(object):
         # Adds a new Projection to the Composition.
         # If the projection has allready been added, passes.
         ########
-        # Ensures that the projection does not already exist at this place in the graph
-        if any([binding.projection is projection for binding in self.graph[receiver]]):
+        # Ensures that the projection does not already exist in the graph at this place
+        if any([edge.projection is projection for edge in self.graph.get_incoming(receiver)]):
             return
-        # Add connection to graph
-        self.graph[receiver].append(Binding(sender, projection, receiver))
+        # Add edge to graph
+        self.graph.add_edge(sender, projection, receiver)
         self.graph_analyzed = False # Added projection so must re-analyze graph
-
-
-    def set_origin(self, mech):
-        if mech not in self.origin_mechanisms:
-            self.origin_mechanisms.append(mech)
-
-    def remove_origin(self, mech):
-        if mech in self.origin_mechanisms:
-            self.origin_mechanisms.remove(mech)
 
     def analyze_graph(self):
         ########
@@ -86,7 +132,6 @@ class Composition(object):
         # - Terminal: Terminal mechanisms provide the output of the composition. By
         #   default, those which do not send any projections, but they may also be
         #   specified explicitly.
-        # - Monitored: Monitored mechanisms send projections to Target mechanisms.
         # - Init_cycle: Init_cycle mechanisms send projections that close recurrent
         #   loops in the composition (or projections that are explicitly specified as
         #   recurrent). They need an initial value so that their receiving mechanisms
@@ -102,28 +147,93 @@ class Composition(object):
         #   All Terminal mechanisms are added to this category automatically.
         # - Target: Target mechanisms receive target values for the composition to be
         #   used by learning and control. They are usually Comparator mechanisms that
-        #   compare the target value to the output of another mechanism in the composition
-        # XXX Singleton: Singleton mechanisms are mechanisms that are both origin and terminal
-        # XXX in the composition. ##NOT IMPLEMENTED
+        #   compare the target value to the output of another mechanism in the composition.
+        # - Monitored: Monitored mechanisms send projections to Target mechanisms.
         ########
 
         # Clear old information
-        self.origin_mechanisms = []
-        self.terminal_mechanisms = []
-        self.monitored_mechanisms = []
-        self.init_cycle_mechanisms = []
-        self.cycle_mechanisms = []
+        for mech in self.origin_mechanisms:
+            self.remove_origin(mech)
+        for mech in self.terminal_mechanisms:
+            self.remove_terminal(mech)
+        for mech in self.init_cycle_mechanisms:
+            self.remove_init_cycle(mech)
+        for mech in self.cycle_mechanisms:
+            self.remove_cycle(mech)
 
-        # Get list of mechanisms that send projections to identify Terminals
-        senders = [binding.sender for binding in itertools.chain.from_interable(self.graph.keys())]
-        # Identify Origin Mechanisms
-        for mech in self.graph.keys(): # Iterate through mechanisms
-            if self.graph[mech] is []: # If there are no incoming bindings
-                self.origin_mechanisms.append(mech) # Add as Origin
-        # Identify Terminal Mechanisms
-            if mech not in senders: # If the mech does not send any projections
-                self.terminal_mechanisms.append(mech) # Add as Terminal
-        
-
-
+        # Identify Origin mechanisms
+        for mech in self.graph.mechanisms:
+            if self.graph.get_incoming(mech) == []:
+                self.set_origin(mech)
+        # Identify Terminal mechanisms
+            if self.graph.get_outgoing(mech) == []:
+                self.set_terminal(mech)
+        # Identify Init_cycle and Cycle mechanisms
+        visited = [] # Keep track of all mechanisms that have been visited
+        for origin_mech in self.origin_mechanisms: # Cycle through origin mechanisms first
+            visited_current_path = [] # Track all mechanisms visited from the current origin
+            next_visit_stack = [] # Keep a stack of mechanisms to be visited next
+            next_visit_stack.append(origin_mech)
+            for mech in next_visit_stack: # While the stack isn't empty
+                visited.append(mech) # Mark the mech as visited
+                visited_current_path.append(mech) # And visited during the current path
+                children = self.graph.get_children(mech) # Get the children of that mechanism
+                for child in children:
+                    # If the child has been visited this path and is not already initialized
+                    if child in visited_current_path:
+                        if mech not in init_cycle_mechanisms:
+                            self.set_init_cycle(mech) # Set the parent as Init_cycle
+                        if child not in self.cycle_mechanisms:
+                            self.set_cycle(child) # And the child as Cycle
+                    elif child not in visited: # Else if the child has not been explored
+                        next_visit_stack.append(child) # Add it to the visit stack
+        for mech in self.graph.mechanisms:
+            if mech not in visited: # Check the rest of the mechanisms
+                visited_current_path = []
+                next_visit_stack = []
+                next_visit_stack.append(mech)
+                for remaining_mech in next_visit_stack:
+                    visited.append(remaining_mech)
+                    visited_current_path.append(remaining_mech)
+                    children = self.graph.get_children(remaining_mech)
+                    for child in children:
+                        if child in visited_current_path:
+                            if remaining_mech not in self.init_cycle_mechanisms:
+                                self.set_init_cycle(remaining_mech)
+                            if child not in self.cycle_mechanisms:
+                                self.set_cycle(child)
+                        elif child not in visited:
+                            next_visit_stack.append(child)
         return
+
+    def set_origin(self, mech):
+        if mech not in self.origin_mechanisms: # If mechanism isn't in Origin list already
+            self.origin_mechanisms.append(mech) # Add to Origin list
+
+    def remove_origin(self, mech):
+        if mech in self.origin_mechanisms: # If mechanism is in Origin list
+            self.origin_mechanisms.remove(mech) # Remove from Origin list
+
+    def set_terminal(self, mech):
+        if mech not in self.terminal_mechanisms: # If mechanism isn't in Terminal list already
+            self.terminal_mechanisms.append(mech) # Add to Terminal list
+
+    def remove_terminal(self, mech):
+        if mech in self.terminal_mechanisms: # If mechanism is in Terminal list
+            self.terminal_mechanisms.remove(mech) # Remove from Terminal list
+
+    def set_init_cycle(self, mech):
+        if mech not in self.init_cycle_mechanisms: # If mechanism isn't in Init_cycle list already
+            self.init_cycle_mechanisms.append(mech) # Add to Init_cycle list
+
+    def remove_init_cycle(self, mech):
+        if mech in self.init_cycle_mechanisms: # If mechanism is in Init_cycle list
+            self.init_cycle_mechanisms.remove(mech) # Remove from Init_cycle list
+
+    def set_cycle(self, mech):
+        if mech not in self.init_cycle_mechanisms: # If mechanism isn't in Cycle list already
+            self.cycle_mechanisms.append(mech) # Add to Init_cycle list
+
+    def remove_cycle(self, mech):
+        if mech in self.init_cycle_mechanisms: # If mechanism is in Cycle list
+            self.cycle_mechanisms.remove(mech) # Remove from Cycle list
--- PsyNeuLink/composition_tests.py
+++ PsyNeuLink/composition_tests.py
@@ -1,85 +1,190 @@
-import composition as composition
+import composition
 from PsyNeuLink.Components.Mechanisms.Mechanism import mechanism
 from PsyNeuLink.Components.Projections.MappingProjection import MappingProjection
 import time
 
+test_constructor = False
+test_add_mechanism = False
+test_add_projection = False
+test_analyze_graph = True
+
 # Unit tests for each function of the Composition class #######################
 # Unit tests for Composition.Composition()
-print("Constructor Tests:")
-print("Test 1: No args")
-comp = composition.Composition()
-print("passed")
-print("Test 2: Second call no args")
-comp_2 = composition.Composition()
-print("passed")
-print("Test 3: Timing no args")
-count = 10000
-start = time.time()
-for i in range(count):
+if test_constructor:
+    print("Constructor Tests:")
+
+    print("Test 1: No args")
     comp = composition.Composition()
-end = time.time()
-print("passed in " + str(end-start) + " seconds for " + str(count) + " calls")
+    assert isinstance(comp, composition.Composition)
+    print("passed")
+
+    print("Test 2: Second call no args")
+    comp_2 = composition.Composition()
+    assert isinstance(comp, composition.Composition)
+    print("passed")
+
+    print("Test 3: Timing no args")
+    count = 10000
+    start = time.time()
+    for i in range(count):
+        comp = composition.Composition()
+    end = time.time()
+    print("passed in " + str(end-start) + " seconds for " + str(count) + " calls")
 
 # Unit tests for Composition.add_mechanism
-print("\n" + "add_mechanism tests:")
-comp = composition.Composition()
-print("Test 1: Basic Test")
-comp.add_mechanism(mechanism())
-print("passed")
-print("Test 2: Second call")
-comp.add_mechanism(mechanism())
-print("passed")
-print("Test 3: Adding same mechanism twice")
-mech = mechanism()
-comp.add_mechanism(mech)
-comp.add_mechanism(mech)
-print("passed")
-print("Test 4: Timing and Stress Test")
-count = 100
-mech_list = []
-for i in range(count):
-    mech_list.append(mechanism())
-start = time.time()
-for i in range(count):
-    comp.add_mechanism(mech_list[i])
-end = time.time()
-print("passed in " + str(end-start) + " seconds for " + str(count) + " calls")
+if test_add_mechanism:
+    print("\n" + "add_mechanism tests:")
+    comp = composition.Composition()
+
+    print("Test 1: Basic Test")
+    comp.add_mechanism(mechanism())
+    print("passed")
+
+    print("Test 2: Second call")
+    comp.add_mechanism(mechanism())
+    print("passed")
+
+    print("Test 3: Adding same mechanism twice")
+    mech = mechanism()
+    comp.add_mechanism(mech)
+    comp.add_mechanism(mech)
+    print("passed")
+
+    print("Test 4: Timing and Stress Test")
+    count = 100
+    mech_list = []
+    for i in range(count):
+        mech_list.append(mechanism())
+    start = time.time()
+    for i in range(count):
+        comp.add_mechanism(mech_list[i])
+    end = time.time()
+    print("passed in " + str(end-start) + " seconds for " + str(count) + " calls")
 
 # Unit tests for Composition.add_projection
-print("\n" + "add_projection tests:")
-print("Test 1: Basic Test")
-comp = composition.Composition()
-A = mechanism()
-B = mechanism()
-comp.add_mechanism(A)
-comp.add_mechanism(B)
-comp.add_projection(A, MappingProjection(), B)
-print("passed")
-print("Test 2: Second call")
-comp.add_projection(A, MappingProjection(), B)
-print("passed")
-print("Test 3: Adding same projection twice")
-comp = composition.Composition()
-A = mechanism()
-B = mechanism()
-comp.add_mechanism(A)
-comp.add_mechanism(B)
-proj = MappingProjection()
-comp.add_projection(A, proj, B)
-comp.add_projection(A, proj, B)
-print("passed")
-print("Test 4: Timing and Stress Test")
-comp = composition.Composition()
-A = mechanism()
-B = mechanism()
-comp.add_mechanism(A)
-comp.add_mechanism(B)
-count = 10000
-proj_list = []
-for i in range(count):
-    proj_list.append(MappingProjection())
-start = time.time()
-for i in range(count):
-    comp.add_projection(A, proj_list[i], B)
-end = time.time()
-print("passed in " + str(end-start) + " seconds for " + str(count) + " calls")
+if test_add_projection:
+    print("\n" + "add_projection tests:")
+
+    print("Test 1: Basic Test")
+    comp = composition.Composition()
+    A = mechanism()
+    B = mechanism()
+    comp.add_mechanism(A)
+    comp.add_mechanism(B)
+    comp.add_projection(A, MappingProjection(), B)
+    print("passed")
+
+    print("Test 2: Second call")
+    comp.add_projection(A, MappingProjection(), B)
+    print("passed")
+
+    print("Test 3: Adding same projection twice")
+    comp = composition.Composition()
+    A = mechanism()
+    B = mechanism()
+    comp.add_mechanism(A)
+    comp.add_mechanism(B)
+    proj = MappingProjection()
+    comp.add_projection(A, proj, B)
+    comp.add_projection(A, proj, B)
+    print("passed")
+
+    print("Test 4: Timing and Stress Test")
+    comp = composition.Composition()
+    A = mechanism()
+    B = mechanism()
+    comp.add_mechanism(A)
+    comp.add_mechanism(B)
+    count = 1000
+    proj_list = []
+    for i in range(count):
+        proj_list.append(MappingProjection())
+    start = time.time()
+    for i in range(count):
+        comp.add_projection(A, proj_list[i], B)
+    end = time.time()
+    print("passed in " + str(end-start) + " seconds for " + str(count) + " calls")
+
+# Unit tests for Composition.analyze_graph
+if test_analyze_graph:
+    print("\n" + "analyze_graph tests:")
+
+    print("Test 1: Empty Call")
+    comp = composition.Composition()
+    comp.analyze_graph()
+    print("passed")
+
+    print("Test 2: Singleton")
+    comp = composition.Composition()
+    A = mechanism()
+    comp.add_mechanism(A)
+    comp.analyze_graph()
+    assert A in comp.graph.mechanisms
+    assert A in comp.origin_mechanisms
+    assert A in comp.terminal_mechanisms
+    print("passed")
+
+    print("Test 3: Two independent")
+    comp = composition.Composition()
+    A = mechanism()
+    B = mechanism()
+    comp.add_mechanism(A)
+    comp.add_mechanism(B)
+    comp.analyze_graph()
+    assert A in comp.origin_mechanisms
+    assert B in comp.origin_mechanisms
+    assert A in comp.terminal_mechanisms
+    assert B in comp.terminal_mechanisms
+    print("passed")
+
+    print("Test 4: Two in a row")
+    comp = composition.Composition()
+    A = mechanism()
+    B = mechanism()
+    comp.add_mechanism(A)
+    comp.add_mechanism(B)
+    comp.add_projection(A, MappingProjection(), B)
+    comp.analyze_graph()
+    assert A in comp.origin_mechanisms
+    assert B not in comp.origin_mechanisms
+    assert A not in comp.terminal_mechanisms
+    assert B in comp.terminal_mechanisms
+    print("passed")
+
+    print("Test 5: Two recursive (A)<->(B)")
+    comp = composition.Composition()
+    A = mechanism()
+    B = mechanism()
+    comp.add_mechanism(A)
+    comp.add_mechanism(B)
+    comp.add_projection(A, MappingProjection(), B)
+    comp.add_projection(B, MappingProjection(), A)
+    comp.analyze_graph()
+    assert A not in comp.origin_mechanisms
+    assert B not in comp.origin_mechanisms
+    assert A not in comp.terminal_mechanisms
+    assert B not in comp.terminal_mechanisms
+    assert A in comp.cycle_mechanisms
+    assert B in comp.init_cycle_mechanisms
+    print("passed")
+
+    print("Test 6: Two origins pointing to recursive pair (A)->(B)<->(C)<-(D)")
+    comp = composition.Composition()
+    A = mechanism()
+    B = mechanism()
+    C = mechanism()
+    D = mechanism()
+    comp.add_mechanism(A)
+    comp.add_mechanism(B)
+    comp.add_mechanism(C)
+    comp.add_mechanism(D)
+    comp.add_projection(A, MappingProjection(), B)
+    comp.add_projection(C, MappingProjection(), B)
+    comp.add_projection(B, MappingProjection(), C)
+    comp.add_projection(D, MappingProjection(), C)
+    comp.analyze_graph()
+    assert A in comp.origin_mechanisms
+    assert D in comp.origin_mechanisms
+    assert B in comp.cycle_mechanisms
+    assert C in comp.init_cycle_mechanisms
+    print("passed")
