693e165c8df6611d1a2572776c46b2f6722aba8e
jdcpni <pniintel55>
2017-02-25 13:34:20 -0500

    [RUNNING]
    • Run, System and Process:
       - implemented learning_rate that applies to all learningProjections in the process or system;
          assigned at run time, and applies to every run (sets LearningProjection.function_object.learning_rate).
       - implemented execution_token:
           assigned by Run(), System.execute() and Process.execute()  to all mechanisms licensed to execute
           as part of process or system currently being run
    
    • System:
      - execute(): now properly adds "LEARNING" to context in calls for learning
      - _execute_learning(): now calls MappingProjection.parameterStates[MATRIX].update() rather than LearningProjection.execute
          this now produces identical results to learning using processes, and validated for backpropagation against external code
    
    • ParameterState:
      - update(): now assigns function_params to function's params (except for matrix:  MappingProjection does that on its own)
          [TBI: implement as a @property, assigned at time that param is created on the owner]
    
    • ObjectiveMechanism:
       - Replaced IDENTITY_MATRIX with AUTO_ASSIGN_MATRIX as default for input projections
       - Refactored to:
             use variable as monitored_value arg (which replaces monitor arg)
             take inputState (as object, str for name, or specification dictionary) as monitored_value spec
             call _instantiate_monitoring_projections() for OutputState and Mechanism monitored_value specs
             include learning_role attribute (to specify TARGETs)
    
    • LearningProjection:
       - refactored to used ObjectiveMechanism as comparator
    
    [FROM Moon Delight]

--- PsyNeuLink/Components/Mechanisms/AdaptiveMechanisms/LearningProjection_OLD.py
+++ PsyNeuLink/Components/Mechanisms/AdaptiveMechanisms/LearningProjection_OLD.py
@@ -6,7 +6,8 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 
-# *******************************************  LearningProjection **********************************************************
+#                                              @@@@@@@ OLD @@@@@@
+# *******************************************  LearningProjection ******************************************************
 
 """
 .. _LearningProjection_Overview:
