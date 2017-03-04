6db884d92619792995160cb920c84e1d4d41158b
jdc@princeton.edu <jdc@princeton.edu>
2017-03-04 08:55:39 -0500

    Merge branch 'devel' of https://github.com/jdcpni/PsyNeuLink into devel
    # Conflicts:
    #	.idea/runConfigurations/_Scratch_Pad.xml
    #	Scripts/Scratch Pad.py

--- PsyNeuLink/Components/Projections/LearningProjection.py
+++ PsyNeuLink/Components/Projections/LearningProjection.py
@@ -477,8 +477,9 @@ class LearningProjection(Projection_Base):
         if not INITIALIZING in context and self.reportOutputPref:
             print("\n{} weight change matrix: \n{}\n".format(self.name, self.weight_change_matrix))
 
-        # # TEST PRINT
-        # print("\nr### WEIGHT CHANGES FOR {} TRIAL {}:\n{}".format(self.name, CentralClock.trial, self.value))
+        # TEST PRINT
+        print("\n@@@ WEIGHT CHANGES FOR {} TRIAL {}:\n{}".format(self.name, CentralClock.trial, self.value))
+        # print("\n@@@ WEIGHT CHANGES CALCULATED FOR {} TRIAL {}".format(self.name, CentralClock.trial))
 
         return self.value
 
