5fdc8a1d53dd22a04f36545ccbc238347ca04f61
jdc@princeton.edu <jdc@princeton.edu>
2017-03-23 06:03:48 -0400

    [RUNNING]
    • System and Process:
       - learning_rate bugs fixed
    
    • LearningMechanism:
       - learning_rate parameter implemented (getter and setter for function's learning_rate param)
    
    [FROM Minstrel]

--- Scripts/Multilayer Learning Test Script.py
+++ Scripts/Multilayer Learning Test Script.py
@@ -92,7 +92,7 @@ z = process(default_input_value=[0, 0],
                            Output_Layer],
             clamp_input=SOFT_CLAMP,
             learning=LEARNING,
-            learning_rate=1.0,
+            # learning_rate=1.0,
             target=[0,0,1],
             prefs={VERBOSE_PREF: False,
                    REPORT_OPUTPUT_PREF: True})
@@ -155,7 +155,7 @@ elif COMPOSITION is SYSTEM:
     # SYSTEM VERSION:
     x = system(processes=[z],
                targets=[0, 0, 1],
-               learning_rate=0.0)
+               learning_rate=1.0)
 
     x.reportOutputPref = True
     composition = x
