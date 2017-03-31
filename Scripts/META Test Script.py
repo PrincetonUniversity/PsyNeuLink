8ce014b5b8ee037abdccb746b63c9c28f5e691dc
jdc <jdc@princeton.edu>
2017-03-31 09:55:47 -0400

    [RUNNING]
    [FROM Minstrel]

--- Scripts/META Test Script.py
+++ Scripts/META Test Script.py
@@ -1,5 +1,35 @@
+# import numpy as np
+# import random
+#
+# # Insures that runs are deterministic for use in Jenkins testing
+# random.seed(0)
+# np.random.seed(0)
+#
+# scripts =  [
+#             'EVC System Laming Validation Test Script.py',
+#             'Multilayer Learning Test Script.py',
+#             'Reinforcement Learning Test Script.py',
+#             'DDM Test Script.py',
+#             'Stroop Model Test Script.py',
+#             'Stroop Model Learning Test Script.py',
+#             'Mixed NN & DDM script.py',
+#             'System Graph and Input Test Script.py',
+#             'Documentation Examples Script.py'
+#             ]
+#
+# # foo_bar = __import__(script)
+# for script in scripts:
+#     file = open(script)
+#     print("\nRUNNING {}\n".format(script))
+#     exec(file.read())
+#     file.close()
+#     print ("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
+#
+
 import numpy as np
 import random
+import subprocess
+import sys
 
 # Insures that runs are deterministic for use in Jenkins testing
 random.seed(0)
@@ -21,7 +51,9 @@ scripts =  [
 for script in scripts:
     file = open(script)
     print("\nRUNNING {}\n".format(script))
-    exec(file.read())
+    # exec(file.read())
+    for line in subprocess.check_output([sys.executable, script]).splitlines():
+        print(str(line, 'utf-8'))
     file.close()
     print ("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
 
