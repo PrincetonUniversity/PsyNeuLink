678d04cc884e41c194e47a00971c984f0517228f
Peter Johnson <petersjohnson16@gmail.com>
2017-04-01 14:15:37 -0400

    Fixed typecheck version number

--- setup.py
+++ setup.py
@@ -63,5 +63,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-install_requires=['toposort==1.4', 'numpy', 'typecheck-decorator'], #mpi4py only if MPI_IMPLEMENTATION becomes True
+install_requires=['toposort==1.4', 'numpy', 'typecheck-decorator==1.2'], #mpi4py only if MPI_IMPLEMENTATION becomes True
 )
