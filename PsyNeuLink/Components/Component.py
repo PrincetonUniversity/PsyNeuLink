d53f25911a4e59a79c899605cacddcfe9d2e9f2b
jdc@princeton.edu <jdc@princeton.edu>
2017-04-13 13:18:59 -0400

    • Component   - make_prop():  added call to assign_params() if paramValidation

--- PsyNeuLink/Components/Component.py
+++ PsyNeuLink/Components/Component.py
@@ -2247,7 +2247,7 @@ def make_property(name, default_value):
     def setter(self, val):
 
         if self.paramValidationPref:
-            self.assign_params(request_set={backing_field:val})
+            self.assign_params(request_set={backing_field[1:]:val})
         else:
             setattr(self, backing_field, val)
 
