bfe18971c58cb300bf0d9d305edd1c71cdcd0ed6
jdc@princeton.edu <jdc@princeton.edu>
2017-04-13 21:11:38 -0400

    Merge branch 'devel' into make_prop->assign_params
    # Conflicts:
    #	PsyNeuLink/Components/Component.py

--- TODO List.py
+++ TODO List.py
@@ -56,6 +56,7 @@
 #   got rid of special cases for Objective function altogether (since comparator is just special case of derivative = 0)
 #   added attribute to Projections:  has_learning_projection
 
+# FIX: Stroop Model Test Script: Process -> System
 # DOCUMENTATION: ?? MOVE `parameter specification dictionary <Mechanism_Creation>` TO Component??
 # FIX: Component: IMPLEMENT:  PROGRAMMATICALLY ADD GETTER AND SETTER PROPERTY FOR EACH FUNCTION_PARAM HERE
 #                 SEE learning_rate IN LearningMechanism FOR EXAMPLE
