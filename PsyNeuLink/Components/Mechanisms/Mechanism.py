240e803c3623d487cb57d565325a1489cb964a78
jdc@princeton.edu <jdc@princeton.edu>
2017-04-21 09:18:52 -0400

    • System   - fixed bug in show_graph_with_control
    • Mechanism
      - mitigated bug in reportMechanism
    
    • ControlMechanism, EVCMechanism
      - disable control and report if control but no controlProjections specified

--- PsyNeuLink/Components/Mechanisms/Mechanism.py
+++ PsyNeuLink/Components/Mechanisms/Mechanism.py
@@ -1722,10 +1722,10 @@ class Mechanism_Base(Mechanism):
         #     input_string = [float("{:0.3}".format(float(i))) for i in input_val].__str__().strip("[]")
         # else:
             input_string = input_val
-        # MODIFIED 2/20/17 NEW:
+        # # MODIFIED 2/20/17 NEW:
         # input_string = [float("{:0.3}".format(float(i))) for i in input_val].__str__().strip("[]")
         # MODIFIED 4/21/17 NEWER: [NEW CRASHES IF input_val IS AN ARRAY]
-        if isinstance(input_val, np.ndarray) and input_val.ndim >1:
+        if isinstance(input_val, np.ndarray) and input_val.ndim > 1 and input_val.shape[1] > 1:
             input_string = input_val
         else:
             input_string = [float("{:0.3}".format(float(i))) for i in input_val].__str__().strip("[]")
