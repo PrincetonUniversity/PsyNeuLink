ce99aae0b9ea9fcdab50443aec3ae10afe3618dd
jdcpni <pniintel55>
2017-04-25 22:10:41 -0400

    • EVCMechanism   - refactored PredictonMechanisms
    [FROM Moon Delight]

--- Scripts/TEST SCRIPTS/EVC System Laming Validation Test Script.py
+++ Scripts/TEST SCRIPTS/EVC System Laming Validation Test Script.py
@@ -98,19 +98,36 @@ def show_trial_header():
 
 def show_results():
     import re
-    results = sorted(zip(mySystem.terminalMechanisms.outputStateNames, mySystem.terminalMechanisms.outputStateValues))
     print('\nRESULTS (time step {}): '.format(CentralClock.time_step))
-    print ('\tDrift rate control signal (from EVC): {}'.
+    print('\tCONTROL SIGNAL VALUES:')
+    print ('\t\tDrift rate control signal (from EVC): {}'.
            # format(re.sub('[\[,\],\n]','',str(float(Decision.parameterStates[DRIFT_RATE].value)))))
            format(re.sub('[\[,\],\n]','',str("{:0.3}".format(float(Decision.parameterStates[DRIFT_RATE].value))))))
-    print ('\tThreshold control signal (from EVC): {}'.
+    print ('\t\tThreshold control signal (from EVC): {}'.
            format(re.sub('[\[,\],\n]','',str(float(Decision.parameterStates[THRESHOLD].value))),
                   mySystem.controller.outputStates['threshold_ControlSignal'].value,
                   Decision.parameterStates[THRESHOLD].receivesFromProjections[0].value
                   ))
-    for result in results:
-        print("\t{}: {}".format(result[0],
+    # results = sorted(zip(mySystem.terminalMechanisms.outputStateNames,
+    #                      mySystem.terminalMechanisms.outputStateValues))
+    # for result in results:
+    #     print("\t{}: {}".format(result[0],
+    #                             re.sub('[\[,\],\n]','',str("{:0.3}".format(float(result[1]))))))
+    results_for_decision = \
+        [result for result in sorted(zip(mySystem.terminalMechanisms.outputStateNames,
+                                           mySystem.terminalMechanisms.outputStateValues)) if 'DDM' in result[0]]
+    print("\tDecision:")
+    for result in results_for_decision:
+        print("\t\t{}: {}".format(result[0],
                                 re.sub('[\[,\],\n]','',str("{:0.3}".format(float(result[1]))))))
+    results_for_reward = \
+        [result for result in sorted(zip(mySystem.terminalMechanisms.outputStateNames,
+                                           mySystem.terminalMechanisms.outputStateValues)) if 'transfer' in result[0]]
+    print("\tReward:")
+    for result in results_for_reward:
+        print("\t\t{}: {}".format(result[0],
+                                re.sub('[\[,\],\n]','',str("{:0.3}".format(float(result[1]))))))
+
 
 # Run system:
 
