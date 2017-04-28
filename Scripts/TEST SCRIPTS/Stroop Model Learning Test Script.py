9b66b7f8b1e666a4087234ed46ebb729692a7550
jdc@princeton.edu <jdc@princeton.edu>
2017-04-28 10:23:12 -0400

    • System   - fixed bug in which error_matrix was not being assigned to     learning function for reassigned LearningMechanism
    [FROM Quill]

--- Scripts/TEST SCRIPTS/Stroop Model Learning Test Script.py
+++ Scripts/TEST SCRIPTS/Stroop Model Learning Test Script.py
@@ -109,7 +109,7 @@ mySystem.run(num_executions=2,
 #
 # PsyNeuLink response & weights after 1st trial:
 #
-# Response:
+# Response (NOTE: THIS IS FROM LAST TRIAL):
 #  [ 0.50899214  0.54318254]
 # Hidden-Output:
 # [[ 0.01462766  1.01351195]
