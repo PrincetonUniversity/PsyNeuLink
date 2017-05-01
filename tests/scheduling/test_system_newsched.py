cf1842aaadf59ba617862cfadfd72a1f070f994c
kmantel <kmantel@princeton.edu>
2017-05-01 18:00:21 -0400

    update TestInit to reflect proper scheduling behavior with learning

--- tests/scheduling/test_system_newsched.py
+++ tests/scheduling/test_system_newsched.py
@@ -21,20 +21,30 @@ class TestInit:
         Word_Input = TransferMechanism(name='Word Input', function=Linear(slope = 0.2995))
 
         # Processing Mechanisms (Control)
-        Color_Hidden = TransferMechanism(name='Colors Hidden',
-                                       function=Logistic(gain=(1.0, ControlProjection)))
-        Word_Hidden = TransferMechanism(name='Words Hidden',
-                                       function=Logistic(gain=(1.0, ControlProjection)))
-        Output = TransferMechanism(name='Output',
-                                       function=Logistic(gain=(1.0, ControlProjection)))
+        Color_Hidden = TransferMechanism(
+            name='Colors Hidden',
+            function=Logistic(gain=(1.0, ControlProjection)),
+        )
+        Word_Hidden = TransferMechanism(
+            name='Words Hidden',
+            function=Logistic(gain=(1.0, ControlProjection)),
+        )
+        Output = TransferMechanism(
+            name='Output',
+            function=Logistic(gain=(1.0, ControlProjection)),
+        )
 
         # Decision Mechanisms
-        Decision = DDM(function=BogaczEtAl(drift_rate=(1.0),
-                                           threshold=(0.1654),
-                                           noise=(0.5),
-                                           starting_point=(0),
-                                           t0=0.25),
-                       name='Decision')
+        Decision = DDM(
+            function=BogaczEtAl(
+                drift_rate=(1.0),
+                threshold=(0.1654),
+                noise=(0.5),
+                starting_point=(0),
+                t0=0.25,
+            ),
+            name='Decision',
+        )
         # Outcome Mechanisms:
         Reward = TransferMechanism(name='Reward')
 
@@ -42,34 +52,39 @@ class TestInit:
         ColorNamingProcess = process(
             default_input_value = [0],
             pathway = [Color_Input, Color_Hidden, Output, Decision],
-            name = 'Color Naming Process')
+            name = 'Color Naming Process',
+        )
 
         WordReadingProcess = process(
             default_input_value = [0],
             pathway = [Word_Input, Word_Hidden, Output, Decision],
-            name = 'Word Reading Process')
+            name = 'Word Reading Process',
+        )
 
         RewardProcess = process(
             default_input_value = [0],
-            pathway = [(Reward, 1)],
-            name = 'RewardProcess')
+            pathway = [Reward],
+            name = 'RewardProcess',
+        )
 
         # System:
-        mySystem = system(processes=[ColorNamingProcess, WordReadingProcess, RewardProcess],
-                          controller=EVCMechanism,
-                          enable_controller=True,
-                          monitor_for_control=[Reward, (DDM_PROBABILITY_UPPER_THRESHOLD, 1, -1)],
-                          name='EVC Gratton System')
+        mySystem = system(
+            processes=[ColorNamingProcess, WordReadingProcess, RewardProcess],
+            controller=EVCMechanism,
+            enable_controller=True,
+            monitor_for_control=[Reward, (DDM_PROBABILITY_UPPER_THRESHOLD, 1, -1)],
+            name='EVC Gratton System',
+        )
 
         sched = Scheduler(system=mySystem)
 
-        integrator_ColorInputPrediction = mySystem.mechanisms[7]
-        integrator_RewardPrediction = mySystem.mechanisms[8]
-        integrator_WordInputPrediction = mySystem.mechanisms[9]
+        integrator_ColorInputPrediction = mySystem.executionList[7][0]
+        integrator_RewardPrediction = mySystem.executionList[8][0]
+        integrator_WordInputPrediction = mySystem.executionList[9][0]
 
         expected_consideration_queue = [
-            {Color_Input, Word_Input, Reward},
-            {Color_Hidden, Word_Hidden, integrator_ColorInputPrediction, integrator_WordInputPrediction, integrator_RewardPrediction},
+            {Color_Input, Word_Input, Reward, integrator_ColorInputPrediction, integrator_WordInputPrediction, integrator_RewardPrediction},
+            {Color_Hidden, Word_Hidden},
             {Output},
             {Decision}
         ]
