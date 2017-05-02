32da1649565134e8dbf0fd70aebdf0cf27c4aeea
kmantel <kmantel@princeton.edu>
2017-05-02 17:11:19 -0400

    add tests from documentation examples to pytest; add pytest-randomly module to handle resetting seeds for each test

--- tests/system/test_system.py
+++ tests/system/test_system.py
@@ -1,5 +1,5 @@
-import logging
-import numpy
+import numpy as np
+import random
 
 from PsyNeuLink.Components.System import system
 from PsyNeuLink.Components.Process import process
@@ -7,6 +7,7 @@ from PsyNeuLink.Components.Mechanisms.ProcessingMechanisms.TransferMechanism imp
 from PsyNeuLink.Components.Mechanisms.ProcessingMechanisms.DDM import *
 from PsyNeuLink.Components.Mechanisms.AdaptiveMechanisms.ControlMechanisms.EVCMechanism import EVCMechanism
 from PsyNeuLink.Components.Projections.ControlProjection import ControlProjection
+from PsyNeuLink.Components.Projections.MappingProjection import MappingProjection
 from PsyNeuLink.Components.Functions.Function import Linear, Logistic, BogaczEtAl, Integrator
 from PsyNeuLink.scheduling.Scheduler import Scheduler
 from PsyNeuLink.scheduling.condition import *
@@ -103,3 +104,64 @@ def test_danglingControlledMech():
     )
 
     # no assert, should only complete without error
+
+class TestDocumentationExamples:
+    def test_mechs_in_pathway(seed0):
+        mechanism_1 = TransferMechanism()
+        mechanism_2 = DDM()
+        some_params = {PARAMETER_STATE_PARAMS:{THRESHOLD:2,NOISE:0.1}}
+        my_process = process(pathway=[mechanism_1, TransferMechanism, (mechanism_2, some_params, 0)])
+        result = my_process.execute()
+
+        assert(result == np.array([2]))
+
+    def test_default_projection(seed0):
+        mechanism_1 = TransferMechanism()
+        mechanism_2 = TransferMechanism()
+        mechanism_3 = DDM()
+        my_process = process(pathway=[mechanism_1, mechanism_2, mechanism_3])
+        result = my_process.execute()
+
+        assert(result == np.array([1.]))
+
+    def test_inline_projection_using_existing_projection(seed0):
+        mechanism_1 = TransferMechanism()
+        mechanism_2 = TransferMechanism()
+        mechanism_3 = DDM()
+        projection_A = MappingProjection()
+        my_process = process(pathway=[mechanism_1, projection_A, mechanism_2, mechanism_3])
+        result = my_process.execute()
+
+        assert(result == np.array([1.]))
+
+    def test_inline_projection_using_keyword(seed0):
+        mechanism_1 = TransferMechanism()
+        mechanism_2 = TransferMechanism()
+        mechanism_3 = DDM()
+        my_process = process(pathway=[mechanism_1, RANDOM_CONNECTIVITY_MATRIX, mechanism_2, mechanism_3])
+        result = my_process.execute()
+
+        assert(result == np.array([1.]))
+
+    def test_standalone_projection(seed0):
+        mechanism_1 = TransferMechanism()
+        mechanism_2 = TransferMechanism()
+        mechanism_3 = DDM()
+        projection_A = MappingProjection(sender=mechanism_1, receiver=mechanism_2)
+        my_process = process(pathway=[mechanism_1, mechanism_2, mechanism_3])
+        result = my_process.execute()
+
+        assert(result == np.array([1.]))
+
+    def test_process_learning(seed0):
+        mechanism_1 = TransferMechanism(function=Logistic)
+        mechanism_2 = TransferMechanism(function=Logistic)
+        mechanism_3 = TransferMechanism(function=Logistic)
+        my_process = process(
+            pathway=[mechanism_1, mechanism_2, mechanism_3],
+            learning=LEARNING_PROJECTION,
+            target=[0],
+        )
+        result = my_process.execute()
+
+        np.testing.assert_allclose(result, np.array([0.65077768]))
