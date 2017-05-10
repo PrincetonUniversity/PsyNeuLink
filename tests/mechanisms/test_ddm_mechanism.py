a2c896da08109363d45fd76b689b64c52deab542
KristenManning <kristenmanning16@gmail.com>
2017-05-10 16:28:14 -0400

    adding tests to ddm pytest based on what the output would be if variable were working properly (replacing with inputState.value temporarily)

--- tests/mechanisms/test_ddm_mechanism.py
+++ tests/mechanisms/test_ddm_mechanism.py
@@ -11,11 +11,10 @@ import pytest
 
 # ------------------------------------------------------------------------------------------------
 # TEST 1
-# variable = List
 # noise = Single float
 
-def test_DDM():
-    stim = 100
+def test_DDM_zero_noise():
+    stim = 10
     T = DDM(
             name='T',
             function = Integrator(
@@ -27,4 +26,66 @@ def test_DDM():
             time_scale=TimeScale.TIME_STEP
            )
     val = float(T.execute(stim)[0])
-    assert val == -0.10702570836876696
+    assert val == 10
+
+# ------------------------------------------------------------------------------------------------
+# TEST 2
+# noise = Single float
+
+def test_DDM_noise_0_5():
+    stim = 10
+    T = DDM(
+            name='T',
+            function = Integrator(
+                                    integration_type= DIFFUSION,
+                                    noise = 0.5,
+                                    rate = 1.0,
+                                    time_step_size=1.0
+                                  ),
+            time_scale=TimeScale.TIME_STEP
+           )
+    val = float(T.execute(stim)[0])
+    assert val == 9.892974291631234
+
+# ------------------------------------------------------------------------------------------------
+# TEST 3
+# noise = Single float
+
+def test_DDM_noise_2_0():
+    stim = 10
+    T = DDM(
+        name='T',
+        function=Integrator(
+            integration_type=DIFFUSION,
+            noise=2.0,
+            rate=1.0,
+            time_step_size=1.0
+        ),
+        time_scale=TimeScale.TIME_STEP
+    )
+    val = float(T.execute(stim)[0])
+    assert val == 9.785948583262465
+# ------------------------------------------------------------------------------------------------
+
+# INVALID NOISE:
+
+# ------------------------------------------------------------------------------------------------
+# TEST 1
+# noise = Single int
+
+def test_DDM_noise_2():
+    with pytest.raises(FunctionError) as error_text:
+        stim = 10
+        T = DDM(
+            name='T',
+            function=Integrator(
+                integration_type=DIFFUSION,
+                noise=2,
+                rate=1.0,
+                time_step_size=1.0
+            ),
+            time_scale=TimeScale.TIME_STEP
+        )
+        val = float(T.execute(stim)[0])
+    assert "noise parameter" in str(error_text.value) \
+           and "must be a float, function, array or list of floats, or array or list of functions" in str(error_text.value)
