0ef9daa6d92c7f8ea9eb2250a81c4061e35cd665
kmantel <kmantel@princeton.edu>
2017-05-17 19:10:16 -0400

    add pytest helper function for making solution to "ufunc isfinite" error for assert_allclose prettier

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,26 @@ def pytest_runtest_call(item):
     seed = 0
     random.seed(seed)
     np.random.seed(seed)
+
+@pytest.helpers.register
+def expand_np_ndarray(arr):
+    try:
+        iter(arr)
+    except TypeError:
+        return arr.tolist()
+
+    results_list = []
+    for elem in arr:
+        try:
+            iter(elem)
+        except TypeError:
+            elem = [elem]
+
+        for nested_elem in elem:
+            nested_elem = nested_elem.tolist()
+            try:
+                iter(nested_elem)
+            except TypeError:
+                nested_elem = [nested_elem]
+            results_list.extend(nested_elem)
+    return results_list
