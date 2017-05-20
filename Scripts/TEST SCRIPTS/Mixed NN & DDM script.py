dc6f41dc11950f5a79371e57237fc80c7374ba13
jdc@princeton.edu <jdc@princeton.edu>
2017-05-20 08:54:55 -0400

    • Project   receivesFromProjections -> afferents   sendsToProjections -> efferents   eliminated ParamValueProjection
    • Mechanism, OutputState:
      - updated docstring to reflect implementation of standard_output_states
    
    • ComparatorMechanism
      - implemented (convenience subclass of ObjectiveMechanism)
    
    • State
      - refactoring of _instantiate_state to use _parse_state_spec

--- Scripts/TEST SCRIPTS/Mixed NN & DDM script.py
+++ Scripts/TEST SCRIPTS/Mixed NN & DDM script.py
@@ -4,9 +4,9 @@ from PsyNeuLink.Components.Process import process
 from PsyNeuLink.Globals.Keywords import *
 from PsyNeuLink.Globals.Run import run
 
-# import random
-# random.seed(0)
-# np.random.seed(0)
+import random
+random.seed(0)
+np.random.seed(0)
 
 
 myInputLayer = TransferMechanism(name='Input Layer',
