6b62f013a7827ba6302ee84f369c78893aa92ae3
jdc@princeton.edu <jdc@princeton.edu>
2017-05-22 10:22:46 -0400

    • Project   receivesFromProjections -> afferents   sendsToProjections -> efferents   eliminated ParamValueProjection
    • Mechanism, OutputState:
      - updated docstring to reflect implementation of standard_output_states
    
    • ObjectiveMechanism
      - eliminated parse_monitored_values() - now uses parse_state_spec
      - refactored _instantiate_monitoring_projections()
      - now calls super()._instantiate_input_states()
    
    • ComparatorMechanism
      - implemented (convenience subclass of ObjectiveMechanism)
    
    • State
      - refactoring of _instantiate_state to use _parse_state_spec
      - implemented ability to specify matrix for MappingProjection to
           inputState in 2-item tuple (comparable to CONTROL and LEARNING)
    
    • Mixed NN & DDM Script
      - explicitly create randon_matrix
        (to standardize output with old devel)

--- PsyNeuLink/Components/States/State.py
+++ PsyNeuLink/Components/States/State.py
@@ -1239,8 +1239,6 @@ class State_Base(State):
                 else:
                     projection_value = projection.value
 
-                TEST = True
-
             else:
                 # Update all non-LearningProjections and get value
                 projection_value = projection.execute(params=projection_params,
