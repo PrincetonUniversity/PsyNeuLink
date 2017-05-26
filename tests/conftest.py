3cdb4d24263f33693aca24842653a51d00eaf7d2
kmantel <kmantel@princeton.edu>
2017-05-26 18:16:49 -0400

    add EVC stress test (run stress tests with --stress argument to pytest)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import random
-import time
+# import time
 import numpy as np
 
 # def pytest_addoption(parser):
@@ -11,11 +11,29 @@ import numpy as np
 #         help='the seed to use for each test'
 #     )
 
+mark_stress_tests = 'stress'
+
+marks_default_skip = [mark_stress_tests]
+
+
+# skip stress tests by default, add command option to include
+# http://blog.devork.be/2009/12/skipping-slow-test-by-default-in-pytest.html
+def pytest_addoption(parser):
+    parser.addoption('--{0}'.format(mark_stress_tests), action='store_true', default=False, help='Run {0} tests (long)'.format(mark_stress_tests))
+
+
+def pytest_runtest_setup(item):
+    for m in marks_default_skip:
+        if getattr(item.obj, m, None) and not item.config.getvalue(m):
+            pytest.skip('{0} tests not requested'.format(m))
+
+
 def pytest_runtest_call(item):
     # seed = int(item.config.getoption('--pnl-seed'))
     seed = 0
     random.seed(seed)
     np.random.seed(seed)
+
 
 @pytest.helpers.register
 def expand_np_ndarray(arr):
