facffd54e5abad1c66ba8257f4ff9daf9bb1025c
kmantel <kmantel@princeton.edu>
2017-06-07 18:10:51 -0400

    reset EVC gratton test to only check for system results

--- tests/control/test_EVC.py
+++ tests/control/test_EVC.py
@@ -1,6 +1,4 @@
 import pytest
-import functools
-import logging
 import numpy as np
 
 from PsyNeuLink.Components.Mechanisms.AdaptiveMechanisms.ControlMechanisms.EVCMechanism import EVCMechanism
@@ -13,9 +11,6 @@ from PsyNeuLink.Components.Functions.Function import Linear, BogaczEtAl, Exponen
 from PsyNeuLink.Globals.Keywords import ALLOCATION_SAMPLES, IDENTITY_MATRIX, MEAN, RESULT, VARIANCE
 from PsyNeuLink.Globals.Preferences.ComponentPreferenceSet import ComponentPreferenceSet, kpVerbosePref, kpReportOutputPref
 from PsyNeuLink.Globals.Preferences.PreferenceSet import PreferenceEntry, PreferenceLevel
-from PsyNeuLink.Globals.TimeScale import TimeScale
-
-logger = logging.getLogger(__name__)
 
 
 def test_EVC():
@@ -401,52 +396,95 @@ def test_EVC_gratton():
 
     mySystem.controller.reportOutputPref = True
 
-    # mySystem.show_graph()
-
-    def evaluate_by_trial(system=None, expected_output=None):
-        trial = system.scheduler_processing.times[TimeScale.LIFE][TimeScale.TRIAL]
-        assert trial < len(expected_output)
-
-        for i in range(len(expected_output[trial])):
-            val, expected = expected_output[i]
-            np.testing.assert_allclose(val, expected, atol=1e-08, err_msg='Failed on trial {1} expected_output[{0}]'.format(i, trial))
-
-    expected_output = [
-        [
-            # 0 = {tuple} <class 'tuple'>: ('Target Stimulus', [array([ 0.3324]), array(0.3324), array(0.0)])
-            # 1 = {tuple} <class 'tuple'>: ('Flanker Stimulus', [array([ 0.35452218]), array(0.3545221843), array(0.0)])
-            # 2 = {tuple} <class 'tuple'>: ('Target Representation', [array([ 0.59832]), array(0.5983199999999999), array(0.0)])
-            # 3 = {tuple} <class 'tuple'>: ('Flanker Representation', [array([ 0.63813993]), array(0.6381399317399999), array(0.0)])
-            # 4 = {tuple} <class 'tuple'>: ('Automatic Component', [array([ 0.68692218]), array(0.6869221843), array(0.0)])
-            # 5 = {tuple} <class 'tuple'>: ('Decision', [array([ 0.2645]), array([ 0.28289958]), array([ 0.98320731]), array([ 0.01679269])])
-            # 6 = {tuple} <class 'tuple'>: ('Reward', [array([ 100.]), array(100.0), array(0.0)])
-            # (mySystem.controller.EVC_max, np.array([90.8924436701277])),
-            # (mySystem.controller.EVC_max_policy, np.array([1.2, 1.4])),
-            # (mySystem.controller.EVC_max_state_values, np.array([[98.32073139]])),
-        ],
-        #         [
-        # 0 = {tuple} <class 'tuple'>: ('Target Stimulus', [array([ 0.3324]), array(0.3324), array(0.0)])
-        # 1 = {tuple} <class 'tuple'>: ('Flanker Stimulus', [array([-0.35452218]), array(-0.3545221843), array(0.0)])
-        # 2 = {tuple} <class 'tuple'>: ('Target Representation', [array([ 0.59832]), array(0.5983199999999999), array(0.0)])
-        # 3 = {tuple} <class 'tuple'>: ('Flanker Representation', [array([-0.63813993]), array(-0.6381399317399999), array(0.0)])
-        # 4 = {tuple} <class 'tuple'>: ('Automatic Component', [array([-0.02212218]), array(-0.022122184300000014), array(0.0)])
-        # 5 = {tuple} <class 'tuple'>: ('Decision', [array([-0.2645]), array([ 0.42944107]), array([ 0.46727945]), array([ 0.53272055])])
-        # 6 = {tuple} <class 'tuple'>: ('Reward', [array([ 100.]), array(100.0), array(0.0)])
-        #         ],
-        #         [
-        # 0 = {tuple} <class 'tuple'>: ('Target Stimulus', [array([ 0.3324]), array(0.3324), array(0.0)])
-        # 1 = {tuple} <class 'tuple'>: ('Flanker Stimulus', [array([ 0.35452218]), array(0.3545221843), array(0.0)])
-        # 2 = {tuple} <class 'tuple'>: ('Target Representation', [array([ 0.59832]), array(0.5983199999999999), array(0.0)])
-        # 3 = {tuple} <class 'tuple'>: ('Flanker Representation', [array([ 0.63813993]), array(0.6381399317399999), array(0.0)])
-        # 4 = {tuple} <class 'tuple'>: ('Automatic Component', [array([ 0.68692218]), array(0.6869221843), array(0.0)])
-        # 5 = {tuple} <class 'tuple'>: ('Decision', [array([ 0.2645]), array([ 0.28289958]), array([ 0.98320731]), array([ 0.01679269])])
-        # 6 = {tuple} <class 'tuple'>: ('Reward', [array([ 100.]), array(100.0), array(0.0)])
-        #         ]
-
+    expected_results_array = [
+        0.2645,  0.32257753,  0.94819408, 100.,
+        0.2645,  0.31663196,  0.95508757, 100.,
+        0.2645,  0.31093566,  0.96110142, 100.,
+        0.2645,  0.30548947,  0.96633839, 100.,
+        0.2645,  0.30029103,  0.97089165, 100.,
+        0.2645,  0.3169957,  0.95468427, 100.,
+        0.2645,  0.31128378,  0.9607499, 100.,
+        0.2645,  0.30582202,  0.96603252, 100.,
+        0.2645,  0.30060824,  0.9706259, 100.,
+        0.2645,  0.29563774,  0.97461444, 100.,
+        0.2645,  0.31163288,  0.96039533, 100.,
+        0.2645,  0.30615555,  0.96572397, 100.,
+        0.2645,  0.30092641,  0.97035779, 100.,
+        0.2645,  0.2959409,  0.97438178, 100.,
+        0.2645,  0.29119255,  0.97787196, 100.,
+        0.2645,  0.30649004,  0.96541272, 100.,
+        0.2645,  0.30124552,  0.97008732, 100.,
+        0.2645,  0.29624499,  0.97414704, 100.,
+        0.2645,  0.29148205,  0.97766847, 100.,
+        0.2645,  0.28694892,  0.98071974, 100.,
+        0.2645,  0.30156558,  0.96981445, 100.,
+        0.2645,  0.29654999,  0.97391021, 100.,
+        0.2645,  0.29177245,  0.97746315, 100.,
+        0.2645,  0.28722523,  0.98054192, 100.,
+        0.2645,  0.28289958,  0.98320731, 100.,
+        0.2645,  0.28289958,  0.98320731, 100.,
+        0.2645,  0.42963678,  0.47661181, 100.,
+        0.2645,  0.42846471,  0.43938586, 100.,
+        -0.2645,  0.42628176,  0.40282965, 100.,
+        0.2645,  0.42314468,  0.36732207, 100.,
+        -0.2645,  0.41913221,  0.333198, 100.,
+        0.2645,  0.42978939,  0.51176048, 100.,
+        0.2645,  0.42959394,  0.47427693, 100.,
+        -0.2645,  0.4283576,  0.43708106, 100.,
+        0.2645,  0.4261132,  0.40057958, 100.,
+        -0.2645,  0.422919,  0.36514906, 100.,
+        0.2645,  0.42902209,  0.54679323, 100.,
+        0.2645,  0.42980788,  0.50942101, 100.,
+        -0.2645,  0.42954704,  0.47194318, 100.,
+        -0.2645,  0.42824656,  0.43477897, 100.,
+        0.2645,  0.42594094,  0.3983337, 100.,
+        -0.2645,  0.42735293,  0.58136855, 100.,
+        -0.2645,  0.42910149,  0.54447221, 100.,
+        0.2645,  0.42982229,  0.50708112, 100.,
+        -0.2645,  0.42949608,  0.46961065, 100.,
+        -0.2645,  0.42813159,  0.43247968, 100.,
+        -0.2645,  0.42482049,  0.61516258, 100.,
+        0.2645,  0.42749136,  0.57908829, 100.,
+        0.2645,  0.42917687,  0.54214925, 100.,
+        -0.2645,  0.42983261,  0.50474093, 100.,
+        -0.2645,  0.42944107,  0.46727945, 100.,
+        -0.2645,  0.42944107,  0.46727945, 100.,
+        0.2645,  0.32257753,  0.94819408, 100.,
+        0.2645,  0.31663196,  0.95508757, 100.,
+        0.2645,  0.31093566,  0.96110142, 100.,
+        0.2645,  0.30548947,  0.96633839, 100.,
+        0.2645,  0.30029103,  0.97089165, 100.,
+        0.2645,  0.3169957,  0.95468427, 100.,
+        0.2645,  0.31128378,  0.9607499, 100.,
+        0.2645,  0.30582202,  0.96603252, 100.,
+        0.2645,  0.30060824,  0.9706259, 100.,
+        0.2645,  0.29563774,  0.97461444, 100.,
+        0.2645,  0.31163288,  0.96039533, 100.,
+        0.2645,  0.30615555,  0.96572397, 100.,
+        0.2645,  0.30092641,  0.97035779, 100.,
+        0.2645,  0.2959409,  0.97438178, 100.,
+        0.2645,  0.29119255,  0.97787196, 100.,
+        0.2645,  0.30649004,  0.96541272, 100.,
+        0.2645,  0.30124552,  0.97008732, 100.,
+        0.2645,  0.29624499,  0.97414704, 100.,
+        0.2645,  0.29148205,  0.97766847, 100.,
+        0.2645,  0.28694892,  0.98071974, 100.,
+        0.2645,  0.30156558,  0.96981445, 100.,
+        0.2645,  0.29654999,  0.97391021, 100.,
+        0.2645,  0.29177245,  0.97746315, 100.,
+        0.2645,  0.28722523,  0.98054192, 100.,
+        0.2645,  0.28289958,  0.98320731, 100.,
+        0.2645,  0.28289958,  0.98320731, 100.,
     ]
 
     mySystem.run(
         num_executions=nTrials,
         inputs=stim_list_dict,
-        call_after_trial=functools.partial(evaluate_by_trial, system=mySystem, expected_output=expected_output)
+    )
+
+    np.testing.assert_allclose(
+        pytest.helpers.expand_np_ndarray(mySystem.results),
+        expected_results_array,
+        atol=1e-08,
+        verbose=True,
     )
