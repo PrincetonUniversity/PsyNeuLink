e38b1b1f6aee4a1182dad7d6fd5108780949263a
kmantel <kmantel@princeton.edu>
2017-06-26 19:14:26 -0400

    add doc comments to scheduling

--- PsyNeuLink/scheduling/condition.py
+++ PsyNeuLink/scheduling/condition.py
@@ -12,6 +12,7 @@
 
 COMMENT:
     [**NAME OF THE MODULE SHOULD BE CAPITALIZED]
+    K: Agreed, along with scheduling folder to match the project style
 COMMENT
 
 .. _Condition_Overview
@@ -21,8 +22,8 @@ Overview
 
 `Conditions <Condition>` are used to specify when `Mechanisms <Mechanism>` execute.  They fall broadly into two
 categories: ones that specify the behavior of a Mechanism irrespective of others (e.g., the exact number of times it
-should be executed, and whether this should be based on its `value <Mechanism.value>`); and ones that specify whether
-and how its execution depends on other Mechanisms (e.g., the frequency with which it executes relative to others,
+should be executed, and whether this should be based on its `value <Mechanism.value>`); K: I'm not sure what that means
+and ones that specify whether and how its execution depends on other Mechanisms (e.g., the frequency with which it executes relative to others,
 or that it begin executing and/or execute repeatedly until a Condition is met for some other Mechanism).  Each
 Condition is associated with an `owner <Condition.owner>` (a `Mechanism` to which the Condition belongs), and a
 `scheduler <Condition.scheduler>` that maintains most of the data required to test for satisfaction of the condition.
@@ -34,15 +35,29 @@ Creating Conditions
 
 COMMENT:
     [**??IS THE FOLLOWING CORRECT?  owner AND scheduler DON'T SEEM TO BE ARGS OF Condition.__init__??]
+    K: this is correct and by design. 1. they are never used outside of adding them to schedulers, at which point
+    the scheduler and owner are known. They must be assigned overwritten at this time otherwise those attributes
+    make no sense. Additionally not all Conditions have owners or schedulers (e.g. Always)
 Conditions can be created at any time, and take effect immediately for the execution of any `Scheduler(s) <Scheduler>`
-with which they are associated.  The `owner <Condition.owner>` and `scheduler <Condition.scheduler>` can also be
-specified explicitly, in the corresponding arguments of its constructor;  however, usually these can be determined
-and assigned automatically based on the context in which the Condition is created [**?? EXAMPLE?].  The Condition's
+with which they are associated. (K: I don't think this is accurate) The `owner <Condition.owner>` and `scheduler <Condition.scheduler>` can also be
+specified explicitly, in the corresponding arguments of its constructor; (K: they can't be specified in the constructor) however, usually these can be determined
+and assigned automatically based on the context in which the Condition is created [**?? EXAMPLE?]. (K: example is basically
+any example script using the add_condition method - it's really not important.)  The Condition's
 **dependencies** and **func** arguments must both be explicitly specified.  These are used to determine whether a
-Condition is satisfied during each `round of execution <LINK>`: `func <Condition.func>` is called with
+Condition is satisfied during each `round of execution <LINK>` (K: round of execution is poorly defined and should refer to a TimeScale)
+: `func <Condition.func>` is called with
 `dependencies <Condition.dependencies>` as its parameter (and optionally, additional named and unnamed arguments).
 COMMENT:
      [**??func AND dependencies NEED TO BE CLARIFIED:  WHAT FORMAT, EXAMPLE OF HOW THEY WORK??]
+    K: It's explained in the previous version
+             Each Condition must
+            - be a subclass of `Condition`<Condition>
+            - pass `dependencies` as the first argument to the __init__ function of Condition
+            - pass `func` as the second argument to the __init__ function of Condition
+
+        In determining whether a Condition is satisfied, `func` is called with `dependencies` as parameter (and optionally,
+        additional named and unnamed arguments).
+    They are not in an exact format by design, because they can be customized by any advanced user.
 COMMENT
 
 Hint:
@@ -70,6 +85,9 @@ Structure
 COMMENT:
      **??DESCRIBE HOW CONDITIONS ARE STRUCTURED;
      INCLUDE FULL LIST OF CONDITIONS
+    K: they are listed in the bottom of the Condition doc page by nature of being classes in this file, at least on the version
+    I see
+
 COMMENT
 
 .. Condition_Execution:
@@ -79,6 +97,8 @@ Execution
 
 COMMENT:
      **??DESCRIBE HOW CONITION IS EVALUATED
+    K: It doesn't "execute" exactly. A condition is satisfied when its is_satisfied function returns True, and is_satisfied
+    is called when the scheduler runs
 COMMENT
 
 
@@ -158,11 +178,12 @@ class Condition(object):
     Arguments
     ---------
 
-    dependencies : **??LIST? DICT?
+    dependencies : **??LIST? DICT? K: ANYTHING
+        not just mechanisms, they can be anything at all
         one or more `Mechanisms <Mechanism>` over which `func <Condition.func>` is evaluated to determine satisfaction
         of the `Condition`;  user must ensure that dependencies are suitable as func parameters
-    func : **??FORMAT?
-        parameters over which func is evaluated to determine satisfaction of the `Condition`
+    func : **??FORMAT? K: probably below in my version was an accident. func is just any function
+        func is evaluated to determine satisfaction of the `Condition`
 
     args :
         additional formal arguments passed to func
@@ -186,7 +207,7 @@ class Condition(object):
         :param self:
         :param dependencies: one or more PNL objects over which func is evaluated to determine satisfaction of the :keyword:`Condition`
             user must ensure that dependencies are suitable as func parameters
-        :param func: parameters over which func is evaluated to determine satisfaction of the :keyword:`Condition`
+        :param func: func is evaluated to determine satisfaction of the :keyword:`Condition`
         :param args: additional formal arguments passed to func
         :param kwargs: additional keyword arguments passed to func
         """
