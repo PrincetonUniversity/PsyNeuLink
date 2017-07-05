1ab5702b3772490af692e4022765e2daaa6e8f00
kmantel <kmantel@princeton.edu>
2017-07-05 17:48:03 -0400

    update TimeScale documentation

--- PsyNeuLink/Scheduling/TimeScale.py
+++ PsyNeuLink/Scheduling/TimeScale.py
@@ -6,8 +6,8 @@
 # See the License for the specific language governing permissions and limitations under the License.
 #
 #
-from enum import Enum
 from PsyNeuLink.Globals.Keywords import CENTRAL_CLOCK
+from enum import Enum
 
 # ********************************************* TimeScale ***************************************************************
 
@@ -23,27 +23,24 @@ class TimeScale(Enum):
     ----------
 
     TIME_STEP
-        the nuclear unit of time, corresponding to the execution of all `Mechanisms <Mechanism>`allowed to execute
+        the nuclear unit of time, corresponding to the execution of all `Mechanism <Mechanism>`\ s allowed to execute
         from a single `consideration_set` of a `Scheduler`, and which are considered to have executed simultaneously.
 
     PASS
         a full iteration through all of the `consideration_sets <consideration_set>` in a `Scheduler's <Scheduler>`
         `consideration_queue`, consisting of one or more `TIME_STEPs <TIME_STEP>`, over which every `Component`
-        `specified to Scheduler <Scheduler_Creation>` is considered for execution at least once.
+        `specified to a Scheduler <Scheduler_Creation>` is considered for execution at least once.
 
     TRIAL
-        an open-ended unit of time consisting of all TIME_STEPs occurring within the scope of a single input to a
+        an open-ended unit of time consisting of all action that occurs within the scope of a single input to a
         `Composition`.
 
     RUN
-        the scope of a call to the `run <Composition.run>` method of a `Composition`, consisting of one more
+        the scope of a call to the `run <Composition.run>` method of a `Composition`, consisting of one or more
         `TRIALs <TRIAL>`.
-        COMMENT:
-            a loosely-defined unit of time consisting of one or more `TRIALs <TRIAL>`.
-        COMMENT
 
     LIFE
-        the number of `TIME_STEPs <TIME_STEP>` since the creation of an object.
+        the scope of time since the creation of an object.
     """
     TIME_STEP = 0
     PASS = 1
@@ -56,6 +53,7 @@ class Clock:
     """Clock object used by a `Components <Component>` and `Compositions <Composition>` to reference the current
     state of their assigned `Scheduler`.
     """
+
     def __init__(self, name):
         self.name = name
         self.time_step = 0
@@ -68,6 +66,7 @@ class Clock:
         self.block = 0
         self.task = 0
 
+
 CentralClock = Clock(name=CENTRAL_CLOCK)
 
 
@@ -77,7 +76,3 @@ class CurrentTime:
         self.trial = CentralClock.trial
         self.block = CentralClock.block
         self.task = CentralClock.task
-
-
-
-
