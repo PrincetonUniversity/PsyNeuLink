b07a0ae0639ba2b19e650d904d7114fa71276e61
jdc@princeton.edu <jdc@princeton.edu>
2017-07-15 11:57:34 -0400

    • Mechanism   - implemented default_value as read-only property (for _default_value)

--- PsyNeuLink/Components/System.py
+++ PsyNeuLink/Components/System.py
@@ -1498,7 +1498,7 @@ class System_Base(System):
                     raise SystemError("{} (entry in initial_values arg) is not a Mechanism in \'{}\'".
                                       format(mech.name, self.name))
                 mech._update_value
-                if not iscompatible(value, mech._default_value[0]):
+                if not iscompatible(value, mech.default_value[0]):
                     raise SystemError("{} (in initial_values arg for \'{}\') is not a valid value for {}".
                                       format(value, self.name, append_type_to_name(self)))
 
