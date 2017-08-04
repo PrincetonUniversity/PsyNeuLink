dae1c1e0f31eaed1aebfa67d4aeb576ada3a0f53
Jan Vesely <jan.vesely@rutgers.edu>
2017-08-04 02:15:13 -0400

    llvm/tests: Add custom sized function to matmul test
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/llvm/matmul.py
+++ tests/llvm/matmul.py
@@ -4,6 +4,7 @@ import ctypes
 import numpy as np
 import PsyNeuLink.llvm as pnlvm
 import timeit
+from llvmlite import ir
 
 ITERATIONS=100
 DIM_X=1000
@@ -37,9 +38,43 @@ for _ in range(ITERATIONS):
 stop = timeit.default_timer()
 print("LLVM time elapsed {:f}".format(stop-start))
 
+if not np.allclose(llvm_res, result):
+    print("TEST FAILED LLVM results differ!")
+    print(llvm_res)
+    print(result)
+
+start = timeit.default_timer()
+
+with pnlvm.LLVMBuilderContext() as ctx:
+    double_ptr_ty = ctx.float_ty.as_pointer()
+    func_ty = ir.FunctionType(ir.VoidType(), (double_ptr_ty, double_ptr_ty, double_ptr_ty))
+
+    # get builtin IR
+    builtin = ctx.get_llvm_function('__pnl_builtin_vxm')
+
+    # Create square vector matrix multiply
+    function = ir.Function(ctx.module, func_ty, name="vxsqm")
+    _x = ctx.int32_ty(x)
+    _y = ctx.int32_ty(y)
+    _v, _m, _o = function.args
+    block = function.append_basic_block(name="entry")
+    builder = ir.IRBuilder(block)
+    builder.call(builtin, [_v, _m, _x, _y, _o])
+    builder.ret_void()
+
+stop = timeit.default_timer()
+print("Build time elapsed {:f}".format(stop-start))
+
+binf2 = pnlvm.LLVMBinaryFunction.get('vxsqm')
+start = timeit.default_timer()
+for _ in range(ITERATIONS):
+    binf2(ct_vec, ct_mat, ct_res)
+stop = timeit.default_timer()
+print("LLVM-custom time elapsed {:f}".format(stop-start))
+
 # Use all close to ignore rounding errors
 if not np.allclose(llvm_res, result):
-    print("TEST FAILED results differ!")
+    print("TEST FAILED LLVM-custom results differ!")
     print(llvm_res)
     print(result)
 else:
