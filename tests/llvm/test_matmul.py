736d5fe04dc21b49c13277dd364a89ba9e5bfd82
Jan Vesely <jan.vesely@rutgers.edu>
2017-08-06 22:24:39 -0400

    llvm,tests: Convert matmul benchmark to pytest
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

---
+++ tests/llvm/test_matmul.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+import ctypes
+import numpy as np
+import PsyNeuLink.llvm as pnlvm
+import timeit
+import pytest
+from llvmlite import ir
+
+ITERATIONS=100
+DIM_X=1000
+DIM_Y=2000
+
+matrix = np.random.rand(DIM_X, DIM_Y)
+vector = np.random.rand(DIM_X)
+llvm_res = np.random.rand(DIM_Y)
+result = np.dot(vector, matrix)
+
+#start = timeit.default_timer()
+@pytest.mark.llvm
+def test_matmul_numpy(benchmark):
+    numpy_res = benchmark(np.dot, vector, matrix)
+    assert np.allclose(numpy_res, result)
+#stop = timeit.default_timer()
+#print("Numpy time elapsed {:f}".format(stop-start))
+
+#start = timeit.default_timer()
+ct_vec = vector.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+ct_mat = matrix.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+ct_res = llvm_res.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+x, y = matrix.shape
+#stop = timeit.default_timer()
+#print("Convert time elapsed {:f}".format(stop-start))
+#
+#
+#start = timeit.default_timer()
+@pytest.mark.llvm
+def test_matmul_llvm(benchmark):
+    llvm_fun = pnlvm.LLVMBinaryFunction.get('__pnl_builtin_vxm')
+    benchmark(llvm_fun, ct_vec, ct_mat, x, y, ct_res)
+    assert np.allclose(llvm_res, result)
+
+#for _ in range(ITERATIONS):
+#    llvm_fun(ct_vec, ct_mat, x, y, ct_res)
+#stop = timeit.default_timer()
+#print("LLVM time elapsed {:f}".format(stop-start))
+#
+#if not np.allclose(llvm_res, result):
+#    print("TEST FAILED LLVM results differ!")
+#    print(llvm_res)
+#    print(result)
+#
+#start = timeit.default_timer()
+
+custom_name = None
+
+with pnlvm.LLVMBuilderContext() as ctx:
+    custom_name = ctx.module.get_unique_name("vxsqm")
+    double_ptr_ty = ctx.float_ty.as_pointer()
+    func_ty = ir.FunctionType(ir.VoidType(), (double_ptr_ty, double_ptr_ty, double_ptr_ty))
+
+    # get builtin IR
+    builtin = ctx.get_llvm_function('__pnl_builtin_vxm')
+
+    # Create square vector matrix multiply
+    function = ir.Function(ctx.module, func_ty, name=custom_name)
+    _x = ctx.int32_ty(x)
+    _y = ctx.int32_ty(y)
+    _v, _m, _o = function.args
+    block = function.append_basic_block(name="entry")
+    builder = ir.IRBuilder(block)
+    builder.call(builtin, [_v, _m, _x, _y, _o])
+    builder.ret_void()
+
+# This triggers recompile if needed so it should be included in the measurement
+#stop = timeit.default_timer()
+#print("Build time elapsed {:f}".format(stop-start))
+#
+#start = timeit.default_timer()
+@pytest.mark.llvm
+def test_matmul_llvm_cont_dim(benchmark):
+    binf2 = pnlvm.LLVMBinaryFunction.get(custom_name)
+    benchmark(binf2, ct_vec, ct_mat, ct_res)
+    assert np.allclose(llvm_res, result)
+#for _ in range(ITERATIONS):
+#    binf2(ct_vec, ct_mat, ct_res)
+#stop = timeit.default_timer()
+#print("LLVM-custom time elapsed {:f}".format(stop-start))
+#
+## Use all close to ignore rounding errors
+#if not np.allclose(llvm_res, result):
+#    print("TEST FAILED LLVM-custom results differ!")
+#    print(llvm_res)
+#    print(result)
+#else:
+#    print("TEST PASSED")
