0481c293196c50c6b0a7a52ca4bd5ecea1bb26ac
kmantel <kmantel@princeton.edu>
2017-08-09 15:01:27 -0400

    add return value to for EVCMechanism call to self._update_input_states

--- PsyNeuLink/Components/Mechanisms/AdaptiveMechanisms/ControlMechanisms/EVCMechanism.py
+++ PsyNeuLink/Components/Mechanisms/AdaptiveMechanisms/ControlMechanisms/EVCMechanism.py
@@ -1436,9 +1436,8 @@ class EVCMechanism(ControlMechanism_Base):
 
         # Get outcomes for current allocation_policy
         #    = the values of the monitored output states (self.input_states)
-        #    stored in self.input_value = list(self.variable)
         # self.monitoring_mechanism.execute(context=EVC_SIMULATION)
-        self._update_input_states(runtime_params=runtime_params, time_scale=time_scale,context=context)
+        monitored_states = self._update_input_states(runtime_params=runtime_params, time_scale=time_scale,context=context)
 
         for i in range(len(self.control_signals)):
             self.control_signal_costs[i] = self.control_signals[i].cost
