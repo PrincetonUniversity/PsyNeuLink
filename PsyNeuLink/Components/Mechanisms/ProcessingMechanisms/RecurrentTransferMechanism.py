79ff5a226e7fc9be546d6ab0da62a08976c4b144
dcw3 <dcw3@princeton.edu>
2017-08-10 10:08:48 -0400

    Minor Doc Changes

--- PsyNeuLink/Components/Mechanisms/ProcessingMechanisms/RecurrentTransferMechanism.py
+++ PsyNeuLink/Components/Mechanisms/ProcessingMechanisms/RecurrentTransferMechanism.py
@@ -14,6 +14,7 @@
 # ****************************************  RecurrentTransferMechanism *************************************************
 
 """
+.. _Recurrent_Transfer_Overview:
 
 Overview
 --------
