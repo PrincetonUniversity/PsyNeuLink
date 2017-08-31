eb6fcc374ae6227c226aeac5842090cb28cd706b
KristenManning <kristenmanning16@gmail.com>
2017-08-31 14:00:22 -0400

    increasing navigation_depth (levels of categories in docs menu) from 4 to 5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -146,6 +146,9 @@ import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    'navigation_depth': 5
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
