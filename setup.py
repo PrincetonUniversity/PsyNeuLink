240d8e34fa53b6f0ae7645dc7298b33aa0c398dd
kmantel <kmantel@princeton.edu>
2017-09-21 15:46:09 -0400

    update setup.py supported Python versions to include only 3.5 and 3.6 (does not work on < 3.4 due to numpy, does not work on 3.4)

--- setup.py
+++ setup.py
@@ -47,9 +47,8 @@ setup(
 
         # Supported Python Versions
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
