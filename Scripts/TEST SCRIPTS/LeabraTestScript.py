7e1322921c37bb009040e59973d1d15a66238d88
dcw3 <dcw3@princeton.edu>
2017-10-18 12:40:43 -0400

    Test Changes

--- Scripts/TEST SCRIPTS/LeabraTestScript.py
+++ Scripts/TEST SCRIPTS/LeabraTestScript.py
@@ -6,39 +6,24 @@ import random
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     import leabra
-from PsyNeuLink.Library.Mechanisms.ProcessingMechanisms.LeabraMechanism import LeabraMechanism
+from PsyNeuLink.Library.Mechanisms.ProcessingMechanisms.LeabraMechanism import LeabraMechanism, build_network
 from PsyNeuLink.Components.Mechanisms.ProcessingMechanisms.TransferMechanism import TransferMechanism
 from PsyNeuLink.Components.Projections.PathwayProjections.MappingProjection import MappingProjection
 from PsyNeuLink.Components.Functions.Function import Linear
 from PsyNeuLink.Components.Process import process
 from PsyNeuLink.Components.System import system
+import time
 
-def printWeights(link):
-    print('link.pre: ', link.pre)
-    print('link.post: ', link.post)
-    print('link.wt: ', link.wt)
-    print('link.fwt: ', link.fwt)
-    print('link.dwt: ', link.dwt)
+random.seed(1)
+input_size=4
+output_size=2
+hidden_layers=4
+hidden_sizes=[2, 3, 4, 5]
+input_pattern = [[0, 1, 3, 4]]
+training_pattern = [[0, 1]]
+num_trials = 1000
 
-random.seed(1)  # to avoid the bug with mixed types in statistics._sum
-
-L3 = LeabraMechanism(input_size=4, output_size=2, hidden_layers=4, hidden_sizes=[2, 3, 4, 5], name='L', training_flag=True)
-
-print(L3.function)
-print(L3.function_object.network)
-printWeights(L3.function_object.network.connections[0].links[0])
-printWeights(L3.function_object.network.connections[1].links[1])
-
-L3.execute(input=[[0, 1, 3, 4], [10, 20]])
-L3.execute(input=[[0, 1, 3, 4], [10, 20]])
-L3.execute(input=[[0, 1, 3, 4], [10, 20]])
-L3.execute(input=[[0, 1, 3, 4], [10, 20]])
-L3.execute(input=[[0, 1, 3, 4], [10, 20]])
-
-printWeights(L3.function_object.network.connections[0].links[0])
-printWeights(L3.function_object.network.connections[1].links[1])
-print(L3.value)
-print(L3.function_object.network.layers[-1].activities)
+L3 = LeabraMechanism(input_size=input_size, output_size=output_size, hidden_layers=hidden_layers, hidden_sizes=hidden_sizes, name='L', training_flag=True)
 
 T1 = TransferMechanism(name='T1', size=4, function=Linear)
 T2 = TransferMechanism(name='T2', size=2, function=Linear)
@@ -46,28 +31,38 @@ T2 = TransferMechanism(name='T2', size=2, function=Linear)
 p1 = process(pathway=[T1, L3])
 proj = MappingProjection(sender=T2, receiver=L3.input_states[1])
 p2 = process(pathway=[T2, L3])
+T2.output_state.efferents[1].matrix = T2.output_state.efferents[1].matrix * 0
 s = system(processes=[p1, p2])
-s.run(inputs={T1: [[0, 1, 3, 4]], T2: [[1, 4]]})
-print(L3.function_object.network.layers[-1].activities)
-print(L3.output_state.value)
+start_time = time.process_time()
+tmp = s.run(inputs={T1: input_pattern.copy()*num_trials, T2: training_pattern.copy()*num_trials})
+print('PNL time to run: ', time.process_time()-start_time)
 
-# L = LeabraMechanism(input_size=4, output_size=2, hidden_layers=2, name='L')
-#
-# print(L.function)
-# print(L.function_object.network)
-#
-# L.execute(input=[[0, 1, 3, 4], [10, 20]])
-# print(L.value)
-# print(L.function_object.network.layers[-1].activities)
-#
-# L2 = LeabraMechanism(input_size=4, output_size=2, hidden_layers=4, hidden_sizes=[2, 3, 4, 5], name='L')
-#
-# print(L2.function)
-# print(L2.function_object.network)
-#
-# L2.execute(input=[[0, 1, 3, 4], [10, 20]])
-# print(L2.value)
-# print(L2.function_object.network.layers[-1].activities)
-# layers = L2.function_object.network.layers
-# for i in range(len(layers)):
-#     print('layer {} has size {}'.format(i, layers[i].size))
+# begin: optional print statements
+print('PNL Output: ', tmp)
+print('types within PNL output: ', type(tmp), type(tmp[0]), type(tmp[0][0]), type(tmp[0][0][0]))
+print('activities and types: ', L3.function_object.network.layers[-1].activities, type(L3.function_object.network.layers[-1].activities[0]))
+unit = [unit.act_m for unit in L3.function_object.network.layers[-1].units]
+print('output layer unit values and their types: ', unit, type(unit), type(unit[0]))
+print('output state value and type: ', L3.output_state.value, type(L3.output_state.value), type(L3.output_state.value[0]))
+print("L3 value and L3 variable: ", L3.value, L3.variable)
+# end: optional print statements
+
+random.seed(1)
+leabra_net = build_network(n_input=input_size, n_output=output_size, n_hidden=hidden_layers, hidden_sizes=hidden_sizes)
+leabra_net.set_inputs({'input_layer': np.zeros(4)})
+leabra_net.trial()
+def train_network(network, input_pattern, output_pattern):
+    """Run one trial on the network"""
+    assert len(network.layers[0].units) == len(input_pattern)
+
+    assert len(network.layers[-1].units) == len(output_pattern)
+    network.set_inputs({'input_layer': input_pattern})
+    network.set_outputs({'output_layer': output_pattern})
+
+    network.trial()
+    return [unit.act_m for unit in network.layers[-1].units]
+start_time = time.process_time()
+for _ in range(num_trials):
+    train_network(leabra_net, input_pattern[0].copy(), training_pattern[0])
+print('Leabra time to run: ', time.process_time() - start_time)
+print('Leabra Output: ', [unit.act_m for unit in leabra_net.layers[-1].units], type([unit.act_m for unit in leabra_net.layers[-1].units][0]))
