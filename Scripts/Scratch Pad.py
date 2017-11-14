2855c803b3acdafd4dc1b70893d17bee05595425
jdc@princeton.edu <jdc@princeton.edu>
2017-11-13 19:55:51 -0500

    • Mechanism, Component   - preprocess_variable: in Mechanism, handles specifciation of variable by input_states arg                          otherwise, ignored

--- Scripts/Scratch Pad.py
+++ Scripts/Scratch Pad.py
@@ -530,10 +530,6 @@ for control_signal in my_ctl_mech.control_signals:
 # my_control_mech = pnl.ControlMechanism(control_signals=[{pnl.MECHANISM: my_mech,
 #                                                          pnl.PARAMETER_STATES: [pnl.DRIFT_RATE, pnl.THRESHOLD]}])
 
-m = pnl.TransferMechanism(default_variable=[0, 0, 0])
-i = pnl.InputState(owner=m, variable=[0, 0, 0])
-T = pnl.TransferMechanism(input_states=[i])
-
 assert True
 
 # --------------------------------------------------------------------------------------------------
