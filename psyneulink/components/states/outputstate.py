99742015ab41ef3830e72ddf61bc114638482d95
jdcpni <pniintel55>
2017-11-27 22:56:04 -0500

    -

--- psyneulink/components/states/outputstate.py
+++ psyneulink/components/states/outputstate.py
@@ -425,6 +425,7 @@ and then assigned either as::
     ...                                     decision_entropy_output_state])
 
 or::
+
     >>> another_decision_entropy_output_state = pnl.OutputState(name='DECISION ENTROPY',
     ...                                                index=2,
     ...                                                calculate=pnl.Stability(metric=pnl.ENTROPY).function)
