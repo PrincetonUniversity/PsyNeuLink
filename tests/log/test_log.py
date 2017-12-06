332aeb866de3b189bf6da2bcf582cf6092418c29
jdc@princeton.edu <jdc@princeton.edu>
2017-12-05 19:15:01 -0500

    • Component:   - make_property (Line 2883): changed return value

--- tests/log/test_log.py
+++ tests/log/test_log.py
@@ -21,21 +21,37 @@ class TestLog:
                                      'time_constant': 'OFF'}
         assert T2.loggable_items == {'InputState-0': 'OFF',
                                      'slope': 'OFF',
-                                     'MappingProjection from mech_A to mech_B': 'OFF',
+                                     'MappingProjection from T1 to T2': 'OFF',
                                      'RESULTS': 'OFF',
                                      'intercept': 'OFF',
                                      'noise': 'OFF',
                                      'time_constant': 'OFF'}
+
         T1.log_items(pnl.NOISE)
         T1.log_items(pnl.RESULTS)
         T2.log_items(Pj)
+
+        assert T1.loggable_items == {'Process-0_Input Projection': 'OFF',
+                                     'InputState-0': 'OFF',
+                                     'slope': 'OFF',
+                                     'RESULTS': 'EXECUTION',
+                                     'intercept': 'OFF',
+                                     'noise': 'EXECUTION',
+                                     'time_constant': 'OFF'}
+        assert T2.loggable_items == {'InputState-0': 'OFF',
+                                     'slope': 'OFF',
+                                     'MappingProjection from T1 to T2': 'EXECUTION',
+                                     'RESULTS': 'OFF',
+                                     'intercept': 'OFF',
+                                     'noise': 'OFF',
+                                     'time_constant': 'OFF'}
 
         Ps.execute()
         Ps.execute()
         Ps.execute()
 
         assert T1.logged_items == {'RESULTS': 'EXECUTION', 'noise': 'EXECUTION'}
-        assert T2.logged_items == {'MappingProjection from mech_A to mech_B': 'EXECUTION'}
+        assert T2.logged_items == {'MappingProjection from T1 to T2': 'EXECUTION'}
 
         # assert T1.log.print_entries() ==
         # # Log for mech_A:
@@ -60,12 +76,11 @@ class TestLog:
         # # 1         'noise'...........................................' EXECUTING  PROCESS Process-0'.......................................    0.0
 
         assert T1.log.csv(entries=['noise', 'RESULTS'], owner_name=False, quotes=None) == \
-                        "\'Entry\', \'noise\', \'RESULTS\'\n 0,  0.,  0. 0.\n1,  0.,  0.  0.\n2,  0.,  0.  0.\n"
+                        "\'Entry\', \'noise\', \'RESULTS\'\n0,  0.,  0.  0.\n1,  0.,  0.  0.\n2,  0.,  0.  0.\n"
 
-        # assert T2.log.csv(entries=proj_A_to_B, owner_name=True, quotes=True))
-        # 'Entry', 'MappingProjection from mech_A to mech_B'
-        # 0,  1.
-        # 1,  1.
+        assert T2.log.csv(entries=Pj, owner_name=True, quotes=True) == \
+                "\'Entry\', \'MappingProjection from T1 to T2\'\n0,  1.\n1,  1."
 
         assert T1.log.nparray(entries=['noise', 'RESULTS'], header=False, owner_name=True) == \
-               np.array([[[0], [1], [2]], [[ 0.], [ 0.], [ 0.]], [[ 0.,  0.], [ 0.,  0.],[ 0., 0.]]])
+               np.array([[[0], [1], [2]], [[ 0.], [ 0.], [ 0.]], [[ 0.,  0.], [ 0.,  0.],[ 0.,  0.]]])
+
