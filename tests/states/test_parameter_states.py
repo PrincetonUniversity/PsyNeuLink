2f5e8b55387aa188b019ebf2982aea9265aa9c8a
KristenManning <kristenmanning16@gmail.com>
2017-12-07 16:34:11 -0500

    adding mod_slope to the current parameter state pytests

--- tests/states/test_parameter_states.py
+++ tests/states/test_parameter_states.py
@@ -3,21 +3,25 @@ from psyneulink.components.functions.function import Linear
 
 class TestParameterStates:
     def test_inspect_function_params(self):
-        A = TransferMechanism(function=Linear(slope=0.1))
+        A = TransferMechanism()
+        # B = TransferMechanism()
         print()
         print(A.function)
         print(A._function)
         print("A.function_object.slope --> ", A.function_object.slope)
         print("A.function_object._slope --> ", A.function_object._slope)
+        print("A.function_object.mod_slope --> ", A.function_object.mod_slope)
         # print("A.function_object.base_value_slope --> ", A.function_object.base_value_slope)
         print("- - - - - SETTING A.function_object.slope = 0.2 - - - - -")
         A.function_object.slope = 0.2
         print("A.user_params -->", A.user_params)
         print("A.function_object.slope --> ", A.function_object.slope)
         print("A.function_object._slope --> ", A.function_object._slope)
+        print("A.function_object.mod_slope --> ", A.function_object.mod_slope)
         # print("A.function_object.base_value_slope --> ", A.function_object.base_value_slope)
         print("- - - - - EXECUTING A - - - - -")
         A.execute()
         print("A.function_object.slope --> ", A.function_object.slope)
         print("A.function_object._slope --> ", A.function_object._slope)
+        print("A.function_object.mod_slope --> ", A.function_object.mod_slope)
         # print("A.function_object.base_value_slope --> ", A.function_object.base_value_slope)
