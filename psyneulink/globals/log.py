615eb9522c94d3894df927bd12a3dc7250f22f6e
jdc@princeton.edu <jdc@princeton.edu>
2017-12-14 07:11:10 -0500

    • Log   - docstring: reinstated test on log_value (now that pref bug is fixed)

--- psyneulink/globals/log.py
+++ psyneulink/globals/log.py
@@ -128,8 +128,8 @@ LogConditions (e.g., LogCondition.EXECUTION | LogCondition.LEARNING).
    COMMENT:
    FIX: THIS EXAMPLE CAN'T CURRENTLY BE EXECUTED AS IT PERMANENTLY SETS THE LogPref FOR ALL TransferMechanism
    COMMENT
-    >> import psyneulink as pnl
-    >> T = pnl.TransferMechanism(
+    >>> import psyneulink as pnl
+    >>> T = pnl.TransferMechanism(
     ...        prefs={pnl.LOG_PREF: pnl.PreferenceEntry(pnl.LogCondition.INITIALIZATION, pnl.PreferenceLevel.INSTANCE)})
 
 .. hint::
@@ -155,7 +155,6 @@ another, and logs the `noise <TransferMechanism.noise>` and *RESULTS* `OutputSta
 `MappingProjection` from the first to the second::
 
     # Create a Process with two TransferMechanisms, and get a reference for the Projection created between them:
-    >>> import psyneulink as pnl
     >>> my_mech_A = pnl.TransferMechanism(name='mech_A', size=2)
     >>> my_mech_B = pnl.TransferMechanism(name='mech_B', size=3)
     >>> my_process = pnl.Process(pathway=[my_mech_A, my_mech_B])
