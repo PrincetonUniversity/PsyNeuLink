206be04c6056ecc81cc39090c888a1f270f979a5
KristenManning <kristenmanning16@gmail.com>
2018-01-17 13:48:39 -0500

    adding pytest to track changes in attr, _attr, mod_param_name, and userparams[param_name] values of Transfer Mechanism's configurable params when (1) param values are updated (2) mechanism is executed

--- tests/states/test_parameter_states.py
+++ tests/states/test_parameter_states.py
@@ -1,15 +1,11 @@
 from psyneulink.components.mechanisms.processing.transfermechanism import TransferMechanism
-from psyneulink.components.mechanisms.processing.integratormechanism import IntegratorMechanism
 from psyneulink.components.functions.function import Linear
-from psyneulink.components.functions.function import Logistic
+import numpy as np
 
 class TestParameterStates:
-    def test_inspect_function_params_slope(self):
-
+    def test_inspect_function_params_slope_noise(self):
         A = TransferMechanism()
         B = TransferMechanism()
-        # C = TransferMechanism(function=Linear(slope=2))
-
         assert A.function_object.slope == 1.0
         assert B.function_object.slope == 1.0
         assert A.function_object._slope == 1.0
@@ -60,12 +56,10 @@ class TestParameterStates:
         assert A.mod_noise == 0.0
         assert B.mod_noise == 0.0
 
-        # print("A.function_object.base_value_slope --> ", A.function_object.base_value_slope)
-        print("- - - - - EXECUTING A ", A.name, "- - - - -")
-        print(A.execute(1.0))
+        A.execute(1.0)
         assert A.mod_slope == [0.2]
-        print("- - - - - EXECUTING B - - - - -")
-        print(B.execute(1.0))
+
+        B.execute(1.0)
 
         assert A.function_object.slope == 0.2
         assert B.function_object.slope == 0.7
@@ -81,42 +75,90 @@ class TestParameterStates:
         assert A.mod_noise == 0.5
         assert B.mod_noise == 0.6
 
-    def test_inspect_mechanism_params_noise(self):
-        print("\n\n========================== starting second test ==========================\n\n")
-        C = TransferMechanism(function=Linear(slope=2.5))
-        print(C.function_object)
-        print(C.paramClassDefaults)
-        print(C.function_object.paramClassDefaults)
-        # C = TransferMechanism()
-        print("C.function_object.slope --> ", C.function_object.slope)
-        print("C.function_object._slope --> ", C.function_object._slope)
-        print("executing: ", C.execute(1.0))
-        C.function_object.slope=2.0
-        print("setting slope to 2.0")
-        print("C.function_object.slope --> ", C.function_object.slope)
-        print("C.function_object._slope --> ", C.function_object._slope)
-        print("executing: ", C.execute(1.0))
-        print("C.noise --> ", C.noise)
-        print("C._noise --> ", C._noise)
-        # print("C.mod_noise --> ", C.mod_noise)
-        # print("C.base_value_noise --> ", C.base_value_noise)
-        print("- - - - - SETTING C.noise = 0.2 - - - - -")
-        C.noise = 0.2
-        print("C.user_params -->", C.user_params)
-        print("C.noise --> ", C.noise)
-        print("C._noise --> ", C._noise)
-        # print("C.mod_noise --> ", C.mod_noise)
-        # print("C.base_value_noise --> ", C.base_value_noise)
-        print("- - - - - EXECUTING A - - - - -")
-        C.execute(1.0)
-        print("C.noise --> ", C.noise)
-        print("C._noise --> ", C._noise)
-        # print("C.mod_noise --> ", C.mod_noise)
+class TestConfigurableParameters:
+    def test_configurable_params(self):
+        old_value = 0.2
+        new_value = 0.7
+        T = TransferMechanism(function=Linear(slope=old_value,
+                                              intercept=old_value),
+                              noise=old_value,
+                              smoothing_factor=old_value)
 
-    def test_make_property(self):
-        print("making mechanism A")
-        A = TransferMechanism(function=Logistic)
-        print(A.function_object.gain)
-        print("\n\n ============================== \n\n")
-        print("making mechanism B")
-        B = TransferMechanism()
+        # SLOPE - - - - - - - -
+
+        assert np.allclose(T.user_params["function_params"]["slope"], old_value)
+        assert np.allclose(T.function_object.slope, old_value)
+        assert np.allclose(T.function_object._slope, old_value)
+        assert np.allclose(T.mod_slope, old_value)
+
+        T.function_object.slope = new_value
+
+        assert np.allclose(T.user_params["function_params"]["slope"], new_value)
+        assert np.allclose(T.function_object.slope, new_value)
+        assert np.allclose(T.function_object._slope, new_value)
+        assert np.allclose(T.mod_slope, old_value)
+
+        # INTERCEPT - - - - - - - -
+
+        assert np.allclose(T.user_params["function_params"]["intercept"], old_value)
+        assert np.allclose(T.function_object.intercept, old_value)
+        assert np.allclose(T.function_object._intercept, old_value)
+        assert np.allclose(T.mod_intercept, old_value)
+
+        T.function_object.intercept = new_value
+
+        assert np.allclose(T.user_params["function_params"]["intercept"], new_value)
+        assert np.allclose(T.function_object.intercept, new_value)
+        assert np.allclose(T.function_object._intercept, new_value)
+        assert np.allclose(T.mod_intercept, old_value)
+
+        # SMOOTHING FACTOR - - - - - - - -
+
+        assert np.allclose(T.user_params["smoothing_factor"], old_value)
+        assert np.allclose(T.smoothing_factor, old_value)
+        assert np.allclose(T._smoothing_factor, old_value)
+        assert np.allclose(T.mod_smoothing_factor, old_value)
+
+        T.smoothing_factor = new_value
+
+        assert np.allclose(T.user_params["smoothing_factor"], new_value)
+        assert np.allclose(T.smoothing_factor, new_value)
+        assert np.allclose(T._smoothing_factor, new_value)
+        assert np.allclose(T.mod_smoothing_factor, old_value)
+
+        # NOISE - - - - - - - -
+
+        assert np.allclose(T.user_params["noise"], old_value)
+        assert np.allclose(T.noise, old_value)
+        assert np.allclose(T._noise, old_value)
+        assert np.allclose(T.mod_noise, old_value)
+
+        T.noise = new_value
+
+        assert np.allclose(T.user_params["noise"], new_value)
+        assert np.allclose(T.noise, new_value)
+        assert np.allclose(T._noise, new_value)
+        assert np.allclose(T.mod_noise, old_value)
+
+        T.execute(1.0)
+
+        assert np.allclose(T.user_params["function_params"]["slope"], new_value)
+        assert np.allclose(T.function_object.slope, new_value)
+        assert np.allclose(T.function_object._slope, new_value)
+        assert np.allclose(T.mod_slope, new_value)
+
+        assert np.allclose(T.user_params["function_params"]["intercept"], new_value)
+        assert np.allclose(T.function_object.intercept, new_value)
+        assert np.allclose(T.function_object._intercept, new_value)
+        assert np.allclose(T.mod_intercept, new_value)
+
+        assert np.allclose(T.user_params["smoothing_factor"], new_value)
+        assert np.allclose(T.smoothing_factor, new_value)
+        assert np.allclose(T._smoothing_factor, new_value)
+        assert np.allclose(T.mod_smoothing_factor, new_value)
+
+        assert np.allclose(T.user_params["noise"], new_value)
+        assert np.allclose(T.noise, new_value)
+        assert np.allclose(T._noise, new_value)
+        assert np.allclose(T.mod_noise, new_value)
+
