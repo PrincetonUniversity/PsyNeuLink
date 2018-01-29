4494e598265e74667b0ae33a4e1a5556ceb8c6fb
KristenManning <kristenmanning16@gmail.com>
2018-01-29 18:05:31 -0500

    adding pytests for possible combinations of variable and matrix specifications for LinearMatrix as a function of a mechanism

--- tests/mechanisms/test_processing_mechanism.py
+++ tests/mechanisms/test_processing_mechanism.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pytest
 from psyneulink.components.mechanisms.processing.processingmechanism import ProcessingMechanism
-from psyneulink.components.mechanisms.processing.integratormechanism import IntegratorMechanism
-from psyneulink.components.functions.function import Reduce, LinearCombination, CombineMeans, Linear, Exponential, Logistic, SoftMax, LinearMatrix, Integrator, SimpleIntegrator, ConstantIntegrator, AdaptiveIntegrator, DriftDiffusionIntegrator, OrnsteinUhlenbeckIntegrator, AccumulatorIntegrator, FHNIntegrator, AGTUtilityIntegrator, BogaczEtAl, NavarroAndFuss, NormalDist, UniformToNormalDist, ExponentialDist, UniformDist, GammaDist, WaldDist, Stability, Distance, Hebbian, Reinforcement, BackPropagation, TDLearning
+from psyneulink.components.functions.function import FunctionError, Reduce, LinearCombination, CombineMeans, Linear, Exponential, Logistic, SoftMax, LinearMatrix, Integrator, SimpleIntegrator, ConstantIntegrator, AdaptiveIntegrator, DriftDiffusionIntegrator, OrnsteinUhlenbeckIntegrator, AccumulatorIntegrator, FHNIntegrator, AGTUtilityIntegrator, BogaczEtAl, NavarroAndFuss, NormalDist, UniformToNormalDist, ExponentialDist, UniformDist, GammaDist, WaldDist, Stability, Distance, Hebbian, Reinforcement, BackPropagation, TDLearning
 
 class TestProcessingMechanismFunctions:
 
@@ -47,24 +46,6 @@ class TestProcessingMechanismFunctions:
         PM1 = ProcessingMechanism(function=SoftMax)
         PM1.execute(1.0)
         # assert np.allclose(PM1.value, 1.0)
-
-    def test_processing_mechanism_LinearMatrix_function(self):
-        # PM1 = ProcessingMechanism(function=LinearMatrix()
-        #                           )
-        # PM1.execute(1.0)
-        # assert np.allclose(PM1.value, 1.0)
-        PM2 = ProcessingMechanism(function=LinearMatrix(matrix=[4.0])
-                                  )
-        PM2.execute(1.0)
-        # assert np.allclose(PM1.value, 1.0)
-
-        PM3 = ProcessingMechanism(function=LinearMatrix(default_variable=np.atleast_2d([1.0, 2.0, 3.0]),
-                                                        matrix=[[1.0, 0.0, 0.0],
-                                                                [0.0, 2.0, 0.0],
-                                                                [0.0, 0.0, 3.0]]),
-                                  default_variable=np.atleast_2d([1.0, 2.0, 3.0]))
-
-        print(PM3.execute([1.0, 2.0, 3.0]))
 
     def test_processing_mechanism_SimpleIntegrator_function(self):
         PM1 = ProcessingMechanism(function=SimpleIntegrator)
@@ -178,3 +159,91 @@ class TestProcessingMechanismFunctions:
         PM1.execute([[1.0], [2.0], [3.0]])
         # assert np.allclose(PM1.value, 1.0)
 
+class TestLinearMatrixFunction:
+
+    def test_valid_matrix_specs(self):
+        # Note: default matrix specification is None
+
+        PM_default = ProcessingMechanism(function=LinearMatrix())
+        PM_default.execute(1.0)
+
+        assert np.allclose(PM_default.value, 1.0)
+
+        PM_default_len_2_var = ProcessingMechanism(function=LinearMatrix(default_variable=[[0.0, 0.0]]),
+                                                   default_variable=[[0.0, 0.0]])
+        PM_default_len_2_var.execute([[1.0, 2.0]])
+
+        assert np.allclose(PM_default_len_2_var.value, [[3., 3.]])
+
+        PM_default_2d_var = ProcessingMechanism(function=LinearMatrix(default_variable=[[0.0, 0.0],
+                                                                                        [0.0, 0.0],
+                                                                                        [0.0, 0.0]]),
+                                                   default_variable=[[0.0, 0.0],
+                                                                     [0.0, 0.0],
+                                                                     [0.0, 0.0]])
+        PM_default_2d_var.execute([[1.0, 0.0],
+                                   [0.0, 2.0],
+                                   [3.0, 0.0]])
+
+        assert np.allclose(PM_default_2d_var.value, [[1.0, 1.0],
+                                                     [2.0, 2.0],
+                                                     [3.0, 3.0]])
+
+        # PM_float = ProcessingMechanism(function=LinearMatrix(matrix=4.0))
+        # PM_float.execute(1.0)
+        #
+        # assert np.allclose(PM_float.value, 4.0)
+
+        PM_1d_list = ProcessingMechanism(function=LinearMatrix(matrix=[4.0]))
+        PM_1d_list.execute(1.0)
+
+        assert np.allclose(PM_1d_list.value, 4.0)
+
+        PM_2d_list = ProcessingMechanism(function=LinearMatrix(matrix=[[4.0, 5.0],
+                                                                       [6.0, 7.0],
+                                                                       [8.0, 9.0],
+                                                                       [10.0, 11.0]],
+                                                               default_variable=[[0.0, 0.0, 0.0, 0.0],
+                                                                                 [0.0, 0.0, 0.0, 0.0],
+                                                                                 [0.0, 0.0, 0.0, 0.0]]),
+                                         default_variable=[[0.0, 0.0, 0.0, 0.0],
+                                                           [0.0, 0.0, 0.0, 0.0],
+                                                           [0.0, 0.0, 0.0, 0.0]])
+        PM_2d_list.execute([[1.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 0.0, 1.0]])
+
+        assert np.allclose(PM_2d_list.value, [[4.0, 5.0],
+                                              [8.0, 9.0],
+                                              [10.0, 11.0]])
+
+        PM_1d_array = ProcessingMechanism(function=LinearMatrix(matrix=np.array([4.0])))
+        PM_1d_array.execute(1.0)
+
+        assert np.allclose(PM_1d_array.value, 4.0)
+
+        PM_2d_array = ProcessingMechanism(function=LinearMatrix(matrix=np.array([[4.0]])))
+        PM_2d_array.execute(1.0)
+
+        assert np.allclose(PM_2d_array.value, 4.0)
+
+        PM_matrix = ProcessingMechanism(function=LinearMatrix(matrix=np.matrix([[4.0]])))
+        PM_matrix.execute(1.0)
+
+        assert np.allclose(PM_matrix.value, 4.0)
+
+    def test_invalid_matrix_specs(self):
+        # Note: default matrix specification is None
+        with pytest.raises(FunctionError) as error_text:
+            PM_mismatched_float = ProcessingMechanism(function=LinearMatrix(default_variable=[[0.0, 0.0],
+                                                                                          [0.0, 0.0],
+                                                                                          [0.0, 0.0]],
+                                                                        matrix=[[1.0, 0.0, 0.0, 0.0],
+                                                                                [0.0, 2.0, 0.0, 0.0],
+                                                                                [0.0, 0.0, 3.0, 0.0],
+                                                                                [0.0, 0.0, 0.0, 4.0]]),
+                                                  default_variable=[[0.0, 0.0],
+                                                                    [0.0, 0.0],
+                                                                    [0.0, 0.0]])
+        assert "Specification of matrix and/or default_variable" in str(error_text.value) and \
+               "not compatible for multiplication" in str(error_text.value)
