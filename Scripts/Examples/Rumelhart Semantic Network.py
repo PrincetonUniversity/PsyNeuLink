9014c8a26255db8be01654f29383358d1be03f43
jdc@princeton.edu <jdc@princeton.edu>
2018-02-16 08:48:05 -0500

    • Learning   added test_rumelhart_semantic_network

--- Scripts/Examples/Rumelhart Semantic Network.py
+++ Scripts/Examples/Rumelhart Semantic Network.py
@@ -3,15 +3,15 @@ import numpy as np
 import typecheck as tc
 
 # Semantic Network:
-#
-#       REP PROP QUAL ACT
-#         \___\__/____/
-#             |
-#           HIDDEN
-#            / \
-#       HIDDEN REL_IN
-#          /
-#       REP_IN
+#                         _
+#       REP PROP QUAL ACT  |
+#         \___\__/____/    |
+#             |        _   | Output Processes
+#           HIDDEN      | _|
+#            / \        |
+#       HIDDEN REL_IN   |  Input Processes
+#          /            |
+#       REP_IN         _|
 
 print ("SCRATCH PAD:  Semantic Network")
 
@@ -24,6 +24,8 @@ prop_out = pnl.TransferMechanism(size=12, function=pnl.Logistic, name='PROP_OUT'
 qual_out = pnl.TransferMechanism(size=13, function=pnl.Logistic, name='QUAL_OUT')
 act_out = pnl.TransferMechanism(size=14, function=pnl.Logistic, name='ACT_OUT')
 
+
+# Version using sequential Processes
 rep_hidden_proc = pnl.Process(pathway=[rep_in, rep_hidden, rel_hidden],
                               learning=pnl.LEARNING,
                               name='REP_HIDDEN_PROC')
@@ -50,5 +52,16 @@ rel_act_proc = pnl.Process(pathway=[rel_hidden, act_out],
 
 sys = pnl.System(processes=[rep_hidden_proc, rel_hidden_proc, rel_rep_proc, rel_prop_proc, rel_qual_proc, rel_act_proc])
 assert True
+# sys.show_graph(show_learning=pnl.ALL, show_dimensions=True)
 
-sys.show_graph(show_learning=pnl.ALL, show_dimensions=True)
+# Alternate version using crossing Processes:
+
+
+rep_proc = pnl.Process(pathway=[rep_in, rep_hidden, rel_hidden, rep_out])
+rel_proc = pnl.Process(pathway=[rel_in, rel_hidden, prop_out])
+sys_alt = pnl.System(processes=[rep_proc, rel_proc, rel_qual_proc, rel_act_proc])
+
+sys_alt.show_graph(show_learning=pnl.ALL, show_dimensions=True)
+
+
+#
