f2cbbbd5229b2cbde784e67142201c77c73f5fe0
jdc@princeton.edu <jdc@princeton.edu>
2018-02-16 21:06:56 -0500

    -

--- Scripts/Examples/Rumelhart Semantic Network.py
+++ Scripts/Examples/Rumelhart Semantic Network.py
@@ -27,21 +27,21 @@ act_out = pnl.TransferMechanism(size=14, function=pnl.Logistic, name='ACT_OUT')
 
 # VERIFY: CONSTRUCTION WORKING, BUT LEARNING NEEDS TO BE VALIDATED
 # Version using sequential Processes  --------------------------------------------------------------------------
-# rep_hidden_proc = pnl.Process(pathway=[rep_in, rep_hidden, rel_hidden],
-#                               learning=pnl.LEARNING,
-#                               name='REP_HIDDEN_PROC')
-#
-# rel_hidden_proc = pnl.Process(pathway=[rel_in, rel_hidden],
-#                               learning=pnl.LEARNING,
-#                               name='REL_HIDDEN_PROC')
-#
-# rel_rep_proc = pnl.Process(pathway=[rel_hidden, rep_out],
-#                            learning=pnl.LEARNING,
-#                            name='REL_REP_PROC')
-#
-# rel_prop_proc = pnl.Process(pathway=[rel_hidden, prop_out],
-#                             learning=pnl.LEARNING,
-#                             name='REL_PROP_PROC')
+rep_hidden_proc = pnl.Process(pathway=[rep_in, rep_hidden, rel_hidden],
+                              learning=pnl.LEARNING,
+                              name='REP_HIDDEN_PROC')
+
+rel_hidden_proc = pnl.Process(pathway=[rel_in, rel_hidden],
+                              learning=pnl.LEARNING,
+                              name='REL_HIDDEN_PROC')
+
+rel_rep_proc = pnl.Process(pathway=[rel_hidden, rep_out],
+                           learning=pnl.LEARNING,
+                           name='REL_REP_PROC')
+
+rel_prop_proc = pnl.Process(pathway=[rel_hidden, prop_out],
+                            learning=pnl.LEARNING,
+                            name='REL_PROP_PROC')
 
 rel_qual_proc = pnl.Process(pathway=[rel_hidden, qual_out],
                             learning=pnl.LEARNING,
@@ -51,7 +51,7 @@ rel_act_proc = pnl.Process(pathway=[rel_hidden, act_out],
                            learning=pnl.LEARNING,
                            name='REL_ACT_PROC')
 
-# sys = pnl.System(processes=[rep_hidden_proc, rel_hidden_proc, rel_rep_proc, rel_prop_proc, rel_qual_proc, rel_act_proc])
+sys = pnl.System(processes=[rep_hidden_proc, rel_hidden_proc, rel_rep_proc, rel_prop_proc, rel_qual_proc, rel_act_proc])
 # sys.show_graph(show_learning=pnl.ALL, show_dimensions=True)
 
 # # ---------------------------------------------------------------------------------------------------------------
@@ -63,17 +63,50 @@ rel_act_proc = pnl.Process(pathway=[rel_hidden, act_out],
 # rel_proc = pnl.Process(pathway=[rel_in, rel_hidden],
 #                        learning=pnl.LEARNING,
 #                        name='REL_PROC')
-# sys_alt = pnl.System(processes=[rep_proc, rel_proc, rel_qual_proc, rel_act_proc])
-# sys_alt.show_graph(show_learning=pnl.ALL, show_dimensions=True)
+# sys = pnl.System(processes=[rep_proc, rel_proc, rel_qual_proc, rel_act_proc])
+# sys.show_graph(show_learning=pnl.ALL, show_dimensions=True)
 
-# ---------------------------------------------------------------------------------------------------------------
+# # ---------------------------------------------------------------------------------------------------------------
 # # VERIFY: CONSTRUCTION WORKING, BUT LEARNING NEEDS TO BE VALIDATED
-# Alternate version using crossing Processes:
-rep_proc = pnl.Process(pathway=[rep_in, rep_hidden, rel_hidden, rep_out],
-                       learning=pnl.LEARNING,
-                       name='REP_PROC')
-rel_proc = pnl.Process(pathway=[rel_in, rel_hidden, prop_out],
-                       learning=pnl.LEARNING,
-                       name='REL_PROC')
-sys_alt = pnl.System(processes=[rep_proc, rel_proc, rel_qual_proc, rel_act_proc])
-sys_alt.show_graph(show_learning=pnl.ALL, show_dimensions=True)
+# # Alternate version using crossing Processes:
+# rep_proc = pnl.Process(pathway=[rep_in, rep_hidden, rel_hidden, rep_out],
+#                        learning=pnl.LEARNING,
+#                        name='REP_PROC')
+# rel_proc = pnl.Process(pathway=[rel_in, rel_hidden, prop_out],
+#                        learning=pnl.LEARNING,
+#                        name='REL_PROC')
+# sys = pnl.System(processes=[rep_proc, rel_proc, rel_qual_proc, rel_act_proc])
+# # sys.show_graph(show_learning=pnl.ALL, show_dimensions=True)
+
+
+# STRUCTURE VALIDATION:
+
+def get_learning_mech(name):
+    return next(lm for lm in sys.learning_mechanisms if lm.name == name)
+
+REP_IN_to_REP_HIDDEN_LM = get_learning_mech('MappingProjection from REP_IN to REP_HIDDEN LearningMechanism')
+REP_HIDDEN_to_REL_HIDDEN_LM = get_learning_mech('MappingProjection from REP_HIDDEN to REL_HIDDEN LearningMechanism')
+REL_IN_to_REL_HIDDEN_LM = get_learning_mech('MappingProjection from REL_IN to REL_HIDDEN LearningMechanism')
+REL_HIDDEN_to_REP_OUT = get_learning_mech('MappingProjection from REL_HIDDEN to REP_OUT LearningMechanism')
+REL_HIDDEN_to_PROP_OUT = get_learning_mech('MappingProjection from REL_HIDDEN to PROP_OUT LearningMechanism')
+REL_HIDDEN_to_QUAL_OUT = get_learning_mech('MappingProjection from REL_HIDDEN to QUAL_OUT LearningMechanism')
+REL_HIDDEN_to_ACT_OUT = get_learning_mech('MappingProjection from REL_HIDDEN to ACT_OUT LearningMechanism')
+
+# Validate error_signal Projections for REP_IN to REP_HIDDEN
+# assert len(sys.mechanisms[5].input_states) == 3
+assert len(REP_IN_to_REP_HIDDEN_LM.input_states) == 3
+assert REP_IN_to_REP_HIDDEN_LM.input_states[2].path_afferents[0].sender.owner == REP_HIDDEN_to_REL_HIDDEN_LM
+
+# Validate error_signal Projections to LearningMechanisms for REP_HIDDEN_to REL_HIDDEN Projections
+assert len(REP_HIDDEN_to_REL_HIDDEN_LM.input_states) == 7
+assert REP_HIDDEN_to_REL_HIDDEN_LM.input_states[3].path_afferents[0].sender.owner == REL_HIDDEN_to_REP_OUT
+assert REP_HIDDEN_to_REL_HIDDEN_LM.input_states[4].path_afferents[0].sender.owner == REL_HIDDEN_to_PROP_OUT
+assert REP_HIDDEN_to_REL_HIDDEN_LM.input_states[5].path_afferents[0].sender.owner == REL_HIDDEN_to_QUAL_OUT
+assert REP_HIDDEN_to_REL_HIDDEN_LM.input_states[6].path_afferents[0].sender.owner == REL_HIDDEN_to_ACT_OUT
+
+# Validate error_signal Projections to LearningMechanisms for REL_IN to REL_HIDDEN Projections
+assert len(REL_IN_to_REL_HIDDEN_LM.input_states) == 7
+assert REL_IN_to_REL_HIDDEN_LM.input_states[3].path_afferents[0].sender.owner == REL_HIDDEN_to_REP_OUT
+assert REL_IN_to_REL_HIDDEN_LM.input_states[4].path_afferents[0].sender.owner == REL_HIDDEN_to_PROP_OUT
+assert REL_IN_to_REL_HIDDEN_LM.input_states[5].path_afferents[0].sender.owner == REL_HIDDEN_to_QUAL_OUT
+assert REL_IN_to_REL_HIDDEN_LM.input_states[6].path_afferents[0].sender.owner == REL_HIDDEN_to_ACT_OUT
--- tests/learning/test_rumelhart_semantic_network.py
+++ tests/learning/test_rumelhart_semantic_network.py
@@ -57,19 +57,33 @@ class TestRumelhartSemanticNetwork:
                                     rel_act_proc])
 
         # Structural validation:
+        def get_learning_mech(name):
+            return next(lm for lm in sys.learning_mechanisms if lm.name == name)
+
+        REP_IN_to_REP_HIDDEN_LM = get_learning_mech('MappingProjection from REP_IN to REP_HIDDEN LearningMechanism')
+        REP_HIDDEN_to_REL_HIDDEN_LM = get_learning_mech('MappingProjection from REP_HIDDEN to REL_HIDDEN LearningMechanism')
+        REL_IN_to_REL_HIDDEN_LM = get_learning_mech('MappingProjection from REL_IN to REL_HIDDEN LearningMechanism')
+        REL_HIDDEN_to_REP_OUT = get_learning_mech('MappingProjection from REL_HIDDEN to REP_OUT LearningMechanism')
+        REL_HIDDEN_to_PROP_OUT = get_learning_mech('MappingProjection from REL_HIDDEN to PROP_OUT LearningMechanism')
+        REL_HIDDEN_to_QUAL_OUT = get_learning_mech('MappingProjection from REL_HIDDEN to QUAL_OUT LearningMechanism')
+        REL_HIDDEN_to_ACT_OUT = get_learning_mech('MappingProjection from REL_HIDDEN to ACT_OUT LearningMechanism')
+
         # Validate error_signal Projections for REP_IN to REP_HIDDEN
-        assert len(sys.mechanisms[5].input_states) == 3
-        assert sys.mechanisms[5].input_states[2].path_afferents[0].sender.owner == sys.mechanisms[4]
+        # assert len(sys.mechanisms[5].input_states) == 3
+        assert len(REP_IN_to_REP_HIDDEN_LM.input_states) == 3
+        assert REP_IN_to_REP_HIDDEN_LM.input_states[2].path_afferents[0].sender.owner == REP_HIDDEN_to_REL_HIDDEN_LM
+
         # Validate error_signal Projections to LearningMechanisms for REP_HIDDEN_to REL_HIDDEN Projections
-        assert len(sys.mechanisms[4].input_states) == 7
-        assert sys.mechanisms[4].input_states[3].path_afferents[0].sender.owner == sys.mechanisms[11]
-        assert sys.mechanisms[4].input_states[4].path_afferents[0].sender.owner == sys.mechanisms[14]
-        assert sys.mechanisms[4].input_states[5].path_afferents[0].sender.owner == sys.mechanisms[17]
-        assert sys.mechanisms[4].input_states[6].path_afferents[0].sender.owner == sys.mechanisms[20]
+        assert len(REP_HIDDEN_to_REL_HIDDEN_LM.input_states) == 7
+        assert REP_HIDDEN_to_REL_HIDDEN_LM.input_states[3].path_afferents[0].sender.owner == REL_HIDDEN_to_REP_OUT
+        assert REP_HIDDEN_to_REL_HIDDEN_LM.input_states[4].path_afferents[0].sender.owner == REL_HIDDEN_to_PROP_OUT
+        assert REP_HIDDEN_to_REL_HIDDEN_LM.input_states[5].path_afferents[0].sender.owner == REL_HIDDEN_to_QUAL_OUT
+        assert REP_HIDDEN_to_REL_HIDDEN_LM.input_states[6].path_afferents[0].sender.owner == REL_HIDDEN_to_ACT_OUT
+
         # Validate error_signal Projections to LearningMechanisms for REL_IN to REL_HIDDEN Projections
-        assert len(sys.mechanisms[8].input_states) == 7
-        assert sys.mechanisms[8].input_states[3].path_afferents[0].sender.owner == sys.mechanisms[11]
-        assert sys.mechanisms[8].input_states[4].path_afferents[0].sender.owner == sys.mechanisms[14]
-        assert sys.mechanisms[8].input_states[5].path_afferents[0].sender.owner == sys.mechanisms[17]
-        assert sys.mechanisms[8].input_states[6].path_afferents[0].sender.owner == sys.mechanisms[20]
+        assert len(REL_IN_to_REL_HIDDEN_LM.input_states) == 7
+        assert REL_IN_to_REL_HIDDEN_LM.input_states[3].path_afferents[0].sender.owner == REL_HIDDEN_to_REP_OUT
+        assert REL_IN_to_REL_HIDDEN_LM.input_states[4].path_afferents[0].sender.owner == REL_HIDDEN_to_PROP_OUT
+        assert REL_IN_to_REL_HIDDEN_LM.input_states[5].path_afferents[0].sender.owner == REL_HIDDEN_to_QUAL_OUT
+        assert REL_IN_to_REL_HIDDEN_LM.input_states[6].path_afferents[0].sender.owner == REL_HIDDEN_to_ACT_OUT
 
