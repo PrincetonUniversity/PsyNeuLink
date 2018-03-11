75b013afde17da28a8c1c65ab033d7afbe84ec24
jdcpni <jdc@princeton.edu>
2018-03-11 14:53:13 -0400

    • Function (#717)
    Incorporate changes to LearingFunctions from old PR:
      - remove activation_function from constructors
      - docstring changes
      - other minor stuff?

--- psyneulink/components/mechanisms/adaptive/learning/learningauxilliary.py
+++ psyneulink/components/mechanisms/adaptive/learning/learningauxilliary.py
@@ -398,7 +398,7 @@ def _instantiate_learning_components(learning_projection, context=None):
         # FIX: GET AND PASS ANY PARAMS ASSIGNED IN LearningProjection.learning_function ARG:
         # FIX:     ACTIVATION FUNCTION AND/OR LEARNING RATE
         learning_function = Hebbian(default_variable=activation,
-                                    activation_function=lc.activation_mech_fct,
+                                    # activation_function=lc.activation_mech_fct,
                                     learning_rate=learning_rate)
 
         objective_mechanism = lc.activation_mech
@@ -417,7 +417,7 @@ def _instantiate_learning_components(learning_projection, context=None):
         # FIX: GET AND PASS ANY PARAMS ASSIGNED IN LearningProjection.learning_function ARG:
         # FIX:     ACTIVATION FUNCTION AND/OR LEARNING RATE
         learning_function = Reinforcement(default_variable=[activation_input, activation_output, error_signal],
-                                          activation_function=lc.activation_mech_fct,
+                                          # activation_function=lc.activation_mech_fct,
                                           learning_rate=learning_rate)
 
     elif learning_function.componentName is TDLEARNING_FUNCTION:
@@ -431,7 +431,7 @@ def _instantiate_learning_components(learning_projection, context=None):
         learning_function = TDLearning(default_variable=[activation_input,
                                                          activation_output,
                                                          error_signal],
-                                       activation_function=lc.activation_mech_fct,
+                                       # activation_function=lc.activation_mech_fct,
                                        learning_rate=learning_rate)
 
     # BACKPROPAGATION LEARNING FUNCTION
