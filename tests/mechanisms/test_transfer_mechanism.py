dc931cc4ebe86c55017ea59a9ed20273e516e083
Jan Vesely <jan.vesely@rutgers.edu>
2018-04-13 15:44:22 -0400

    tests, TransferMechanism: Enable multiple input output test for llvm
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/mechanisms/test_transfer_mechanism.py
+++ tests/mechanisms/test_transfer_mechanism.py
@@ -1034,7 +1034,7 @@ class TestTransferMechanismMultipleInputStates:
     @pytest.mark.mechanism
     @pytest.mark.transfer_mechanism
     @pytest.mark.mimo
-    @pytest.mark.parametrize('mode', ['Python'])
+    @pytest.mark.parametrize('mode', ['Python', 'LLVM'])
     @pytest.mark.benchmark(group="MIMO")
     def test_multiple_output_states_for_multiple_input_states(self, benchmark, mode):
         T = TransferMechanism(input_states=['a','b','c'])
