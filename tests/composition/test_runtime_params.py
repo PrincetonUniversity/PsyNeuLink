2b83aee07387f44681bf30b375f73e88a887bc68
Kristen Manning <kristenmanning16@gmail.com>
2018-05-15 16:55:14 -0400

    updating runtime params pytests to use new composition.results attr

--- tests/composition/test_runtime_params.py
+++ tests/composition/test_runtime_params.py
@@ -42,7 +42,8 @@ class TestRuntimeParams:
 
         # Runtime param used for noise
         # ONLY mechanism value should reflect runtime param -- attr should be changed back by the time we inspect it
-        C.run(inputs={T: 2.0}, runtime_params={T: {"noise": 10.0}})
+        C.run(inputs={T: 2.0},
+              runtime_params={T: {"noise": 10.0}})
         assert T.noise == 0.0
         assert T.parameter_states['noise'].value == 0.0
         assert T.value == 12.0
@@ -61,29 +62,23 @@ class TestRuntimeParams:
         # S = Scheduler(composition=C)
         C.add_mechanism(T)
 
-        results = []
-
-        def call_after_trial():
-
-            results.append(T.output_state.value)
-
         # Runtime param used for noise
         # ONLY mechanism value should reflect runtime param -- attr should be changed back by the time we inspect it
         C.run(inputs={T: 2.0},
               runtime_params={T: {"noise": (10.0, AtTrial(1))}},
               # scheduler_processing=S,
-              num_trials=4,
-              call_after_trial=call_after_trial)
+              num_trials=4)
 
         # Runtime param NOT used for noise
-        C.run(inputs={T: 2.0},
-              call_after_trial=call_after_trial)
+        C.run(inputs={T: 2.0})
 
-        assert np.allclose(results, [np.array([2.]),     # Trial 0 - condition not satisfied yet
-                                     np.array([12.]),    # Trial 1 - condition satisfied
-                                     np.array([2.]),     # Trial 2 - condition no longer satisfied (not sticky)
-                                     np.array([2.]),     # Trial 3 - condition no longer satisfied (not sticky)
-                                     np.array([2.])])    # New run (runtime param no longer applies)
+        print(C.results)
+
+        assert np.allclose(C.results, [np.array([[2.]]),     # Trial 0 - condition not satisfied yet
+                                       np.array([[12.]]),    # Trial 1 - condition satisfied
+                                       np.array([[2.]]),     # Trial 2 - condition no longer satisfied (not sticky)
+                                       np.array([[2.]]),     # Trial 3 - condition no longer satisfied (not sticky)
+                                       np.array([[2.]])])    # New run (runtime param no longer applies)
 
     def test_composition_run_with_sticky_condition(self):
 
@@ -95,28 +90,20 @@ class TestRuntimeParams:
         assert T.noise == 0.0
         assert T.parameter_states['noise'].value == 0.0
 
-        results = []
-
-        def call_after_trial():
-
-            results.append(T.output_state.value)
-
         # Runtime param used for noise
         # ONLY mechanism value should reflect runtime param -- attr should be changed back by the time we inspect it
         C.run(inputs={T: 2.0},
               runtime_params={T: {"noise": (10.0, AfterTrial(1))}},
-              num_trials=4,
-              call_after_trial=call_after_trial)
+              num_trials=4)
 
         # Runtime param NOT used for noise
-        C.run(inputs={T: 2.0},
-              call_after_trial=call_after_trial)
+        C.run(inputs={T: 2.0})
 
-        assert np.allclose(results, [np.array([2.]),      # Trial 0 - condition not satisfied yet
-                                     np.array([2.]),      # Trial 1 - condition not satisfied yet
-                                     np.array([12.]),     # Trial 2 - condition satisfied
-                                     np.array([12.]),     # Trial 3 - condition satisfied (sticky)
-                                     np.array([2.])])     # New run (runtime param no longer applies)
+        assert np.allclose(C.results, [np.array([[2.]]),      # Trial 0 - condition not satisfied yet
+                                       np.array([[2.]]),      # Trial 1 - condition not satisfied yet
+                                       np.array([[12.]]),     # Trial 2 - condition satisfied
+                                       np.array([[12.]]),     # Trial 3 - condition satisfied (sticky)
+                                       np.array([[2.]])])     # New run (runtime param no longer applies)
 
     def test_composition_run_with_combined_condition(self):
 
@@ -125,26 +112,18 @@ class TestRuntimeParams:
         C = Composition()
         C.add_mechanism(T)
 
-        results = []
-
-        def call_after_trial():
-
-            results.append(T.output_state.value)
-
         # Runtime param used for noise
         # ONLY mechanism value should reflect runtime param -- attr should be changed back by the time we inspect it
         C.run(inputs={T: 2.0},
               runtime_params={T: {"noise": (10.0, Any(AtTrial(1), AfterTrial(2)))}},
-              num_trials=5,
-              call_after_trial=call_after_trial)
+              num_trials=5)
 
         # Runtime param NOT used for noise
-        C.run(inputs={T: 2.0},
-              call_after_trial=call_after_trial)
+        C.run(inputs={T: 2.0})
 
-        assert np.allclose(results, [np.array([2.]),      # Trial 0 - NOT condition 0, NOT condition 1
-                                     np.array([12.]),     # Trial 1 - condition 0, NOT condition 1
-                                     np.array([2.]),      # Trial 2 - NOT condition 0, NOT condition 1
-                                     np.array([12.]),     # Trial 3 - NOT condition 0, condition 1
-                                     np.array([12.]),     # Trial 4 - NOT condition 0, condition 1
-                                     np.array([2.])])     # New run (runtime param no longer applies)
+        assert np.allclose(C.results,[np.array([[2.]]),      # Trial 0 - NOT condition 0, NOT condition 1
+                                      np.array([[12.]]),     # Trial 1 - condition 0, NOT condition 1
+                                      np.array([[2.]]),      # Trial 2 - NOT condition 0, NOT condition 1
+                                      np.array([[12.]]),     # Trial 3 - NOT condition 0, condition 1
+                                      np.array([[12.]]),     # Trial 4 - NOT condition 0, condition 1
+                                      np.array([[2.]])])     # New run (runtime param no longer applies)
