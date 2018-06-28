3649530cff48804f7f94b14c52d87d4c4df35a7c
jdcpni <jdc@princeton.edu>
2018-06-28 15:12:33 -0400

    Test/controlsignal/test modulation param (#840)
    * • NieuwenhuisModel:
      Added LC_process
    
    * • InputState
      _parse_state_specific_specs:
          cast projection_spec.state.value as np.array to get ndim & shape
    
    * • DDM
      SELECTED_INPUT_ARRAY lambda function variable:
          v[2] is sometimes 1d and sometimes 2d??
    
    * 1
    
    * -
    
    * • Process
      - _instantiate_pathway:
          fixed to allow ControlMechanism as solo Mechanism in Process
          (otherwise, as recently implemented, moved to end of Process)
    • ControlMechanism
      - _execute: implemented Linear as default function
    
    * • Process
      - _instantiate_pathway:
          fixed to allow ControlMechanism as solo Mechanism in Process
          (otherwise, as recently implemented, moved to end of Process)
    
    • ControlMechanism
      - _execute: implemented Linear as default function
    
    • Tests
      mechanisms/test_control_mechanisms: added test for OVERRIDE modulation
    
    * Merge branches 'devel' and 'test/controlsignal/test_modulation_param' of https://github.com/PrincetonUniversity/PsyNeuLink into test/controlsignal/test_modulation_param
    
    # Conflicts:
    #	.idea/runConfigurations/_RL_DDM.xml
    #	psyneulink/components/mechanisms/adaptive/control/controlmechanism.py

--- tests/mechanisms/test_input_state_spec.py
+++ tests/mechanisms/test_input_state_spec.py
@@ -1,4 +1,5 @@
 import numpy as np
+import psyneulink as pnl
 import pytest
 
 from psyneulink.components.functions.function import LinearCombination, Reduce
@@ -15,6 +16,21 @@ mismatches_specified_default_variable_error_text = 'not compatible with its spec
 mismatches_default_variable_format_error_text = 'is not compatible with its expected format'
 mismatches_size_error_text = 'not compatible with the default variable determined from size parameter'
 belongs_to_another_mechanism_error_text = 'that belongs to another Mechanism'
+
+
+@pytest.fixture(scope='module')
+def clear_registry():
+    # Clear Registry to have a stable reference for indexed suffixes of default names
+    from psyneulink.components.component import DeferredInitRegistry
+    from psyneulink.components.system import SystemRegistry
+    from psyneulink.components.process import ProcessRegistry
+    from psyneulink.components.mechanisms.mechanism import MechanismRegistry
+    from psyneulink.components.projections.projection import ProjectionRegistry
+    pnl.clear_registry(DeferredInitRegistry)
+    pnl.clear_registry(SystemRegistry)
+    pnl.clear_registry(ProcessRegistry)
+    pnl.clear_registry(MechanismRegistry)
+    pnl.clear_registry(ProjectionRegistry)
 
 
 class TestInputStateSpec:
