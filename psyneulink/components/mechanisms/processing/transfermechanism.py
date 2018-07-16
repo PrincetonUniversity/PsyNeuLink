modifying _get_instantaneous_function_input method of transfer mechanism which now needs to select the correct noise item and variable item when a normalizing function is used

psyneulink/components/mechanisms/processing/transfermechanism.py
@@ -963,8 +963,13 @@ class TransferMechanism(ProcessingMechanism_Base):
         super()._instantiate_output_states(context=context)
 
     def _get_instantaneous_function_input(self, function_variable, noise):
-
-        noise = self._try_execute_param(noise, function_variable)
+        if isinstance(self.function_object, NormalizingFunction):
+            if self._current_variable_index == 0:
+                self._current_noise = self._try_execute_param(noise, function_variable)
+            noise = self._current_noise[self._current_variable_index]
+            function_variable = function_variable[self._current_variable_index]
+        else:
+            noise = self._try_execute_param(noise, function_variable)
         if (np.array(noise) != 0).any():
             current_input = function_variable + noise
         else:
@@ -1065,7 +1070,9 @@ class TransferMechanism(ProcessingMechanism_Base):
                                                              runtime_params=runtime_params,
                                                              context=context)
                 value_item = self._clip_result(clip, value_item)
-                value.append(value_item)
+                # execute returns 2d even though we passed in 1d
+                # (we passed in one item of a 2d variable)
+                value.append(np.squeeze(value_item))
 
         else:
             value = super(Mechanism, self)._execute(variable=variable,
@@ -1091,7 +1098,6 @@ class TransferMechanism(ProcessingMechanism_Base):
                 self.previous_value = None
 
     def _parse_function_variable(self, variable, context=None):
-
         if context is ContextFlags.INSTANTIATE:
 
             return super(TransferMechanism, self)._parse_function_variable(variable=variable, context=context)