PrincetonUniversity/PsyNeuLink

commit c0408834cc8b7c18625e6b1f567626b52871c9c3

make most Functions assigned to Mechanisms auto convert value to 2D
	- uses standard Mechanism 2D conversion that considers embedded lists/arrays
	1D
	- disable conversions for some Functions that do not make sense to convert
		- Reinforcement
		- BackPropagation
	- modify tests due to random offset
	- Fixes #895

psyneulink/components/functions/function.py
@@ -754,7 +754,18 @@ class Function_Base(Function):
         # region Type conversion (specified by output_type):
         # Convert to 2D array, irrespective of value type:
         if output_type is FunctionOutputType.NP_2D_ARRAY:
-            value = np.atleast_2d(value)
+            # KDM 8/10/18: mimicking the conversion that Mechanism does to its values, because
+            # this is what we actually wanted this method for. Can be changed to pure 2D np array in
+            # future if necessary
+
+            converted_to_2d = np.atleast_2d(value)
+            # If return_value is a list of heterogenous elements, return as is
+            #     (satisfies requirement that return_value be an array of possibly multidimensional values)
+            if converted_to_2d.dtype == object:
+                pass
+            # Otherwise, return value converted to 2d np.array
+            else:
+                value = converted_to_2d
 
         # Convert to 1D array, irrespective of value type:
         # Note: if 2D array (or higher) has more than two items in the outer dimension, generate exception
@@ -10867,6 +10878,15 @@ class Reinforcement(LearningFunction):  # --------------------------------------
                          prefs=prefs,
                          context=ContextFlags.CONSTRUCTOR)
 
+    @property
+    def output_type(self):
+        return self._output_type
+
+    @output_type.setter
+    def output_type(self, value):
+        # disabled because it happens during normal execution, may be confusing
+        # warnings.warn('output_type conversion disabled for {0}'.format(self.__class__.__name__))
+        self._output_type = None
 
     def _validate_variable(self, variable, context=None):
         variable = self._update_variable(super()._validate_variable(variable, context))
@@ -10966,7 +10986,7 @@ class Reinforcement(LearningFunction):  # --------------------------------------
         # Construct weight change matrix with error term in proper element
         weight_change_matrix = np.diag(error_array)
 
-        return self.convert_output_type([error_array, error_array])
+        return [error_array, error_array]
 
 
 # Argument names:
@@ -11163,6 +11183,15 @@ class BackPropagation(LearningFunction):
                          prefs=prefs,
                          context=ContextFlags.CONSTRUCTOR)
 
+    @property
+    def output_type(self):
+        return self._output_type
+
+    @output_type.setter
+    def output_type(self, value):
+        # disabled because it happens during normal execution, may be confusing
+        # warnings.warn('output_type conversion disabled for {0}'.format(self.__class__.__name__))
+        self._output_type = None
 
     def _validate_variable(self, variable, context=None):
         variable = self._update_variable(super()._validate_variable(variable, context))
@@ -11365,7 +11394,7 @@ class BackPropagation(LearningFunction):
         # Weight changes = delta rule (learning rate * activity * error)
         weight_change_matrix = learning_rate * activation_input * dE_dW
 
-        return self.convert_output_type([weight_change_matrix, dE_dW])
+        return [weight_change_matrix, dE_dW]
 
 
 class TDLearning(Reinforcement):

psyneulink/components/mechanisms/mechanism.py
@@ -930,6 +930,7 @@ Class Reference
 
 import inspect
 import logging
+import warnings
 
 from collections import OrderedDict
 from inspect import isclass
@@ -938,7 +939,7 @@ import numpy as np
 import typecheck as tc
 
 from psyneulink.components.component import Component, function_type, method_type
-from psyneulink.components.functions.function import Linear
+from psyneulink.components.functions.function import FunctionOutputType, Linear
 from psyneulink.components.shellclasses import Function, Mechanism, Projection, State
 from psyneulink.components.states.inputstate import InputState, DEFER_VARIABLE_SPEC_TO_MECH_MSG
 from psyneulink.components.states.modulatorysignals.modulatorysignal import _is_modulatory_spec
@@ -1987,6 +1988,14 @@ class Mechanism_Base(Mechanism):
                        for input_state, default_exponent in zip(self.input_states, default_exponents)]
             self.function_object._exponents = exponents
 
+        # this may be removed when the restriction making all Mechanism values 2D np arrays is lifted
+        # ignore warnings of certain Functions that disable conversion
+        with warnings.catch_warnings():
+            warnings.simplefilter(action='ignore', category=UserWarning)
+            self.function_object.output_type = FunctionOutputType.NP_2D_ARRAY
+            self.function_object.enable_output_type_conversion = True
+        self.function_object._instantiate_value(context)
+
     def _instantiate_attributes_after_function(self, context=None):
 
         self._instantiate_output_states(context=context)
@@ -2779,7 +2788,7 @@ class Mechanism_Base(Mechanism):
             else:
                 x_range = [-10.0, 10.0]
         x_space = np.linspace(x_range[0],x_range[1])
-        plt.plot(x_space, self.function(x_space), lw=3.0, c='r')
+        plt.plot(x_space, self.function(x_space)[0], lw=3.0, c='r')
         plt.show()
 
     @tc.typecheck

tests/learning/test_reinforcement.py
@@ -88,27 +88,27 @@ def test_reinforcement():
 
     expected_output = [
         (input_layer.output_states.values, [np.array([1., 1., 1.])]),
-        (action_selection.output_states.values, [np.array([0.        , 3.38417298, 0.        ])]),
-        (pytest.helpers.expand_np_ndarray(mech_objective_action.output_states.values), pytest.helpers.expand_np_ndarray([np.array([6.61582702]), np.array(43.7691671006736)])),
+        (action_selection.output_states.values, [np.array([0.      , 0.      , 2.283625])]),
+        (pytest.helpers.expand_np_ndarray(mech_objective_action.output_states.values), pytest.helpers.expand_np_ndarray([np.array([7.716375]), np.array(59.542443140625004)])),
         (pytest.helpers.expand_np_ndarray(mech_learning_input_to_action.output_states.values), pytest.helpers.expand_np_ndarray([
-            [np.array([0.        , 0.33079135, 0.        ]), np.array([0.        , 0.33079135, 0.        ])]
+            [np.array([0.        , 0.        , 0.38581875]), np.array([0.        , 0.        , 0.38581875])]
         ])),
         (reward_prediction_weights.mod_matrix, np.array([
             [1.,         0.,         0.        ],
-            [0.,         3.71496434, 0.        ],
-            [0.,         0.,         2.283625  ]
+            [0.,         3.38417298, 0.        ],
+            [0.,         0.,         2.66944375],
         ])),
         (results, [
             [np.array([0., 1., 0.])],
             [np.array([0.  , 1.45, 0.  ])],
             [np.array([0.    , 1.8775, 0.    ])],
-            [np.array([0.      , 2.283625, 0.      ])],
             [np.array([0., 0., 1.])],
             [np.array([0.  , 0.  , 1.45])],
-            [np.array([0.        , 2.66944375, 0.        ])],
+            [np.array([0.      , 2.283625, 0.      ])],
             [np.array([0.    , 0.    , 1.8775])],
+            [np.array([0.        , 2.66944375, 0.        ])],
             [np.array([0.        , 3.03597156, 0.        ])],
-            [np.array([0.        , 3.38417298, 0.        ])]
+            [np.array([0.      , 0.      , 2.283625])]
         ]),
     ]
 

tests/mechanisms/test_ddm_mechanism.py
@@ -262,7 +262,7 @@ def test_DDM_noise_0_5():
 
     val = float(T.execute(stim)[0])
 
-    assert val == 9.308960184035778
+    assert val == 10.67181396275914
 
 # ------------------------------------------------------------------------------------------------
 # TEST 3
@@ -280,7 +280,7 @@ def test_DDM_noise_2_0():
         )
     )
     val = float(T.execute(stim)[0])
-    assert val == 8.617920368071555
+    assert val == 11.34362792551828
 
 # ------------------------------------------------------------------------------------------------
 

tests/mechanisms/test_integrator_mechanism.py
@@ -846,8 +846,8 @@ class TestIntegratorNoise:
 
         val2 = float(I.execute(0))
 
-        np.testing.assert_allclose(val, 11.867557990149967)
-        np.testing.assert_allclose(val2, 4.022722120123589)
+        np.testing.assert_allclose(val, 9.02272212012359)
+        np.testing.assert_allclose(val2, 5.950088417525589)
 
     @pytest.mark.mechanism
     @pytest.mark.integrator_mechanism
@@ -862,7 +862,7 @@ class TestIntegratorNoise:
 
         val = I.execute([10, 10, 10, 10])[0]
 
-        np.testing.assert_allclose(val, [10.14404357, 11.45427351, 10.76103773, 10.12167502])
+        np.testing.assert_allclose(val, [10.44386323, 10.33367433, 11.49407907, 9.79484174])
 
     @pytest.mark.mechanism
     @pytest.mark.integrator_mechanism
@@ -876,7 +876,7 @@ class TestIntegratorNoise:
 
         val = float(I.execute(10))
 
-        np.testing.assert_allclose(val, 1.8675579901499675)
+        np.testing.assert_allclose(val, -0.977277879876411)
 
     @pytest.mark.mechanism
     @pytest.mark.integrator_mechanism
@@ -890,7 +890,7 @@ class TestIntegratorNoise:
         )
 
         val = I.execute([10, 10, 10, 10])[0]
-        np.testing.assert_allclose(val, [0.14404357, 1.45427351, 0.76103773, 0.12167502])
+        np.testing.assert_allclose(val, [0.44386323, 0.33367433, 1.49407907, -0.20515826])
 
     @pytest.mark.mechanism
     @pytest.mark.integrator_mechanism
@@ -904,7 +904,7 @@ class TestIntegratorNoise:
 
         val = float(I.execute(10))
 
-        np.testing.assert_allclose(val, -0.977277879876411)
+        np.testing.assert_allclose(val, 0.9500884175255894)
 
     @pytest.mark.mechanism
     @pytest.mark.integrator_mechanism
@@ -919,7 +919,7 @@ class TestIntegratorNoise:
 
         val = I.execute([10, 10, 10, 10])[0]
 
-        np.testing.assert_allclose(val, [1.45427351, 0.76103773, 0.12167502, 0.44386323])
+        np.testing.assert_allclose(val, [0.33367433, 1.49407907, -0.20515826, 0.3130677])
 
     @pytest.mark.mechanism
     @pytest.mark.integrator_mechanism
@@ -933,7 +933,7 @@ class TestIntegratorNoise:
 
         val = float(I.execute(10))
 
-        np.testing.assert_allclose(val, 11.867557990149967)
+        np.testing.assert_allclose(val, 9.02272212012359)
 
     @pytest.mark.mechanism
     @pytest.mark.integrator_mechanism
@@ -948,7 +948,7 @@ class TestIntegratorNoise:
 
         val = I.execute([10, 10, 10, 10])[0]
 
-        np.testing.assert_allclose(val, [10.14404357, 11.45427351, 10.76103773, 10.12167502])
+        np.testing.assert_allclose(val, [10.44386323, 10.33367433, 11.49407907, 9.79484174])
 
     @pytest.mark.mechanism
     @pytest.mark.integrator_mechanism
@@ -961,7 +961,7 @@ class TestIntegratorNoise:
         )
 
         val = I.execute(10.0)
-        assert np.allclose(val, [[[15.01078952]], [[1.]]])
+        assert np.allclose(val, [[[14.17598662]], [[ 1.        ]]])
 
 # COMMENTED OUT UNTIL OU INTEGRATOR IS VALIDATED
     @pytest.mark.mechanism

tests/mechanisms/test_mechanisms.py
@@ -27,3 +27,16 @@ class TestMechanism:
 
         assert T.input_state.function_object.instance_defaults.variable == result_variable[0]
         assert T.input_state.function_object.instance_defaults.value == result_variable[0]
+
+    @pytest.mark.mechanism
+    @pytest.mark.parametrize(
+        'mechanism_type, default_variable, mechanism_value, function_value',
+        [
+            (pnl.ObjectiveMechanism, [0, 0, 0], np.array([[0, 0, 0]]), np.array([[0, 0, 0]]))
+        ]
+    )
+    def test_value_shapes(self, mechanism_type, default_variable, mechanism_value, function_value):
+        M = mechanism_type(default_variable=default_variable)
+
+        assert M.instance_defaults.value.shape == mechanism_value.shape
+        assert M.function_object.instance_defaults.value.shape == function_value.shape

commit 17b09bffe1e783917af0a3e3607c6cb1af56d3fb

Functions: remove conversion for Functions that return tuples
	- corrects problem with FHNIntegrator+LCControlMechanism introduced in
	c0408834cc
	- #895

psyneulink/components/functions/function.py
@@ -6338,6 +6338,16 @@ class DriftDiffusionIntegrator(Integrator):  # ---------------------------------
 
         self.has_initializers = True
 
+    @property
+    def output_type(self):
+        return self._output_type
+
+    @output_type.setter
+    def output_type(self, value):
+        # disabled because it happens during normal execution, may be confusing
+        # warnings.warn('output_type conversion disabled for {0}'.format(self.__class__.__name__))
+        self._output_type = None
+
     def _validate_noise(self, noise):
         if not isinstance(noise, float):
             raise FunctionError(
@@ -6402,7 +6412,7 @@ class DriftDiffusionIntegrator(Integrator):  # ---------------------------------
             if not np.isscalar(variable):
                 self.previous_time = np.broadcast_to(self.previous_time, variable.shape).copy()
 
-        return self.convert_output_type(self.previous_value), self.previous_time
+        return self.previous_value, self.previous_time
 
 class OrnsteinUhlenbeckIntegrator(Integrator):  # ----------------------------------------------------------------------
     """
@@ -6580,6 +6590,16 @@ class OrnsteinUhlenbeckIntegrator(Integrator):  # ------------------------------
                 "Invalid noise parameter for {}. OrnsteinUhlenbeckIntegrator requires noise parameter to be a float. "
                 "Noise parameter is used to construct the standard DDM noise distribution".format(self.name))
 
+    @property
+    def output_type(self):
+        return self._output_type
+
+    @output_type.setter
+    def output_type(self, value):
+        # disabled because it happens during normal execution, may be confusing
+        # warnings.warn('output_type conversion disabled for {0}'.format(self.__class__.__name__))
+        self._output_type = None
+
     def function(self,
                  variable=None,
                  params=None,
@@ -6635,8 +6655,8 @@ class OrnsteinUhlenbeckIntegrator(Integrator):  # ------------------------------
             if not np.isscalar(variable):
                 self.previous_time = np.broadcast_to(self.previous_time, variable.shape).copy()
 
+        return self.previous_value, self.previous_time
 
-        return self.convert_output_type(self.previous_value), self.previous_time
 
 class FHNIntegrator(Integrator):  # --------------------------------------------------------------------------------
     """
@@ -7097,6 +7117,16 @@ class FHNIntegrator(Integrator):  # --------------------------------------------
             prefs=prefs,
             context=ContextFlags.CONSTRUCTOR)
 
+    @property
+    def output_type(self):
+        return self._output_type
+
+    @output_type.setter
+    def output_type(self, value):
+        # disabled because it happens during normal execution, may be confusing
+        # warnings.warn('output_type conversion disabled for {0}'.format(self.__class__.__name__))
+        self._output_type = None
+
     def _validate_params(self, request_set, target_set=None, context=None):
         super()._validate_params(request_set=request_set,
                                  target_set=target_set,
@@ -7394,7 +7424,7 @@ class FHNIntegrator(Integrator):  # --------------------------------------------
             if not np.isscalar(variable):
                 self.previous_time = np.broadcast_to(self.previous_time, variable.shape).copy()
 
-        return self.convert_output_type(self.previous_v), self.convert_output_type(self.previous_w), self.previous_time
+        return self.previous_v, self.previous_w, self.previous_time
 
 class AccumulatorIntegrator(Integrator):  # ----------------------------------------------------------------------------
     """
@@ -8424,6 +8454,16 @@ class BogaczEtAl(IntegratorFunction):  # ---------------------------------------
                          prefs=prefs,
                          context=ContextFlags.CONSTRUCTOR)
 
+    @property
+    def output_type(self):
+        return self._output_type
+
+    @output_type.setter
+    def output_type(self, value):
+        # disabled because it happens during normal execution, may be confusing
+        # warnings.warn('output_type conversion disabled for {0}'.format(self.__class__.__name__))
+        self._output_type = None
+
     def function(self,
                  variable=None,
                  params=None,
@@ -8516,7 +8556,7 @@ class BogaczEtAl(IntegratorFunction):  # ---------------------------------------
             #    (i.e., reports p(upper) if drift is positive, and p(lower if drift is negative)
             er = (is_neg_drift == 1) * (1 - er) + (is_neg_drift == 0) * (er)
 
-        return self.convert_output_type(rt), self.convert_output_type(er)
+        return rt, er
 
     def derivative(self, output=None, input=None):
         """