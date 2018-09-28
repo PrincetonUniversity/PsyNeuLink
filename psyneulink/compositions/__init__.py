094915c7eec9dbe2c2e85d9f7d106cc81f881f99
dcw3 <dcw3@princeton.edu>
2018-09-28 15:24:19 -0400

    add autodiffcomposition to __init__.py

--- psyneulink/compositions/__init__.py
+++ psyneulink/compositions/__init__.py
@@ -5,7 +5,9 @@ from . import pathwaycomposition
 from .composition import *
 from .systemcomposition import *
 from .pathwaycomposition import *
+from .autodiffcomposition import *
 
 __all__ = list(composition.__all__)
 __all__.extend(systemcomposition.__all__)
 __all__.extend(pathwaycomposition.__all__)
+__all__.extend(autodiffcomposition.__all__)
