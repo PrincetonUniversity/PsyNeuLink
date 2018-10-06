93b4d9472f2627cf1dfcdab6b40b48ff2cc19729
Jan Vesely <jan.vesely@rutgers.edu>
2018-10-06 14:59:13 -0400

    llvm/helpers: Add support for EveryNCalls scheduling condition
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/llvm/helpers.py
+++ psyneulink/llvm/helpers.py
@@ -65,15 +65,35 @@ def load_extract_scalar_array_one(builder, ptr):
         val = builder.extract_value(val, [0])
     return val
 
-def generate_sched_condition(ctx, builder, condition, cond_ptr, comp_nodes):
+def __ts_compare(ctx, builder, ts1, ts2, comp):
+    assert comp == '<'
+    part_eq = []
+    part_cmp = []
 
-    from psyneulink.scheduling.condition import All, AllHaveRun, Always
+    for element in range(3):
+        a = builder.extract_value(ts1, element)
+        b = builder.extract_value(ts2, element)
+        part_eq.append(builder.icmp_unsigned('==', a, b))
+        part_cmp.append(builder.icmp_unsigned(comp, a, b))
+
+    trial = builder.and_(builder.not_(part_eq[0]), part_cmp[0])
+    run = builder.and_(part_eq[0],
+                       builder.and_(builder.not_(part_eq[1]), part_cmp[1]))
+    step = builder.and_(builder.and_(part_eq[0], part_eq[1]),
+                        part_cmp[2])
+
+    return step
+    return builder.or_(trial, builder.or_(run, step))
+
+def generate_sched_condition(ctx, builder, condition, cond_ptr, comp_nodes, node):
+
+    from psyneulink.scheduling.condition import All, AllHaveRun, Always, EveryNCalls
     if isinstance(condition, Always):
         return ir.IntType(1)(1)
     elif isinstance(condition, All):
         agg_cond = ir.IntType(1)(1)
         for cond in condition.args:
-            cond_res = generate_sched_condition(ctx, builder, cond, cond_ptr, comp_nodes)
+            cond_res = generate_sched_condition(ctx, builder, cond, cond_ptr, comp_nodes, node)
             agg_cond = builder.and_(agg_cond, cond_res)
         return agg_cond
     elif isinstance(condition, AllHaveRun):
@@ -85,6 +105,31 @@ def generate_sched_condition(ctx, builder, condition, cond_ptr, comp_nodes):
             node_runs = builder.load(node_runs_ptr)
             node_ran = builder.icmp_unsigned('>', node_runs, ctx.int32_ty(0))
             return builder.and_(run_cond, node_ran)
+    elif isinstance(condition, EveryNCalls):
+        target, count = condition.args
+
+        zero = ctx.int32_ty(0)
+        target_idx = ctx.int32_ty(comp_nodes.index(target))
+
+        array_ptr = builder.gep(cond_ptr, [zero, ctx.int32_ty(1)])
+        target_status = builder.load(builder.gep(array_ptr, [zero, target_idx]))
+
+        # Check number of runs
+        target_runs = builder.extract_value(target_status, 0, target.name + " runs")
+        ran = builder.icmp_unsigned('>', target_runs, zero)
+        remainder = builder.urem(target_runs, ctx.int32_ty(count))
+        divisible = builder.icmp_unsigned('==', remainder, zero)
+        completedNruns = builder.and_(ran, divisible)
+
+        # Check that we have not run yet
+        my_idx = ctx.int32_ty(comp_nodes.index(node))
+        my_time_stamp_ptr = builder.gep(array_ptr, [zero, my_idx, ctx.int32_ty(1)])
+        my_time_stamp = builder.load(my_time_stamp_ptr)
+        target_time_stamp = builder.extract_value(target_status, 1)
+        ran_after_me = __ts_compare(ctx, builder, my_time_stamp, target_time_stamp, '<')
+
+        # Return: target.calls % N == 0 AND me.last_time < target.last_time
+        return builder.and_(completedNruns, ran_after_me)
     else:
         print("ERROR: Unsupported scheduling condition: ", condition)
         assert False
