• LVOCControlMechanism   - _exeucte:  fixed bug in computation of previous cost

psyneulink/core/components/mechanisms/adaptive/control/controlmechanism.py
@@ -932,10 +932,12 @@ class ControlMechanism(AdaptiveMechanism_Base):
         control_signal.owner = self
 
         # Update control_signal_costs to accommodate instantiated Projection
-        try:
-            self.control_signal_costs = np.append(self.control_signal_costs, np.empty((1,1)),axis=0)
-        except AttributeError:
-            self.control_signal_costs = np.empty((1,1))
+        # MODIFIED 11/2/18 OLD:
+        # try:
+        #     self.control_signal_costs = np.append(self.control_signal_costs, np.empty((1,1)),axis=0)
+        # except AttributeError:
+        #     self.control_signal_costs = np.empty((1,1))
+        # MODIFIED 11/2/18 END
 
         # UPDATE output_states AND control_projections -------------------------------------------------------------
 
@@ -1196,9 +1198,16 @@ class ControlMechanism(AdaptiveMechanism_Base):
     #         return np.array([c.instance_defaults.variable for c in self.control_signals])
 
     @property
+    def allocation_policy(self):
+        return self.value
+
+    # MODIFIED 11/2/18 NEW:
+    @property
+    def costs(self):
+        return [c._compute_costs(c.variable) for c in self.control_signals]
+    # MODIFIED 11/2/18 END
+
+    @property
     def control_projections(self):
         return [projection for control_signal in self.control_signals for projection in control_signal.efferents]
 
-    @property
-    def allocation_policy(self):
-        return self.value

psyneulink/library/components/mechanisms/adaptive/control/lvoccontrolmechanism.py
@@ -704,14 +704,14 @@ class LVOCControlMechanism(OptimizationControlMechanism):
         if not self.current_execution_count:
             # Initialize prediction_vector and control_signals on first trial
             # Note:  initialize prediction_vector to 1's so that learning_function returns specified priors
-            self._previous_prediction_vector = np.full_like(self.prediction_vector.vector, 1)
+            self._previous_prediction_vector = np.full_like(self.prediction_vector.vector, 0)
             self.prediction_weights = self.learning_function.function([self._previous_prediction_vector, 0])
         else:
             # Update prediction_weights
-            previous_cost = np.sum(self._previous_prediction_vector[self.prediction_vector.idx[PV.COST.value]])
-            # costs are assigned as negative in prediction_vector.update, so add them here
-            # self.outcome is for previous trials, so use previous_cost
-            net_outcome = self.outcome + previous_cost
+            #     self.outcome is for previous trial, so use previous_cost
+            previous_cost = np.sum(self.costs)
+            net_outcome = self.outcome - previous_cost
+
             self.prediction_weights = self.learning_function.function([self._previous_prediction_vector, net_outcome])
 
             # Update prediction_vector with current feature_values and control_signals and store for next trial