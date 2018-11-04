557d8bab6100cb369ec210f030efb4bc79321a2e
Jan Vesely <jan.vesely@rutgers.edu>
2018-11-04 15:46:19 -0500

    llvm, LCControlMechanism: convert to array_ptr_loop
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/library/components/mechanisms/adaptive/control/agt/lccontrolmechanism.py
+++ psyneulink/library/components/mechanisms/adaptive/control/agt/lccontrolmechanism.py
@@ -845,9 +845,8 @@ class LCControlMechanism(ControlMechanism):
         vi = builder.gep(mf_out, [ctx.int32_ty(0), ctx.int32_ty(1)])
         vo = builder.gep(new_out, [ctx.int32_ty(0), ctx.int32_ty(0)])
 
-        vector_length = ctx.int32_ty(gain_ty.count)
         index = None
-        with pnlvm.helpers.for_loop_zero_inc(builder, vector_length, "LC_gain") as (builder, index):
+        with pnlvm.helpers.array_ptr_loop(builder, vi, "LC_gain") as (builder, index):
             in_ptr = builder.gep(vi, [ctx.int32_ty(0), index])
             val = builder.load(in_ptr);
             val = builder.fmul(val, scaling_factor)
