8b91b5249b272bd90718c16a3b4eb62736c4716f
Jan Vesely <jan.vesely@rutgers.edu>
2018-11-04 15:45:44 -0500

    llvm, function: Do not expand loop body arguments if we only pass them to body function
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/components/functions/function.py
+++ psyneulink/core/components/functions/function.py
@@ -2525,9 +2525,8 @@ class LinearCombination(
         kwargs = {"ctx": ctx, "vi": arg_in, "vo": arg_out, "params": params}
         inner = functools.partial(self.__gen_llvm_combine, **kwargs)
 
-        index = None
-        with helpers.array_ptr_loop(builder, arg_out, "linear") as (builder, index):
-            inner(builder, index)
+        with helpers.array_ptr_loop(builder, arg_out, "linear") as args:
+            inner(*args)
         return builder
 
     @property
@@ -3531,9 +3530,8 @@ class TransferFunction(Function_Base):
         inner = functools.partial(self._gen_llvm_transfer, **kwargs)
 
         assert arg_in.type.pointee.count == arg_out.type.pointee.count
-        index = None
-        with helpers.array_ptr_loop(builder, arg_in, "transfer_loop") as (builder, index):
-            inner(builder, index)
+        with helpers.array_ptr_loop(builder, arg_in, "transfer_loop") as args:
+            inner(*args)
 
         return builder
 
@@ -4721,9 +4719,8 @@ class SoftMax(TransferFunction):
         kwargs = {"ctx": ctx, "vi": arg_in, "vo": arg_out, "max_ptr": max_ptr, "gain": gain, "max_ind_ptr": max_ind_ptr, "exp_sum_ptr": exp_sum_ptr}
         inner = functools.partial(self.__gen_llvm_exp_sum_max, **kwargs)
 
-        index = None
-        with helpers.array_ptr_loop(builder, arg_in, "exp_sum_max") as (builder, index):
-            inner(builder, index)
+        with helpers.array_ptr_loop(builder, arg_in, "exp_sum_max") as args:
+            inner(*args)
 
         output_type = self.get_current_function_param(OUTPUT_TYPE)
         exp_sum = builder.load(exp_sum_ptr)
@@ -4733,8 +4730,8 @@ class SoftMax(TransferFunction):
         if output_type == ALL:
             kwargs = {"ctx": ctx, "vi": arg_in, "vo": arg_out, "gain": gain, "exp_sum": exp_sum}
             inner = functools.partial(self.__gen_llvm_exp_div, **kwargs)
-            with helpers.array_ptr_loop(builder, arg_in, "exp_div") as (builder, index):
-                inner(builder, index)
+            with helpers.array_ptr_loop(builder, arg_in, "exp_div") as args:
+                inner(*args)
         elif output_type == MAX_VAL:
             ptri = builder.gep(arg_in, [ctx.int32_ty(0), index])
             exp_f = ctx.module.declare_intrinsic("llvm.exp", [ctx.float_ty])
@@ -7232,9 +7229,8 @@ class AdaptiveIntegrator(Integrator):  # ---------------------------------------
 
         kwargs = {"ctx": ctx, "vi": arg_in, "vo": arg_out, "params": params, "state": context}
         inner = functools.partial(self.__gen_llvm_integrate, **kwargs)
-        index = None
-        with helpers.array_ptr_loop(builder, arg_in, "integrate") as (builder, index):
-            inner(builder, index)
+        with helpers.array_ptr_loop(builder, arg_in, "integrate") as args:
+            inner(*args)
 
         return builder
 
@@ -8692,9 +8688,8 @@ class FHNIntegrator(Integrator):  # --------------------------------------------
             raise FunctionError("Invalid integration method ({}) selected for {}".
                                 format(integration_method, self.name))
 
-        index = None
-        with helpers.array_ptr_loop(builder, arg_in, method + "_body") as (builder, index):
-            func(builder, index)
+        with helpers.array_ptr_loop(builder, arg_in, method + "_body") as args:
+            func(*args)
 
         # Save context
         result = builder.load(arg_out)
@@ -11642,9 +11637,8 @@ class Distance(ObjectiveFunction):
 
         input_length = arg_in.type.pointee.element.count
         vector_length = ctx.int32_ty(input_length)
-        index = None
-        with helpers.for_loop_zero_inc(builder, vector_length, self.metric) as (builder, index):
-            inner(builder, index)
+        with helpers.for_loop_zero_inc(builder, vector_length, self.metric) as args:
+            inner(*args)
 
         sqrt = ctx.module.declare_intrinsic("llvm.sqrt", [ctx.float_ty])
         fabs = ctx.module.declare_intrinsic("llvm.fabs", [ctx.float_ty])
