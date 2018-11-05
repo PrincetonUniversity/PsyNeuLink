ce0eaadeaea0644740648eaf28f1791dec4d7d26
KristenManning <kristenmanning16@gmail.com>
2018-11-05 11:41:11 -0500

    import error

--- psyneulink/core/components/mechanisms/adaptive/control/__init__.py
+++ psyneulink/core/components/mechanisms/adaptive/control/__init__.py
@@ -1,14 +1,11 @@
 from . import controlmechanism
 from . import defaultcontrolmechanism
 from . import optimizationcontrolmechanism
-from . import modelbasedoptimizationcontrolmechanism
 
 from .controlmechanism import *
 from .defaultcontrolmechanism import *
-from . import optimizationcontrolmechanism
-from . import modelbasedoptimizationcontrolmechanism
+from .optimizationcontrolmechanism import *
 
 __all__ = list(controlmechanism.__all__)
 __all__.extend(defaultcontrolmechanism.__all__)
 __all__.extend(optimizationcontrolmechanism.__all__)
-__all__.extend(modelbasedoptimizationcontrolmechanism.__all__)
