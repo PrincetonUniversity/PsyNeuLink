81d01c4a5838042875d21c2b1a2257347b8d10f9
KristenManning <kristenmanning16@gmail.com>
2018-11-07 16:59:02 -0500

    removed 'controller.py' now that MBOCM replaced controller

--- psyneulink/library/components/mechanisms/adaptive/control/evc/__init__.py
+++ psyneulink/library/components/mechanisms/adaptive/control/evc/__init__.py
@@ -1,11 +1,8 @@
 from . import evcauxiliary
 from . import evccontrolmechanism
-from . import evccontroller
 
 from .evcauxiliary import *
 from .evccontrolmechanism import *
-from .evccontroller import *
 
 __all__ = list(evcauxiliary.__all__)
 __all__.extend(evccontrolmechanism.__all__)
-__all__.extend(evccontroller.__all__)
