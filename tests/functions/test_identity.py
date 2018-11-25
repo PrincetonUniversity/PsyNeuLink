950b5f16bd8b62b3dc25013453a6c55e98b268bb
Jan Vesely <jan.vesely@rutgers.edu>
2018-11-25 02:27:21 -0500

    llvm, tests/functions: Test CUDA execution of the Identity function
    Fix type in name file.
    Reuse execution structure in llvm test.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/functions/test_identity.py
+++ tests/functions/test_identity.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pytest
+
 import psyneulink.core.components.functions.function as Function
 import psyneulink.core.globals.keywords as kw
-import pytest
+import psyneulink.core.llvm as pnlvm
 
 @pytest.mark.function
 @pytest.mark.identity_function
@@ -21,5 +23,19 @@ def test_basic(size, benchmark):
 def test_llvm(size, benchmark):
     variable = np.random.rand(size)
     f = Function.Identity(default_variable=variable)
-    res = benchmark(f.bin_function, variable)
+    m = pnlvm.execution.FuncExecution(f, None)
+    res = benchmark(m.execute, variable)
+    assert np.allclose(res, variable)
+
+@pytest.mark.llvm
+@pytest.mark.function
+@pytest.mark.identity_function
+@pytest.mark.parametrize("size", [1, 2, 4, 8, 16])
+@pytest.mark.benchmark
+@pytest.mark.skipif(not pnlvm.ptx_enabled, reason="PTX engine not enabled/available")
+def test_ptx_cuda(size, benchmark):
+    variable = np.random.rand(size)
+    f = Function.Identity(default_variable=variable)
+    m = pnlvm.execution.FuncExecution(f, None)
+    res = benchmark(m.cuda_execute, variable)
     assert np.allclose(res, variable)
