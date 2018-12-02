c0cf4379f803d1451d2ae20a455693398c8312da
Jan Vesely <jan.vesely@rutgers.edu>
2018-12-01 20:45:12 -0500

    llvm, tests/mechanisms/ObjectiveMechanism: Add CUDA/PTX test
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/mechanisms/test_objective_mechanism.py
+++ tests/mechanisms/test_objective_mechanism.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+import psyneulink.core.llvm as pnlvm
 from psyneulink.core.components.mechanisms.processing.objectivemechanism import ObjectiveMechanism
 
 VECTOR_SIZE=4
@@ -11,12 +12,22 @@ class TestObjectiveMechanism:
     @pytest.mark.mechanism
     @pytest.mark.objective_mechanism
     @pytest.mark.benchmark(group="ObjectiveMechanism")
-    @pytest.mark.parametrize("mode", ['Python', 'LLVM'])
+    @pytest.mark.parametrize('mode', ['Python',
+                                      pytest.param('LLVM', marks=[pytest.mark.llvm]),
+                                      pytest.param('PTX', marks=[pytest.mark.cuda, pytest.mark.skipif(not pnlvm.ptx_enabled, reason="PTX engine not enabled/available")])])
     def test_objective_mech_inputs_list_of_ints(self, benchmark, mode):
 
         O = ObjectiveMechanism(
             name='O',
             default_variable=[0 for i in range(VECTOR_SIZE)],
         )
-        val = benchmark(O.execute, [10.0 for i in range(VECTOR_SIZE)], bin_execute=(mode == 'LLVM'))
+        if mode == 'Python':
+            val = benchmark(O.execute, [10.0 for i in range(VECTOR_SIZE)])
+        elif mode == 'LLVM':
+            e = pnlvm.execution.MechExecution(O, None)
+            val =  benchmark(e.execute, [10.0 for i in range(VECTOR_SIZE)])
+        elif mode == 'PTX':
+            e = pnlvm.execution.MechExecution(O, None)
+            val =  benchmark(e.cuda_execute, [10.0 for i in range(VECTOR_SIZE)])
+
         assert np.allclose(val, [[10.0 for i in range(VECTOR_SIZE)]])
