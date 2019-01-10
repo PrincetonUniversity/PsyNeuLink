0af00ec1e0337d1870018206a6a2f43c5e44c84b
Jan Vesely <jan.vesely@rutgers.edu>
2019-01-09 20:24:51 -0500

    llvm, composition: Clarify comment about nested running nested compositions
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/compositions/composition.py
+++ psyneulink/core/compositions/composition.py
@@ -3011,7 +3011,7 @@ class Composition(Composition_Base, metaclass=ComponentsMeta):
                         __execution.cuda_execute(inputs)
                         return __execution.extract_node_output(self.output_CIM)
 
-                # Filter out mechanisms. Nested nodes are not executed in this mode
+                # Filter out mechanisms. Nested compositions are not executed in this mode
                 mechanisms = [n for n in self._all_nodes if isinstance(n, Mechanism)]
                 # Generate all mechanism wrappers
                 for m in mechanisms:
