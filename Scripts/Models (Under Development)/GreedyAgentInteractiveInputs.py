ec663507fc97b2e263fbe993298176ae18428cef
KristenManning <kristenmanning16@gmail.com>
2019-01-23 17:06:49 -0500

    clean up interactive inputs version of greedy agent script

--- Scripts/Models (Under Development)/GreedyAgentInteractiveInputs.py
+++ Scripts/Models (Under Development)/GreedyAgentInteractiveInputs.py
@@ -1,12 +1,9 @@
-import timeit
 import numpy as np
 from psyneulink import *
-
 from gym_forager.envs.forager_env import ForagerEnv
 
 # Runtime Switches:
-PNL=True
-# RENDER = True
+RENDER = True
 PERCEPT_DISTORT = False
 PNL_COMPILE = False
 
@@ -17,15 +14,14 @@ PNL_COMPILE = False
 # These should probably be replaced by reference to ForagerEnv constants:
 obs_len = 2
 action_len = 2
-player_coord_idx = slice(0,2)
-predator_coord_idx = slice(3,5)
-prey_coord_idx = slice(6,8)
+player_coord_idx = slice(0, 2)
+predator_coord_idx = slice(3, 5)
+prey_coord_idx = slice(6, 8)
 player_value_idx = 2
 predator_value_idx = 5
 prey_value_idx = 8
 
 player_len = prey_len = predator_len = obs_len
-
 
 # *********************************************************************************************************************
 # **************************************  MECHANISMS AND COMPOSITION  *************************************************
@@ -44,43 +40,43 @@ reward = TransferMechanism(name="REWARD")
 
 # Use ComparatorMechanism to compute direction of action as difference of coordinates between player and prey:
 # note: unitization is done in main loop, to allow compilation of LinearCombination function) (TBI)
-greedy_action_mech = ComparatorMechanism(name='MOTOR OUTPUT',sample=player,target=prey)
+greedy_action_mech = ComparatorMechanism(name='MOTOR OUTPUT', sample=player, target=prey)
 
 agent_comp = Composition(name='PREDATOR-PREY COMPOSITION')
 agent_comp.add_c_node(player)
 agent_comp.add_c_node(prey)
 agent_comp.add_c_node(greedy_action_mech)
 
-# agent_comp.show_graph()
+agent_comp.env = ForagerEnv() # NEW: ForagerEnv must be stored in an attribute on the Composition
 
-
-# *********************************************************************************************************************
-# ******************************************   RUN SIMULATION  ********************************************************
-# *********************************************************************************************************************
-
-# num_trials = 4
-agent_comp.env = ForagerEnv()
-first_observation = agent_comp.env.reset()
-def get_next_input(env, result):
-    action = np.where(0. in result[0], 0, result[0] / np.abs(result[0]))
-    observation = env.step(action)[0]
-    # print("observation = ", observation)
-    return {player: [observation[player_coord_idx]],
-            prey: [observation[prey_coord_idx]]}
 def main():
-    reward = 0
-    done = False
-    steps = 100
-    start_time = timeit.default_timer()
-    run_results = agent_comp.run(inputs=get_next_input,
-                                 num_trials=steps,
-                                 # bin_execute=BIN_EXECUTE
-                                 )
-    stop_time = timeit.default_timer()
-    total_time = stop_time -start_time
-    print("Total Time = ", total_time)
-    print(total_time/steps, " seconds per step")
-    print(steps/total_time, " steps per second")
+
+    # NEW: get_next_input interactively returns a new input from the ForagerEnv
+    #      (rather than specifying a pre-determined list of input values)
+    def get_next_input(env, result):
+        action = np.where(result[0] == 0, 0, result[0] / np.abs(result[0]))
+        env_step = env.step(action)
+        observation = env_step[0]
+        done = env_step[2]
+        if not done:
+            # NEW: This function MUST return a dictionary of input values for a single trial for each INPUT node
+            return {player: [observation[player_coord_idx]],
+                    prey: [observation[prey_coord_idx]]}
+        return done
+
+    if RENDER:
+        agent_comp.env.render()
+
+    BIN_EXECUTE = 'Python'
+    if PNL_COMPILE:
+        BIN_EXECUTE = 'LLVM'
+
+    max_steps = 100 # maximum number of steps before agent quits
+    num_games = 3
+    for i in range(3):
+        agent_comp.run(inputs=get_next_input,     # specify the NAME of a fn in lieu of an inputs dict
+                       num_trials=max_steps,      # maximum number of steps before agent quits
+                       bin_execute=BIN_EXECUTE)
 
 if __name__ == "__main__":
     main()
