5a65be1d6756505bbbff7bb47db49bcc09ce045c
Jan Vesely <jan.vesely@rutgers.edu>
2019-02-05 12:09:34 -0500

    llvm, component: Store direct reference to llvmlite Function object.
    This will be used to avoid unnecessary lookups.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/components/component.py
+++ psyneulink/core/components/component.py
@@ -917,7 +917,7 @@ class Component(object, metaclass=ComponentsMeta):
 
     _deepcopy_shared_keys = frozenset([
         'init_args',
-        '_Component__llvm_function_name',
+        '_Component__llvm_function',
         '_Component__llvm_bin_function',
     ])
 
@@ -1156,16 +1156,16 @@ class Component(object, metaclass=ComponentsMeta):
             self.function.context.initialization_status = ContextFlags.INITIALIZED
         # MODIFIED 12/4/18 END
 
-        self.__llvm_function_name = None
+        self.__llvm_function = None
         self.__llvm_bin_function = None
         self._compilation_data = self._CompilationData(owner=self)
 
     @property
     def _llvm_symbol_name(self):
-        if self.__llvm_function_name is None:
-            self.__llvm_function_name = self._gen_llvm_function()
+        if self.__llvm_function is None:
+            self.__llvm_function = self._gen_llvm_function()
             self.__llvm_bin_function = None
-        return self.__llvm_function_name
+        return self.__llvm_function.name
 
     @property
     def _llvmBinFunction(self):
@@ -1174,7 +1174,6 @@ class Component(object, metaclass=ComponentsMeta):
         return self.__llvm_bin_function
 
     def _gen_llvm_function(self):
-        func_name = None
         llvm_func = None
         with pnlvm.LLVMBuilderContext() as ctx:
             func_ty = pnlvm.ir.FunctionType(pnlvm.ir.VoidType(),
@@ -1201,8 +1200,7 @@ class Component(object, metaclass=ComponentsMeta):
 
             builder.ret_void()
 
-        return func_name
-
+        return llvm_func
 
     def __repr__(self):
         return '({0} {1})'.format(type(self).__name__, self.name)
@@ -1211,7 +1209,7 @@ class Component(object, metaclass=ComponentsMeta):
     def __deepcopy__(self, memo):
         fun = get_deepcopy_with_shared_Components(self._deepcopy_shared_keys)
         newone = fun(self, memo)
-        newone.__dict__['_Component__llvm_function_name'] = None
+        newone.__dict__['_Component__llvm_function'] = None
         newone.__dict__['_Component__llvm_bin_function'] = None
 
         if newone.parameters is not newone.class_parameters:
