8d4959d4205d58254697d2de9996e338c1870489
KristenManning <kristenmanning16@gmail.com>
2019-02-13 14:08:42 -0500

    add pytest for hebbian learning

--- tests/composition/test_learning.py
+++ tests/composition/test_learning.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class TestHebbian:
 
-    def test_simple_course_example(self):
+    def test_simple_hebbian(self):
         Hebb_C = pnl.Composition()
         size = 9
 
@@ -11,40 +11,19 @@ class TestHebbian:
             size=size,
             function=pnl.Linear,
             enable_learning=True,
+            hetero=0.,
+            auto=0.,
             name='Hebb2',
         )
 
         Hebb_C.add_node(Hebb2)
-        def print_info():
-            print('\nWeight matrix:\n', Hebb2.matrix, '\nActivity: ', Hebb2.value, "\n")
 
         src = [1, 0, 0, 1, 0, 0, 1, 0, 0]
 
         inputs_dict = {Hebb2: np.array(src)}
 
         Hebb_C.run(num_trials=5,
-                   call_after_trial=print_info,
                    inputs=inputs_dict)
+        activity = Hebb2.value
 
-    def test_simple_course_example_old(self):
-        size = 9
-
-        Hebb2 = pnl.RecurrentTransferMechanism(
-            size=size,
-            function=pnl.Linear,
-            enable_learning=True,
-            name='Hebb2',
-        )
-        proc = pnl.Process(pathway=[Hebb2])
-        Hebb_C = pnl.System(processes=[proc])
-
-        def print_info():
-            print('\nWeight matrix:\n', Hebb2.matrix, '\nActivity: ', Hebb2.value, "\n")
-            # pprint(Hebb2.recurrent_projection.mod_matrix)
-
-        src = [1, 0, 0, 1, 0, 0, 1, 0, 0]
-
-        inputs_dict = {Hebb2: np.array(src)}
-        Hebb_C.run(num_trials=5,
-                   call_after_trial=print_info,
-                   inputs=inputs_dict)
+        assert np.allclose(activity, [[1.86643089, 0., 0., 1.86643089, 0., 0., 1.86643089, 0., 0.]])
