f36c1b6b4bd3b0eba04291da8213216a3c240bc4
jdcpni <jdc@princeton.edu>
2019-02-23 14:01:26 -0500

    Feat/models/ddqn new (#1099)
    * -
    
    * -
    
    * -
    
    * • Predator-Prey Model DQN - added printouts and modified costs
    
    * • Predator-Prey Model DQN
      more mods to script
    
    * -
    
    * • Predator-Prey Model DQN:
      - refactored for simpler handling of optimal_action
      - changed objective_function metric to DIFFERENCE
    
    * • Predator-Prey Model DQN:
      - refactored for simpler handling of optimal_action
      - changed objective_function metric to DIFFERENCE
    
    * • Predator-Prey Model DQN:
      - refactored for simpler handling of optimal_action
      - changed objective_function metric to DIFFERENCE
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * • Predator-Prey Model DQN
      - fixed bug in which frame buffer was getting updated during sims
      - added constants for control signal parameters
      - added switch for optimal vs. agent action
      - added verbosity levels for printout
    
    * • Predator-Prey Model DQN
      - fixed bug in which frame buffer was getting updated during sims
      - added constants for control signal parameters
      - added switch for optimal vs. agent action
      - added verbosity levels for printout
    
    * -
    
    * -

--- Scripts/Models (Under Development)/Predator-Prey Model DQN.py
+++ Scripts/Models (Under Development)/Predator-Prey Model DQN.py
@@ -1,25 +1,39 @@
 import timeit
 import numpy as np
 from psyneulink import *
-
 from double_dqn import DoubleDQNAgent
-
-from gym_forager.envs.forager_env import ForagerEnv
 
 # *********************************************************************************************************************
 # *********************************************** CONSTANTS ***********************************************************
 # *********************************************************************************************************************
 
-# Runtime Switches:
+# Runtime switches:
 MPI_IMPLEMENTATION = True
-RENDER = False
+RENDER = True
 PNL_COMPILE = False
 RUN = True
 SHOW_GRAPH = False
-# MODEL_PATH = '/Users/jdc/Dropbox (Princeton)/Documents (DropBox)/Python/double-dqn/models/trained_models/policy_net_trained_0.99_20190214-1651.pt'
 MODEL_PATH = '../../../double-dqn/models/trained_models/policy_net_trained_0.99_20190214-1651.pt'
 
-# These should probably be replaced by reference to ForagerEnv constants:
+# Switch for determining actual action taken in each step
+OPTIMAL_ACTION = 'OPTIMAL_ACTION'
+AGENT_ACTION = 'AGENT_ACTION'
+ACTION = AGENT_ACTION
+
+# Verbosity levels for console printout
+ACTION_REPORTING = 3
+SIMULATION_REPORTING = 2
+STANDARD_REPORTING = 1
+VERBOSE = STANDARD_REPORTING
+
+# ControlSignal parameters
+COST_RATE = -.05
+COST_BIAS = -3
+ALLOCATION_SAMPLES = [0, 500]
+
+
+# Environment coordinates
+# (these should probably be replaced by reference to ForagerEnv constants)
 obs_len = 2
 obs_coords = 2
 action_len = 2
@@ -41,11 +55,6 @@ prey_coord_slice = slice(prey_obs_start_idx,prey_value_idx)
 
 player_len = prey_len = predator_len = obs_coords
 
-cost_rate = -.05
-cost_bias = -3
-min_alloc = 0
-max_alloc = 500
-
 
 # **********************************************************************************************************************
 # **************************************  CREATE COMPOSITION ***********************************************************
@@ -56,41 +65,60 @@ max_alloc = 500
 # ddqn_agent = DoubleDQNAgent(env=env, model_load_path='', eval_mode=True)
 ddqn_agent = DoubleDQNAgent(model_load_path=MODEL_PATH,
                             eval_mode=True,
-                            render=False
+                            # render=False
                             )
-perceptual_state = None
-veridical_state = None
 
 def new_episode():
-    global perceptual_state
-    global veridical_state
-    observation = ddqn_agent.env.reset()
-    g=GaussianDistort()
-    perceptual_state = ddqn_agent.buffer.next(g(observation), is_new_episode=True)
-    veridical_state = ddqn_agent.buffer.next(observation, is_new_episode=True)
+    # Start new episode with veridical state
+
+    initial_observation = ddqn_agent.env.reset()
+
+    # Initialize both states to verdical state based on first observation
+    perceptual_state = veridical_state = ddqn_agent.buffer.next(initial_observation, is_new_episode=True)
 
 def get_optimal_action(observation):
     # Get new state based on observation:
     veridical_state = ddqn_agent.buffer.next(np.array(observation))
     optimal_action = np.array(ddqn_agent._io_map(ddqn_agent._select_action(veridical_state).item()))
+    if VERBOSE >= ACTION_REPORTING:
+        print(f'\n\nOPTIMAL OBSERVATION: {observation}'
+              f'\nVERIDICAL STATE: {veridical_state.reshape(12,)}'
+              f'\nOPTIMAL ACTION: {optimal_action}')
     return optimal_action
 
 # **************************************  PROCESSING MECHANISMS ********************************************************
 
 # Perceptual Mechanisms
-player_percept = ProcessingMechanism(size=prey_len, function=GaussianDistort, name="PLAYER PERCEPT")
-predator_percept = ProcessingMechanism(size=predator_len, function=GaussianDistort, name="PREDATOR PERCEPT")
-prey_percept = ProcessingMechanism(size=prey_len, function=GaussianDistort, name="PREY PERCEPT")
+player_percept = ProcessingMechanism(size=prey_len, function=GaussianDistort(), name="PLAYER PERCEPT")
+predator_percept = ProcessingMechanism(size=predator_len, function=GaussianDistort(), name="PREDATOR PERCEPT")
+prey_percept = ProcessingMechanism(size=prey_len, function=GaussianDistort(), name="PREY PERCEPT")
 
+# Mechanism used to encode optimal action from call to Run
 optimal_action_mech = ProcessingMechanism(size=action_len, name="OPTIMAL ACTION")
 
+actual_agent_frame_buffer = None
+
 def get_action(variable=[[0,0],[0,0],[0,0]]):
-    global perceptual_state
+    global actual_agent_frame_buffer
     # Convert variable to observation:
-    observation = np.array(list(variable[0]) + list(variable[1]) + list(variable[2]))
-    # Get new state based on observation:
+    observation = variable.reshape(6,)
+
+    # Get new state
+    # - first cache initial state of buffer
+    buffer_cache = ddqn_agent.buffer.buffer.copy()
+    # - then get new state based on current observation
     perceptual_state = ddqn_agent.buffer.next(observation)
+    # Save frame buffer in case needed to restore buffer to state following perceptual observation
+    actual_agent_frame_buffer = ddqn_agent.buffer.buffer
+    # - finally, restore frame buffer to initial state for use by next simulation or actual action
+    ddqn_agent.buffer.buffer = buffer_cache
+
+    # Get and return action
     action = np.array(ddqn_agent._io_map(ddqn_agent._select_action(perceptual_state).item()))
+    if VERBOSE >= ACTION_REPORTING:
+        print(f'\n\nACTUAL OBSERVATION: {observation}'
+              f'\nACTUAL PERCEPTUAL STATE: {perceptual_state.reshape(12,)}'
+              f'\nACTUAL ACTION FROM FUNCTION: {action}')
     return action
 
 # Action Mechanism
@@ -131,17 +159,17 @@ ocm = OptimizationControlMechanism(features={SHADOW_INPUTS:[player_percept, pred
                                    objective_mechanism=ObjectiveMechanism(function=objective_function,
                                                                           monitor=[action_mech, optimal_action_mech]),
                                    control_signals=[ControlSignal(projections=(VARIANCE,player_percept),
-                                                                  allocation_samples=[min_alloc, max_alloc],
-                                                                  intensity_cost_function=Exponential(rate=cost_rate,
-                                                                                                      bias=cost_bias)),
+                                                                  allocation_samples=ALLOCATION_SAMPLES,
+                                                                  intensity_cost_function=Exponential(rate=COST_RATE,
+                                                                                                      bias=COST_BIAS)),
                                                     ControlSignal(projections=(VARIANCE,predator_percept),
-                                                                  allocation_samples=[min_alloc, max_alloc],
-                                                                  intensity_cost_function=Exponential(rate=cost_rate,
-                                                                                                      bias=cost_bias)),
+                                                                  allocation_samples=ALLOCATION_SAMPLES,
+                                                                  intensity_cost_function=Exponential(rate=COST_RATE,
+                                                                                                      bias=COST_BIAS)),
                                                     ControlSignal(projections=(VARIANCE,prey_percept),
-                                                                  allocation_samples=[min_alloc, max_alloc],
-                                                                  intensity_cost_function=Exponential(rate=cost_rate,
-                                                                                                      bias=cost_bias))])
+                                                                  allocation_samples=ALLOCATION_SAMPLES,
+                                                                  intensity_cost_function=Exponential(rate=COST_RATE,
+                                                                                                      bias=COST_BIAS))])
 # Add controller to Composition
 agent_comp.add_model_based_optimizer(ocm)
 agent_comp.enable_model_based_optimizer = True
@@ -159,25 +187,40 @@ if SHOW_GRAPH:
 num_episodes = 1
 
 def main():
-    reward = 0
-    done = False
+
     if RENDER:
         ddqn_agent.env.render()  # If visualization is desired
     else:
-        print("Running simulation...")
+        print('\nRunning simulation... ')
+
     steps = 0
     start_time = timeit.default_timer()
-    new_episode()
     for _ in range(num_episodes):
         observation = ddqn_agent.env.reset()
-        optimal_action=[0,0]
+        new_episode()
         while True:
             execution_id = 'TEST'
             if PNL_COMPILE:
                 BIN_EXECUTE = 'LLVM'
             else:
                 BIN_EXECUTE = 'Python'
+
+            if VERBOSE >= STANDARD_REPORTING:
+                print(f'\nSTEP: {steps} ************************************************')
+
+            # Cache frame buffer
+            trial_start_buffer = ddqn_agent.buffer.buffer.copy()
+            # Get optimal action based on observation
             optimal_action = get_optimal_action(observation)
+            # Save frame buffer after optimal action
+            optimal_agent_frame_buffer = ddqn_agent.buffer.buffer
+            # Restore initial state of frame buffer (for use by Composition)
+            ddqn_agent.buffer.buffer = trial_start_buffer
+
+            if VERBOSE >= ACTION_REPORTING:
+                print(f'\nOUTER LOOP OPTIMAL ACTION:{optimal_action}')
+
+            # Get agent's action based on perceptual distoration of observation (and application of control)
             run_results = agent_comp.run(inputs={player_percept:[observation[player_coord_slice]],
                                                  predator_percept:[observation[predator_coord_slice]],
                                                  prey_percept:[observation[prey_coord_slice]],
@@ -185,50 +228,63 @@ def main():
                                          execution_id=execution_id,
                                          bin_execute=BIN_EXECUTE,
                                          )
-            action = np.where(run_results[0]==0,0,run_results[0]/np.abs(run_results[0]))
-
+            agent_action = np.where(run_results[0]==0,0,run_results[0]/np.abs(run_results[0]))
+            
             def print_controller():
-                print('SIMULATION:')
-                for sample, value in zip(ocm.saved_samples, ocm.saved_values):
-                    print('\t\tSample: {} Value: {}'.format(sample, value))
+                if VERBOSE >= SIMULATION_REPORTING:
+                    print('\nSIMULATION RESULTS:')
+                    for sample, value in zip(ocm.saved_samples, ocm.saved_values):
+                        print(f'\t\tSample: {sample} Value: {value}')
+                # print(f'\nOCM Allocation:\n\t{repr(list(np.squeeze(ocm.parameters.control_allocation.get(execution_id))))})
+                print(f'\nOCM:'
+                      f'\n\tControlSignals:'
+                      f'\n\t\tPlayer:\t\t{ocm.control_signals[0].parameters.value.get(execution_id)}'
+                      f'\n\t\tPredator\t{ocm.control_signals[1].parameters.value.get(execution_id)}'
+                      f'\n\t\tPrey:\t\t{ocm.control_signals[2].parameters.value.get(execution_id)}'
+                      f'\n\n\tControlSignal Costs:'
+                      f'\n\t\tPlayer:\t\t{ocm.control_signals[0].parameters.cost.get(execution_id)}'
+                      f'\n\t\tPredator:\t{ocm.control_signals[1].parameters.cost.get(execution_id)}'
+                      f'\n\t\tPrey:\t\t{ocm.control_signals[2].parameters.cost.get(execution_id)}')
 
-                print('OCM Allocation:\n\t{}'.
-                      format(repr(list(np.squeeze(ocm.parameters.control_allocation.get(execution_id))))))
+            if VERBOSE >= ACTION_REPORTING:
+                print(f'OUTER LOOP RUN RESULTS:{run_results}')
+                print(f'OUTER LOOP AGENT ACTION:{agent_action}')
 
-            print('\n**********************\nSTEP: ', steps)
+            if VERBOSE >= STANDARD_REPORTING:
+                if agent_comp.model_based_optimizer_mode is BEFORE:
+                    print_controller()
+                print(f'\nObservations:'
+                      f'\n\tPlayer:\n\t\tveridical: {player_percept.parameters.variable.get(execution_id)}'
+                      f'\n\t\tperceived: {player_percept.parameters.value.get(execution_id)}'
+                      f'\n\tPredator:\n\t\tveridical: {predator_percept.parameters.variable.get(execution_id)}'
+                      f'\n\t\tperceived: {predator_percept.parameters.value.get(execution_id)}'
+                      f'\n\tPrey:\n\t\tveridical: {prey_percept.parameters.variable.get(execution_id)}'
+                      f'\n\t\tperceived: {prey_percept.parameters.value.get(execution_id)}'
+                      f'\n\nActions:\n\tAgent: {agent_action}\n\tOptimal: {optimal_action}'
+                      f'\n\nOutcome:\n\t{ocm.objective_mechanism.parameters.value.get(execution_id)}'
+                      )
+                if agent_comp.model_based_optimizer_mode is AFTER:
+                    print_controller()
 
-            if agent_comp.model_based_optimizer_mode is BEFORE:
-                print_controller()
+            # Restore frame buffer to state after optimal action taken (at beginning of trial)
+            # This is so that agent's action's can be compared to optimal ones on a trial-by-trial basis
+            ddqn_agent.buffer.buffer = optimal_agent_frame_buffer
+            # # The following allows accumulation of agent's errors (assumes simulations are run before actual action)
+            # ddqn_agent.buffer.buffer = actual_agent_frame_buffer
 
-            print('Observations:')
-            print('\tPlayer:\n\t\tveridical: {}\n\t\tperceived: {}'.format(player_percept.parameters.variable.get(execution_id),
-                                                                           player_percept.parameters.value.get(execution_id)))
-            print('\tPredator:\n\t\tveridical: {}\n\t\tperceived: {}'.format(predator_percept.parameters.variable.get(execution_id),
-                                                                           predator_percept.parameters.value.get(execution_id)))
-            print('\tPrey:\n\t\tveridical: {}\n\t\tperceived: {}'.format(prey_percept.parameters.variable.get(execution_id),
-                                                                         prey_percept.parameters.value.get(execution_id)))
-            print('Actions:')
-            print('\tActual: {}\n\tOptimal: {}'.format(action, optimal_action))
-            print('Outcome: {}'.format(ocm.objective_mechanism.parameters.value.get(execution_id)))
+            if ACTION is OPTIMAL_ACTION:
+                action = optimal_action
+            elif ACTION is AGENT_ACTION:
+                action = agent_action
+            else:
+                assert False, "Must choose either OPTIMAL_ACTION or AGENT_ACTION"
 
-            print('OCM ControlSignals:')
-            print('\tPlayer:\t\t{}\n\tPredator\t{}\n\tPrey:\t\t{}'.
-                  format(ocm.control_signals[0].parameters.value.get(execution_id),
-                         ocm.control_signals[1].parameters.value.get(execution_id),
-                         ocm.control_signals[2].parameters.value.get(execution_id)))
-
-            print('OCM ControlSignal Costs:')
-            print('\tPlayer:\t\t{}\n\tPredator:\t{}\n\tPrey:\t\t{}'.
-                  format(ocm.control_signals[0].parameters.cost.get(execution_id),
-                         ocm.control_signals[1].parameters.cost.get(execution_id),
-                         ocm.control_signals[2].parameters.cost.get(execution_id)))
-
-            if agent_comp.model_based_optimizer_mode is AFTER:
-                print_controller()
-
-
-            # Get observation for next iteration (based on action taken on this one)
+            # Get observation for next iteration based on optimal action taken in this one
             observation, reward, done, _ = ddqn_agent.env.step(action)
+
+            print(f'\nAction Taken (using {ACTION}): {action}')
+
+
             steps += 1
             if done:
                 break
