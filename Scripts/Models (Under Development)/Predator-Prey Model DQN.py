b9fa999777cb7f00baae686856bdf4341c78fba9
jdc@princeton.edu <jdc@princeton.edu>
2019-02-23 18:13:09 -0500

    -

--- Scripts/Models (Under Development)/Predator-Prey Model DQN.py
+++ Scripts/Models (Under Development)/Predator-Prey Model DQN.py
@@ -11,7 +11,7 @@ from double_dqn import DoubleDQNAgent
 MPI_IMPLEMENTATION = True
 RENDER = True
 PNL_COMPILE = False
-RUN = False
+RUN = True
 SHOW_GRAPH = True
 MODEL_PATH = '../../../double-dqn/models/trained_models/policy_net_trained_0.99_20190214-1651.pt'
 
