4a214df0bfe2e4d2839c7e60ebb9bd1f57c44038
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-05 20:47:55 -0500

    llvm, functions/DistanceFunction: Allow more than 2 vectors in distance function
    Python version also allows and ignores more then 2 vectors.
    Move assertions earlier.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/components/functions/objectivefunctions.py
+++ psyneulink/core/components/functions/objectivefunctions.py
@@ -732,6 +732,11 @@ class Distance(ObjectiveFunction):
         builder.store(acc_y2_val, acc_y2)
 
     def _gen_llvm_function_body(self, ctx, builder, params, _, arg_in, arg_out):
+        assert isinstance(arg_in.type.pointee, pnlvm.ir.ArrayType)
+        assert isinstance(arg_in.type.pointee.element, pnlvm.ir.ArrayType)
+        # FIXME python version also ignores other vectors
+        assert arg_in.type.pointee.count >= 2
+
         v1 = builder.gep(arg_in, [ctx.int32_ty(0), ctx.int32_ty(0), ctx.int32_ty(0)])
         v2 = builder.gep(arg_in, [ctx.int32_ty(0), ctx.int32_ty(1), ctx.int32_ty(0)])
 
@@ -770,10 +775,6 @@ class Distance(ObjectiveFunction):
             inner = functools.partial(self.__gen_llvm_pearson, **kwargs)
         else:
             raise RuntimeError('Unsupported metric')
-
-        assert isinstance(arg_in.type.pointee, pnlvm.ir.ArrayType)
-        assert isinstance(arg_in.type.pointee.element, pnlvm.ir.ArrayType)
-        assert arg_in.type.pointee.count == 2
 
         input_length = arg_in.type.pointee.element.count
         vector_length = ctx.int32_ty(input_length)
