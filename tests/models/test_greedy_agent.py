413af198d5199b8ab1620ed6024cb02db8312942
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-07 00:57:37 -0500

    llvm, tests/models/PredatorPrey: Enable coarser grained compiled invocation
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/models/test_greedy_agent.py
+++ tests/models/test_greedy_agent.py
@@ -124,6 +124,8 @@ def test_simplified_greedy_agent_random(benchmark, mode):
 @pytest.mark.benchmark(group="Predator Prey")
 @pytest.mark.parametrize("mode", ['Python',
     pytest.param('LLVM', marks=[pytest.mark.llvm]),
+    pytest.param('LLVMExec', marks=[pytest.mark.llvm]),
+    pytest.param('LLVMRun', marks=[pytest.mark.llvm]),
 ])
 def test_predator_prey(benchmark, mode):
     # These should probably be replaced by reference to ForagerEnv constants:
