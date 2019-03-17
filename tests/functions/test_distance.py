b1f14756435e0200ef14ce7ffb77f5dde7c9c411
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-16 21:24:18 -0400

    llvm, functions/Distance: Implement COSINE metric
    Ignores 'normalize' parameter.
    Add test.
    
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/functions/test_distance.py
+++ tests/functions/test_distance.py
@@ -23,6 +23,9 @@ test_data = [
     (test_var, kw.MAX_ABS_DIFF, True,  None, np.max(abs(v1 - v2))),
     (test_var, kw.DIFFERENCE, False, None, np.sum(np.abs(v1 - v2))),
     (test_var, kw.DIFFERENCE, True,  None, np.sum(np.abs(v1 - v2))/norm),
+    (test_var, kw.COSINE, False, None, 1 - np.abs(np.sum(v1 * v2) / (
+                                             np.sqrt(np.sum(v1 ** 2)) *
+                                             np.sqrt(np.sum(v2 ** 2))) )),
     (test_var, kw.NORMED_L0_SIMILARITY, False, None, 1 - np.sum(np.abs(v1 - v2)/4)),
     (test_var, kw.NORMED_L0_SIMILARITY, True, None, (1 - np.sum(np.abs(v1 - v2)/4))/norm),
     (test_var, kw.EUCLIDEAN, False, None, np.linalg.norm(v1 - v2)),
@@ -43,6 +46,7 @@ names = [
     "MAX_ABS_DIFF NORMALIZED",
     "DIFFERENCE",
     "DIFFERENCE NORMALIZED",
+    "COSINE",
     "NORMED_L0_SIMILARITY",
     "NORMED_L0_SIMILARITY NORMALIZED",
     "EUCLIDEAN",
