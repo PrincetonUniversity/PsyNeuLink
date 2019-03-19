PrincetonUniversity/PsyNeuLink

commit b1f14756435e0200ef14ce7ffb77f5dde7c9c411

llvm, functions/Distance: Implement COSINE metric
Ignores 'normalize' parameter.
Add test.

Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

psyneulink/core/components/functions/objectivefunctions.py
@@ -676,6 +676,29 @@ class Distance(ObjectiveFunction):
         new_acc = builder.fadd(acc_val, prod)
         builder.store(new_acc, acc)
 
+    def __gen_llvm_cosine(self, builder, index, ctx, v1, v2, numer_acc, denom1_acc, denom2_acc):
+        ptr1 = builder.gep(v1, [index])
+        ptr2 = builder.gep(v2, [index])
+        val1 = builder.load(ptr1)
+        val2 = builder.load(ptr2)
+
+        # Numerator
+        numer = builder.load(numer_acc)
+        val = builder.fmul(val1, val2)
+        numer = builder.fadd(numer, val)
+        builder.store(numer, numer_acc)
+
+        # Denominator1
+        denom = builder.load(denom1_acc)
+        val = builder.fmul(val1, val1)
+        denom = builder.fadd(denom, val)
+        builder.store(denom, denom1_acc)
+        # Denominator2
+        denom = builder.load(denom2_acc)
+        val = builder.fmul(val2, val2)
+        denom = builder.fadd(denom, val)
+        builder.store(denom, denom2_acc)
+
     def __gen_llvm_max_diff(self, builder, index, ctx, v1, v2, max_diff_ptr):
         ptr1 = builder.gep(v1, [index])
         ptr2 = builder.gep(v2, [index])
@@ -752,6 +775,17 @@ class Distance(ObjectiveFunction):
             inner = functools.partial(self.__gen_llvm_sum_product, **kwargs)
         elif self.metric == CROSS_ENTROPY:
             inner = functools.partial(self.__gen_llvm_cross_entropy, **kwargs)
+        elif self.metric is COSINE:
+            del kwargs['acc']
+            numer_acc = builder.alloca(ctx.float_ty)
+            denom1_acc = builder.alloca(ctx.float_ty)
+            denom2_acc = builder.alloca(ctx.float_ty)
+            for loc in numer_acc, denom1_acc, denom1_acc:
+                builder.store(ctx.float_ty(0), loc)
+            kwargs['numer_acc'] = numer_acc
+            kwargs['denom1_acc'] = denom1_acc
+            kwargs['denom2_acc'] = denom2_acc
+            inner = functools.partial(self.__gen_llvm_cosine, **kwargs)
         elif self.metric == MAX_ABS_DIFF:
             del kwargs['acc']
             max_diff_ptr = builder.alloca(ctx.float_ty)
@@ -793,6 +827,18 @@ class Distance(ObjectiveFunction):
             ret = builder.call(sqrt, [ret])
         elif self.metric == MAX_ABS_DIFF:
             ret = builder.load(max_diff_ptr)
+        elif self.metric is COSINE:
+            numer = builder.load(numer_acc)
+            denom1 = builder.load(denom1_acc)
+            denom1 = builder.call(sqrt, [denom1])
+            denom2 = builder.load(denom2_acc)
+            denom2 = builder.call(sqrt, [denom2])
+            denom = builder.fmul(denom1, denom2)
+
+            ret = builder.fdiv(numer, denom)
+            ret = builder.call(fabs, [ret])
+            ret = builder.fsub(ret.type(1), ret)
+
         elif self.metric == CORRELATION:
             n = ctx.float_ty(input_length)
             acc_xy = builder.load(acc_xy_ptr)
@@ -847,8 +893,9 @@ class Distance(ObjectiveFunction):
             ret = builder.call(fabs, [corr])
             ret = builder.fsub(ctx.float_ty(1), ret)
 
-        # MAX_ABS_DIFF ignores normalization
-        if self.normalize and self.metric != MAX_ABS_DIFF and self.metric != CORRELATION:
+        # MAX_ABS_DIFF, CORRELATION, and COSINE ignore normalization
+        ignores = frozenset((MAX_ABS_DIFF, CORRELATION, COSINE))
+        if self.normalize and self.metric not in ignores:
             norm_factor = input_length
             if self.metric == ENERGY:
                 norm_factor = norm_factor ** 2

tests/functions/test_distance.py
@@ -23,6 +23,9 @@ test_data = [
     (test_var, kw.MAX_ABS_DIFF, True,  None, np.max(abs(v1 - v2))),
     (test_var, kw.DIFFERENCE, False, None, np.sum(np.abs(v1 - v2))),
     (test_var, kw.DIFFERENCE, True,  None, np.sum(np.abs(v1 - v2))/norm),
+    (test_var, kw.COSINE, False, None, 1 - np.abs(np.sum(v1 * v2) / (
+                                             np.sqrt(np.sum(v1 ** 2)) *
+                                             np.sqrt(np.sum(v2 ** 2))) )),
     (test_var, kw.NORMED_L0_SIMILARITY, False, None, 1 - np.sum(np.abs(v1 - v2)/4)),
     (test_var, kw.NORMED_L0_SIMILARITY, True, None, (1 - np.sum(np.abs(v1 - v2)/4))/norm),
     (test_var, kw.EUCLIDEAN, False, None, np.linalg.norm(v1 - v2)),
@@ -43,6 +46,7 @@ names = [
     "MAX_ABS_DIFF NORMALIZED",
     "DIFFERENCE",
     "DIFFERENCE NORMALIZED",
+    "COSINE",
     "NORMED_L0_SIMILARITY",
     "NORMED_L0_SIMILARITY NORMALIZED",
     "EUCLIDEAN",

commit c728c24ad28662fac8906a3cd72d0082badb5844

llvm, functions/Distance: Fix location initialization in COSINE metric
Fixes: b1f14756435e0200ef14ce7ffb77f5dde7c9c411 ("llvm,
functions/Distance: Implement COSINE metric")

Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

psyneulink/core/components/functions/objectivefunctions.py
@@ -780,7 +780,7 @@ class Distance(ObjectiveFunction):
             numer_acc = builder.alloca(ctx.float_ty)
             denom1_acc = builder.alloca(ctx.float_ty)
             denom2_acc = builder.alloca(ctx.float_ty)
-            for loc in numer_acc, denom1_acc, denom1_acc:
+            for loc in numer_acc, denom1_acc, denom2_acc:
                 builder.store(ctx.float_ty(0), loc)
             kwargs['numer_acc'] = numer_acc
             kwargs['denom1_acc'] = denom1_acc