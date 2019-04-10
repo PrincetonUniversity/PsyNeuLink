d2763da3bf5e8a16713c7a3cd606b22b1e83d60d
Jan Vesely <jan.vesely@rutgers.edu>
2019-04-10 19:50:46 -0400

    llvm: Stop using _instance_defaults.
    Use component.defaults instead.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/llvm/builder_context.py
+++ psyneulink/core/llvm/builder_context.py
@@ -135,12 +135,7 @@ class LLVMBuilderContext:
         if hasattr(component, '_get_input_struct_type'):
             return component._get_input_struct_type(self)
 
-        # KDM 12/28/18: <_instance_defaults_note> left _instance_defaults in place so that this code could use it.
-        # Ideally this would be simply .defaults. After going through the special handler above, component becomes a
-        # super() object, which seems to return the .defaults attr of the class associated with the super() object,
-        # whereas _instance_defaults retuns the .defaults attr of the instance associated. I don't know whether
-        # is a design or a convenience measure for workarounds, so I left this in place.
-        default_var = component._instance_defaults.variable
+        default_var = component.defaults.variable
         return self.convert_python_struct_to_llvm_ir(default_var)
 
     def get_output_struct_type(self, component):
