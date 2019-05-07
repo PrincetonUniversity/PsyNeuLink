b922d90cb3f439ff6cae6d1ee4071e642fc020fc
jdcpni <jdc@princeton.edu>
2019-05-07 17:15:29 -0400

    Feat/episodicmemorymech/no value (#1178)
    * • EpisodicMemoryMechanism
      - added _instanitate_output_states override
        to delete ASSOC OutputState if no ASSOC is specified
    
    * • EpisodicMemoryMechanism
      - added _instanitate_output_states override
        to delete ASSOC OutputState if no ASSOC is specified
      - changed CUE to CONTENT
    
    * • EpisodicMemoryMechanism
      - added _instantiate_output_states override
        to delete ASSOC OutputState if no ASSOC is specified
      - changed CUE to CONTENT
    
    • DNDFunction:
      - made vals field optional (depending on size of variable)
    
    * • EpisodicMemoryMechanism
      - added _instantiate_output_states override
        to delete ASSOC OutputState if no ASSOC is specified
      - changed CUE to CONTENT
    
    * • EpisodicMemoryMechanism
      - added _instantiate_output_states override
        to delete ASSOC OutputState if no ASSOC is specified
      - changed CUE to CONTENT
    
    * • EpisodicMemoryMechanism
      - added _instantiate_output_states override
        to delete ASSOC OutputState if no ASSOC is specified
      - changed CUE to CONTENT
    
    * • EpisodicMemoryMechanism
      - added _instantiate_output_states override
        to delete ASSOC OutputState if no ASSOC is specified
      - changed CUE to CONTENT
    
    * • EpisodicMemoryMechanism
      - made ASSOC optional
      - added _instantiate_output_states override
        to delete ASSOC OutputState if no ASSOC is specified
      - changed CUE to CONTENT
    
    * • tests/functions/test_memory:
      - added test for no ASSOC
    
    * -
    
    * - fixed test_episodic_memory

--- tests/functions/test_memory.py
+++ tests/functions/test_memory.py
@@ -17,29 +17,29 @@ test_data = [
 # Default initializer does not work
 #    (Functions.Buffer, test_var, {'rate':RAND1}, [[0.0],[0.0]]),
     (Functions.Buffer, test_var[0], {'history':512, 'rate':RAND1, 'initializer':[test_var[0]]}, [[0.03841128, 0.05005587, 0.04218721, 0.0381362 , 0.02965146, 0.04520592, 0.03062659, 0.0624149 , 0.06744644, 0.02683695],[0.14519169, 0.18920736, 0.15946443, 0.1441519 , 0.11208025, 0.17087491, 0.11576615, 0.23592355, 0.25494239, 0.10144161]]),
-    (Functions.DND, test_var, {'rate':RAND1}, [[
+    (Functions.ContentAddressableMemory, test_var, {'rate':RAND1}, [[
        0.5488135039273248, 0.7151893663724195, 0.6027633760716439, 0.5448831829968969, 0.4236547993389047, 0.6458941130666561, 0.4375872112626925, 0.8917730007820798, 0.9636627605010293, 0.3834415188257777], [
        0.7917250380826646, 0.5288949197529045, 0.5680445610939323, 0.925596638292661, 0.07103605819788694, 0.08712929970154071, 0.02021839744032572, 0.832619845547938, 0.7781567509498505, 0.8700121482468192 ]]),
-    (Functions.DND, test_var, {'rate':RAND1, 'retrieval_prob':0.5},
+    (Functions.ContentAddressableMemory, test_var, {'rate':RAND1, 'retrieval_prob':0.5},
        [[ 0. for i in range(SIZE) ],[ 0. for i in range(SIZE) ]]),
-    (Functions.DND, test_var, {'rate':RAND1, 'storage_prob':0.1},
+    (Functions.ContentAddressableMemory, test_var, {'rate':RAND1, 'storage_prob':0.1},
        [[ 0. for i in range(SIZE) ],[ 0. for i in range(SIZE) ]]),
-    (Functions.DND, test_var, {'rate':RAND1, 'retrieval_prob':0.9, 'storage_prob':0.9}, [[
+    (Functions.ContentAddressableMemory, test_var, {'rate':RAND1, 'retrieval_prob':0.9, 'storage_prob':0.9}, [[
        0.5488135039273248, 0.7151893663724195, 0.6027633760716439, 0.5448831829968969, 0.4236547993389047, 0.6458941130666561, 0.4375872112626925, 0.8917730007820798, 0.9636627605010293, 0.3834415188257777], [
        0.7917250380826646, 0.5288949197529045, 0.5680445610939323, 0.925596638292661, 0.07103605819788694, 0.08712929970154071, 0.02021839744032572, 0.832619845547938, 0.7781567509498505, 0.8700121482468192 ]]),
-    (Functions.DND, test_var, {'initializer':test_initializer, 'rate':RAND1}, [[
+    (Functions.ContentAddressableMemory, test_var, {'initializer':test_initializer, 'rate':RAND1}, [[
        0.5488135039273248, 0.7151893663724195, 0.6027633760716439, 0.5448831829968969, 0.4236547993389047, 0.6458941130666561, 0.4375872112626925, 0.8917730007820798, 0.9636627605010293, 0.3834415188257777], [
        0.7917250380826646, 0.5288949197529045, 0.5680445610939323, 0.925596638292661, 0.07103605819788694, 0.08712929970154071, 0.02021839744032572, 0.832619845547938, 0.7781567509498505, 0.8700121482468192 ]]),
-# Disable noise tests for now as they trigger failure in DND lookup
-#    (Functions.DND, test_var, {'rate':RAND1, 'noise':RAND2}, [[
+# Disable noise tests for now as they trigger failure in ContentAddressableMemory lookup
+#    (Functions.ContentAddressableMemory, test_var, {'rate':RAND1, 'noise':RAND2}, [[
 #       0.79172504, 0.52889492, 0.56804456, 0.92559664, 0.07103606, 0.0871293 , 0.0202184 , 0.83261985, 0.77815675, 0.87001215 ],[
 #       1.3230471933615413, 1.4894230558066361, 1.3769970655058605, 1.3191168724311135, 1.1978884887731214, 1.4201278025008728, 1.2118209006969092, 1.6660066902162964, 1.737896449935246, 1.1576752082599944
 #]]),
-#    (Functions.DND, test_var, {'rate':RAND1, 'noise':[RAND2], 'retrieval_prob':0.5},
+#    (Functions.ContentAddressableMemory, test_var, {'rate':RAND1, 'noise':[RAND2], 'retrieval_prob':0.5},
 #       [[ 0. for i in range(SIZE) ],[ 0. for i in range(SIZE) ]]),
-#    (Functions.DND, test_var, {'rate':RAND1, 'noise':RAND2, 'storage_prob':0.5},
+#    (Functions.ContentAddressableMemory, test_var, {'rate':RAND1, 'noise':RAND2, 'storage_prob':0.5},
 #       [[ 0. for i in range(SIZE) ],[ 0. for i in range(SIZE) ]]),
-#    (Functions.DND, test_var, {'initializer':test_initializer, 'rate':RAND1, 'noise':RAND2}, [[
+#    (Functions.ContentAddressableMemory, test_var, {'initializer':test_initializer, 'rate':RAND1, 'noise':RAND2}, [[
 #       0.79172504, 0.52889492, 0.56804456, 0.92559664, 0.07103606, 0.0871293 , 0.0202184 , 0.83261985, 0.77815675, 0.87001215 ],[
 #       1.3230471933615413, 1.4894230558066361, 1.3769970655058605, 1.3191168724311135, 1.1978884887731214, 1.4201278025008728, 1.2118209006969092, 1.6660066902162964, 1.737896449935246, 1.1576752082599944
 #]]),
@@ -49,15 +49,15 @@ test_data = [
 names = [
     "Buffer",
 #    "Buffer Initializer",
-    "DND",
-    "DND Random Retrieval",
-    "DND Random Storage",
-    "DND Random Retrieval-Storage",
-    "DND Initializer",
-#    "DND Noise",
-#    "DND Noise Random Retrieval",
-#    "DND Noise Random Storage",
-#    "DND Initializer Noise",
+    "ContentAddressableMemory",
+    "ContentAddressableMemory Random Retrieval",
+    "ContentAddressableMemory Random Storage",
+    "ContentAddressableMemory Random Retrieval-Storage",
+    "ContentAddressableMemory Initializer",
+#    "ContentAddressableMemory Noise",
+#    "ContentAddressableMemory Noise Random Retrieval",
+#    "ContentAddressableMemory Noise Random Storage",
+#    "ContentAddressableMemory Initializer Noise",
 ]
 
 GROUP_PREFIX="IntegratorFunction "
@@ -111,10 +111,10 @@ def test_ptx_cuda(func, variable, params, expected, benchmark):
     assert np.allclose(res, expected)
     benchmark(m.cuda_execute, variable)
 
-# Test of DND without LLVM:
+# Test of ContentAddressableMemory without LLVM:
 from psyneulink import *
 
-def test_DND_with_initializer_and_key_size_same_as_val_size():
+def test_ContentAddressableMemory_with_initializer_and_key_size_same_as_val_size():
 
     stimuli = {'A': [[1,2,3],[4,5,6]],
                'B': [[8,9,10],[11,12,13]],
@@ -124,10 +124,10 @@ def test_DND_with_initializer_and_key_size_same_as_val_size():
                'F': [[10,10,30],[40,50,60]],
                }
 
-    dnd = EpisodicMemoryMechanism(
-            cue_size=3,
+    em = EpisodicMemoryMechanism(
+            content_size=3,
             assoc_size=3,
-            function = DND(
+            function = ContentAddressableMemory(
                     initializer=np.array([stimuli['F'], stimuli['F']]),
                     duplicate_keys_allowed=True,
                     duplicate_keys_select=RANDOM)
@@ -135,36 +135,36 @@ def test_DND_with_initializer_and_key_size_same_as_val_size():
 
     retrieved_keys=[]
     for key in sorted(stimuli.keys()):
-        retrieved = [i for i in dnd.execute(stimuli[key])]
+        retrieved = [i for i in em.execute(stimuli[key])]
         retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
         retrieved_keys.append(retrieved_key)
     assert retrieved_keys == [['F'], ['A'], ['A'], ['C'], ['B'], ['F']]
 
     stim = 'C'
-    dnd.function.duplicate_keys_select = OLDEST
-    retrieved = [i for i in dnd.function.get_memory(stimuli[stim][0])]
+    em.function.duplicate_keys_select = OLDEST
+    retrieved = [i for i in em.function.get_memory(stimuli[stim][0])]
     retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
     assert retrieved_key == ['A']
 
-    dnd.function.duplicate_keys_select = NEWEST
-    retrieved = [i for i in dnd.function.get_memory(stimuli[stim][0])]
+    em.function.duplicate_keys_select = NEWEST
+    retrieved = [i for i in em.function.get_memory(stimuli[stim][0])]
     retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
     assert retrieved_key == ['D']
 
     # Test that after allowing dups, warning is issued and memory with zeros is returned
-    dnd.function.duplicate_keys_allowed = False
+    em.function.duplicate_keys_allowed = False
     stim = 'A'
 
     with pytest.warns(UserWarning) as warning_msg:
-        retrieved = dnd.execute(stimuli[stim])
+        retrieved = em.execute(stimuli[stim])
     warning_txt = warning_msg[0].message.args[0]
-    assert 'More than one item matched key ([1 2 3]) in memory for DND' in str(warning_txt)
+    assert 'More than one item matched key ([1 2 3]) in memory for ContentAddressableMemory' in str(warning_txt)
     retrieved_key = [k for k,v in stimuli.items() if v==list(retrieved)] or [None]
     assert retrieved_key == [None]
     assert retrieved[0] == [0, 0, 0]
     assert retrieved[1] == [0, 0, 0]
 
-def test_DND_with_initializer_and_key_size_diff_from_val_size():
+def test_ContentAddressableMemory_with_initializer_and_key_size_diff_from_val_size():
 
     stimuli = {'A': [[1,2,3],[4,5,6,7]],
                'B': [[8,9,10],[11,12,13,14]],
@@ -174,10 +174,10 @@ def test_DND_with_initializer_and_key_size_diff_from_val_size():
                'F': [[10,10,30],[40,50,60,70]],
                }
 
-    dnd = EpisodicMemoryMechanism(
-            cue_size=3,
+    em = EpisodicMemoryMechanism(
+            content_size=3,
             assoc_size=4,
-            function = DND(
+            function = ContentAddressableMemory(
                     initializer=np.array([stimuli['F'], stimuli['F']]),
                     duplicate_keys_allowed=True,
                     duplicate_keys_select=RANDOM)
@@ -185,36 +185,36 @@ def test_DND_with_initializer_and_key_size_diff_from_val_size():
 
     retrieved_keys=[]
     for key in sorted(stimuli.keys()):
-        retrieved = [i for i in dnd.execute(stimuli[key])]
+        retrieved = [i for i in em.execute(stimuli[key])]
         retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
         retrieved_keys.append(retrieved_key)
     assert retrieved_keys == [['F'], ['A'], ['A'], ['C'], ['B'], ['F']]
 
     stim = 'C'
-    dnd.function.duplicate_keys_select = OLDEST
-    retrieved = [i for i in dnd.function.get_memory(stimuli[stim][0])]
+    em.function.duplicate_keys_select = OLDEST
+    retrieved = [i for i in em.function.get_memory(stimuli[stim][0])]
     retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
     assert retrieved_key == ['A']
 
-    dnd.function.duplicate_keys_select = NEWEST
-    retrieved = [i for i in dnd.function.get_memory(stimuli[stim][0])]
+    em.function.duplicate_keys_select = NEWEST
+    retrieved = [i for i in em.function.get_memory(stimuli[stim][0])]
     retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
     assert retrieved_key == ['D']
 
     # Test that after allowing dups, warning is issued and memory with zeros is returned
-    dnd.function.duplicate_keys_allowed = False
+    em.function.duplicate_keys_allowed = False
     stim = 'A'
 
     with pytest.warns(UserWarning) as warning_msg:
-        retrieved = dnd.execute(stimuli[stim])
+        retrieved = em.execute(stimuli[stim])
     warning_txt = warning_msg[0].message.args[0]
-    assert 'More than one item matched key ([1, 2, 3]) in memory for DND Function-0' in str(warning_txt)
+    assert 'More than one item matched key ([1, 2, 3]) in memory for ContentAddressableMemory' in str(warning_txt)
     retrieved_key = [k for k,v in stimuli.items() if v==list(retrieved)] or [None]
     assert retrieved_key == [None]
     assert retrieved[0] == [0, 0, 0]
     assert retrieved[1] == [0, 0, 0, 0]
 
-def test_DND_without_initializer_and_key_size_same_as_val_size():
+def test_ContentAddressableMemory_without_initializer_and_key_size_same_as_val_size():
 
     stimuli = {'A': [[1,2,3],[4,5,6]],
                'B': [[8,9,10],[11,12,13]],
@@ -224,46 +224,46 @@ def test_DND_without_initializer_and_key_size_same_as_val_size():
                'F': [[10,10,30],[40,50,60]],
                }
 
-    dnd = EpisodicMemoryMechanism(
-            cue_size=3,
+    em = EpisodicMemoryMechanism(
+            content_size=3,
             assoc_size=3,
-            function = DND(
+            function = ContentAddressableMemory(
                     duplicate_keys_allowed=True,
                     duplicate_keys_select=RANDOM)
     )
 
     retrieved_keys=[]
     for key in sorted(stimuli.keys()):
-        retrieved = [i for i in dnd.execute(stimuli[key])]
+        retrieved = [i for i in em.execute(stimuli[key])]
         retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
         retrieved_keys.append(retrieved_key)
     assert retrieved_keys == [[None], ['A'], ['A'], ['A'], ['B'], ['D']]
 
     stim = 'C'
-    dnd.function.duplicate_keys_select = OLDEST
-    retrieved = [i for i in dnd.function.get_memory(stimuli[stim][0])]
+    em.function.duplicate_keys_select = OLDEST
+    retrieved = [i for i in em.function.get_memory(stimuli[stim][0])]
     retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
     assert retrieved_key == ['A']
 
-    dnd.function.duplicate_keys_select = NEWEST
-    retrieved = [i for i in dnd.function.get_memory(stimuli[stim][0])]
+    em.function.duplicate_keys_select = NEWEST
+    retrieved = [i for i in em.function.get_memory(stimuli[stim][0])]
     retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
     assert retrieved_key == ['D']
 
     # Test that after allowing dups, warning is issued and memory with zeros is returned
-    dnd.function.duplicate_keys_allowed = False
+    em.function.duplicate_keys_allowed = False
     stim = 'A'
 
     with pytest.warns(UserWarning) as warning_msg:
-        retrieved = dnd.execute(stimuli[stim])
+        retrieved = em.execute(stimuli[stim])
     warning_txt = warning_msg[0].message.args[0]
-    assert 'More than one item matched key ([1 2 3]) in memory for DND' in str(warning_txt)
+    assert 'More than one item matched key ([1 2 3]) in memory for ContentAddressableMemory' in str(warning_txt)
     retrieved_key = [k for k,v in stimuli.items() if v==list(retrieved)] or [None]
     assert retrieved_key == [None]
     assert retrieved[0] == [0, 0, 0]
     assert retrieved[1] == [0, 0, 0]
 
-def test_DND_without_initializer_and_key_size_diff_from_val_size():
+def test_ContentAddressableMemory_without_initializer_and_key_size_diff_from_val_size():
 
     stimuli = {'A': [[1,2,3],[4,5,6,7]],
                'B': [[8,9,10],[11,12,13,14]],
@@ -273,46 +273,82 @@ def test_DND_without_initializer_and_key_size_diff_from_val_size():
                'F': [[10,10,30],[40,50,60,70]],
                }
 
-    dnd = EpisodicMemoryMechanism(
-            cue_size=3,
+    em = EpisodicMemoryMechanism(
+            content_size=3,
             assoc_size=4,
-            function = DND(
+            function = ContentAddressableMemory(
                     duplicate_keys_allowed=True,
                     duplicate_keys_select=RANDOM)
     )
 
     retrieved_keys=[]
     for key in sorted(stimuli.keys()):
-        retrieved = [i for i in dnd.execute(stimuli[key])]
+        retrieved = [i for i in em.execute(stimuli[key])]
         retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
         retrieved_keys.append(retrieved_key)
     assert retrieved_keys == [[None], ['A'], ['A'], ['A'], ['B'], ['D']]
 
     stim = 'C'
-    dnd.function.duplicate_keys_select = OLDEST
-    retrieved = [i for i in dnd.function.get_memory(stimuli[stim][0])]
+    em.function.duplicate_keys_select = OLDEST
+    retrieved = [i for i in em.function.get_memory(stimuli[stim][0])]
     retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
     assert retrieved_key == ['A']
 
-    dnd.function.duplicate_keys_select = NEWEST
-    retrieved = [i for i in dnd.function.get_memory(stimuli[stim][0])]
+    em.function.duplicate_keys_select = NEWEST
+    retrieved = [i for i in em.function.get_memory(stimuli[stim][0])]
     retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
     assert retrieved_key == ['D']
 
     # Test that after allowing dups, warning is issued and memory with zeros is returned
-    dnd.function.duplicate_keys_allowed = False
+    em.function.duplicate_keys_allowed = False
     stim = 'A'
 
     with pytest.warns(UserWarning) as warning_msg:
-        retrieved = dnd.execute(stimuli[stim])
+        retrieved = em.execute(stimuli[stim])
     warning_txt = warning_msg[0].message.args[0]
-    assert 'More than one item matched key ([1, 2, 3]) in memory for DND Function-0' in str(warning_txt)
+
+    assert 'More than one item matched key ([1, 2, 3]) in memory for ContentAddressableMemory' in str(warning_txt)
     retrieved_key = [k for k,v in stimuli.items() if v==list(retrieved)] or [None]
     assert retrieved_key == [None]
     assert retrieved[0] == [0, 0, 0]
     assert retrieved[1] == [0, 0, 0, 0]
 
 
+def test_ContentAddressableMemory_without_assoc():
+
+    stimuli = {'A': [[1,2,3]],
+               'B': [[8,9,10]],
+               'C': [[1,2,3]],
+               'D': [[1,2,3]],
+               'E': [[9,8,4]],
+               'F': [[10,10,30]],
+               }
+
+    em = EpisodicMemoryMechanism(
+            name='EPISODIC MEMORY MECH',
+            content_size=3,
+            function = ContentAddressableMemory(
+                    # initializer=np.array([stimuli['F'], stimuli['F']]),
+                    duplicate_keys_allowed=True,
+                    duplicate_keys_select=RANDOM,
+                    retrieval_prob = 1.0
+            )
+    )
+
+    for key in sorted(stimuli.keys()):
+        print(f'\nCurrent memory: \n{em.memory}\n')
+        retrieved = [i for i in em.execute(stimuli[key])]
+        retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
+        print(f'\nExecuted with stimulus {key}: {stimuli[key]};'
+              f'\nRetrieved memory {retrieved_key[0]}: \n\t{retrieved}')
+
+    retrieved_keys=[]
+    for key in sorted(stimuli.keys()):
+        retrieved = [i for i in em.execute(stimuli[key])]
+        retrieved_key = [k for k,v in stimuli.items() if v==retrieved] or [None]
+        retrieved_keys.append(retrieved_key)
+
+    assert retrieved_keys == [['A', 'C', 'D'], ['B'], ['A', 'C', 'D'], ['A', 'C', 'D'], ['E'], ['F']]
 
 
 
--- tests/mechanisms/test_episodic_memory.py
+++ tests/mechanisms/test_episodic_memory.py
@@ -1,36 +1,36 @@
 import numpy as np
 import pytest
 
-from psyneulink.core.components.functions.statefulfunctions.memoryfunctions import DND
+from psyneulink.core.components.functions.statefulfunctions.memoryfunctions import ContentAddressableMemory
 from psyneulink.library.components.mechanisms.processing.integrator.episodicmemorymechanism import EpisodicMemoryMechanism
 import psyneulink.core.llvm as pnlvm
 
 np.random.seed(0)
-CUE_SIZE=10
+CONTENT_SIZE=10
 ASSOC_SIZE=10
-test_var = np.random.rand(2, CUE_SIZE)
+test_var = np.random.rand(2, CONTENT_SIZE)
 test_initializer = {tuple(test_var[0]): test_var[1]}
 
 
 test_data = [
-    (test_var, DND, {'default_variable':test_var}, [[
+    (test_var, ContentAddressableMemory, {'default_variable':test_var}, [[
        0.5488135039273248, 0.7151893663724195, 0.6027633760716439, 0.5448831829968969, 0.4236547993389047, 0.6458941130666561, 0.4375872112626925, 0.8917730007820798, 0.9636627605010293, 0.3834415188257777], [
        0.7917250380826646, 0.5288949197529045, 0.5680445610939323, 0.925596638292661, 0.07103605819788694, 0.08712929970154071, 0.02021839744032572, 0.832619845547938, 0.7781567509498505, 0.8700121482468192 ]]),
-    (test_var, DND, {'default_variable':test_var, 'retrieval_prob':0.5},
-       [[ 0. for i in range(CUE_SIZE) ],[ 0. for i in range(ASSOC_SIZE) ]]),
-    (test_var, DND, {'default_variable':test_var, 'storage_prob':0.1},
-       [[ 0. for i in range(CUE_SIZE) ],[ 0. for i in range(ASSOC_SIZE) ]]),
-    (test_var, DND, {'default_variable':test_var, 'retrieval_prob':0.9, 'storage_prob':0.9}, [[
+    (test_var, ContentAddressableMemory, {'default_variable':test_var, 'retrieval_prob':0.5},
+       [[ 0. for i in range(CONTENT_SIZE) ],[ 0. for i in range(ASSOC_SIZE) ]]),
+    (test_var, ContentAddressableMemory, {'default_variable':test_var, 'storage_prob':0.1},
+       [[ 0. for i in range(CONTENT_SIZE) ],[ 0. for i in range(ASSOC_SIZE) ]]),
+    (test_var, ContentAddressableMemory, {'default_variable':test_var, 'retrieval_prob':0.9, 'storage_prob':0.9}, [[
        0.5488135039273248, 0.7151893663724195, 0.6027633760716439, 0.5448831829968969, 0.4236547993389047, 0.6458941130666561, 0.4375872112626925, 0.8917730007820798, 0.9636627605010293, 0.3834415188257777], [
        0.7917250380826646, 0.5288949197529045, 0.5680445610939323, 0.925596638292661, 0.07103605819788694, 0.08712929970154071, 0.02021839744032572, 0.832619845547938, 0.7781567509498505, 0.8700121482468192 ]]),
 ]
 
 # use list, naming function produces ugly names
 names = [
-    "DND",
-    "DND Random Retrieval",
-    "DND Random Storage",
-    "DND Random Retrieval-Storage",
+    "ContentAddressableMemory",
+    "ContentAddressableMemory Random Retrieval",
+    "ContentAddressableMemory Random Storage",
+    "ContentAddressableMemory Random Retrieval-Storage",
 ]
 
 @pytest.mark.function
@@ -39,7 +39,7 @@ names = [
 @pytest.mark.benchmark
 def test_basic(variable, func, params, expected, benchmark):
     f= func(seed=0, **params)
-    m = EpisodicMemoryMechanism(cue_size=len(variable[0]), assoc_size=len(variable[1]), function=f)
+    m = EpisodicMemoryMechanism(content_size=len(variable[0]), assoc_size=len(variable[1]), function=f)
     m.execute(variable)
     m.execute(variable)
     res = [s.value for s in m.output_states]
@@ -55,7 +55,7 @@ def test_basic(variable, func, params, expected, benchmark):
 @pytest.mark.benchmark
 def test_llvm(variable, func, params, expected, benchmark):
     f= func(seed=0, **params)
-    m = EpisodicMemoryMechanism(cue_size=len(variable[0]), assoc_size=len(variable[1]), function=f)
+    m = EpisodicMemoryMechanism(content_size=len(variable[0]), assoc_size=len(variable[1]), function=f)
     e = pnlvm.execution.MechExecution(m)
     e.execute(variable)
     res = e.execute(variable)
@@ -72,7 +72,7 @@ def test_llvm(variable, func, params, expected, benchmark):
 @pytest.mark.benchmark
 def test_ptx_cuda(variable, func, params, expected, benchmark):
     f= func(seed=0, **params)
-    m = EpisodicMemoryMechanism(cue_size=len(variable[0]), assoc_size=len(variable[1]), function=f)
+    m = EpisodicMemoryMechanism(content_size=len(variable[0]), assoc_size=len(variable[1]), function=f)
     e = pnlvm.execution.MechExecution(m)
     e.cuda_execute(variable)
     res = e.cuda_execute(variable)
