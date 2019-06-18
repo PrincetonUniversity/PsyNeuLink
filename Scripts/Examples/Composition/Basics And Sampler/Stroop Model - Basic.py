dc36ac1f24e9dd281ec3b5759dd24be75c631a8d
jdcpni <jdc@princeton.edu>
2019-06-18 08:09:26 -0400

    Feat/composition/show graph conditions (#1231)
    * -
    
    * • version:
      added root_dir global variable
    
    • Composition:
      - show_graph:
        added movie_dir and show_condition options
        default GIFS directory is now in root_dir/show_graph_OUTPUT/GIFS
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -

--- Scripts/Examples/Composition/Basics And Sampler/Stroop Model - Basic.py
+++ Scripts/Examples/Composition/Basics And Sampler/Stroop Model - Basic.py
@@ -31,7 +31,7 @@ decision = DDM(name='DECISION',
 decision_pathway = [output, decision]
 
 # Construct the Composition:
-Stroop_model = Composition(name='Stroop Model')
+Stroop_model = Composition(name='Stroop Model - Basic')
 Stroop_model.add_linear_processing_pathway(color_pathway)
 Stroop_model.add_linear_processing_pathway(word_pathway)
 Stroop_model.add_linear_processing_pathway(task_color_pathway)
@@ -43,6 +43,8 @@ red = [1,0]
 green = [0,1]
 word = [0,1]
 color = [1,0]
+
+Stroop_model.show_graph(show_node_structure=True)
 
 Stroop_model.run(inputs={color_input:red, word_input:green, task_input:color},
                  num_trials=2,
