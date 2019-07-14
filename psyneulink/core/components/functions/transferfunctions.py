3729242a06715bf66cef9c9480bbe48d1cdfaafb
Jan Vesely <jan.vesely@rutgers.edu>
2019-07-14 15:06:35 -0400

    llvm, functions/transferfunctions: Cleanup function generation code.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/components/functions/transferfunctions.py
+++ psyneulink/core/components/functions/transferfunctions.py
@@ -149,14 +149,14 @@ class TransferFunction(Function_Base):
             assert arg_in.type == arg_out.type
             # Array elements need all to be of the same size
             length = arg_in.type.pointee.count * arg_in.type.pointee.element.count
-            arg_in = builder.bitcast(arg_in, pnlvm.ir.ArrayType(ctx.float_ty, length).as_pointer())
-            arg_out = builder.bitcast(arg_out, pnlvm.ir.ArrayType(ctx.float_ty, length).as_pointer())
+            arr_ty = pnlvm.ir.ArrayType(ctx.float_ty, length);
+            arg_in = builder.bitcast(arg_in, arr_ty.as_pointer())
+            arg_out = builder.bitcast(arg_out, arr_ty.as_pointer())
 
-        kwargs = {"ctx": ctx, "vi": arg_in, "vo": arg_out, "params": params, "state":state}
-
-        assert arg_in.type.pointee.count == arg_out.type.pointee.count
+        assert arg_in.type == arg_out.type
         with pnlvm.helpers.array_ptr_loop(builder, arg_in, "transfer_loop") as args:
-            self._gen_llvm_transfer(*args, **kwargs)
+            self._gen_llvm_transfer(ctx=ctx, vi=arg_in, vo=arg_out,
+                                    params=params, state=state, *args)
 
         return builder
 
@@ -2377,13 +2377,14 @@ class SoftMax(TransferFunction):
         ptro = builder.gep(arg_out, [ctx.int32_ty(0), index])
 
         if output_type == ALL:
-            kwargs = {"ctx": ctx, "vi": arg_in, "vo": arg_out, "gain": gain, "exp_sum": exp_sum}
             with pnlvm.helpers.array_ptr_loop(builder, arg_in, "exp_div") as args:
-                self.__gen_llvm_exp_div(*args, **kwargs)
+                self.__gen_llvm_exp_div(ctx=ctx, vi=arg_in, vo=arg_out,
+                                        gain=gain, exp_sum=exp_sum, *args)
         elif output_type == MAX_VAL:
             # zero out the output array
             with pnlvm.helpers.array_ptr_loop(builder, arg_in, "zero_output") as (b,i):
                 b.store(ctx.float_ty(0), b.gep(arg_out, [ctx.int32_ty(0), i]))
+
             ptri = builder.gep(arg_in, [ctx.int32_ty(0), index])
             exp_f = ctx.get_builtin("exp", [ctx.float_ty])
             orig_val = builder.load(ptri)
