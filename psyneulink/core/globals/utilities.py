ddc0c6adadf6dfe17ecec3a7347d554260500fb5
Jan Vesely <jan.vesely@rutgers.edu>
2019-07-27 13:20:14 -0400

    Consistently use triple double quotes for docstrings
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/globals/utilities.py
+++ psyneulink/core/globals/utilities.py
@@ -571,7 +571,7 @@ def scalar_distance(measure, value, scale=1, offset=0):
 
 from itertools import chain, combinations
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    """powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"""
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
