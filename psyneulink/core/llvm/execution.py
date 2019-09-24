PrincetonUniversity/PsyNeuLink

commit c3079f72db09509743847534399654779472a9e0

llvm/execution/autodiff: Use numpy to get pointer to input and target data
We don't use the specific structures layout.
Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

psyneulink/core/llvm/execution.py
@@ -500,22 +500,18 @@ class CompExecution(CUDAExecution):
         epochs = autodiff_stimuli.get("epochs", 0)
 
         autodiff_stimuli_struct = [epochs, len(next(iter(inputs.values())))]
-        ctx = pnlvm.builder_context.LLVMBuilderContext.get_global()
         
-        # autodiff_values keeps the ctype values on the stack, so it doesn't get gc'd TODO: Make sure this works as intended
+        # autodiff_values keeps the ctype values on the stack, so it doesn't get gc'd
         autodiff_values = []
         def make_node_data(dictionary, node):
             values = dictionary.get(node)
             if values is not None:
                 dimensionality = len(values[0])
                 idx = self._composition._get_node_index(node)
-                values_ir_ty = ctx.convert_python_struct_to_llvm_ir(values)
-                values_c_ty = _convert_llvm_ir_to_ctype(values_ir_ty)
-                values = values_c_ty(*_tupleize(values))
+                values = np.asfarray(values)
                 autodiff_values.append(values)
 
-                return (idx, dimensionality,
-                    ctypes.cast(values,ctypes.c_void_p).value)
+                return (idx, dimensionality, values.ctypes.data)
             return tuple()
 
         autodiff_param_cty = self._bin_run_func.byref_arg_types[1]

commit 0899f42c90584b60933e2715507dab5345543fe2

llvm/execution: Drop unused import
Fixes: c3079f72db09509743847534399654779472a9e0
Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

psyneulink/core/llvm/execution.py
@@ -17,7 +17,6 @@ from collections import defaultdict
 import numpy as np
 
 from psyneulink.core import llvm as pnlvm
-from .builder_context import *
 from . import helpers, jit_engine
 from .debug import debug_env
 