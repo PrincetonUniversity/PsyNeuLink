b5dae26ed7c0fb1f5aae36be449a43c13c488a06
jdc@princeton.edu <jdc@princeton.edu>
2019-09-28 13:43:21 -0400

    -

--- tests/models/test_greedy_agent.py
+++ tests/models/test_greedy_agent.py
@@ -190,17 +190,20 @@ def test_predator_prey(benchmark, mode, samples):
     agent_comp.enable_controller = True
     ocm.comp_execution_mode = mode
 
-    def print_control_values():
-        print(f"player_obs VARANCE: {ocm.control_signals[0].value}")
-        print(f"predator_obs VARANCE: {ocm.control_signals[0].value}")
-        print(f"prey_obs VARANCE: {ocm.control_signals[0].value}")
-
     input_dict = {player_obs:[[1.1576537,  0.60782117]],
                   predator_obs:[[-0.03479106, -0.47666293]],
                   prey_obs:[[-0.60836214,  0.1760381 ]],
                  }
-    run_results = agent_comp.run(inputs=input_dict, num_trials=2, bin_execute=mode,
-                                 call_before_trial=print_control_values())
+    run_results = agent_comp.run(inputs=input_dict, num_trials=2, bin_execute=mode)
+
+    print(f"player_obs control allocation: {ocm.control_signals[0].parameters.allocation.get(agent_comp)}")
+    print(f"player_obs control value: {ocm.control_signals[0].parameters.value.get(agent_comp)}")
+    print(f"predator_obs control allocation: {ocm.control_signals[0].parameters.allocation.get(agent_comp)}")
+    print(f"predator_obs VARANCE: {ocm.control_signals[0].parameters.value.get(agent_comp)}")
+    print(f"prey_obs control allocation: {ocm.control_signals[0].parameters.allocation.get(agent_comp)}")
+    print(f"prey_obs control value: {ocm.control_signals[0].parameters.value.get(agent_comp)}")
+    print(f"action mech variable: {greedy_action_mech.parameters.variable.get(agent_comp)}")
+    print(f"action mech value: {greedy_action_mech.parameters.value.get(agent_comp)}")
 
     if len(samples) == 2:
         assert np.allclose(run_results[0], [[-19.06547277,   5.47274121]])
