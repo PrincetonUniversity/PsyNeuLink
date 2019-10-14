7174294dc067a85c665edd3f4e78b77096f83408
Jan Vesely <jan.vesely@rutgers.edu>
2019-10-13 20:19:01 -0400

    llvm: Convert few remaining uses of 'context' for stateful parameters
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/components/component.py
+++ psyneulink/core/components/component.py
@@ -1261,12 +1261,12 @@ class Component(JSONDumpable, metaclass=ComponentsMeta):
             llvm_func = builder.function
 
             llvm_func.attributes.add('alwaysinline')
-            params, context, arg_in, arg_out = llvm_func.args[:len(args)]
+            params, state, arg_in, arg_out = llvm_func.args[:len(args)]
             if len(extra_args) == 0:
-                for p in params, context, arg_in, arg_out:
+                for p in params, state, arg_in, arg_out:
                     p.attributes.add('noalias')
 
-            builder = self._gen_llvm_function_body(ctx, builder, params, context, arg_in, arg_out)
+            builder = self._gen_llvm_function_body(ctx, builder, params, state, arg_in, arg_out)
             builder.ret_void()
 
         return llvm_func
