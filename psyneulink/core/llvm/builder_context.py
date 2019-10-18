5b02bb697aa2c5134fe2f378af4b21dae4e8d1b1
Jan Vesely <jan.vesely@rutgers.edu>
2019-10-17 20:33:20 -0400

    llvm/builder_context: Call caching generator wrapper directly
    Instead of manual generation and querying the name.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/llvm/builder_context.py
+++ psyneulink/core/llvm/builder_context.py
@@ -486,7 +486,7 @@ class LLVMBuilderContext:
         builder.call(input_cim_f, [context, params, comp_in, data, data])
 
         # Call pytorch internal compiled llvm func
-        pytorch_forward_func = self.get_llvm_function(self.gen_llvm_function(pytorch_model).name)
+        pytorch_forward_func = self.get_llvm_function(pytorch_model)
         input_cim_idx = composition._get_node_index(composition.input_CIM)
 
         model_context = context
