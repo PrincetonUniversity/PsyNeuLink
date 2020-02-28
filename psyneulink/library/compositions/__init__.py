b61552ffea0f8708f624b38417aec94593d8e6bc
Jan Vesely <jan.vesely@rutgers.edu>
2020-02-28 11:18:43 -0500

    compositions: Reorganize imports. Import composition_runner without pytorch
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/library/compositions/__init__.py
+++ psyneulink/library/compositions/__init__.py
@@ -1,25 +1,15 @@
-__all__ = []
+from .regressioncfa import *
+from .compositionrunner import *
+__all__ = list(regressioncfa.__all__)
+__all__.extend(compositionrunner.__all__)
 
 try:
     import torch
-    del torch
-
-    from . import autodiffcomposition
-    from . import pytorchmodelcreator
-    from . import compositionrunner
-    from . import regressioncfa
-    from . import regressioncfa
 
     from .autodiffcomposition import *
     from .pytorchmodelcreator import *
-    from .regressioncfa import *
-    from .compositionrunner import *
 
-    __all__ = list(autodiffcomposition.__all__)
+    __all__.extend(autodiffcomposition.__all__)
     __all__.extend(pytorchmodelcreator.__all__)
-    __all__.extend(regressioncfa.__all__)
-    __all__.extend(compositionrunner.__all__)
 except ImportError:
-    from . import regressioncfa
-    from .regressioncfa import *
-    __all__.extend(regressioncfa.__all__)
+    pass
