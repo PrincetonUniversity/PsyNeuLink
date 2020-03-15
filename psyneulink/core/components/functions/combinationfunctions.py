09de66e38893055e9dcbe991bc9ca4bdc09a977f
Jan Vesely <jan.vesely@rutgers.edu>
2020-03-15 18:33:11 -0400

    llvm, functions/LinearCombination: Fix exponent indexing
    Add test for non-scalar exponents.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/components/functions/combinationfunctions.py
+++ psyneulink/core/components/functions/combinationfunctions.py
@@ -1408,11 +1408,12 @@ class LinearCombination(
             ptri = b.gep(vi, [ctx.int32_ty(0), idx, index])
             in_val = b.load(ptri)
 
-            exponent_index = b.mul(ctx.int32_ty(vo.type.pointee.count),
-                                   b.sub(idx, idx.type(1)))
-            exponent_index = b.add(exponent_index, index)
             exponent = self._gen_llvm_load_param(ctx, b, params, EXPONENTS,
-                                                 exponent_index, 1.0)
+                                                 idx, 1.0)
+            # Vector of vectors (even 1-element vectors)
+            if isinstance(exponent.type, pnlvm.ir.ArrayType):
+                assert len(exponent.type) == 1 # FIXME: Add support for matrix weights
+                exponent = b.extract_value(exponent, [0])
             # FIXME: Remove this micro-optimization,
             #        it should be handled by the compiler
             if not isinstance(exponent, pnlvm.ir.Constant) or exponent.constant != 1.0:
