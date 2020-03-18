7575972e6dbbf5ec89a7b596aa89d7f821ed0bab
jdc@princeton.edu <jdc@princeton.edu>
2020-03-18 06:23:35 -0400

    * TransferMechanims   - is_finished: modify to call get_mod_termination_threshold if (parameter_port is) available

--- Scripts/Debug/bryant_lca_with_termination.py
+++ Scripts/Debug/bryant_lca_with_termination.py
@@ -25,33 +25,49 @@ activation = pnl.LCAMechanism(default_variable=[[0.0, 0.0]],
                               termination_threshold=3,
                               name='Task Activations [Act 1, Act 2]')
 
-comp = pnl.Composition(controller_mode=pnl.BEFORE)
+# Create controller
+csiController = pnl.ControlMechanism(
+        objective_mechanism=pnl.ObjectiveMechanism(monitor=[cueInterval]),
+        # monitor_for_control=cueInterval,
+        control_signals=[(pnl.TERMINATION_THRESHOLD, activation)],
+        modulation=pnl.OVERRIDE
+)
+
+comp = pnl.Composition(
+        controller_mode=pnl.BEFORE
+)
 comp.add_node(cueInterval)
 comp.add_node(taskLayer)
 comp.add_node(activation)
 comp.add_projection(sender=taskLayer, receiver=activation)
+# comp.add_node(csiController)
+comp.add_controller(csiController)
+comp.scheduler.add_condition(activation,pnl.WhenFinished(csiController))
+# comp.enable_controller=True
 
-activation.set_log_conditions([pnl.RESULT])
 
-# Create controller
-csiController = pnl.ControlMechanism(objective_mechanism=pnl.ObjectiveMechanism(monitor=[cueInterval]),
-                                     control_signals=[(pnl.TERMINATION_THRESHOLD, activation)],
-                                     # modulation=pnl.OVERRIDE
-                                     )
+cueInterval.set_log_conditions([pnl.VALUE])
+activation.set_log_conditions([pnl.RESULT, 'termination_threshold'])
+csiController.set_log_conditions([pnl.VALUE])
+taskLayer.set_log_conditions([pnl.VALUE])
 
-# comp.add_controller(csiController)
-comp.add_node(csiController)
-comp.enable_controller=True
+# csiController.control_signals[0].set_log_conditions([pnl.VALUE])
+
 
 def print_ctl(context):
-    print('\ncontrol_signal:', csiController.control_signals[0].value)
-    print('parameter_port:', activation.parameter_ports['termination_threshold'].value)
-    print('mod_term_thresh:', activation.mod_termination_threshold),
+    print('\ncontrol_signal:', csiController.control_signals[0].parameters.value.get(context))
+    print('parameter_port:', activation.parameter_ports['termination_threshold'].parameters.value.get(context))
+    print('mod_term_thresh:', activation.get_mod_termination_threshold(context)),
     print('term_thresh w/ context:', activation.parameters.termination_threshold.get(context))
 
+comp.show_graph()
 inputs = {taskLayer: [[1, 0], [1, 0], [1, 0]],
           cueInterval: [[1], [5], [1]]}
 
 comp.run(inputs, bin_execute=False, call_after_trial=print_ctl)
 
 activation.log.print_entries()
+csiController.log.print_entries()
+cueInterval.log.print_entries()
+taskLayer.log.print_entries()
+# csiController.control_signals[0].log.print_entries()
