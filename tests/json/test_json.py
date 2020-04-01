6ad98a5e5cb10de367a180bd2ff71d07fd6e7c87
Katherine Mantel <katherine@mantel.pw>
2020-03-31 23:51:16 -0400

    tests: json: split json equivalence and write_json_file tests
    So each test tests one thing, and we don't have to worry about a second trial
    for the tested composition.

--- tests/json/test_json.py
+++ tests/json/test_json.py
@@ -17,22 +17,25 @@ stroop_stimuli = {
 }
 
 
+json_results_parametrization = [
+    ('model_basic.py', 'comp', '{A: 1}'),
+    ('model_nested_comp_with_scheduler.py', 'comp', '{A: 1}'),
+    (
+        'model_with_control.py',
+        'comp',
+        '{Input: [0.5, 0.123], reward: [20, 20]}'
+    ),
+    (
+        'stroop_conflict_monitoring.py',
+        'Stroop_model',
+        str(stroop_stimuli).replace("'", '')
+    )
+]
+
+
 @pytest.mark.parametrize(
     'filename, composition_name, input_dict_str',
-    [
-        ('model_basic.py', 'comp', '{A: 1}'),
-        ('model_nested_comp_with_scheduler.py', 'comp', '{A: 1}'),
-        (
-            'model_with_control.py',
-            'comp',
-            '{Input: [0.5, 0.123], reward: [20, 20]}'
-        ),
-        (
-            'stroop_conflict_monitoring.py',
-            'Stroop_model',
-            str(stroop_stimuli).replace("'", '')
-        )
-    ]
+    json_results_parametrization
 )
 def test_json_results_equivalence(
     filename,
@@ -40,8 +43,8 @@ def test_json_results_equivalence(
     input_dict_str,
 ):
     # Get python script from file and execute
-    full_filename = f'{os.path.dirname(__file__)}/{filename}'
-    with open(full_filename, 'r') as orig_file:
+    filename = f'{os.path.dirname(__file__)}/{filename}'
+    with open(filename, 'r') as orig_file:
         exec(orig_file.read())
         exec(f'{composition_name}.run(inputs={input_dict_str})')
         orig_results = eval(f'{composition_name}.results')
@@ -56,6 +59,26 @@ def test_json_results_equivalence(
     new_results = eval(f'{composition_name}.results')
     assert orig_results == new_results
 
+
+@pytest.mark.parametrize(
+    'filename, composition_name, input_dict_str',
+    json_results_parametrization
+)
+def test_write_json_file(
+    filename,
+    composition_name,
+    input_dict_str,
+):
+    # Get python script from file and execute
+    filename = f'{os.path.dirname(__file__)}/{filename}'
+    with open(filename, 'r') as orig_file:
+        exec(orig_file.read())
+        exec(f'{composition_name}.run(inputs={input_dict_str})')
+        orig_results = eval(f'{composition_name}.results')
+
+    # reset random seed
+    pnl.core.globals.utilities.set_global_seed(0)
+
     # Save json_summary of Composition to file and read back in.
     json_filename = filename.replace('.py','.json')
     exec(f'pnl.write_json_file({composition_name}, json_filename)')
@@ -63,5 +86,3 @@ def test_json_results_equivalence(
     exec(f'{composition_name}.run(inputs={input_dict_str})')
     final_results = eval(f'{composition_name}.results')
     assert orig_results == final_results
-
-
