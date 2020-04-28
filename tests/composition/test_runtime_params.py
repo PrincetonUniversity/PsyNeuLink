9fc32345e211aeb878705fecde3cea37c8278318
jdcpni <pniintel55>
2020-04-28 11:37:20 -0400

    • test_system removed

--- tests/composition/test_runtime_params.py
+++ tests/composition/test_runtime_params.py
@@ -3,9 +3,97 @@ import numpy as np
 from psyneulink.core.components.mechanisms.processing.transfermechanism import TransferMechanism
 from psyneulink.core.compositions.composition import Composition
 from psyneulink.core.scheduling.condition import AfterTrial, Any, AtTrial
-from psyneulink.core.scheduling.scheduler import Scheduler
 
-class TestRuntimeParams:
+class TestMechanismRuntimeParams:
+
+    def test_mechanism_execute_function_param(self):
+
+        # Construction
+        T = TransferMechanism()
+        assert T.function.slope == 1.0
+        assert T.parameter_ports['slope'].value == 1.0
+
+        # Runtime param used for slope
+        T.execute(runtime_params={"slope": 10.0}, input=2.0)
+        assert T.function.slope == 10.0
+        assert T.parameter_ports['slope'].value == 10.0
+        assert T.value == 20.0
+
+        # Runtime param NOT used for slope
+        T.execute(input=2.0)
+        assert T.function.slope == 1.0
+        assert T.parameter_ports['slope'].value == 1.0
+        assert T.value == 2.0
+
+    def test_mechanism_execute_mechanism_param(self):
+
+        # Construction
+        T = TransferMechanism()
+        assert T.noise == 0.0
+        assert T.parameter_ports['noise'].value == 0.0
+
+        # Runtime param used for noise
+        T.execute(runtime_params={"noise": 10.0}, input=2.0)
+        assert T.noise == 10.0
+        assert T.parameter_ports['noise'].value == 10.0
+        assert T.value == 12.0
+
+        # Runtime param NOT used for noise
+        T.execute(input=2.0)
+        assert T.noise == 0.0
+        assert T.parameter_ports['noise'].value == 0.0
+        assert T.value == 2.0
+
+    def test_runtime_params_reset_isolated(self):
+
+        T = TransferMechanism()
+
+        # Intercept attr updated
+        T.function.intercept = 2.0
+        assert T.function.intercept == 2.0
+
+        # Runtime param used for slope
+        T.execute(runtime_params={"slope": 10.0}, input=2.0)
+        assert T.function.slope == 10.0
+        assert T.parameter_ports['slope'].value == 10.0
+
+        # Intercept attr NOT affected by runtime params
+        assert T.function.intercept == 2.0
+        assert T.value == 22.0
+
+        # Runtime param NOT used for slope
+        T.execute(input=2.0)
+        assert T.function.slope == 1.0
+        assert T.parameter_ports['slope'].value == 1.0
+
+        # Intercept attr NOT affected by runtime params reset
+        assert T.function.intercept == 2.0
+        assert T.value == 4.0
+
+    def test_runtime_params_reset_to_most_recent_val(self):
+        # NOT instance defaults
+
+        # Construction
+        T = TransferMechanism()
+        assert T.function.slope == 1.0
+        assert T.parameter_ports['slope'].value == 1.0
+
+        # Set slope attribute value directly
+        T.function.slope = 2.0
+        assert T.function.slope == 2.0
+
+        # Runtime param used for slope
+        T.execute(runtime_params={"slope": 10.0}, input=2.0)
+        assert T.function.slope == 10.0
+        assert T.parameter_ports['slope'].value == 10.0
+        assert T.value == 20.0
+
+        # Runtime param NOT used for slope - reset to most recent slope value (2.0)
+        T.execute(input=2.0)
+        assert T.function.slope == 2.0
+        assert T.value == 4.0
+
+class TestCompositionRuntimeParams:
 
     def test_composition_run_function_param_no_condition(self):
 
