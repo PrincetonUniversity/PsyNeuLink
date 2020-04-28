085f4b4dd34dd9e08fa5ab0aa49539d8d1cdf99c
Samyak K. G <samyak.k.gupta@gmail.com>
2020-04-27 21:28:30 -0400

    fixup! Refactor/autodiff (#1488)

--- psyneulink/library/compositions/compositionrunner.py
+++ psyneulink/library/compositions/compositionrunner.py
@@ -201,14 +201,11 @@ class CompositionRunner():
             self._composition.run(inputs=minibatched_input, skip_initialization=skip_initialization, context=context, skip_analyze_graph=True, bin_execute=bin_execute, *args, **kwargs)
             skip_initialization = True
 
-        # FIXME: compiled run values differ from pytorch run
-        results = self._composition.parameters.results.get(context)[-1 * num_trials * minibatch_size:] # return results from last epoch
-        # if bin_execute is not False and bin_execute != 'Python':
-        #     results = [x for x in self._composition.parameters.results.get(context)[-1 * num_trials * minibatch_size:]] # return results from last epoch
-        # else:
-        #     results = [x[0] for x in self._composition.parameters.results.get(context)[-1 * num_trials * minibatch_size:]] # return results from last epoch
+        num_epoch_results = num_trials // minibatch_size # number of results expected from final epoch
+        results = self._composition.parameters.results.get(context)[-1 * num_epoch_results:] # return results from last epoch
 
         return results
+
 class EarlyStopping(object):
     def __init__(self, mode='min', min_delta=0, patience=10):
         self.mode = mode
