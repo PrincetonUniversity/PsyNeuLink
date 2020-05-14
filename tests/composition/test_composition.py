87a99b4e1bdff9cd0764d7053ae89bc5b8a9bc82
Samyak K. G <samyak.k.gupta@gmail.com>
2020-05-13 21:14:43 -0400

    tests: Add test for functional inputs to learning

--- tests/composition/test_composition.py
+++ tests/composition/test_composition.py
@@ -5477,6 +5477,61 @@ class TestInputSpecifications:
                                                [np.array([4.])], [np.array([5.])], [np.array([6.])], [np.array([7.])],
                                                [np.array([8.])], [np.array([9.])]]
 
+    def test_function_as_learning_input(self):
+        num_epochs=2
+
+        xor_inputs = np.array(  # the inputs we will provide to the model
+            [[0, 0],
+             [0, 1],
+             [1, 0],
+             [1, 1]])
+    
+        xor_targets = np.array(  # the outputs we wish to see from the model
+            [[0],
+             [1],
+             [1],
+             [0]])
+    
+        in_to_hidden_matrix = np.random.rand(2,10)
+        hidden_to_out_matrix = np.random.rand(10,1)
+
+        input_comp = pnl.TransferMechanism(name='input_comp',
+                                    default_variable=np.zeros(2))
+
+        hidden_comp = pnl.TransferMechanism(name='hidden_comp',
+                                    default_variable=np.zeros(10),
+                                    function=pnl.Logistic())
+
+        output_comp = pnl.TransferMechanism(name='output_comp',
+                                    default_variable=np.zeros(1),
+                                    function=pnl.Logistic())
+
+        in_to_hidden_comp = pnl.MappingProjection(name='in_to_hidden_comp',
+                                    matrix=in_to_hidden_matrix.copy(),
+                                    sender=input_comp,
+                                    receiver=hidden_comp)
+
+        hidden_to_out_comp = pnl.MappingProjection(name='hidden_to_out_comp',
+                                    matrix=hidden_to_out_matrix.copy(),
+                                    sender=hidden_comp,
+                                    receiver=output_comp)
+
+        xor_comp = pnl.Composition()
+
+        backprop_pathway = xor_comp.add_backpropagation_learning_pathway([input_comp,
+                                                                            in_to_hidden_comp,
+                                                                            hidden_comp,
+                                                                            hidden_to_out_comp,
+                                                                            output_comp],
+                                                                            learning_rate=10)
+        def test_function(trial_num):
+            return {
+                input_comp: xor_inputs[trial_num]
+            }
+
+        xor_comp.learn(inputs=test_function,
+              num_trials=4)
+
     @pytest.mark.parametrize("mode", ['Python',
                                       pytest.param('LLVMRun', marks=pytest.mark.llvm),
                                       pytest.param('PTXRun', marks=[pytest.mark.llvm, pytest.mark.cuda]),
