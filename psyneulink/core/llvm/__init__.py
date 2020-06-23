28fe3d8d136f2e4c09a1de3a9d47fca56b2076b1
Jan Vesely <jan.vesely@rutgers.edu>
2020-06-22 23:21:35 -0400

    llvm/cuda: Pass number of cuda threads as keyword arg
    It shouldn't be included in 'cuda_call' args.
    Fixes: ecf9dfbdac16fc747f32b83b82284cecb63f2058
           ("llvm/cuda: Add number of threads to cuda_wrap_call invocation")
    
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/llvm/__init__.py
+++ psyneulink/core/llvm/__init__.py
@@ -93,7 +93,7 @@ class LLVMBinaryFunction:
 
     def cuda_wrap_call(self, *args, threads=1):
         wrap_args = (jit_engine.pycuda.driver.InOut(a) if isinstance(a, np.ndarray) else a for a in args)
-        self.cuda_call(*wrap_args, threads)
+        self.cuda_call(*wrap_args, threads=threads)
 
     @staticmethod
     @functools.lru_cache(maxsize=32)
