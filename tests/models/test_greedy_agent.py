1267c52def200420e918e68b1523332fc3f63b94
Jan Vesely <jan.vesely@rutgers.edu>
2020-06-28 18:42:06 -0400

    llvm, tests/models: Add large scale variant of predator-prey
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/models/test_greedy_agent.py
+++ tests/models/test_greedy_agent.py
@@ -129,10 +129,13 @@ def test_simplified_greedy_agent_random(benchmark, mode):
      pytest.param('PTXRun', marks=[pytest.mark.llvm, pytest.mark.cuda]),
 ])
 @pytest.mark.parametrize("samples", [[0,10],
+    pytest.param([a / 10.0 for a in range(0, 101)]),
     pytest.param([0,3,6,10], marks=pytest.mark.stress),
     pytest.param([0,2,4,6,8,10], marks=pytest.mark.stress),
 ], ids=lambda x: len(x))
 def test_predator_prey(benchmark, mode, samples):
+    if len(samples) > 10 and mode not in {"LLVMRun", "Python-PTX"}:
+        pytest.skip("This test takes too long")
     # OCM default mode is Python
     mode, ocm_mode = (mode + "-Python").split('-')[0:2]
     benchmark.group = "Predator-Prey " + str(len(samples))
