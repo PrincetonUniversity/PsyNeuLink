dd268c48067dae0ddce753f12e60f8d10e575683
Katherine Mantel <kmantel@princeton.edu>
2020-07-16 01:08:36 -0400

    tests/models/test_documentation_models: make model variants more extensible

--- tests/models/test_documentation_models.py
+++ tests/models/test_documentation_models.py
@@ -8,6 +8,12 @@ import pytest
 
 import psyneulink as pnl
 
+# Variants for test_documentation_models:
+
+# parameters for the model were changed to reduce running time, and will
+# not match the equivalent papers' results
+REDUCED = 'reduced'
+
 
 # NOTE: to add new models, run the script with valid results, then
 # dump the results of a Composition to a file named
@@ -22,13 +28,13 @@ import psyneulink as pnl
 # Using prettier https://prettier.io/ can reduce the line footprint of
 # the resulting file while not totally minifying it
 @pytest.mark.parametrize(
-    'model_name, composition_name, additional_args, reduced',
+    'model_name, composition_name, additional_args, variant',
     [
         pytest.param(
             'Cohen_Huston1994',
             'Bidirectional_Stroop',
             [],
-            False,
+            None,
             marks=pytest.mark.stress
         ),
         pytest.param(
@@ -38,13 +44,13 @@ import psyneulink as pnl
                 '--threshold=0.5',
                 '--settle-trials=10'
             ],
-            True
+            REDUCED
         ),
         pytest.param(
             'Cohen_Huston1994_horse_race',
             'Bidirectional_Stroop',
             [],
-            False,
+            None,
             marks=pytest.mark.stress
         ),
         pytest.param(
@@ -57,22 +63,22 @@ import psyneulink as pnl
                 '--settle-trials=10',
                 '--pre-stimulus-trials=10'
             ],
-            True
+            REDUCED
         ),
-        pytest.param('GilzenratModel', 'task', ['--noise-stddev=0.0'], False),
-        pytest.param('Kalanthroff_PCTC_2018', 'PCTC', [], False, marks=pytest.mark.stress),
-        pytest.param('Kalanthroff_PCTC_2018', 'PCTC', ['--threshold=0.2', '--settle-trials=10'], True),
-        pytest.param('MontagueDayanSejnowski96', 'comp_5a', ['--figure', '5a'], False),
-        pytest.param('MontagueDayanSejnowski96', 'comp_5b', ['--figure', '5b'], False),
-        pytest.param('MontagueDayanSejnowski96', 'comp_5c', ['--figure', '5c'], False),
-        pytest.param('Nieuwenhuis2005Model', 'task', [], False),
+        pytest.param('GilzenratModel', 'task', ['--noise-stddev=0.0'], None),
+        pytest.param('Kalanthroff_PCTC_2018', 'PCTC', [], None, marks=pytest.mark.stress),
+        pytest.param('Kalanthroff_PCTC_2018', 'PCTC', ['--threshold=0.2', '--settle-trials=10'], REDUCED),
+        pytest.param('MontagueDayanSejnowski96', 'comp_5a', ['--figure', '5a'], None),
+        pytest.param('MontagueDayanSejnowski96', 'comp_5b', ['--figure', '5b'], None),
+        pytest.param('MontagueDayanSejnowski96', 'comp_5c', ['--figure', '5c'], None),
+        pytest.param('Nieuwenhuis2005Model', 'task', [], None),
     ]
 )
 def test_documentation_models(
     model_name,
     composition_name,
     additional_args,
-    reduced,
+    variant,
 ):
     models_dir = os.path.join(
         os.path.dirname(__file__),
@@ -88,10 +94,11 @@ def test_documentation_models(
     script_globals = runpy.run_path(model_file)
     sys.argv = old_argv
 
+    suffix = f'-{variant}' if variant is not None else ''
     expected_results_file = os.path.join(
         models_dir,
         'results',
-        f'{model_name}-{composition_name}{"-reduced" if reduced else ""}.json'
+        f'{model_name}-{composition_name}{suffix}.json'
     )
     with open(expected_results_file) as fi:
         expected_results = pnl.convert_all_elements_to_np_array(json.loads(fi.read()))
