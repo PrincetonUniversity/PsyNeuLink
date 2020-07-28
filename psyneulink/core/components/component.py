0e715623d2cc24068355ac34f81cb7a68dd9d953
Jan Vesely <jan.vesely@rutgers.edu>
2020-07-28 19:47:31 -0400

    llvm, Component: Drop 'objective-mechanism' from the list of compiled parameters
    It's not used during computation.
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/components/component.py
+++ psyneulink/core/components/component.py
@@ -1248,6 +1248,8 @@ class Component(JSONDumpable, metaclass=ComponentsMeta):
                      "execution_count", "value"}
         # mechanism functions are handled separately
         blacklist = {"function"} if hasattr(self, 'ports') else {"value"}
+        # 'objective_mechanism' parameter is just for reference
+        blacklist.add("objective_mechanism")
         def _is_compilation_state(p):
             val = p.get()   # memoize for this function
             return val is not None and p.name not in blacklist and \
@@ -1311,6 +1313,8 @@ class Component(JSONDumpable, metaclass=ComponentsMeta):
         # * integration rate -- shape mismatch with param port input
         if hasattr(self, 'ports'):
             blacklist.update(["function", "matrix", "integration_rate"])
+        # 'objective_mechanism' parameter is just for reference
+        blacklist.add("objective_mechanism")
         def _is_compilation_param(p):
             if p.name not in blacklist and not isinstance(p, ParameterAlias):
                 #FIXME: this should use defaults
