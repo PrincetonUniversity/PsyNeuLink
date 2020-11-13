2512da8a5b9bfbbbc713f2c8d58ed0635cbd9c0a
Katherine Mantel <kmantel@princeton.edu>
2020-11-12 23:24:37 -0500

    Port._get_port_function_value: pass context

--- psyneulink/core/components/ports/port.py
+++ psyneulink/core/components/ports/port.py
@@ -782,7 +782,7 @@ from psyneulink.core.components.functions.combinationfunctions import Combinatio
 from psyneulink.core.components.functions.function import Function, get_param_value_for_keyword, is_function_type
 from psyneulink.core.components.functions.transferfunctions import Linear
 from psyneulink.core.components.shellclasses import Mechanism, Projection, Port
-from psyneulink.core.globals.context import Context, ContextFlags
+from psyneulink.core.globals.context import ContextFlags, handle_external_context
 from psyneulink.core.globals.keywords import \
     ADDITIVE, ADDITIVE_PARAM, AUTO_ASSIGN_MATRIX, \
     CONTEXT, CONTROL_PROJECTION_PARAMS, CONTROL_SIGNAL_SPECS, DEFERRED_INITIALIZATION, DISABLE, EXPONENT, \
@@ -2326,7 +2326,10 @@ class Port_Base(Port):
         return builder
 
     @staticmethod
-    def _get_port_function_value(owner, function, variable):
+    # TODO: find out why UNSET is required to avoid many more deepcopies
+    # occurring causing slowdowns
+    @handle_external_context(source=ContextFlags.UNSET)
+    def _get_port_function_value(owner, function, variable, context=None):
         """Execute the function of a Port and return its value
         # FIX: CONSIDER INTEGRATING THIS INTO _EXECUTE FOR PORT?
 
@@ -2334,7 +2337,7 @@ class Port_Base(Port):
         Used primarily during validation, when the function may not have been fully instantiated yet
         (e.g., InputPort must sometimes embed its variable in a list-- see InputPort._get_port_function_value).
         """
-        return function.execute(variable, context=Context(source=ContextFlags.UNSET, execution_id=None))
+        return function.execute(variable, context=context)
 
     @property
     def _dependent_components(self):
