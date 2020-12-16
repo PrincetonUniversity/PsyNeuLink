d2bab6e95ea4610625cc7e42c7babec4af86e93a
Katherine Mantel <kmantel@princeton.edu>
2020-12-16 18:47:37 -0500

    conditions: Not: rewrite to correct json_summary output

--- psyneulink/core/scheduling/condition.py
+++ psyneulink/core/scheduling/condition.py
@@ -720,9 +720,9 @@ class Not(Condition):
     def __init__(self, condition):
         self.condition = condition
 
-        def inner_func(*args, **kwargs):
+        def inner_func(condition, *args, **kwargs):
             return not condition.is_satisfied(*args, **kwargs)
-        super().__init__(inner_func)
+        super().__init__(inner_func, condition)
 
     @Condition.owner.setter
     def owner(self, value):
