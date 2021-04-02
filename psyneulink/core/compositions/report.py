bce695373a370805b200b73ca0ac600e9076b5f3
jdcpni <pniintel55>
2021-04-01 22:59:57 -0400

    -

--- psyneulink/core/compositions/report.py
+++ psyneulink/core/compositions/report.py
@@ -1746,7 +1746,11 @@ class Report:
                     num_trials_str = f' of {output_report.num_trials}'
             else:
                 num_trials_str = ''
-            if output_report.num_trials > 1 or trial_num+1 > 1:
+
+            # # This disallows the plural in following construction: "1 of 1 trial"
+            # if output_report.num_trials > 1 or trial_num+1 > 1:
+            # This allows the plural in following construction: "! of 1 trials"
+            if output_report.num_trials > 1 or trial_num+1 > 1 or num_trials_str:
                 s = 's'
             else:
                 s = ''
