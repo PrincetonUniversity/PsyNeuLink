e9224ad5a158bd6c79caf839b70eaa1040e482eb
jdcpni <jdc@princeton.edu>
2021-04-24 23:29:43 -0400

    -

--- psyneulink/core/components/functions/statefulfunctions/memoryfunctions.py
+++ psyneulink/core/components/functions/statefulfunctions/memoryfunctions.py
@@ -639,6 +639,93 @@ class ContentAddressableMemory(MemoryFunction): # ------------------------------
         array([[ 1.,  5.],
                [10., 11.]])
 
+    # FIX: ADD EXAMPLES FOR ENTRIES WITH DIFFERENT SHAPES
+    # FIX: ADD EXAMPLES FOR DUPLICATE ENTRIES
+    *Duplicate entries*
+
+    By default, duplicate entries are precluded from a ContentAddressableMechanism's `memory
+    <ContentAddressableMechanism.memory>`.  So, for an initializer with identical entries, only one copy of
+    the duplicates will be stored::
+
+        >>> c = ContentAddressableMemory(initializer=[[[1,2],[3,4]],
+        ...                                           [[1,2],[3,4]]])
+        >>> c.memory
+        array([[[1., 2.],
+                [3., 4.]]])
+
+    and using the same array as input to the function will retrieve that array but not store another copy::
+
+        >>> c([[1,2],[3,4]])
+        array([[1., 2.],
+               [3., 4.]])
+        >>> c.memory
+        array([[[1., 2.],
+                [3., 4.]]])
+
+    Only fields with non-zero weights in `distance_field_weights <ContentAddressableMechanism.distance_field_weights>`
+    are considered when evaluating whether entries are duplicates. So, in the following example, where the weight
+    for the second field is 0, the two entries are considered duplicates and only the first is stored::
+
+        >>> c = ContentAddressableMemory(initializer=[[[1,2],[3,4]],
+        ...                                           [[1,2],[5,6]]],
+        ...                              distance_field_weights=[1,0])
+        >>> c.memory
+        array([[[1., 2.],
+                [3., 4.]]])
+
+    Duplicates can be allowed by setting the **duplicate_entries_allowed** argument to True or *OVERWRITE*.  Setting
+    it to True allows duplicate entries to accumulate in `memory <ContentAddressableMechanism.memory>`, as shown
+    here::
+
+        >>> c = ContentAddressableMemory(initializer=[[[1,2],[3,4]],
+        ...                                            [[1,5],[10,11]]],
+        ...                              duplicate_entries_allowed=True)
+        >>> c([[1,2],[3,4]])
+        array([[1., 2.],
+               [3., 4.]])
+        >>> c.memory
+        array([[[ 1.,  2.],
+                [ 3.,  4.]],
+        <BLANKLINE>
+               [[ 1.,  5.],
+                [10., 11.]],
+        <BLANKLINE>
+               [[ 1.,  2.],
+                [ 3.,  4.]]])
+
+    Duplicates are determined by comparing entries using the functions `distance_function
+    <ContentAddressableMechanism.distance_function>`;  if the `distance <ContentAddressableMechanism.distance>`
+    is less than `duplicate_threshold <ContentAddressableMechanism.duplicate_threshold>`, they are considered to be
+    duplicates;  otherwise they are treated a distinct entries.  By default, `duplicate_threshold
+    <ContentAddressableMechanism.duplicate_threshold>` is 0.  In the folloiwng example it is increased, so that
+    two very similar, but non-identical entries, are nonetheless treated as duplicates::
+
+        >>> c = ContentAddressableMemory(initializer=[[[1, 2.0], [3, 4]],
+        ...                                           [[1, 2.5], [3, 4]]],
+        ...                              duplicate_entries_allowed=False,
+        ...                              duplicate_threshold=0.2)
+
+        >>> c.memory
+        array([[[1., 2.],
+                [3., 4.]]])
+
+    Setting **duplicate_entries_allowed** argument to *OVERWRITE* allows an entry to replace one that is considered
+    duplicate, even if it is not identical, as in the following example::
+
+        >>> c.duplicate_entries_allowed=OVERWRITE
+        >>> c([[1, 2.1], [3, 4]])
+        array([[[1., 2.],
+                [3., 4.]]])
+        >>> c.memory
+        array([[[1. , 2.1],
+                [3. , 4. ]]])
+
+    Note that the entry considered to be the duplicate (``[[1, 2.1], [3, 4]]``) is returned, and then replaced in
+    `memory <ContentAddressableMechanism.memory>`.  Finally, if `duplicate_entries_allowed
+    <ContentAddressableMechanism.duplicate_entries_allowed>` is True, and duplicates have accumulated, the
+    `equidistant_entries_select <ContentAddressableMemory.equidistant_entries_select>` attribute can be used to
+    specify how to select among them for retrieval, either by chosing randomly (*RANDOM*) or selecting either the
+    first one (*OLDEST*) or last one (*NEWEST*) stored.
 
     .. _ContentAddressableMemory_Class_Reference:
 
