6c64f23ea2fae53b943a60e2a51b653859d17c01
jdcpni <jdc@princeton.edu>
2021-05-02 21:01:37 -0400

    -

--- psyneulink/core/components/functions/__init__.py
+++ psyneulink/core/components/functions/__init__.py
@@ -1,12 +1,11 @@
 from . import function
-from . import userdefinedfunction
 from .nonstatefulfunctions import selectionfunctions, objectivefunctions, optimizationfunctions, combinationfunctions, \
     learningfunctions, transferfunctions, distributionfunctions
 from . import statefulfunctions
+from .statefulfunctions import integratorfunctions, memoryfunctions
+from . import userdefinedfunction
 
 from .function import *
-from .statefulfunctions import *
-from .userdefinedfunction import *
 from psyneulink.core.components.functions.nonstatefulfunctions.combinationfunctions import *
 from psyneulink.core.components.functions.nonstatefulfunctions.transferfunctions import *
 from psyneulink.core.components.functions.nonstatefulfunctions.selectionfunctions import *
@@ -14,6 +13,10 @@ from psyneulink.core.components.functions.nonstatefulfunctions.distributionfunct
 from psyneulink.core.components.functions.nonstatefulfunctions.objectivefunctions import *
 from psyneulink.core.components.functions.nonstatefulfunctions.optimizationfunctions import *
 from psyneulink.core.components.functions.nonstatefulfunctions.learningfunctions import *
+from .statefulfunctions import *
+from psyneulink.core.components.functions.statefulfunctions.integratorfunctions import *
+from psyneulink.core.components.functions.statefulfunctions.memoryfunctions import *
+from .userdefinedfunction import *
 
 __all__ = list(function.__all__)
 __all__.extend(userdefinedfunction.__all__)
@@ -25,3 +28,5 @@ __all__.extend(distributionfunctions.__all__)
 __all__.extend(objectivefunctions.__all__)
 __all__.extend(optimizationfunctions.__all__)
 __all__.extend(learningfunctions.__all__)
+__all__.extend(integratorfunctions.__all__)
+__all__.extend(memoryfunctions.__all__)
