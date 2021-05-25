294fc601cec6697cb578b8cd59eb570b17f5dd54
Jan Vesely <jan.vesely@rutgers.edu>
2021-05-25 13:51:12 -0400

    Require at least Python 3.7
    Include support for Python 3.9
    
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- setup.py
+++ setup.py
@@ -57,13 +57,13 @@ setup(
 
         # Supported Python Versions
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # Require recent python
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 
     # What does your project relate to?
     keywords='cognitive modeling',
