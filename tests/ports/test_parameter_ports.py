1fb7793de379f6d6f1c1a2ab33e94d06b7161ea6
jdcpni <jdc@princeton.edu>
2022-02-18 07:42:20 -0500

    • test_parameter_ports.py:   - add test_no_path_afferents()   - add test_no_efferents()

--- tests/ports/test_parameter_ports.py
+++ tests/ports/test_parameter_ports.py
@@ -71,10 +71,26 @@ class TestParameterPorts:
         assert "Contructor for ParameterPort cannot be called directly(context: None" in str(error_text.value)
 
     def test_no_path_afferents(self):
-        pass
+        A = TransferMechanism()
+        with pytest.raises(pnl.PortError) as error:
+            A.parameter_ports['slope'].path_afferents
+        assert '"ParameterPorts do not have \'path_afferents\'; (access attempted for TransferMechanism-0[slope])."' \
+               in str(error.value)
+        with pytest.raises(pnl.PortError) as error:
+            A.parameter_ports['slope'].path_afferents = ['test']
+        assert '"ParameterPorts are not allowed to have \'path_afferents\' ' \
+               '(assignment attempted for TransferMechanism-0[slope])."' in str(error.value)
 
     def test_no_efferents(self):
-        pass
+        A = TransferMechanism()
+        with pytest.raises(pnl.PortError) as error:
+            A.parameter_ports['slope'].efferents
+        assert '"ParameterPorts do not have \'efferents\'; (access attempted for TransferMechanism-0[slope])."' \
+               in str(error.value)
+        with pytest.raises(pnl.PortError) as error:
+            A.parameter_ports['slope'].efferents = ['test']
+        assert '"ParameterPorts are not allowed to have \'efferents\' ' \
+               '(assignment attempted for TransferMechanism-0[slope])."' in str(error.value)
 
 class TestConfigurableParameters:
     def test_configurable_params(self):
