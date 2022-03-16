bc8a045caeed195c60d68578e6f02e95707a914c
jdcpni <pniintel55>
2022-03-16 13:29:54 -0400

    • conf.py:   comment out 'sphinx_autodoc_typehints' (crashing for sphinx_autodoc_typehints v.1.17.0)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
-    'sphinx_autodoc_typehints',
+    # 'sphinx_autodoc_typehints',
     'technical_note'
     # 'sphinx.ext.imgmath'
 ]
