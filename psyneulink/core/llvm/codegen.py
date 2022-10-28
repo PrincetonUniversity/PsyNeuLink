PrincetonUniversity/PsyNeuLink

commit 7c04633da357a9997987bcffcb653f8c0386e7e2

llvm, ocm/evaluate: Add support for "evaluate_type_all_results"
The new evaluation type retrieves all results (every trial of every
evaluated sample).
The new evaluation type changes the output type, and calls
the adds "simulation_results" to the "simulation" and "run" tags used
by the compiled composition function.

Enable results reporting in simulation tag when compiling with
"simulation_results" tag.

This new evaluation mode is enabled by passing "get_results=True" to
either 'cuda_evaluate' or 'thread_evaluate'.

Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

psyneulink/core/llvm/codegen.py
@@ -1008,7 +1008,7 @@ def gen_composition_run(ctx, composition, *, tags:frozenset):
 
     # simulation does not care about the output
     # it extracts results of the controller objective mechanism
-    if simulation:
+    if simulation and not "simulation_results" in tags:
         data_out.attributes.remove('nonnull')
 
     if not simulation and "const_data" in debug_env:
@@ -1085,11 +1085,11 @@ def gen_composition_run(ctx, composition, *, tags:frozenset):
     data_in_ptr = builder.gep(data_in, [input_idx])
 
     # Call execution
-    exec_tags = tags.difference({"run"})
+    exec_tags = tags.difference({"run", "simulation_results"})
     exec_f = ctx.import_llvm_function(composition, tags=exec_tags)
     builder.call(exec_f, [state, params, data_in_ptr, data, cond])
 
-    if not simulation:
+    if not simulation or "simulation_results" in tags:
         # Extract output_CIM result
         idx = composition._get_node_index(composition.output_CIM)
         result_ptr = builder.gep(data, [ctx.int32_ty(0), ctx.int32_ty(0),

psyneulink/core/components/functions/nonstateful/optimizationfunctions.py
@@ -1689,7 +1689,7 @@ class GridSearch(OptimizationFunction):
         if ocm is not None:
             assert ocm.function is self
             sample_t = ocm._get_evaluate_alloc_struct_type(ctx)
-            value_t = ocm._get_evaluate_output_struct_type(ctx)
+            value_t = ocm._get_evaluate_output_struct_type(ctx, tags)
         else:
             obj_func = ctx.import_llvm_function(self.objective_function)
             sample_t = obj_func.args[2].type.pointee

psyneulink/core/components/mechanisms/modulatory/control/optimizationcontrolmechanism.py
@@ -3210,7 +3210,10 @@ class OptimizationControlMechanism(ControlMechanism):
                                            context=context
                                            )
 
-    def _get_evaluate_output_struct_type(self, ctx):
+    def _get_evaluate_output_struct_type(self, ctx, tags):
+        if "evaluate_type_all_results" in tags:
+            return ctx.get_output_struct_type(self.agent_rep)
+        assert "evaluate_type_objective" in tags
         # Returns a scalar that is the predicted net_outcome
         return ctx.float_ty
 
@@ -3314,6 +3317,8 @@ class OptimizationControlMechanism(ControlMechanism):
         my_idx = self.composition._get_node_index(self)
         my_params = builder.gep(nodes_params, [ctx.int32_ty(0),
                                                ctx.int32_ty(my_idx)])
+        num_trials_per_estimate_ptr = pnlvm.helpers.get_param_ptr(builder, self,
+                                                                  my_params, "num_trials_per_estimate")
         func_params = pnlvm.helpers.get_param_ptr(builder, self,
                                                   my_params, "function")
         search_space = pnlvm.helpers.get_param_ptr(builder, self.function,
@@ -3324,6 +3329,8 @@ class OptimizationControlMechanism(ControlMechanism):
 
             if "evaluate_type_objective" in tags:
                 out_idx = idx
+            elif "evaluate_type_all_results" in tags:
+                out_idx = builder.mul(idx, builder.load(num_trials_per_estimate_ptr))
             else:
                 assert False, "Evaluation type not detected in tags, or unknown: {}".format(tags)
 
@@ -3340,7 +3347,7 @@ class OptimizationControlMechanism(ControlMechanism):
         args = [ctx.get_param_struct_type(self.agent_rep).as_pointer(),
                 ctx.get_state_struct_type(self.agent_rep).as_pointer(),
                 self._get_evaluate_alloc_struct_type(ctx).as_pointer(),
-                self._get_evaluate_output_struct_type(ctx).as_pointer(),
+                self._get_evaluate_output_struct_type(ctx, tags).as_pointer(),
                 ctx.get_input_struct_type(self.agent_rep).as_pointer(),
                 ctx.get_data_struct_type(self.agent_rep).as_pointer()]
 
@@ -3387,8 +3394,10 @@ class OptimizationControlMechanism(ControlMechanism):
                                                        ctx.int32_ty(controller_idx)])
 
         # Get simulation function
-        sim_f = ctx.import_llvm_function(self.agent_rep,
-                                         tags=frozenset({"run", "simulation"}))
+        agent_tags = {"run", "simulation"}
+        if "evaluate_type_all_results" in tags:
+            agent_tags.add("simulation_results")
+        sim_f = ctx.import_llvm_function(self.agent_rep, tags=frozenset(agent_tags))
 
         # Apply allocation sample to simulation data
         assert len(self.output_ports) == len(allocation_sample.type.pointee)
@@ -3437,9 +3446,11 @@ class OptimizationControlMechanism(ControlMechanism):
         num_inputs = builder.alloca(ctx.int32_ty, name="num_sim_inputs")
         builder.store(num_inputs.type.pointee(1), num_inputs)
 
-        # Simulations don't store output
-        if "evaluate_type_objective" in tags:
+        # Simulations don't store output unless we run parameter fitting
+        if 'evaluate_type_objective' in tags:
             comp_output = sim_f.args[4].type(None)
+        elif 'evaluate_type_all_results' in tags:
+            comp_output = arg_out
         else:
             assert False, "Evaluation type not detected in tags, or unknown: {}".format(tags)
 
@@ -3462,6 +3473,8 @@ class OptimizationControlMechanism(ControlMechanism):
             builder.call(net_outcome_f, [controller_params, controller_state,
                                          allocation_sample, objective_val_ptr,
                                          arg_out])
+        elif "evaluate_type_all_results" in tags:
+            pass
         else:
             assert False, "Evaluation type not detected in tags, or unknown: {}".format(tags)
 

psyneulink/core/llvm/execution.py
@@ -679,11 +679,12 @@ class CompExecution(CUDAExecution):
             assert runs_np[0] <= runs, "Composition ran more times than allowed!"
             return _convert_ctype_to_python(ct_out)[0:runs_np[0]]
 
-    def _prepare_evaluate(self, inputs, num_input_sets, num_evaluations):
+    def _prepare_evaluate(self, inputs, num_input_sets, num_evaluations, all_results:bool):
         ocm = self._composition.controller
         assert len(self._execution_contexts) == 1
 
-        tags = {"evaluate", "alloc_range", "evaluate_type_objective"}
+        eval_type = "evaluate_type_all_results" if all_results else "evaluate_type_objective"
+        tags = {"evaluate", "alloc_range", eval_type}
         bin_func = pnlvm.LLVMBinaryFunction.from_obj(ocm, tags=frozenset(tags))
         self.__bin_func = bin_func
 
@@ -702,14 +703,18 @@ class CompExecution(CUDAExecution):
         ct_inputs = self._get_run_input_struct(inputs, num_input_sets, 5)
 
         # Output ctype
-        out_ty = bin_func.byref_arg_types[4] * num_evaluations
+        out_el_ty = bin_func.byref_arg_types[4]
+        if all_results:
+            out_el_ty *= ocm.parameters.num_trials_per_estimate.get(self._execution_contexts[0])
+        out_ty = out_el_ty * num_evaluations
+
 
         # return variable as numpy array. pycuda can use it directly
         return ct_comp_param, ct_comp_state, ct_comp_data, ct_inputs, out_ty
 
-    def cuda_evaluate(self, inputs, num_input_sets, num_evaluations):
+    def cuda_evaluate(self, inputs, num_input_sets, num_evaluations, all_results:bool=False):
         ct_comp_param, ct_comp_state, ct_comp_data, ct_inputs, out_ty = \
-            self._prepare_evaluate(inputs, num_input_sets, num_evaluations)
+            self._prepare_evaluate(inputs, num_input_sets, num_evaluations, all_results)
 
         # Output is allocated on device, but we need the ctype (out_ty).
         cuda_args = (self.upload_ctype(ct_comp_param, 'params'),
@@ -724,9 +729,9 @@ class CompExecution(CUDAExecution):
 
         return ct_results
 
-    def thread_evaluate(self, inputs, num_input_sets, num_evaluations):
+    def thread_evaluate(self, inputs, num_input_sets, num_evaluations, all_results:bool=False):
         ct_param, ct_state, ct_data, ct_inputs, out_ty = \
-            self._prepare_evaluate(inputs, num_input_sets, num_evaluations)
+            self._prepare_evaluate(inputs, num_input_sets, num_evaluations, all_results)
 
         ct_results = out_ty()
         jobs = min(os.cpu_count(), num_evaluations)
@@ -739,7 +744,7 @@ class CompExecution(CUDAExecution):
             results = [ex.submit(self.__bin_func, ct_param, ct_state,
                                  int(i * evals_per_job),
                                  min((i + 1) * evals_per_job, num_evaluations),
-                                 ct_results,
+                                 ctypes.cast(ct_results, self.__bin_func.c_func.argtypes[4]),
                                  ctypes.cast(ctypes.byref(ct_inputs), self.__bin_func.c_func.argtypes[5]),
                                  ct_data)
                        for i in range(jobs)]

commit 925d6e7de6c13e8c7e5faa1d6e4e91cb9d396dd7

llvm/codegen: E713 test for membership should be 'not in'
Fixes: 7c04633da357a9997987bcffcb653f8c0386e7e2
Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

psyneulink/core/llvm/codegen.py
@@ -1008,7 +1008,7 @@ def gen_composition_run(ctx, composition, *, tags:frozenset):
 
     # simulation does not care about the output
     # it extracts results of the controller objective mechanism
-    if simulation and not "simulation_results" in tags:
+    if simulation and "simulation_results" not in tags:
         data_out.attributes.remove('nonnull')
 
     if not simulation and "const_data" in debug_env: