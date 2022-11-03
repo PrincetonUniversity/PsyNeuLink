c5721ea01ad51260b27c8948e7f8b043e10b6488
jdcpni <jdc@princeton.edu>
2022-11-02 22:33:08 -0400

    [skip ci] • test_composition.py:   - add test_pathway_tuple_specs()

--- tests/composition/test_composition.py
+++ tests/composition/test_composition.py
@@ -1012,6 +1012,40 @@ class TestCompositionPathwayArgsAndAdditionMethods:
             assert all(node in comp.get_nodes_by_role(NodeRole.INPUT) for node in {A,C})
             assert all(node in comp.get_nodes_by_role(NodeRole.OUTPUT) for node in {B,D})
 
+
+    config = [
+        ('([{A,B,C},D,E],Proj)', 'a'),
+        ('([{A,B,C},Proj_1,D,E],Proj_2)', 'b'),
+        ('([{A,B,C},D,Proj_1,E],Proj_2)', 'b'),
+        ('Pathway(default_matrix)', 'd')
+    ]
+    @pytest.mark.parametrize('config', config, ids=[x[0] for x in config])
+    def test_pathway_tuple_specs(self, config):
+        A = ProcessingMechanism(name='A')
+        B = ProcessingMechanism(name='B')
+        # B_comparator = ComparatorMechanism(name='B COMPARATOR')
+        C = ProcessingMechanism(name='C')
+        D = ProcessingMechanism(name='D')
+        E = ProcessingMechanism(name='E')
+        F = ProcessingMechanism(name='F')
+        if config[0]=='a':
+            comp = Composition(([{A,B,C},D,E],[2.9]))
+            assert all([p.matrix.base==2.9 for p in D.path_afferents])
+            assert E.path_afferents[0].matrix.base==2.9
+        if config[0]=='b':
+            comp = Composition(([{A,B,C},[1.6],D,E],[2.9]))
+            assert all([p.matrix.base==1.6 for p in D.path_afferents])
+            assert E.path_afferents[0].matrix.base==2.9
+        if config[0]=='c':
+            comp = Composition(([{A,B,C},D,[1.6],E],[2.9]))
+            assert all([p.matrix.base==2.9 for p in D.path_afferents])
+            assert E.path_afferents[0].matrix.base==1.6
+        if config[0]=='d':
+            pway=Pathway([{A,B,C},[1.6],D,E], default_projection_matrix=[2.9])
+            comp = Composition(pway)
+            assert all([p.matrix.base==1.6 for p in D.path_afferents])
+            assert E.path_afferents[0].matrix.base==2.9
+
     def test_add_pathways_bad_arg_error(self):
         I = InputPort(name='I')
         c = Composition()
