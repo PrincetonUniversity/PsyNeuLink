75e26d9853e5360a62d08c4c4b6d5c65ef3b34db
jdcpni <jdc@princeton.edu>
2022-12-16 07:18:56 -0500

    Merge branch 'fit2-compiled' of https://github.com/PrincetonUniversity/PsyNeuLink into fit2-compiled

--- psyneulink/core/compositions/parameterestimationcomposition.py
+++ psyneulink/core/compositions/parameterestimationcomposition.py
@@ -470,6 +470,16 @@ class ParameterEstimationComposition(Composition):
 
         self._validate_params(locals())
 
+        # # MODIFIED 12/15/22 OLD:
+        # # Assign model
+        # if model is not None:
+        #     # If model has been specified, assign as (only) node in PEC, otherwise specification(s) in kwargs are used
+        #     # (Note: _validate_params() ensures that either model or nodes and/or pathways are specified, but not both)
+        #     kwargs.update({'nodes': model})
+        #     self.model = model
+        # else:
+        #     self.model = self
+        # MODIFIED 12/15/22 NEW:
         # IMPLEMENTATION NOTE: this currently assigns pec as ocm.agent_rep (rather than model) to satisfy LLVM
         # Assign model as nested Composition of PEC
         if not model:
@@ -495,6 +505,7 @@ class ParameterEstimationComposition(Composition):
         # Assign model as nested composition in PEC and self.model as self
         kwargs.update({'nodes': model})
         self.model = model
+        # MODIFIED 12/15/22 END
 
         self.optimized_parameter_values = []
 
@@ -541,7 +552,8 @@ class ParameterEstimationComposition(Composition):
         #     - search for seed params in _instantiate_ocm doesn't include pem itself or its functions)
         # IMPLEMENTATION NOTE: self is assigned as agent_rep to satisfy requirements of LLVM
         # TBI: refactor so that agent_rep = model
-        ocm = self._instantiate_ocm(agent_rep=self,
+        ocm = self._instantiate_ocm(# agent_rep=self.model, # STILL REQUIRED FOR test_parameter_estimation_composition
+                                    agent_rep = self,
                                     parameters=parameters,
                                     outcome_variables=outcome_variables,
                                     data=self.data,
@@ -553,7 +565,6 @@ class ParameterEstimationComposition(Composition):
                                     same_seed_for_all_parameter_combinations=same_seed_for_all_parameter_combinations,
                                     return_results=return_results,
                                     context=context)
-
         self.add_controller(ocm, context)
 
         # If we are using data fitting mode.
