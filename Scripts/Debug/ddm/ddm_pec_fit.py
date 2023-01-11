cdd8232eece98b1f2a645676694fbad3da2db627
David Turner <davidt0x@gmail.com>
2023-01-11 15:39:32 -0500

    Change ddm_pec_fit to debug non_decision_time issue

--- Scripts/Debug/ddm/ddm_pec_fit.py
+++ Scripts/Debug/ddm/ddm_pec_fit.py
@@ -4,7 +4,7 @@ import psyneulink as pnl
 import pandas as pd
 
 from psyneulink.core.globals.utilities import set_global_seed
-from psyneulink.core.components.functions.fitfunctions import MaxLikelihoodEstimator
+from psyneulink.core.components.functions.nonstateful.fitfunctions import MaxLikelihoodEstimator
 
 # Let's make things reproducible
 seed = 0
@@ -12,25 +12,43 @@ np.random.seed(seed)
 set_global_seed(seed)
 
 # High-level parameters the impact performance of the test
-num_trials = 40
+num_trials = 25
 time_step_size = 0.01
 num_estimates = 40
 
-ddm_params = dict(starting_value=0.0, rate=0.3, noise=1.0,
-                  threshold=0.6, non_decision_time=0.15, time_step_size=time_step_size)
+ddm_params = dict(
+    starting_value=0.0,
+    rate=0.3,
+    noise=1.0,
+    threshold=0.6,
+    non_decision_time=0.15,
+    time_step_size=time_step_size,
+)
 
 # Create a simple one mechanism composition containing a DDM in integrator mode.
-decision = pnl.DDM(function=pnl.DriftDiffusionIntegrator(**ddm_params),
-                   output_ports=[pnl.DECISION_OUTCOME, pnl.RESPONSE_TIME],
-                   name='DDM')
+decision = pnl.DDM(
+    function=pnl.DriftDiffusionIntegrator(**ddm_params),
+    output_ports=[pnl.DECISION_OUTCOME, pnl.RESPONSE_TIME],
+    name="DDM",
+)
 
 comp = pnl.Composition(pathways=decision)
 
 # Let's generate an "experimental" dataset to fit. This is a parameter recovery test
-# The input will be num_trials trials of the same constant stimulus drift rate of 1
-# trial_inputs = np.concatenate((np.repeat(-30.0, 30), np.repeat(30.0, 30)))[:, None]
-trial_inputs = np.concatenate((np.repeat(-30.0, 2), np.repeat(30.0, 2)))[:, None]
+# Lets make 10% of the trials have a positive stimulus drift rate, and the other 90%
+# have a negative stimulus drift rate.
 # trial_inputs = np.ones((num_trials, 1))
+rng = np.random.default_rng(12345)
+trial_inputs = rng.choice(
+    [5.0, -5.0], size=(num_trials, 1), p=[0.10, 0.9], replace=True
+)
+
+# Make the first and last input positive for sure. This helps make sure inputs are really getting
+# passed to the composition correctly during parameter fitting, and we aren't just getting a single
+# trials worth of a cached input.
+trial_inputs[0] = np.abs(trial_inputs[0])
+trial_inputs[-1] = np.abs(trial_inputs[-1])
+
 inputs_dict = {decision: trial_inputs}
 
 # Store the results of this "experiment" as a numpy array. This should be a
@@ -43,33 +61,42 @@ inputs_dict = {decision: trial_inputs}
 comp.run(inputs=inputs_dict)
 results = comp.results
 
-data_to_fit = pd.DataFrame(np.squeeze(np.array(results)), columns=['decision', 'response_time'])
-data_to_fit['decision'] = data_to_fit['decision'].astype('category')
+data_to_fit = pd.DataFrame(
+    np.squeeze(np.array(results)), columns=["decision", "response_time"]
+)
+data_to_fit["decision"] = data_to_fit["decision"].astype("category")
 
 # Create a parameter estimation composition to fit the data we just generated and hopefully recover the
 # parameters of the DDM.
 
 fit_parameters = {
-    ('rate', decision): np.linspace(0.0, 0.4, 1000),
-    ('threshold', decision): np.linspace(0.5, 1.0, 1000),
-    # ('non_decision_time', decision): np.linspace(0.0, 1.0, 1000),
+    ("rate", decision): np.linspace(-0.5, 0.5, 1000),
+    ("threshold", decision): np.linspace(0.5, 1.0, 1000),
+    ('non_decision_time', decision): np.linspace(0.0, 1.0, 1000),
 }
 
-pec = pnl.ParameterEstimationComposition(name='pec',
-                                         nodes=comp,
-                                         parameters=fit_parameters,
-                                         outcome_variables=[decision.output_ports[pnl.DECISION_OUTCOME],
-                                                            decision.output_ports[pnl.RESPONSE_TIME]],
-                                         data=data_to_fit,
-                                         optimization_function=MaxLikelihoodEstimator(),
-                                         num_estimates=num_estimates,
-                                         num_trials_per_estimate=len(trial_inputs),
-                                         )
+pec = pnl.ParameterEstimationComposition(
+    name="pec",
+    nodes=[comp],
+    parameters=fit_parameters,
+    outcome_variables=[
+        decision.output_ports[pnl.DECISION_OUTCOME],
+        decision.output_ports[pnl.RESPONSE_TIME],
+    ],
+    data=data_to_fit,
+    optimization_function=MaxLikelihoodEstimator(),
+    num_estimates=num_estimates,
+    num_trials_per_estimate=len(trial_inputs),
+)
 
 # pec.controller.parameters.comp_execution_mode.set("LLVM")
 pec.controller.function.parameters.save_values.set(True)
-# ll, sim_data = pec.log_likelihood(0.3, 0.6, inputs={comp: trial_inputs})
 ret = pec.run(inputs={comp: trial_inputs}, num_trials=len(trial_inputs))
 
-# Check that the parameters are recovered and that the log-likelihood is correct
-# assert np.allclose(pec.controller.optimal_parameters, [0.3, 0.6], atol=0.1)
+# Check that the parameters are recovered and that the log-likelihood is correct, set the tolerance pretty high,
+# things are noisy because of the low number of trials and estimates.
+assert np.allclose(
+    pec.controller.optimal_parameters,
+    [ddm_params["rate"], ddm_params["threshold"]],
+    atol=0.1,
+)
