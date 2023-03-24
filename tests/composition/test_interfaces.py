64e52270bc7a242027924572d37ca4e872040ae7
Katherine Mantel <kmantel@princeton.edu>
2023-03-23 21:44:47 -0400

    tests: test_assign_value: fix allclose

--- tests/composition/test_interfaces.py
+++ tests/composition/test_interfaces.py
@@ -46,7 +46,7 @@ class TestExecuteCIM:
         cim = CompositionInterfaceMechanism()
         cim.defaults.variable = [2.0]
         cim.execute()
-        np.testing.assert_allclose(cim.value, [2.0])
+        np.testing.assert_allclose(cim.value, [[2.0]])
 
     def test_standalone_CIM_multiple_input_ports(self):
 
