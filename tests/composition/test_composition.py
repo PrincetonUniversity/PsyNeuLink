10510aeca44a8fda31bba9ec276eeea967055134
Katherine Mantel <kmantel@princeton.edu>
2023-03-28 16:58:05 -0400

    tests: TestRunInputSpecifications test_run_2_mechanisms_reuse_input: fix allclose
    result of Composition.run should just be 2d, containing list of 1d
    output port values, not 1d

--- tests/composition/test_composition.py
+++ tests/composition/test_composition.py
@@ -3235,7 +3235,7 @@ class TestRunInputSpecifications:
         inputs_dict = {A: [[5]]}
         sched = Scheduler(composition=comp)
         output = comp.run(inputs=inputs_dict, scheduler=sched, num_trials=5)
-        assert np.allclose([125], output)
+        np.testing.assert_allclose([[125]], output)
 
     def test_function_as_input(self):
         c = pnl.Composition()
