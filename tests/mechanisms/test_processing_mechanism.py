0f6b866eb4e185f0d5cdaae8241454885f701844
Katherine Mantel <kmantel@princeton.edu>
2023-03-28 17:03:35 -0400

    tests: test_output_ports: fix allclose

--- tests/mechanisms/test_processing_mechanism.py
+++ tests/mechanisms/test_processing_mechanism.py
@@ -245,12 +245,12 @@ class TestLinearMatrixFunction:
 class TestProcessingMechanismStandardOutputPorts:
 
     @pytest.mark.benchmark
-    @pytest.mark.parametrize("op, expected", [(MAX_ONE_HOT, [0, 2, 0]),
-                                              (MAX_INDICATOR, [0, 1, 0]),
-                                              (MAX_ABS_INDICATOR, [0, 0, 1]),
-                                              (MAX_ABS_ONE_HOT, [0, 0, 4]),
-                                              (MAX_VAL, [2]),
-                                              (PROB, [0, 2, 0]),
+    @pytest.mark.parametrize("op, expected", [(MAX_ONE_HOT, [[0, 2, 0]]),
+                                              (MAX_INDICATOR, [[0, 1, 0]]),
+                                              (MAX_ABS_INDICATOR, [[0, 0, 1]]),
+                                              (MAX_ABS_ONE_HOT, [[0, 0, 4]]),
+                                              (MAX_VAL, [[2]]),
+                                              (PROB, [[0, 2, 0]]),
                                              ],
                              ids=lambda x: x if isinstance(x, str) else "")
     def test_output_ports(self, mech_mode, op, expected, benchmark):
