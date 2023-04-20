ad869954d860f705a4711d06e929c768b0b714f7
jdcpni <jdc@princeton.edu>
2023-04-20 14:45:41 -0400

    Fix/control allocation jdc (#2639)
    • composition.py:
      - _add_linear_processing_pathway():
        - add warning for empty nested Composition in pathway
        - add warning--if ControlMechanism is explicitly specified in pathway--that a MappingProjection will be created from 
           its ControlSignal[0] to the InputPort of the next Mechanism in the pathway
    
    • test_interfaces.py:
       - test_parameter_CIM_routing_from_ControlMechanism_pathway_explicit():
             add test for warning about and instantiation of MappingProjection from ControlMechanism
       - test_parameter_CIM_routing_from_ControlMechanism_pathway_imlicit():
             verify that only projections from cm are to parameter_CIM in this case (in contrast to _implicit test above)

--- tests/composition/test_interfaces.py
+++ tests/composition/test_interfaces.py
@@ -513,7 +513,7 @@ class TestConnectCompositionsViaCIMS:
         assert len(comp.input_CIM.user_added_ports['input_ports']) == 0
         assert len(comp.input_CIM.user_added_ports['output_ports']) == 0
 
-    def test_parameter_CIM_port_order(self):
+    def test_parameter_CIM_port_order(self) -> object:
         # Note:  CIM_port order is also tested in TestNodes and test_simplified_necker_cube()
 
         # Inner Composition
@@ -555,8 +555,12 @@ class TestConnectCompositionsViaCIMS:
         self, parameter_CIM_routing_composition
     ):
         ia, ib, cm, icomp, ocomp = parameter_CIM_routing_composition
-        ocomp.add_linear_processing_pathway([cm, icomp])
-
+        warning_msg = f"A MappingProjection has been created from a ControlSignal of 'control_mechanism' " \
+                      f"-- specified in 'pathway' arg for add_linear_procesing_pathway method of 'ocomp' -- " \
+                      f"to another Mechanism in that pathway.  " \
+                      f"If this is not the intended behavior, add 'control_mechanism' separately to 'ocomp'."
+        with pytest.warns(UserWarning, match=warning_msg):
+            ocomp.add_linear_processing_pathway([cm, icomp])
         ocomp._analyze_graph()
         input_nodes = ocomp.get_nodes_by_role(NodeRole.INPUT)
         assert cm in input_nodes
@@ -573,6 +577,8 @@ class TestConnectCompositionsViaCIMS:
         # by the controlled slope of ib (2), resulting in 8
         np.testing.assert_array_almost_equal(ocomp.results, [[[8]], [[8]], [[8]]])
         assert len(ib.mod_afferents) == 1
+        # Verify that MappingProjection from cm to icomp (for which warning was elicited above) is in place
+        assert cm.control_signals[0].efferents[0].receiver.owner == icomp.input_CIM
         assert ib.mod_afferents[0].sender == icomp.parameter_CIM.output_port
         assert icomp.parameter_CIM_ports[ib.parameter_ports['slope']][0].path_afferents[0].sender == cm.output_port
         assert cm in ocomp.graph_processing.dependency_dict[icomp]
@@ -602,6 +608,8 @@ class TestConnectCompositionsViaCIMS:
         assert len(ib.mod_afferents) == 1
         assert ib.mod_afferents[0].sender == icomp.parameter_CIM.output_port
         assert icomp.parameter_CIM_ports[ib.parameter_ports['slope']][0].path_afferents[0].sender == cm.output_port
+        # Verify that only Projections from cm are to parameter_CIM (and not to icomp.input_CIM)
+        assert all([proj.receiver.owner == icomp.parameter_CIM for proj in cm.efferents])
         assert cm not in ocomp.graph_processing.dependency_dict[icomp]
 
     def test_nested_control_projection_count_controller(self):
