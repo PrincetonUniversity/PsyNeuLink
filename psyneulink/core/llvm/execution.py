PrincetonUniversity/PsyNeuLink

commit 3cb0ce4cd0a9aec270a1f73f3d7312ed61a52396

llvm/execution: Improve writeback handling of history and empty structures
Drop context and component parameters to writeback,
the first one should match context used by Execution,
the second neither used nor implemented.
Writeback the most recent stateful value.
Replace empty structures with None.
Only try to match the original shape in data writeback if there is one

Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

fixup writeback

psyneulink/core/llvm/execution.py
@@ -114,20 +114,16 @@ class Execution:
         return struct
 
 
-    def writeback_params_to_pnl(self, context=None, component=None, params=None, ids:str=None, condition:callable=lambda p: True):
-
-        if component is None:
-            component = self._obj
+    def writeback_params_to_pnl(self, params=None, ids:str=None, condition:callable=lambda p: True):
 
         assert (params is None) == (ids is None), "Either both 'params' and 'ids' have to be set or neither"
 
         if params is None:
             # Default to stateful params
-            assert component is self._obj
             params = self._state_struct
             ids = "llvm_state_ids"
 
-        self._copy_params_to_pnl(context, component, params, ids, condition)
+        self._copy_params_to_pnl(self._execution_contexts[0], self._obj, params, ids, condition)
 
 
     def _copy_params_to_pnl(self, context, component, params, ids:str, condition:callable):
@@ -189,11 +185,21 @@ class Execution:
 
                 # Writeback parameter value if the condition matches
                 elif condition(pnl_param):
-                    value = _convert_ctype_to_python(compiled_attribute_param)
-                    # Unflatten the matrix
-                    # FIXME: this seems to break something when generalized for all attributes
-                    value = np.array(value).reshape(pnl_param._get(context).shape)
-                    pnl_param._set(value, context=context)
+                    value = np.ctypeslib.as_array(compiled_attribute_param)
+
+                    # Stateful parameters include history, get the most recent value
+                    if "state" in ids:
+                        value = value[-1]
+
+                    # Replace empty structures with None
+                    if ctypes.sizeof(compiled_attribute_param) == 0:
+                        value = None
+                    else:
+                        # Try to match the shape of the old value
+                        old_value = pnl_param.get(context)
+                        if hasattr(old_value, 'shape'):
+                            value = value.reshape(old_value.shape)
+                    pnl_param.set(value, context=context)
 
 
 class CUDAExecution(Execution):

psyneulink/core/compositions/composition.py
@@ -10212,8 +10212,7 @@ _
 
                     if self._is_learning(context):
                         # copies back matrix to pnl from param struct (after learning)
-                        _comp_ex.writeback_params_to_pnl(context=context,
-                                                         params=_comp_ex._param_struct,
+                        _comp_ex.writeback_params_to_pnl(params=_comp_ex._param_struct,
                                                          ids="llvm_param_ids",
                                                          condition=lambda p: p.name == "matrix")
 

commit f47825c26734ea8b5928a571e8027216c954d793

llvm/execution: Do not construct numpy arrays of empty structures
They won't be used anyway.

Fixes: 3cb0ce4cd0a9aec270a1f73f3d7312ed61a52396
	("llvm/execution: Improve writeback handling of history and
	empty structures")

Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

psyneulink/core/llvm/execution.py
@@ -190,20 +190,21 @@ class Execution:
                     if attribute == "random_state":
                         continue
 
-                    value = np.ctypeslib.as_array(compiled_attribute_param)
-
-                    # Stateful parameters include history, get the most recent value
-                    if "state" in ids:
-                        value = value[-1]
-
                     # Replace empty structures with None
                     if ctypes.sizeof(compiled_attribute_param) == 0:
                         value = None
                     else:
+                        value = np.ctypeslib.as_array(compiled_attribute_param)
+
+                        # Stateful parameters include history, get the most recent value
+                        if "state" in ids:
+                            value = value[-1]
+
                         # Try to match the shape of the old value
                         old_value = pnl_param.get(context)
                         if hasattr(old_value, 'shape'):
                             value = value.reshape(old_value.shape)
+
                     pnl_param.set(value, context=context)
 
 