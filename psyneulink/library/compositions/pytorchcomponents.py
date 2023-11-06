7624236a811052f4370d7533f28d30045689a2a0
jdcpni <jdc@princeton.edu>
2023-11-05 21:04:55 -0500

    [skip ci]

--- psyneulink/library/compositions/pytorchcomponents.py
+++ psyneulink/library/compositions/pytorchcomponents.py
@@ -114,26 +114,26 @@ def pytorch_function_creator(function, device, context=None):
         memory_matrix = get_fct_param_value('memory_matrix')
         axis = get_fct_param_value('axis')
         storage_location = get_fct_param_value('storage_location')
-        # storage_prob = get_fct_param_value('storage_prob')
+        storage_prob = get_fct_param_value('storage_prob')
         decay_rate = get_fct_param_value('decay_rate')
         def func(entry):
-            # FIX: IGNORE STORAGE PROB FOR NOW
             # if random_state.uniform(0, 1) < storage_prob:
-            if decay_rate:
-                memory_matrix *= decay_rate
-            if storage_location is not None:
-                idx_of_min = storage_location
-            else:
-                # Find weakest entry (i.e., with lowest norm) along specified axis of matrix
-                idx_of_min = torch.argmin(torch.linalg.norm(memory_matrix, axis=axis))
-            if axis == 0:
-                memory_matrix[:,idx_of_min] = torch.tensor(entry)
-            elif axis == 1:
-                memory_matrix[idx_of_min,:] = torch.tensor(entry)
-            # FIX: This is not essential (used in Pythonn version to report which entry was updated):
-            self.parameters.entry._set(entry, context)
-            # FIX: This IS ESSENTIAL (it is the main point of the function):
-            self.parameters.memory_matrix._set(memory_matrix, context)
+            if torch.rand(1) < storage_prob:
+                if decay_rate:
+                    memory_matrix *= decay_rate
+                if storage_location is not None:
+                    idx_of_min = storage_location
+                else:
+                    # Find weakest entry (i.e., with lowest norm) along specified axis of matrix
+                    idx_of_min = torch.argmin(torch.linalg.norm(memory_matrix, axis=axis))
+                if axis == 0:
+                    memory_matrix[:,idx_of_min] = torch.tensor(entry)
+                elif axis == 1:
+                    memory_matrix[idx_of_min,:] = torch.tensor(entry)
+                # # FIX: This is not essential (used in Pythonn version to report which entry was updated):
+                # self.parameters.entry._set(entry, context)
+                # FIX: This IS ESSENTIAL (it is the main point of the function):
+                self.parameters.memory_matrix._set(memory_matrix, context)
         return func
 
     else:
