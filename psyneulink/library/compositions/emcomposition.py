9c42d5b7c92216a165ff5b730e5c8b0238b71007
jdcpni <pniintel55>
2024-06-21 15:09:19 -0400

    [skip ci]

--- psyneulink/library/compositions/emcomposition.py
+++ psyneulink/library/compositions/emcomposition.py
@@ -1680,6 +1680,7 @@ class EMComposition(AutodiffComposition):
             self.key_names = parsed_field_names[:self.num_keys]
             self.value_names = parsed_field_names[self.num_keys:]
         else:
+            # FIX THIS TO PRODUCE CLEANER NAMES
             self.key_names = [f'{i} [QUERY]' for i in range(self.num_keys)] if self.num_keys > 1 else ['KEY']
             self.value_names = [f'{i} [VALUE]' for i in range(self.num_values)] if self.num_values > 1 else ['VALUE']
 
