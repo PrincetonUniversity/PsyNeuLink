9d6db2f5a0f6c50826faa4a7cada0757ae2888e7
Younes Strittmatter <ystrittm@gmail.com>
2024-10-31 19:26:25 -0400

    Update Environment.py

--- Scripts/Models (Under Development)/EGO/Using EMComposition/Environment.py
+++ Scripts/Models (Under Development)/EGO/Using EMComposition/Environment.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from torch.utils.data import dataset
 from torch import utils
-from numpy.random import randint
+from random import randint
 
 def one_hot_encode(labels, num_classes):
     """
