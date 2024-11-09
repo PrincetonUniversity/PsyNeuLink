38a59d382180e8ff52c9af3153c86948231a4714
jdcpni <jdc@princeton.edu>
2024-11-09 08:23:25 -0500

    [skip ci]

--- Scripts/Models (Under Development)/EGO/Using EMComposition/CSW/ScriptControl.py
+++ Scripts/Models (Under Development)/EGO/Using EMComposition/CSW/ScriptControl.py
@@ -8,18 +8,19 @@ MODEL_PARAMS = 'TestParams'
 
 CONSTRUCT_MODEL = True                 # THIS MUST BE SET TO True to run the script
 DISPLAY_MODEL =  (                     # Only one of the following can be uncommented:
-    # None                             # suppress display of model
-    {                                  # show simple visual display of model
-        # 'show_pytorch': True,            # show pytorch graph of model
-        'show_learning': pnl.ALL,
-        'show_projections_not_in_composition': True,
-        # 'show_nested': {'show_node_structure': True},
-        # 'exclude_from_gradient_calc_style': 'dashed'# show target mechanisms for learning
-        # 'show_node_structure': True     # show detailed view of node structures and projections
-    }
+    None                             # suppress display of model
+    # {                                  # show simple visual display of model
+    #     # 'show_pytorch': True,            # show pytorch graph of model
+    #     'show_learning': True,
+    #     # 'show_nested_args': {'show_learning': pnl.ALL},
+    #     'show_projections_not_in_composition': True,
+    #     # 'show_nested': {'show_node_structure': True},
+    #     # 'exclude_from_gradient_calc_style': 'dashed'# show target mechanisms for learning
+    #     # 'show_node_structure': True     # show detailed view of node structures and projections
+    # }
 )
-RUN_MODEL = False                      # False => don't run the model
-# RUN_MODEL = True,                       # True => run the model
+# RUN_MODEL = False                      # False => don't run the model
+RUN_MODEL = True,                       # True => run the model
 # REPORT_OUTPUT = ReportOutput.FULL  # Sets console output during run [ReportOutput.ON, .TERSE OR .FULL]
 REPORT_OUTPUT = ReportOutput.OFF     # Sets console output during run [ReportOutput.ON, .TERSE OR .FULL]
 REPORT_PROGRESS = ReportProgress.OFF # Sets console progress bar during run
