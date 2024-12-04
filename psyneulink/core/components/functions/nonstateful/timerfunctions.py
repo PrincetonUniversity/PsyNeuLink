575882af9ff2c09d84f793107f458abc14272a26
jdcpni <jdc@princeton.edu>
2024-12-04 13:08:47 +0100

    [skip ci]

--- psyneulink/core/components/functions/nonstateful/timerfunctions.py
+++ psyneulink/core/components/functions/nonstateful/timerfunctions.py
@@ -659,10 +659,10 @@ class DeceleratingDecay(TimerFunction):  # -------------------------------------
     .. _DeceleratingDecay:
     |
     `function <DeceleratingDecay._function>` returns exponentially decaying transform of `variable
-    <DeceleratingDecay.variable>`
+    <DeceleratingDecay.variable>`:
 
     .. math::
-       \\frac{start+1-threshold}{e^{\\ln(s+1-threshold)\\left(\\frac{variable}{end}\\right)^{rate}}-1+threshold}
+       \\frac{start-threshold+1}{e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{rate}}-1+threshold}
 
     such that:
 
@@ -691,7 +691,9 @@ class DeceleratingDecay(TimerFunction):  # -------------------------------------
     `derivative <DeceleratingDecay.derivative>` returns the derivative of the DeceleratingDecay Function:
 
       .. math::
-        \\frac{start+1-threshold}{e^{\\ln(s+1-threshold)\\left(\\frac{variable}{end}\\right)^{rate}}-1+threshold}
+        \\frac{(start-threshold+1)}{\\left(e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{
+        rate}}-1+threshold\\right)^2} \\cdot e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{rate}}
+        \\cdot \\ln(start-threshold+1) \\cdot \\frac{rate}{end}
 
     See `graph <https://www.desmos.com/calculator/e6ei7o0woq>`_ for interactive plot of the function using `Desmos
     <https://www.desmos.com>`_.
@@ -875,8 +877,329 @@ class DeceleratingDecay(TimerFunction):  # -------------------------------------
         """Derivative of `function <DeceleratingDecay._function>` at **input**:
 
         .. math::
-           \\frac{start * \\ln\\left(\\frac{1}{threshold}\\right) * e^{-\\left(\\frac{variable\\ln\\left(\\frac{1}{
-           threshold}\\right)}{end}\\right)}}{end}
+           \\frac{(start-threshold+1)}{\\left(e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{
+           rate}}-1+threshold\\right)^2} \\cdot e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{rate}}
+           \\cdot \\ln(start-threshold+1) \\cdot \\frac{rate}{end}
+
+        Arguments
+        ---------
+
+        input : number
+            value of the input to the DeceleratingDecay transform at which derivative is to be taken.
+
+        Derivative of `function <DeceleratingDecay._function>` at **input**.
+
+        Returns
+        -------
+        derivative :  number or array
+        """
+
+        start = self._get_current_parameter_value(START, context)
+        end = self._get_current_parameter_value(END, context)
+        threshold = self._get_current_parameter_value(THRESHOLD, context)
+
+        return (start * np.log(1/threshold) / end) * np.exp(-input * np.log(threshold) / end)
+
+    # FIX:
+    def _gen_llvm_transfer(self, builder, index, ctx, vi, vo, params, state, *, tags:frozenset):
+        ptri = builder.gep(vi, [ctx.int32_ty(0), index])
+        ptro = builder.gep(vo, [ctx.int32_ty(0), index])
+
+        end_ptr = ctx.get_param_or_state_ptr(builder, self, END, param_struct_ptr=params)
+        start_ptr = ctx.get_param_or_state_ptr(builder, self, START, param_struct_ptr=params)
+        threshold_ptr = ctx.get_param_or_state_ptr(builder, self, THRESHOLD, param_struct_ptr=params)
+        offset_ptr = ctx.get_param_or_state_ptr(builder, self, OFFSET, param_struct_ptr=params)
+
+        end = pnlvm.helpers.load_extract_scalar_array_one(builder, end_ptr)
+        start = pnlvm.helpers.load_extract_scalar_array_one(builder, start_ptr)
+        threshold = pnlvm.helpers.load_extract_scalar_array_one(builder, threshold_ptr)
+        offset = pnlvm.helpers.load_extract_scalar_array_one(builder, offset_ptr)
+
+        exp_f = ctx.get_builtin("exp", [ctx.float_ty])
+        val = builder.load(ptri)
+        val = builder.fmul(val, end)
+        val = builder.fadd(val, start)
+        val = builder.call(exp_f, [val])
+
+        if "derivative" in tags:
+            # f'(x) = s*r*e^(r*x + b)
+            val = builder.fmul(val, threshold)
+            val = builder.fmul(val, end)
+        else:
+            # f(x) = s*e^(r*x + b) + o
+            val = builder.fmul(val, threshold)
+            val = builder.fadd(val, offset)
+
+        builder.store(val, ptro)
+
+    def _gen_pytorch_fct(self, device, context=None):
+        offset = self._get_pytorch_fct_param_value(OFFSET, device, context)
+        end = self._get_pytorch_fct_param_value(END, device, context)
+        threshold = self._get_pytorch_fct_param_value(THRESHOLD, device, context)
+        start = self._get_pytorch_fct_param_value(START, device, context)
+
+        return lambda x : offset + start * torch.exp(-x * torch.log(1 / threshold) / end)
+
+
+class AsymptoticDecay(TimerFunction):  # ---------------------------------------------------------------------------
+    """
+    DeceleratingDecay(     \
+         default_variable, \
+         start=1.0,        \
+         offset=0.0,       \
+         end=1.0,          \
+         threshold=0,      \
+         tolerance=0.01,   \
+         params=None,      \
+         owner=None,       \
+         name=None,        \
+         prefs=None        \
+         )
+
+    .. _DeceleratingDecay:
+    |
+    `function <DeceleratingDecay._function>` returns exponentially decaying transform of `variable
+    <DeceleratingDecay.variable>` toward an asymptotic value:
+
+    .. math::
+       XXX
+
+    such that:
+
+    .. math::
+        value = start + offset\ for\ variable=0
+
+        value = (start * threshold) + offset\ for\ variable=end
+
+    where:
+
+        **start**, together with `offset <DeceleratingDecay.offset>`, determines the value of the function when
+        `variable <DeceleratingDecay.variable>` = 0, and is used together with `threshold <DeceleratingDecay.threshold>`
+        to determine the value of the function when `variable <DeceleratingDecay.variable>` = `end
+        <DeceleratingDecay.end>`.
+
+        **offset**, together with `start <DeceleratingDecay.start>`, determines the value of the function
+        when `variable <DeceleratingDecay.variable>` = 0, and its linear offset from 0 for all other values;
+
+        **threshold** is asymptotic value toward which the function decays.
+
+        **tolerance** is the fraction of `start <DeceleratingDecay.start>` when, added to `offset
+        <DeceleratingDecay.offset>`, is used to determine the value of the function when `variable
+        <DeceleratingDecay.variable>` should equal `end <DeceleratingDecay.end>`.
+
+        **end** determines the value of `variable <DeceleratingDecay.variable>` at which
+        the value of the function should equal :math:`start * threshold + offset`.
+
+    `derivative <DeceleratingDecay.derivative>` returns the derivative of the DeceleratingDecay Function:
+
+      .. math::
+        \\frac{(start-threshold+1)}{\\left(e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{
+        rate}}-1+threshold\\right)^2} \\cdot e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{rate}}
+        \\cdot \\ln(start-threshold+1) \\cdot \\frac{rate}{end}
+
+    See `graph <https://www.desmos.com/calculator/e6ei7o0woq>`_ for interactive plot of the function using `Desmos
+    <https://www.desmos.com>`_.
+
+    COMMENT:
+    FOR TIMER VERSION:
+    `function <DeceleratingDecay._function>` returns exponentially decaying transform of `variable
+    <DeceleratingDecay.variable>`, that has a value of `start <DeceleratingDecay.start>` + `offset
+    <DeceleratingDecay.offset>` at `variable <DeceleratingDecay.variable>` = 0, and a value of `threshold
+    <DeceleratingDecay.end>` * `start <DeceleratingDecay.start>` + `offset <DeceleratingDecay.offset>` at
+    `variable at `variable <DeceleratingDecay.variable>` = `end <DeceleratingDecay.end>`:
+    COMMENT
+
+    Arguments
+    ---------
+
+    default_variable : number or array : default class_defaults.variable
+        specifies a template for the value to be transformed.
+
+    start : float : default 1.0
+        specifies, together with `offset <DeceleratingDecay.offset>`, the value of the function when `variable
+        <DeceleratingDecay.variable>` = 0; must be greater than 0.
+
+    offset : float : default 0.0
+        specifies, together with `start <DeceleratingDecay.start>`, the value of the function when `variable
+        <DeceleratingDecay.variable>` = 0, and its linear offset for all other values.
+
+    threshold : float : default 0.0
+        specifies the asymptotic value toward which the function decays.
+
+    tolerance : float : default 0.01
+        specifies the fraction of `start <DeceleratingDecay.start>` when added to `offset <DeceleratingDecay.offset>`,
+        that determines the value of the function when `variable <DeceleratingDecay.variable>` = `end
+        <DeceleratingDecay.end>`; must be between 0 and 1.
+
+    end : float : default 1.0
+        specifies the value of `variable <DeceleratingDecay.variable>` at which the `value of the function
+        should equal `start <DeceleratingDecay.start>` * `threshold <DeceleratingDecay.threshold>` + `offset
+        <DeceleratingDecay.offset>`; must be greater than 0.
+
+    params : Dict[param keyword: param value] : default None
+        a `parameter dictionary <ParameterPort_Specification>` that specifies the parameters for the
+        function.  Values specified for parameters in the dictionary override any assigned to those parameters
+        in arguments of the constructor.
+
+    owner : Component
+        `component <Component>` to which to assign the Function.
+
+    name : str : default see `name <Function.name>`
+        specifies the name of the Function.
+
+    prefs : PreferenceSet or specification dict : default Function.classPreferences
+        specifies the `PreferenceSet` for the Function (see `prefs <Function_Base.prefs>` for details).
+
+    Attributes
+    ----------
+
+    variable : number or array
+        contains value to be transformed.
+
+    start : float (>0)
+        determines, together with `offset <DeceleratingDecay.offset>`, the value of the function when `variable
+        <DeceleratingDecay.variable>` = 0.
+
+    offset : float
+        determines, together with `start <DeceleratingDecay.start>`, the value of the function when `variable
+        <DeceleratingDecay.variable>` = 0, and its linear offset for all other values.
+
+    threshold : float
+        determines the asymptotic value toward which the function decays.
+
+    tolerance : float (0,1)
+        determines the fraction of `start <DeceleratingDecay.start>` when added to `offset <DeceleratingDecay.offset>`,
+        that determines the value of the function when `variable <DeceleratingDecay.variable>` = `end
+        <DeceleratingDecay.end>`.
+
+    end : float (>0)
+        determines the value of `variable <DeceleratingDecay.variable>` at which the value of the function should
+        equal `start <DeceleratingDecay.start>` * `threshold <DeceleratingDecay.threshold>` + `offset <DeceleratingDecay.offset>`.
+
+    bounds : (None, None)
+
+    owner : Component
+        `component <Component>` to which the Function has been assigned.
+
+    name : str
+        the name of the Function; if it is not specified in the **name** argument of the constructor, a default is
+        assigned by FunctionRegistry (see `Registry_Naming` for conventions used for default and duplicate names).
+
+    prefs : PreferenceSet or specification dict : Function.classPreferences
+        the `PreferenceSet` for function; if it is not specified in the **prefs** argument of the Function's
+        constructor, a default is assigned using `classPreferences` defined in __init__.py (see `Preferences`
+        for details).
+    """
+
+    componentName = ASYMPTOTIC_DECAY_FUNCTION
+
+    classPreferences = {
+        PREFERENCE_SET_NAME: 'AsymptoticDecayClassPreferences',
+        REPORT_OUTPUT_PREF: PreferenceEntry(False, PreferenceLevel.INSTANCE),
+    }
+
+    _model_spec_class_name_is_generic = True
+
+    class Parameters(TimerFunction.Parameters):
+        """
+            Attributes
+            ----------
+
+                offset
+                    see `offset <DeceleratingDecay.offset>`
+
+                    :default value: 0.0
+                    :type: ``float``
+
+                threshold
+                    see `threshold <DeceleratingDecay.threshold>`
+
+                    :default value: 0.0
+                    :type: ``float``
+
+                tolerance
+                    see `tolerance <DeceleratingDecay.tolerance>`
+
+                    :default value: 0.01
+                    :type: ``float``
+        """
+        offset = Parameter(0.0, modulable=True, aliases=[ADDITIVE_PARAM])
+        threshold = Parameter(0.01, modulable=True, aliases=[SCALE])
+
+        def _validate_threshold(self, threshold):
+            if threshold < 0:
+                return f"must be greater than or equal to 0."
+
+        def _validate_tolerance(self, tolerance):
+            if tolerance <= 0 or tolerance >= 1:
+                return f"must be between 0 and 1."
+
+    @check_user_specified
+    @beartype
+    def __init__(self,
+                 default_variable=None,
+                 start: Optional[ValidParamSpecType] = None,
+                 offset: Optional[ValidParamSpecType] = None,
+                 end: Optional[ValidParamSpecType] = None,
+                 threshold: Optional[ValidParamSpecType] = None,
+                 tolerance: Optional[ValidParamSpecType] = None,
+                 params=None,
+                 owner=None,
+                 prefs:  Optional[ValidPrefSet] = None):
+        super().__init__(
+            default_variable=default_variable,
+            start=start,
+            offset=offset,
+            end=end,
+            threshold=threshold,
+            tolerance=tolerance,
+            params=params,
+            owner=owner,
+            prefs=prefs,
+        )
+
+    def _function(self,
+                 variable=None,
+                 context=None,
+                 params=None,
+                 ):
+        """
+
+        Arguments
+        ---------
+
+        variable : number or array : default class_defaults.variable
+           amount by which to increment timer on current execution;  if this is not specified, the timer is incremented
+           by the value of `increment <DeceleratingDecay.increment>`.
+
+        params : Dict[param keyword: param value] : default None
+            a `parameter dictionary <ParameterPort_Specification>` that specifies the parameters for the
+            function.  Values specified for parameters in the dictionary override any assigned to those parameters in
+            arguments of the constructor.
+
+        Returns
+        -------
+
+        Exponentially decayed value of variable : number or array
+
+        """
+        start = self._get_current_parameter_value(START, context)
+        offset = self._get_current_parameter_value(OFFSET, context)
+        end = self._get_current_parameter_value(END, context)
+        threshold = self._get_current_parameter_value(THRESHOLD, context)
+
+        result = offset + start * np.exp(-variable * np.log(1 / threshold) / end)
+
+        return self.convert_output_type(result)
+
+    @handle_external_context()
+    def derivative(self, input, output=None, context=None):
+        """Derivative of `function <DeceleratingDecay._function>` at **input**:
+
+        .. math::
+           \\frac{(start-threshold+1)}{\\left(e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{
+           rate}}-1+threshold\\right)^2} \\cdot e^{\\ln(start-threshold+1)\\left(\\frac{variable}{end}\\right)^{rate}}
+           \\cdot \\ln(start-threshold+1) \\cdot \\frac{rate}{end}
 
         Arguments
         ---------
