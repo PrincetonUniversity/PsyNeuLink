fix softmax masking

psyneulink/core/components/functions/nonstateful/transferfunctions.py
@@ -3487,7 +3487,7 @@ class SoftMax(TransferFunction):
                 # Apply threshold-based masking
                 if mask_threshold is not None:
                     if torch.any(_input < 0):
-                        warnings.warn(f"SoftMax function: mask_threshold is set to {mask_threshold}, "
+                        warnings.warn(f"Softmax function: mask_threshold is set to {mask_threshold}, "
                                       f"but input contains negative values. "
                                       f"Masking will be applied to the magnitude of the input.")
 