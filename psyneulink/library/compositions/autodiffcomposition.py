PrincetonUniversity/PsyNeuLink

commit beebd2a968fbeebc45e93d9785460a1c1860686a

Add support for batching to AutoDiffCompostion in PyTorch mode. (#3204)
* Changes to allow passing BatchedImpl tensors

When pytorch vmap is used on a function, the function
is called to trace it with tenors passed as inputs
which are not normal pytorch tensors. They are in fact
BatchedImpl tensors that have no underlying storage.
These cannot be converted to double() or numpy arrays
so I needed to remove calls to convert these tensors
in this special case.

* Fix bug in _batch_inputs

_batch_inputs was not actually returning batches of
size batch_size.

* Testing bactch support.

* Fix batch_size=1 case.

* WIP merge for pytorch batching in autodiff

* Fix for when minibatch_size is ndarray

* Cleanup

* Remove handling of batched_results.

* Handle dtype-object in _get_variable_from_input

Not sure this works in general to be honest.

* Fixes from merging in devel.

* Fixes for ragged things.

* More fixes for ragged things.

* Fix bad torch.stack call in execute_input_ports

* Add a real pytorch training test with batching

I made a test that implements an equivalent network
in PyTorch and PNL, initializes them with the same
weights, trains them both and then compares the losses. I think this is similar to some other
tests in the file but this one tests batch size > 1
and it actually trains the pytorch network as well.

* More fixes

* Disable update_autodiff_all_output_values

Now that all_output_values contains the
batch dimension in the first dimension it
is causing and error when passed to
output_CIM.execute because it has the incorrect
number of input ports (unless batch size happens
to match number of input ports). Not sure what
the best fix to this is but disabling this line
seems to work and pass all tests. Need to ask Jon.

* Handle 3D structure better in autodiff update_results.

* Fix formatting

* Fix ragged processing.

A bunch of fixes for things wrong with ragged processing.

* Fix logging test.

psyneulink/library/compositions/autodiffcomposition.py
@@ -1067,9 +1067,9 @@ class AutodiffComposition(Composition):
     def autodiff_forward(self, inputs, targets,
                          synch_with_pnl_options, retain_in_pnl_options,
                          execution_mode, scheduler, context):
-        """Perform forward pass of model and compute loss for a single trial (i.e., a single input) in Pytorch mode.
-        Losses are accumulated in pytorch_rep.track_losses, over calls to this method within a minibatch;
-          at the end of a minibatch, they are averaged and backpropagated by compositionrunner.run_learning()
+        """
+        Perform forward pass of model and compute loss for a batch of trials in Pytorch mode.
+        Losses are then accumulated, error is backpropagated by compositionrunner.run_learning()
           before the next time it calls run(), in a call to backward() by do_gradient_optimization()
           in _batch_inputs() or _batch_function_inputs(),
         """
@@ -1082,7 +1082,10 @@ class AutodiffComposition(Composition):
         # Get value of INPUT nodes for current trial
         curr_tensors_for_inputs = {}
         for component in inputs.keys():
-            curr_tensors_for_inputs[component] = torch.tensor(inputs[component], device=self.device).double()
+            if not isinstance(inputs[component], torch.Tensor):
+                curr_tensors_for_inputs[component] = torch.tensor(inputs[component], device=self.device).double()
+            else:
+                curr_tensors_for_inputs[component] = inputs[component]
 
         # Get value of all OUTPUT nodes for current trial
         curr_tensors_for_outputs = pytorch_rep.forward(curr_tensors_for_inputs, None, context)
@@ -1095,10 +1098,13 @@ class AutodiffComposition(Composition):
 
         # Get value of TARGET nodes for current trial
         curr_tensors_for_targets = {}
-        for component in targets.keys():
-            curr_tensors_for_targets[component] = [torch.tensor(np.atleast_1d(target),
-                                                           device=self.device).double()
-                                              for target in targets[component]]
+        for component, target in targets.items():
+            if isinstance(target, torch.Tensor) or isinstance(target, np.ndarray):
+                curr_tensors_for_targets[component] = [target[:, i, :] for i in range(target.shape[1])]
+            else:
+                # It's  a list, of lists, of torch tensors because it is ragged
+                num_outputs = len(targets[component][0])
+                curr_tensors_for_targets[component] = [torch.stack([batch_elem[i] for batch_elem in target]) for i in range(num_outputs)]
 
         # Get value of TARGET nodes for trained OUTPUT nodes
         curr_target_tensors_for_trained_outputs = {}
@@ -1106,13 +1112,20 @@ class AutodiffComposition(Composition):
             curr_target_tensors_for_trained_outputs[trained_output] = curr_tensors_for_targets[target]
 
         # Calculate and track the loss over the trained OUTPUT nodes
-        for component in curr_tensors_for_trained_outputs.keys():
+        for component, outputs in curr_tensors_for_trained_outputs.items():
             trial_loss = 0
-            for i in range(len(curr_tensors_for_trained_outputs[component])):
+            targets = curr_target_tensors_for_trained_outputs[component]
+            num_outputs = outputs.shape[1] if type(outputs) is torch.Tensor else len(outputs[0])
+            for i in range(num_outputs):
                 # loss only accepts 0 or 1d target. reshape assuming pytorch_rep.minibatch_loss dim is correct
+
+                # Get the output, if it's a torch tensor we can slice, if it's a list of list (its ragged) and we
+                # need to index
+                output = outputs[:, i, :] if type(outputs) is torch.Tensor else torch.stack([batch_elem[i] for batch_elem in outputs])
+
                 comp_loss = self.loss_function(
-                    curr_tensors_for_trained_outputs[component][i],
-                    torch.atleast_1d(curr_target_tensors_for_trained_outputs[component][i].squeeze())
+                    output,
+                    torch.atleast_1d(targets[i])
                 )
                 comp_loss = comp_loss.reshape_as(pytorch_rep.minibatch_loss)
                 trial_loss += comp_loss
@@ -1130,7 +1143,14 @@ class AutodiffComposition(Composition):
                 f"PROGRAM ERROR: {input_port.name} of ouput_CIM for '{self.name}' has more than one afferent."
             port, source, _ = self.output_CIM._get_source_info_from_output_CIM(input_port)
             idx = source.output_ports.index(port)
-            trained_output_values += [curr_tensors_for_trained_outputs[source][idx].detach().cpu().numpy().copy().tolist()]
+            outputs = curr_tensors_for_trained_outputs[source]
+            if type(outputs) is torch.Tensor:
+                output = outputs[:, idx, ...]
+            else:
+                output = torch.stack([batch_elem[idx] for batch_elem in outputs])
+
+            output = output.detach().cpu().numpy().copy().tolist()
+            trained_output_values += [output]
 
         # Get values of all OUTPUT nodes
         all_output_values = []
@@ -1139,7 +1159,25 @@ class AutodiffComposition(Composition):
                 f"PROGRAM ERROR: {input_port.name} of ouput_CIM for '{self.name}' has more than one afferent."
             port, component, _ = self.output_CIM._get_source_info_from_output_CIM(input_port)
             idx = component.output_ports.index(port)
-            all_output_values += [curr_tensors_for_outputs[component][idx].detach().cpu().numpy().copy().tolist()]
+            outputs = curr_tensors_for_outputs[component]
+
+            if type(outputs) is torch.Tensor:
+                output = outputs[:, idx, ...]
+            else:
+                output = torch.stack([batch_elem[idx] for batch_elem in outputs])
+
+            output = output.detach().cpu().numpy().copy().tolist()
+            all_output_values += [output]
+
+        # Turn into a numpy array, possibly ragged
+        try:
+            all_output_values = np.array(all_output_values)
+        except (ValueError, np.VisibleDeprecationWarning):
+            all_output_values = np.array(all_output_values, dtype=object)
+
+        # Swap the first two dimensions (output_port, batch) to (batch, output_port)
+        all_output_values = all_output_values.swapaxes(0, 1)
+
         pytorch_rep.all_output_values = all_output_values
 
         # Get values of TARGET nodes
@@ -1334,6 +1372,11 @@ class AutodiffComposition(Composition):
                                               retain_torch_targets,
                                               retain_torch_losses,
                                               **kwargs))
+
+        if execution_mode == pnlvm.ExecutionMode.PyTorch and not torch_available:
+            raise AutodiffCompositionError(f"'{self.name}.learn()' has been called with ExecutionMode.Pytorch, "
+                                           f"but Pytorch module ('torch') is not installed. "
+                                           f"Please install it with `pip install torch` or `pip3 install torch`")
 
         return super().learn(*args,
                              synch_with_pnl_options=synch_with_pnl_options,
@@ -1555,6 +1598,7 @@ class AutodiffComposition(Composition):
             retain_torch_trained_outputs:Optional[LEARNING_SCALE_LITERALS]=NotImplemented,
             retain_torch_targets:Optional[LEARNING_SCALE_LITERALS]=NotImplemented,
             retain_torch_losses:Optional[LEARNING_SCALE_LITERALS]=NotImplemented,
+            batched_results:bool=False,
             **kwargs):
         """Override to handle synch and retain args if run called directly from run() rather than learn()
         Note: defaults for synch and retain args are NotImplemented, so that the user can specify None if they want
@@ -1562,6 +1606,9 @@ class AutodiffComposition(Composition):
               for details). This is distinct from the user assigning the Parameter default_values(s), which is done
               in the AutodiffComposition constructor and handled by the Parameter._specify_none attribute.
         """
+
+        # Store whether we need to return results list with a batch dimension, or flatten it
+        self.batched_results = batched_results
 
         if not ('synch_with_pnl_options' in kwargs and 'retain_in_pnl_options' in kwargs):
             # No synch_with_pnl_options and retain_in_pnl_options dicts:
@@ -1599,16 +1646,30 @@ class AutodiffComposition(Composition):
 
     def _update_results(self, results, trial_output, execution_mode, synch_with_pnl_options, context):
         if execution_mode is pnlvm.ExecutionMode.PyTorch:
+
+            # Check if the trial_output is atleast 3D
+            is_output_3d = trial_output.ndim >= 3 or (trial_output.ndim == 2 and len(trial_output) > 0 and
+                                                      isinstance(trial_output[0, 0], (np.ndarray, list)))
+
             # FIX: FOR NOW, USE THIS FOR BOTH TRIAL AND MINIBATCH, SINCE CURRENTLY NO DIFFERENCE;
             #      NEED TO FIGURE OUT WHAT TO DO ABOUT UPDATING RESULTS ONCE TRUE BATCHING IS IMPLEMENTED
             if (RESULTS in synch_with_pnl_options
                     and synch_with_pnl_options[RESULTS] in {TRIAL, MINIBATCH}):
                 # Use Composition's own _update_results method since no savings when done trial-by-trial
-                super()._update_results(results, trial_output, execution_mode, synch_with_pnl_options, context)
+                if not self.batched_results and is_output_3d:
+                    for out in trial_output:
+                        super()._update_results(results, out, execution_mode, synch_with_pnl_options, context)
+                else:
+                    super()._update_results(results, trial_output, execution_mode, synch_with_pnl_options, context)
+
             elif (RESULTS in synch_with_pnl_options
                     and synch_with_pnl_options[RESULTS] == RUN):
                 # Use pytorch_reps method to keep a local list of results that are copied to autodiff.results after run
-                self.parameters.pytorch_representation._get(context).retain_results(trial_output)
+                if not self.batched_results and is_output_3d:
+                    for out in trial_output:
+                        self.parameters.pytorch_representation._get(context).retain_results(out)
+                else:
+                    self.parameters.pytorch_representation._get(context).retain_results(trial_output)
         else:
             super()._update_results(results, trial_output, execution_mode, synch_with_pnl_options, context)
 
@@ -1677,7 +1738,7 @@ class AutodiffComposition(Composition):
         return path
 
     @handle_external_context(fallback_most_recent=True)
-    def load(self, path:PosixPath=None, directory:str=None, filename:str=None, context=None):
+    def load(self, path:PosixPath=None, directory:str=None, filename:str=None, context=None, weights_only:bool=False):
         """Loads all weight matrices for all MappingProjections in the AutodiffComposition from file
         Arguments
         ---------
@@ -1709,7 +1770,7 @@ class AutodiffComposition(Composition):
             except IsADirectoryError:
                 raise AutodiffCompositionError(f"'{path}'{error_msg}")
         try:
-            state = torch.load(path)
+            state = torch.load(path, weights_only=weights_only)
         except FileNotFoundError:
             raise AutodiffCompositionError(f"'{path}'{error_msg}")
 

psyneulink/core/components/functions/nonstateful/learningfunctions.py
@@ -506,6 +506,15 @@ class EMStorage(LearningFunction):
                  decay_rate,
                  random_state)->torch.tensor:
             """Decay existing memories and replace weakest entry with entry_to_store (parallel EMStorage._function)"""
+
+            # If the batch_size is not equal to one then we need to raise an exception.
+            if len(entry_to_store.shape) > 2:
+                if entry_to_store.shape[0] != 1:
+                    raise NotImplementedError("EMSStorage has not been implemented for batch sizes greater than 1")
+                else:
+                    # Drop the singleton batch dimension
+                    entry_to_store = entry_to_store[0]
+
             if random_state.uniform(0, 1) < storage_prob:
                 if decay_rate:
                     memory_matrix *= torch.tensor(decay_rate)
@@ -515,9 +524,9 @@ class EMStorage(LearningFunction):
                     # Find weakest entry (i.e., with lowest norm) along specified axis of matrix
                     idx_of_min = torch.argmin(torch.linalg.norm(memory_matrix, axis=axis))
                 if axis == 0:
-                    memory_matrix[:,idx_of_min] = entry_to_store
+                    memory_matrix[:,idx_of_min] = entry_to_store[0]
                 elif axis == 1:
-                    memory_matrix[idx_of_min,:] = entry_to_store
+                    memory_matrix[idx_of_min,:] = entry_to_store[0]
             return memory_matrix
         return func
 

psyneulink/core/components/functions/nonstateful/transformfunctions.py
@@ -1593,16 +1593,17 @@ class LinearCombination(
         weights = self._get_pytorch_fct_param_value('weights', device, context)
         if weights is not None:
             weights = torch.tensor(weights, device=device).double()
+        # Note: the first dimension of x is batch, aggregate over the second dimension
         if self.operation == SUM:
             if weights is not None:
-                return lambda x: torch.sum(x * weights, 0)
+                return lambda x: torch.sum(x * weights, 1)
             else:
-                return lambda x: torch.sum(x, 0)
+                return lambda x: torch.sum(x, 1)
         elif self.operation == PRODUCT:
             if weights is not None:
-                return lambda x: torch.prod(x * weights, 0)
+                return lambda x: torch.prod(x * weights, 1)
             else:
-                return lambda x: torch.prod(x, 0)
+                return lambda x: torch.prod(x, 1)
         else:
             from psyneulink.library.compositions.autodiffcomposition import AutodiffCompositionError
             raise AutodiffCompositionError(f"The 'operation' parameter of {function.componentName} is not supported "

psyneulink/core/components/mechanisms/mechanism.py
@@ -2665,7 +2665,6 @@ class Mechanism_Base(Mechanism):
         for input_item, input_port in zip(input, self.input_ports):
             if input_port.default_input_shape.size == np.array(input_item).size:
                 from psyneulink.core.compositions.composition import RunError
-
                 # Assign input_item as input_port.variable
                 input_port.parameters.variable._set(np.atleast_2d(input_item), context)
 

psyneulink/core/compositions/composition.py
@@ -10282,14 +10282,6 @@ class Composition(Composition_Base, metaclass=ComponentsMeta):
 
         """
 
-        # If Composition is in learning mode, not called from COMMAND_LINE, and not still preparing,
-        #   presumably inputs have already been parsed so shouldn't do it again
-        # FIX: 11/3/23 - NOTE: This circumvents parsing of inputs when they are a func and called from autodiff_forward
-        if (context and (context.runmode & ContextFlags.LEARNING_MODE)
-                and (context.source & ContextFlags.COMPOSITION)
-                and not (context.execution_phase & ContextFlags.PREPARING)):
-            return inputs, 1
-
         # parse a user-provided input dict to format it properly for execution.
         # compute number of input sets and return that as well
         _inputs = self._parse_names_in_inputs(inputs)
@@ -11511,8 +11503,18 @@ _
                 # Prepare stimuli from the outside world  -- collect the inputs for this TRIAL and store them in a dict
                 if input_nodes:
                     try:
+
                         # IMPLEMENTATION NOTE: for autdoiff, the following includes backward pass after forward pass
-                        execution_stimuli = self._parse_trial_inputs(inputs, trial_num, context)
+
+                        # If this is an instance of generator CompositionRunner._batch_inputs, then there is no
+                        # need to call _parse_trial_inputs, as the inputs are already in the correct format
+                        # from the call to _parse_learning_spec
+                        if isgenerator(inputs) and ('CompositionRunner._batch_inputs' in str(inputs) or
+                                                    'CompositionRunner._batch_function_inputs' in str(inputs)):
+                            execution_stimuli = next(inputs)
+                        else:
+                            execution_stimuli = self._parse_trial_inputs(inputs, trial_num, context)
+
                     except StopIteration:
                         break
                 else:

psyneulink/core/globals/utilities.py
@@ -141,6 +141,8 @@ except ImportError:
 from psyneulink.core.globals.keywords import (comparison_operators, DISTANCE_METRICS, EXPONENTIAL, GAUSSIAN, LINEAR,
                                               MATRIX_KEYWORD_VALUES, MPS, NAME, SINUSOID, VALUE)
 
+
+
 __all__ = [
     'append_type_to_name', 'AutoNumber', 'ContentAddressableList', 'convert_to_list', 'convert_to_np_array',
     'convert_all_elements_to_np_array', 'copy_iterable_with_shared', 'get_class_attributes', 'extended_array_equal', 'flatten_list',

commit 11ea075eb47aa904a9c7302f725e26b0e285f5fc

AutodiffComposition: Use utilities.convert_to_np_array
Instead of opencoding the same functionality.

Closes: https://github.com/PrincetonUniversity/PsyNeuLink/issues/3216
Fixes: beebd2a968fbeebc45e93d9785460a1c1860686a
	("Add support for batching to AutoDiffCompostion in PyTorch mode. (#3204)")

Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

psyneulink/library/compositions/autodiffcomposition.py
@@ -349,7 +349,7 @@ from psyneulink.core.globals.keywords import (AUTODIFF_COMPOSITION, CPU, CUDA, E
                                               Loss, LOSSES, MATRIX_WEIGHTS, MINIBATCH, MPS, NODE_VALUES, NODE_VARIABLES,
                                               OPTIMIZATION_STEP, RESULTS, RUN, SOFT_CLAMP,
                                               TARGETS, TRAINED_OUTPUTS, TRIAL)
-from psyneulink.core.globals.utilities import is_numeric_scalar
+from psyneulink.core.globals.utilities import is_numeric_scalar, convert_to_np_array
 from psyneulink.core.scheduling.scheduler import Scheduler
 from psyneulink.core.globals.parameters import Parameter, check_user_specified
 from psyneulink.core.scheduling.time import TimeScale
@@ -1170,10 +1170,7 @@ class AutodiffComposition(Composition):
             all_output_values += [output]
 
         # Turn into a numpy array, possibly ragged
-        try:
-            all_output_values = np.array(all_output_values)
-        except (ValueError, np.VisibleDeprecationWarning):
-            all_output_values = np.array(all_output_values, dtype=object)
+        all_output_values = convert_to_np_array(all_output_values)
 
         # Swap the first two dimensions (output_port, batch) to (batch, output_port)
         all_output_values = all_output_values.swapaxes(0, 1)