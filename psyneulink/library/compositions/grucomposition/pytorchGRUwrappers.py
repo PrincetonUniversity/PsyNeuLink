1b3b8ced31774d3a14dbb33a49216509378bb9a9
jdc <jdc@princeton.edu>
2025-04-01 15:33:22 -0400

    [skip ci]

--- psyneulink/library/compositions/grucomposition/pytorchGRUwrappers.py
+++ psyneulink/library/compositions/grucomposition/pytorchGRUwrappers.py
@@ -564,10 +564,10 @@ class PytorchGRUProjectionWrapper(PytorchProjectionWrapper):
         #                                                                            context=context)
         # MODIFIED 3/31/25 NEW:
         torch_tensor = self.torch_parameter[self.matrix_indices]
-        self.composition_wrapper.composition.copy_projection_matrix_to_torch_param_v2(projection=self.projection,
-                                                                                      torch_param=torch_tensor,
-                                                                                      validate=False,
-                                                                                      context=context)
+        self.composition_wrapper.composition.copy_projection_matrix_to_torch_param(projection=self.projection,
+                                                                                   torch_param=torch_tensor,
+                                                                                   validate=False,
+                                                                                   context=context)
         # MODIFIED 3/31/25 END
 
     def _copy_torch_params_to_pnl_proj(self, context):
@@ -581,10 +581,10 @@ class PytorchGRUProjectionWrapper(PytorchProjectionWrapper):
         #                                                                            validate=False,
         #                                                                            context=context)
         # MODIFIED 3/31 NEW:
-        self.composition_wrapper.composition.copy_torch_param_to_projection_matrix_v2(torch_param=matrix,
-                                                                                      projection=self.projection,
-                                                                                      # validate=False,
-                                                                                      context=context)
+        self.composition_wrapper.composition.copy_torch_param_to_projection_matrix(torch_param=matrix,
+                                                                                   projection=self.projection,
+                                                                                   # validate=False,
+                                                                                   context=context)
         # MODIFIED 3/31 END
 
     def log_matrix(self):
