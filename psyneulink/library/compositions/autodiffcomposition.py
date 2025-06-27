9ac225e8afe241aeff8e1bfbd62d1f1c8642ff18
jdc <jdc@princeton.edu>
2025-06-27 08:58:53 -0400

    [skip ci]

--- psyneulink/library/compositions/autodiffcomposition.py
+++ psyneulink/library/compositions/autodiffcomposition.py
@@ -1197,6 +1197,7 @@ class AutodiffComposition(Composition):
                                                          optimizer_params,
                                                          Context(source=ContextFlags.METHOD,
                                                                  runmode=context.runmode,
+                                                                 # BREADCRUMB 6/27/25
                                                                  execution_id=context.execution_id))
             else:
                 # Otherwise, if call is from Composition constructor, use user-specified params specified in that call
@@ -1213,6 +1214,7 @@ class AutodiffComposition(Composition):
                                                  optimizer_params,
                                                  Context(source=ContextFlags.METHOD,
                                                          runmode=context.runmode,
+                                                         # BREADCRUMB 6/27/25
                                                          execution_id=context.execution_id))
         # Set up loss function
         if self.loss_function is not None:
