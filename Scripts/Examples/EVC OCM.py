6e57a6bc73afecec55fe86ab527ce2dd9502f5f8
jdc <jdc@princeton.edu>
2025-07-16 13:08:57 -0400

    [skip ci]

--- Scripts/Examples/EVC OCM.py
+++ Scripts/Examples/EVC OCM.py
@@ -29,20 +29,36 @@ comp.add_node(Decision, required_roles=[NodeRole.OUTPUT])
 task_execution_pathway = [Input, IDENTITY_MATRIX, Decision]
 comp.add_linear_processing_pathway(task_execution_pathway)
 
-comp.add_model_based_optimizer(optimizer=OptimizationControlMechanism(name='OCM',
-                                                                      agent_rep=comp,
-                                                                      state_features=[Input.input_port, reward.input_port],
-                                                                      state_feature_function=AdaptiveIntegrator(rate=0.5),
-                                                                      objective_mechanism=ObjectiveMechanism(
-                                                                              name='OCM Objective Mechanism',
-                                                                              function=LinearCombination(operation=PRODUCT),
-                                                                              monitor=[reward,
-                                                                                       Decision.output_ports[PROBABILITY_UPPER_THRESHOLD],
-                                                                                       (Decision.output_ports[RESPONSE_TIME], -1, 1)]),
-                                                                      function=GridSearch(),
-                                                                      control_signals=[("drift_rate", Decision),
-                                                                                       ("threshold", Decision)])
-                               )
+# comp.add_model_based_optimizer(optimizer=OptimizationControlMechanism(name='OCM',
+#                                                                       agent_rep=comp,
+#                                                                       state_features=[Input.input_port, reward.input_port],
+#                                                                       state_feature_function=AdaptiveIntegrator(rate=0.5),
+#                                                                       objective_mechanism=ObjectiveMechanism(
+#                                                                               name='OCM Objective Mechanism',
+#                                                                               function=LinearCombination(operation=PRODUCT),
+#                                                                               monitor=[reward,
+#                                                                                        Decision.output_ports[PROBABILITY_UPPER_THRESHOLD],
+#                                                                                        (Decision.output_ports[RESPONSE_TIME], -1, 1)]),
+#                                                                       function=GridSearch(),
+#                                                                       control_signals=[("drift_rate", Decision),
+#                                                                                        ("threshold", Decision)])
+#                                )
+comp.add_controller(OptimizationControlMechanism(name='OCM',
+                                                 agent_rep=comp,
+                                                 state_features=[Input.input_port, reward.input_port],
+                                                 state_feature_function=AdaptiveIntegrator(rate=0.5),
+                                                 objective_mechanism=ObjectiveMechanism(
+                                                     name='OCM Objective Mechanism',
+                                                     function=LinearCombination(operation=PRODUCT),
+                                                     monitor=[reward,
+                                                              Decision.output_ports[PROBABILITY_UPPER_THRESHOLD],
+                                                              (Decision.output_ports[RESPONSE_TIME], -1, 1)]),
+                                                 function=GridSearch(),
+                                                 control_signals=[ControlSignal(modulates=("drift_rate", Decision),
+                                                                                allocation_samples=np.arange(0.1,1.01, 0.3)),
+                                                                  (ControlSignal(modulates=("threshold", Decision),
+                                                                                 allocation_samples=np.arange(0.1, 1.01, 0.3)))])
+                    )
 
 comp.enable_model_based_optimizer = True
 
@@ -51,6 +67,6 @@ stim_list_dict = {
     reward: [20, 20]
 }
 
-comp.show_graph(show_controller=True, show_node_structure=ALL)
-# comp.show_graph(show_controller=True)
+# comp.show_graph(show_controller=True, show_node_structure=ALL)
+comp.show_graph(show_controller=True)
 # comp.run(inputs=stim_list_dict)
