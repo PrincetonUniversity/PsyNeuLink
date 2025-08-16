32e872ff370b9013ee6c0629317aabbc63f8e1df
jdcpni <jdc@princeton.edu>
2025-08-16 12:52:05 -0400

    [skip ci]

--- psyneulink/library/compositions/grucomposition/__init__.py
+++ psyneulink/library/compositions/grucomposition/__init__.py
@@ -1,10 +1,9 @@
-from . import grucomposition
-from .grucomposition import *
-__all__ = list(grucomposition.__all__)
-
 try:
     import torch
+    from . import grucomposition
+    from .grucomposition import *
     from .pytorchGRUwrappers import *
+    __all__ = list(grucomposition.__all__)
     __all__.extend(pytorchGRUwrappers.__all__)
 except:
     pass
