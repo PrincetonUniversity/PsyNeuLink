e60cd1d5b31cd3c9ed0bf674b59a45d90600a9ee
jdcpni <jdc@princeton.edu>
2025-08-16 18:25:51 -0400

    YAIF

--- psyneulink/library/compositions/grucomposition/__init__.py
+++ psyneulink/library/compositions/grucomposition/__init__.py
@@ -1,14 +1,14 @@
-from .grucomposition import *
-from .pytorchGRUwrappers import *
-__all__ = list(grucomposition.__all__)
-__all__.extend(pytorchGRUwrappers.__all__)
+# from .grucomposition import *
+# from .pytorchGRUwrappers import *
+# __all__ = list(grucomposition.__all__)
+# __all__.extend(pytorchGRUwrappers.__all__)
 
-# try:
-#     import torch
-#     from . import grucomposition
-#     from .grucomposition import *
-#     from .pytorchGRUwrappers import *
-#     __all__ = list(grucomposition.__all__)
-#     __all__.extend(pytorchGRUwrappers.__all__)
-# except:
-#     pass
+try:
+    import torch
+    from . import grucomposition
+    from .grucomposition import *
+    from .pytorchGRUwrappers import *
+    __all__ = list(grucomposition.__all__)
+    __all__.extend(pytorchGRUwrappers.__all__)
+except:
+    pass
