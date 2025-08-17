db0ec1befdffeb6d59d7815a70fab202c7e6ec63
jdcpni <jdc@princeton.edu>
2025-08-17 07:42:29 -0400

    [skip ci] More cleanup -- passes torch comparison for learning_rate== 0 but not for !=0.

--- psyneulink/library/compositions/__init__.py
+++ psyneulink/library/compositions/__init__.py
@@ -2,18 +2,18 @@ from .regressioncfa import *
 from .compositionrunner import *
 from .autodiffcomposition import *
 from .emcomposition import *
-from .grucomposition import *
 from psyneulink.library.compositions.emcomposition.emcomposition import *
 from psyneulink.library.compositions.grucomposition.grucomposition import *
 __all__ = list(regressioncfa.__all__)
 __all__.extend(compositionrunner.__all__)
 __all__.extend(autodiffcomposition.__all__)
 __all__.extend(emcomposition.__all__)
-__all__.extend(grucomposition.__all__)
 
 try:
     import torch
     from torch import nn
+    from .grucomposition import *
+    __all__.extend(grucomposition.__all__)
 
     # Some torch releases have silent dependency on a more recent numpy than the one curently required by PNL.
     # This breaks torch numpy bindings, see e.g:  https://github.com/pytorch/pytorch/issues/100690
