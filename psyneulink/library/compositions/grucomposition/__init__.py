9f02a104a08fe4643144d81b5f8c91b296392527
jdcpni <jdc@princeton.edu>
2025-08-17 11:43:50 -0400

    YAIF

--- psyneulink/library/compositions/grucomposition/__init__.py
+++ psyneulink/library/compositions/grucomposition/__init__.py
@@ -3,6 +3,7 @@
 # __all__ = list(grucomposition.__all__)
 # __all__.extend(pytorchGRUwrappers.__all__)
 
+# CONSIDER SETTING torch_available HERE AND IMPORTING THAT INTO grucomposition
 try:
     import torch
     from . import grucomposition
