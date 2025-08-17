5da1dbc34d54a20fe6506f0a91c13d9fea933ba6
Younes Strittmatter <ystrittm@gmail.com>
2025-08-17 11:42:40 -0400

    Update run.py

--- Scripts/Models (Under Development)/EGO/Using EMComposition/Coffee Shop World/younes/pytorch_ego/run.py
+++ Scripts/Models (Under Development)/EGO/Using EMComposition/Coffee Shop World/younes/pytorch_ego/run.py
@@ -3,31 +3,38 @@ import torch
 import numpy as np
 from torch import nn
 
-
 def run_participant(params, data_loader, len_memory=2):
     loss_fn = nn.BCELoss()
     context_module, em_module = gen_model(params, len_memory=len_memory)
     optimizer = torch.optim.SGD(lr=params['learning_rate'], params=context_module.parameters())
     em_preds = []
 
+
     # Loop over each state of the CSW task.
     for trial, (x, _, y) in enumerate(data_loader):
-        context = context_module(x)  # retrieve the context representation from the integrator.
+        if trial == 0:
+            with torch.no_grad():
+                em_module.write(
+                    torch.zeros_like(x),
+                    torch.zeros_like(x),
+                    x,
+                )
+            em_preds.append(torch.zeros_like(x))
         # For each state, optimize the context representation for predicting the next state over n_optimization_steps.
-        for _ in range(params['n_optimization_steps']):
+        if trial >= 0 and trial < 23:
+            context = context_module(x)  # retrieve the context representation from the integrator.
+            for _ in range(params['n_optimization_steps']):
+                # Skip first state bc which sequence within the context is randomly assigned.
+                # i.e., we have not yet observed a full state transition.
 
-
-            # Skip first state bc which sequence within the context is randomly assigned.
-            # i.e., we have not yet observed a full state transition.
-            optimizer.zero_grad()  # Zero the gradients before each optimization step.
-            pred_em = em_module(x, context)  # retrieve the next state prediction from the EM module.
-            loss = loss_fn(pred_em, y)  # calculate the loss between the predicted and actual next state.
-            loss.backward(retain_graph=True)  # compute the gradients of the context module.
-            optimizer.step()  # backprop to update context module weights.
-
-        with torch.no_grad():
+                optimizer.zero_grad()  # Zero the gradients before each optimization step.
+                pred_em = em_module(x, context)  # retrieve the next state prediction from the EM module.
+                loss = loss_fn(pred_em, y)  # calculate the loss between the predicted and actual next state.
+                loss.backward(retain_graph=True)  # compute the gradients of the context module.
+                optimizer.step()  # backprop to update context module weights.
             em_module.write(x, context, y)
-            em_preds.append(pred_em.cpu().detach().numpy())
+            em_preds.append(pred_em.detach().cpu().numpy())
+
 
     # Collect some metrics from the training run for analysis.
     em_preds = np.stack(em_preds).squeeze()
