b99825d8f075438903af1eda9235839ab4af8ee7
jdcpni <jdc@princeton.edu>
2025-11-13 18:56:32 -0500

    • composition.py   - get_composition_for_node:  fix bug in which receiver is a Projection   rather than a Mechanisms if efferent is a LearningProjection

--- Scripts/Models (Under Development)/Minigrid/KADMLP.py
+++ Scripts/Models (Under Development)/Minigrid/KADMLP.py
@@ -388,7 +388,7 @@ def main():
     steps = 0
     start_time = timeit.default_timer()
     entropy_thresh = 0.5
-    # agent_comp.show_graph()
+    agent_comp.show_graph()
     for _ in range(num_trials):
         observation = env.reset()
         while True:
