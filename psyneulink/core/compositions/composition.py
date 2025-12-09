8f8a5e7732d9734df37bd78b7d303a6a08c4d114
jdcpni <jdc@princeton.edu>
2025-12-09 08:51:27 -0500

    [skip ci] • composition.py   _parse_learning_spec(): purge targets when added to inputs dict in _recursive_update() to prevent duplicate targets warning

--- psyneulink/core/compositions/composition.py
+++ psyneulink/core/compositions/composition.py
@@ -10403,12 +10403,27 @@ class Composition(Composition_Base, metaclass=ComponentsMeta):
             """
             Recursively calls update on dictionaries, which prevents deletion of keys
             """
-            for key, val in u.items():
-                if isinstance(val, collections.abc.Mapping):
-                    d[key] = _recursive_update(d.get(key, {}), val)
-                else:
-                    d[key] = val
+            # # MODIFIED TEACHER_TARGET OLD:
+            # for key, val in u.items():
+            #     if isinstance(val, collections.abc.Mapping):
+            #         d[key] = _recursive_update(d.get(key, {}), val)
+            #     else:
+            #         d[key] = val
+            # return d
+            # MODIFIED TEACHER_TARGET NEW:
+            def _recursive_update(d, u):
+                """
+                Recursively calls update on dictionaries, which prevents deletion of keys
+                """
+                for key, val in u.copy().items():
+                    if isinstance(val, collections.abc.Mapping):
+                        d[key] = _recursive_update(d.get(key, {}), val)
+                    else:
+                        d[key] = val
+                    # Remove entry from targets dict to avoid duplicate target warnings
+                    u.pop(key)
             return d
+            # MODIFIED TEACHER_TARGET END
 
         if targets is not None:
             targets = self._infer_target_nodes(targets, execution_mode)
