2b6c68c97265ad9ce5858ba32895e1915fea4432
Jan Vesely <jan.vesely@rutgers.edu>
2025-12-14 21:35:40 -0500

    llvm, Mechanism: Add support for function parameters in output port variable specification
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/ports/test_output_ports.py
+++ tests/ports/test_output_ports.py
@@ -13,10 +13,12 @@ class TestOutputPorts:
                               ((pnl.OWNER_VALUE), [[[1], [2], [3]]], [[[1], [2], [3]]]),
                               ([(pnl.OWNER_VALUE, 0), (pnl.OWNER_VALUE, 1)], [[[1], [2]]], [[[1], [2]]]),
                               ((pnl.OWNER_EXECUTION_COUNT), [[0]], [[4]]),
+                              ([(pnl.PREVIOUS_VALUE, 1, 0), pnl.RATE], [[0, 1]], [[2, 1]]), # From AdaptiveIntegrator
                               ], ids=lambda x: str(x) if len(x) != 1 else '')
     def test_output_port_variable_spec(self, spec, expected1, expected2, mech_mode):
         mech = pnl.ProcessingMechanism(default_variable=[[1.], [2.], [3.]],
                                        name='MyMech',
+                                       function=pnl.AdaptiveIntegrator,
                                        output_ports=[pnl.OutputPort(variable=spec)])
 
         np.testing.assert_allclose(mech.output_values, expected1)
@@ -43,30 +45,28 @@ class TestOutputPorts:
                               (("num_executions", pnl.TimeScale.TRIAL), [2], [2]),
                               (("num_executions", pnl.TimeScale.PASS), [1], [1]),
                               (("num_executions", pnl.TimeScale.TIME_STEP), [1], [1]),
+                              ([pnl.SLOPE, pnl.SCALE], [1, 1], [1, 1]), # From Linear function
                              ], ids=lambda x: str(x) if len(x) != 1 else '')
     @pytest.mark.usefixtures("comp_mode_no_per_node")
-    def tests_output_port_variable_spec_composition(self, comp_mode, spec, expected1, expected2):
+    def test_output_port_variable_spec_composition(self, comp_mode, spec, expected1, expected2):
+        # TimeScale.RUN is not supported in LLVMExec/PTXExec mode since the necessary
+        # counter maintenance is skipped
         if (len(spec) == 2) and (spec[1] == pnl.TimeScale.RUN) and (comp_mode & pnl.ExecutionMode._Exec):
             pytest.skip("{} is not supported in {}".format(spec[1], comp_mode))
 
-        # Test specification of OutputPort's variable
-        # OutputPort mech.output_ports['all'] has a different dimensionality than the other OutputPorts;
-        #    as a consequence, when added as a terminal node, the Composition can't construct an IDENTITY_MATRIX
-        #    from the mech's OutputPorts to the Composition's output_CIM.
-        # FIX: Remove the following line and correct assertions below once above condition is resolved
         var = [[1], [2], [3]]
-        mech = pnl.ProcessingMechanism(default_variable=var, name='MyMech',
-                                       output_ports=[pnl.OutputPort(variable=spec)])
+        mech = pnl.ProcessingMechanism(default_variable=var, name='MyMech',output_ports=[pnl.OutputPort(variable=spec)])
+
         C = pnl.Composition(name='MyComp')
         C.add_node(node=mech)
         C.termination_processing[pnl.TimeScale.TRIAL] = pnl.AtPass(2)
-        outs = C.run(inputs={mech: var}, num_trials=2, execution_mode=comp_mode)
 
         # outs is entire mechanism value, expected is output port value
-        np.testing.assert_allclose(outs[0], expected1)
+        outs = C.run(inputs={mech: var}, num_trials=2, execution_mode=comp_mode)
+        np.testing.assert_allclose(outs, [expected1])
 
         outs = C.run(inputs={mech: var}, num_trials=2, execution_mode=comp_mode)
-        np.testing.assert_allclose(outs[0], expected2)
+        np.testing.assert_allclose(outs, [expected2])
 
     def test_no_path_afferents(self):
         A = pnl.OutputPort()
