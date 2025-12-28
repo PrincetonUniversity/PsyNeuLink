3f71c147f29b21bbe5e361a0248c283cb4753788
jdcpni <jdc@princeton.edu>
2025-12-28 14:21:31 -0500

    [skip ci]
    • pytorchwrappers.py
      - forward(): sort current execution set for consistency of reporting

--- tests/mdf/test_mdf.py
+++ tests/mdf/test_mdf.py
@@ -466,6 +466,7 @@ _test_as_mdf_model_defaults_excluded_classes = {
     pnl.Rearrange: _reason_no_default,
     pnl.UserDefinedFunction: _reason_no_default,
     pnl.EMComposition: 'needs GraphStructureCondition by default (not implemented for MDF)',
+    pnl.LossMechanism: _reason_no_default,
 }
 
 try:
