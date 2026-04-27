e63b40d0b853cd7752235d8eda7998d3df9d5f24
jdc <jdc@princeton.edu>
2026-04-27 14:23:55 -0400

    [skip ci] • test_LossMechanism.py

--- tests/mechanisms/test_LossMechanism.py
+++ tests/mechanisms/test_LossMechanism.py
@@ -15,6 +15,7 @@ class TestLossMechanism:
 
     @pytest.mark.composition
     def test_LossMechanism_construction(self):
+        """Test sample and target assignments to LossMechanism"""
         pway1_mech_A = pnl.ProcessingMechanism(name='pway1_mech_A')
         pway1_mech_B = pnl.ProcessingMechanism(name='pway1_mech_B')
         pway1_mech_C = pnl.ProcessingMechanism(name='pway1_mech_C')
@@ -24,17 +25,17 @@ class TestLossMechanism:
         pway3_mech_A = pnl.ProcessingMechanism(name='pway3_mech_A')
         pway3_mech_B = pnl.ProcessingMechanism(name='pway3_mech_B')
         solo_input_mech = pnl.ProcessingMechanism(name='solo_input_mech')
-        # TEACHER_TARGET BREADCRUMB: THIS FAILLS AS loss_mech IS NOT AUTOMATICALLY ADDED TO COMP
-        # loss_mech = pnl.LossMechanism(sample=pway3_mech_B.output_port, target=solo_input_mech)
+        loss_mech = pnl.LossMechanism(sample=pway3_mech_B.output_port, target=solo_input_mech)
         pway1 = [pway1_mech_A, pway1_mech_B, pway1_mech_C]
         pway2 = [pway2_mech_A, pway2_mech_B, pway2_mech_C]
         pway3 = [pway3_mech_A, pway3_mech_B]
         comp = pnl.AutodiffComposition(pathways=[pway1,pway2,pway3,solo_input_mech],
-                                       targets={pway1_mech_B: pway2_mech_B.output_port,
-                                                pway1_mech_C: solo_input_mech,
-                                                pway2_mech_C: pnl.TARGET,
-                                                pway3_mech_B: pnl.LossMechanism(sample=pway3_mech_B.output_port,
-                                                                                target=solo_input_mech)})
+                                       targets=[(pway1_mech_B, pway2_mech_B.output_port),
+                                                (pway1_mech_C, solo_input_mech),
+                                                (pway2_mech_C, pnl.TARGET),
+                                                loss_mech
+                                                ]
+                                       )
         loss_mechs = comp.loss_mechanisms
 
         assert loss_mechs[0].sample == pway1_mech_B.output_port
@@ -44,7 +45,7 @@ class TestLossMechanism:
         assert loss_mechs[2].sample == pway2_mech_C.output_port
         assert loss_mechs[2].target == comp.target_input_mechanisms[0].output_port
         assert loss_mechs[3].sample == pway3_mech_B.output_port
-        assert loss_mechs[4].target == solo_input_mech.output_port
+        assert loss_mechs[3].target == solo_input_mech.output_port
 
     def test_illegal_manual_LossMechanism(self):
         pway1_mech_A = pnl.ProcessingMechanism(name='pway1_mech_A')
