5d26a22a9c6190438b25b36057d1ff7f1a6c8fc8
jdc <jdc@princeton.edu>
2026-05-27 10:38:17 -0400

    [skip ci] - refactor MatrixMemory to add STORE and RETRIEVE as operations

--- psyneulink/core/components/functions/nonstateful/transformfunctions.py
+++ psyneulink/core/components/functions/nonstateful/transformfunctions.py
@@ -2453,12 +2453,16 @@ class MatrixMemory(TransformFunction): #
 
         """
         variable = np.array(variable)
+        query = variable[0]
+        combined_scores = variable[1]
+        weakest_memory_idx = int(variable[2].squeeze())
+        filler = np.zeros(len(self.parameters.memory._get(context)))
 
         operation_err_msg = (f"PROGRAM ERROR: 'operation'  was not specified in (runtime_)params "
                              f"in call to MatrixMemory for '{self.owner.name}'.")
         try:
             operation = params[OPERATION]
-            assert operation in {COMPUTE_SCORES, ACCESS_MEMORY}
+            assert operation in {COMPUTE_SCORES, ACCESS_MEMORY, STORE, RETRIEVE}
         except:
             # 'operation' is not specified in call from __init__()
             if self.is_initializing:
@@ -2467,19 +2471,23 @@ class MatrixMemory(TransformFunction): #
                 raise FunctionError(operation_err_msg)
 
         if operation == COMPUTE_SCORES:
-            # Note: retrieve only needs query and scores
-            retrieved, match_scores, norms = self._compute_scores(variable[0], context)
-            return retrieved, match_scores, norms
+            scores, norms = self._compute_scores(query, context)
+            memory = query
 
-        # Store memory in place of weakest one if condition is met and storage_prob > 0
-        elif operation == ACCESS_MEMORY:
-            # Note: store only needs query and weakest_memory
-            retrieved_value, combined_scores = self._access_memory(variable, context)
-            filler = np.zeros(len(self.parameters.memory._get(context)))
-            return retrieved_value, combined_scores, filler # Return stored item, combined_scores, and filler for norms
+        # Note: ACCESS_MEMORY invokes both RETRIEVE and STORE; otherwise invoke only the one called
+        elif operation in {ACCESS_MEMORY, RETRIEVE, STORE}:
+            if operation in{ACCESS_MEMORY, RETRIEVE}:
+                memory = self._retrieve_memory(query, combined_scores, context=context)
+            if operation in{ACCESS_MEMORY, STORE}:
+                # # Store memory in place of weakest one if storage_prob > 0
+                self._store_memory(query, weakest_memory_idx, context=context)
+            scores = combined_scores
+            norms = filler
 
         else:
             raise FunctionError(operation_err_msg)
+
+        return memory, scores, norms
 
     def _compute_scores(self, variable, context):
 
@@ -2489,7 +2497,7 @@ class MatrixMemory(TransformFunction): #
         # If this is an initialization run, just return query and zeros for score and norms
         if self.is_initializing:
             scores_template = norms_template = np.zeros(len(memory))
-            return query, scores_template, norms_template
+            return scores_template, norms_template
 
         try:
             scores = self.scores_function(query, context)
@@ -2509,30 +2517,25 @@ class MatrixMemory(TransformFunction): #
 
         norms = np.linalg.norm(memory, axis=1)
 
-        return query, scores, norms
+        return scores, norms
 
-    def _access_memory(self, variable, context=None):
-
-        query = variable[0]
-        scores = variable[1]
-        weakest_memory_idx = int(variable[2].squeeze())
+    def _retrieve_memory(self, query, scores, context=None):
         memory = self.parameters.memory._get(context)
-
-        # First retrieve memory
         retrieved = scores @ memory
+        return retrieved
 
-        # Then store query
+    def _store_memory(self, item_to_store, weakest_memory_idx, context=None):
+        item_to_store = item_to_store
+        memory = self.parameters.memory._get(context)
         storage_prob = self._get_current_parameter_value('storage_prob', context)
         random_state = self.parameters.random_state._get(context)
         if random_state.uniform(0, 1) < storage_prob:
             decay_rate = self.parameters.decay_rate._get(context)
             if decay_rate >= 0.0:
                 memory *= (1-decay_rate)
-            memory[weakest_memory_idx] = query
+            memory[weakest_memory_idx] = item_to_store
             # self.parameters.memory._set(memory, context, override=True)
             self.scores_function.parameters.matrix._set(self.memory.T, context)
-
-        return retrieved, scores
 
     def _normalize_rows(self, memory):
         memory = np.asarray(memory, dtype=float)
