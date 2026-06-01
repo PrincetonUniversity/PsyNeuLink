5ca248c9f650e418920bb3d4639962f1d01f2591
jdcpni <jdc@princeton.edu>
2026-06-01 07:59:37 -0400

    [skip ci]
    • test_mdf.py: 2 failures
    • Giallanza2024_ego_study2.py:
      - failure of sample_mech assignment in PytorchMechanismWrapper

--- psyneulink/library/models/Giallanza2024_ego_study2.py
+++ psyneulink/library/models/Giallanza2024_ego_study2.py
@@ -110,7 +110,7 @@ def construct_model(
     # Stores (state, previous_state, context) triplets and retrieves the successor
     # state based on similarity across these fields. Field weights control how much
     # each field contributes to retrieval.
-    em = pnl.EMComposition(
+    em = pnl.EMComposition2(
         name=EM,
         memory_template=[[0] * state_size,  # state
                          [0] * state_size,  # previous state
