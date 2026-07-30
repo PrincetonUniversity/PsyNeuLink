734ddce519691e4748122f512179703b84425910
Jan Vesely <jan.vesely@rutgers.edu>
2026-07-30 12:46:54 -0400

    llvm: Refactor checks for supported pipeline optimizations
    Print only unsupported optimizations.
    
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/llvm/jit_engine.py
+++ psyneulink/core/llvm/jit_engine.py
@@ -89,29 +89,36 @@ def _binding_initialize():
         __initialized = True
 
 
-def _new_pass_builder(target_machine, opt_level, extra_opts = dict()):
+def _new_pass_builder(target_machine, opt_level, extra_opts=dict()):
     pto = binding.create_pipeline_tuning_options(speed_level=opt_level)
-    pto.loop_vectorization = opt_level != 0
-    pto.slp_vectorization = opt_level != 0
 
-    assert set(extra_opts.keys()).issubset(dir(pto)), dir(pto)
+    opts = {'loop_vectorization': opt_level != 0,
+            'slp_vectorization': opt_level != 0,
+            **extra_opts,
+           }
 
-    for key, value in extra_opts.items():
+    assert set(opts.keys()).issubset(dir(pto)), "Unsupported opt: {}".format(opts.keys().difference(dir(pto)))
+
+    for key, value in opts.items():
         setattr(pto, key, value)
 
     pass_builder = binding.create_pass_builder(target_machine, pto)
 
     return pass_builder
 
-def _old_pass_manager(target_machine, opt_level, extra_opts = dict()):
+def _old_pass_manager(target_machine, opt_level, extra_opts=dict()):
     pass_manager_builder = binding.PassManagerBuilder()
-    pass_manager_builder.loop_vectorize = opt_level != 0
-    pass_manager_builder.slp_vectorize = opt_level != 0
-    pass_manager_builder.opt_level = opt_level
 
-    assert set(extra_opts.keys()).issubset(dir(pass_manager_builder))
+    opts = {'loop_vectorize': opt_level != 0,
+            'slp_vectorize': opt_level != 0,
+            'opt_level': opt_level,
+            **extra_opts,
+           }
 
-    for key, value in extra_opts.items():
+    assert set(opts.keys()).issubset(dir(pass_manager_builder)), \
+        "Unsupported opt: {}".format(opts.keys().difference(dir(pass_manager_builder)))
+
+    for key, value in opts.items():
         setattr(pass_manager_builder, key, value)
 
     # Create module pass manager and populate it with analysis and opt passes
