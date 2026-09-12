088f1aaf49c9a0261f43ac8656fdde4112ba5613
Katherine Mantel <kmantel@princeton.edu>
2026-09-12 10:37:29 +0000

    MappingProjection: avoid re-creating matrix parameter port Accumulator
    When an AccumulatorIntegrator is manually specified as the function for
    the matrix parameter port, use that instance instead of making a new
    one. This also prevents reinitializing it to the integrator's `value`,
    which is one integration step ahead. This is reflected in the changed
    results of:
    
    - `tests/mechanisms/test_control_mechanism.py::TestControlMechanism::test_identicalness_of_control_and_gating`
    - `tests/mechanisms/test_gating_mechanism.py::test_gating_with_composition`

--- tests/mechanisms/test_gating_mechanism.py
+++ tests/mechanisms/test_gating_mechanism.py
@@ -55,9 +55,11 @@ def test_gating_with_composition():
 
     comp.learn(num_trials=3, inputs=stim_list)
 
-    expected_results = [[[0.81493513, 0.85129046, 0.88154205]],
-                        [[0.81331773, 0.85008207, 0.88157851]],
-                        [[0.81168332, 0.84886047, 0.88161468]]]
+    expected_results = [
+        [[0.81493513, 0.85129046, 0.88154205]],
+        [[0.78933061, 0.82552148, 0.85756749]],
+        [[0.77494003, 0.81080666, 0.84407169]],
+    ]
 
     np.testing.assert_allclose(comp.results, expected_results)
 
@@ -68,7 +70,7 @@ def test_gating_with_composition():
 
     stim_list[Gating_Mechanism]=[2.0]
     results = comp.learn(num_trials=1, inputs=stim_list)
-    expected_results = [[0.96941429, 0.9837254 , 0.99217549]]
+    expected_results = [[0.94464869, 0.96532951, 0.97997149]]
     np.testing.assert_allclose(results, expected_results)
 
 def test_gating_with_UDF_with_composition():
