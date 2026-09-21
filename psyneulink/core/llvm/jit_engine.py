5a6a92460d780acf6b8249ed2a888b2bb20db965
Jan Vesely <jvesely@users.noreply.github.com>
2026-09-21 17:15:13 -0400

    llvm: Set module triple and datalayout right after parsing the ir (#3657)
    Avoid linking module with unknown triple/datalayout to those with
    set triple/datalayout.
    
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- psyneulink/core/llvm/jit_engine.py
+++ psyneulink/core/llvm/jit_engine.py
@@ -112,7 +112,9 @@ def _cpu_jit_constructor():
     # And an execution engine with a builtins backing module
     builtins_module = _generate_cpu_builtins_module(LLVMBuilderContext.get_current().float_ty)
 
-    backing_mod = binding.parse_assembly(str(builtins_module))
+    backing_mod = _try_parse_module(builtins_module)
+    backing_mod.triple = cpu_target_machine.triple
+    backing_mod.data_layout = str(cpu_target_machine.target_data)
     backing_mod.verify()
 
     if "dump-llvm-gen" in debug_env:
@@ -203,11 +205,10 @@ class jit_engine:
             print("Total parsed modules in '{}': {}".format(s, self.__parsed_modules))
 
     def opt_and_add_bin_module(self, module):
-        # Tag the module with this engine's target before optimizing. Done per
-        # engine (rather than on the shared IR module) so the CPU and PTX targets
-        # don't clobber each other's triple/layout.
-        module.triple = self._target_machine.triple
-        module.data_layout = str(self._target_machine.target_data)
+        assert module.triple == self._target_machine.triple, \
+            "Triple mismatch: {} vs. {}".format(module.triple, self._target_machine.triple)
+        assert module.data_layout == str(self._target_machine.target_data), \
+            "Data layout mismatch: {} vs. {}".format(module.data_layout, self._target_machine.data_layout)
 
         start = time.perf_counter()
         pass_manager = self._pass_builder.getModulePassManager()
@@ -292,12 +293,16 @@ class jit_engine:
     def compile_staged(self):
         # Parse generated modules and link them
         mod_bundle = binding.parse_assembly("")
+        mod_bundle.triple = self._target_machine.triple
+        mod_bundle.data_layout = str(self._target_machine.target_data)
 
         while self.staged_modules:
             m = self.staged_modules.pop()
 
             start = time.perf_counter()
             new_mod = _try_parse_module(m)
+            new_mod.triple = self._target_machine.triple
+            new_mod.data_layout = str(self._target_machine.target_data)
             finish = time.perf_counter()
 
             if "time_stat" in debug_env:
